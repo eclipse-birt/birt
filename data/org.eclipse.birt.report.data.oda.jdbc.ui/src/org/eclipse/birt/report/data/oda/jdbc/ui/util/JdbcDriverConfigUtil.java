af09df60e511dc215993d1da1095f013106eb0ee
rail <>
2005-02-08 01:55:48 +0000

    Changed to use DTE API for registering and loading drivers

--- data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/util/JdbcDriverConfigUtil.java
+++ data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/util/JdbcDriverConfigUtil.java
@@ -11,30 +11,20 @@ package org.eclipse.birt.report.data.oda.jdbc.ui.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.eclipse.birt.data.oda.util.driverconfig.ConfigManager;
 import org.eclipse.birt.data.oda.util.driverconfig.DriverLibraries;
+import org.eclipse.birt.data.oda.util.driverconfig.DriverSetup;
 import org.eclipse.birt.data.oda.util.driverconfig.LibrariesForOS;
 import org.eclipse.birt.data.oda.util.driverconfig.OpenDataAccessConfig;
 import org.eclipse.birt.data.oda.util.driverconfig.RunTimeInterface;
 import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IExtensionRegistry;
-import org.eclipse.core.runtime.Platform;
-import org.osgi.framework.Bundle;
 
 public class JdbcDriverConfigUtil
 {
 
-	// Corrresponds to a subdirectory within the
-	// ODA home directro
 	private String driverName = "jdbc"; //$NON-NLS-1$
-	private File odaHomeDirectory = null;
 
 	/**
 	 *  
@@ -72,27 +62,25 @@ public class JdbcDriverConfigUtil
 		this.driverName = driverName;
 	}
 
-	private String getDriverLocation( )
+	private File getDriverLocation( )
 	{
-		if ( odaHomeDirectory != null && driverName != null )
-		{
-			return odaHomeDirectory.getAbsolutePath( );
-		}
-
-		return null;
+        registerDriver();
+        ConfigManager configMgr = ConfigManager.getInstance( );
+        return configMgr.getDriverDefaultLibPath(getDriverName());
 	}
+    
+    private final void registerDriver()
+    {
+        DriverSetup.setUp();
+    }
 
 	/**
 	 * @return
 	 */
 	public ArrayList getDriverFiles( )
 	{
+        registerDriver();
 		ConfigManager configMgr = ConfigManager.getInstance( );
-
-		if ( getDriverLocation( ) == null )
-		{
-			registerODADriver( );
-		}
 
 		OpenDataAccessConfig config = null;
 
@@ -134,7 +122,7 @@ public class JdbcDriverConfigUtil
 				// the
 				// oadconfig.xml file exists
 				// i.e the odahomedirectory + "/" + drivername
-				libLocation = this.getDriverLocation( );
+				libLocation = this.getDriverLocation( ).getAbsolutePath();
 			}
 
 			// iterate through the library names
@@ -154,92 +142,4 @@ public class JdbcDriverConfigUtil
 		return files;
 
 	}
-    
-    // Searches Eclipse plugins for all ODA drivers, which implement the birt ODA extension point
-    private void registerODADriver()
-    {
-        // Find all extensions implementing ODA extension point
-        IExtensionRegistry reg = Platform.getExtensionRegistry();
-        if ( reg == null )
-            return;             // Not running within eclipse
-        
-        IExtensionPoint extPoint = reg.getExtensionPoint(
-                                    "org.eclipse.birt.data.oda" );
-        if ( extPoint == null )
-            return;
-        
-        IExtension[] exts = extPoint.getExtensions();
-        for ( int i = 0; i < exts.length; i ++)
-        {
-            IExtension ext = exts[i];
-            IConfigurationElement[] elems = ext.getConfigurationElements();
-            
-            // We expect one "driver" element; find it in the array
-            IConfigurationElement elem = null;
-            for ( int j = 0; elems != null && j < elems.length; j ++)
-            {
-                if ( "driver".equals(elems[j].getName()))
-                {
-                    elem = elems[j];
-                    break;
-                }
-                // This is not an expected element
-                // TODO: log a warming about invalid plugin manifest
-                System.err.println( "Invalid config element in plugin manifest: " + elems[i].getName() );
-            }
-            
-            if ( elem != null )
-            {
-                String name = elem.getAttribute( "name");
-                String configDir = elem.getAttribute( "configFileDir");
-                if ( name != null && name.equals(getDriverName()))
-                {
-                    addODADriverPlugin( ext, name, configDir );
-                    break;
-                }
-            }
-            else
-            {
-                // TODO: log an error about invalid extension
-                // this should not stop us from continuing though
-                System.err.println( "Invalid plugin manifest for ODA driver" );
-            }
-        }
-    }
-    
-    private void addODADriverPlugin( IExtension extension, String driverName, String configDir )
-    {
-        // configDir, if not absolute, is relative to the plugin's root directory
-        // If it is empty, it's the same as the plugin's root
-        if ( configDir == null )
-            configDir = "";
-        File absConfigDir = new File( configDir );
-        if ( ! absConfigDir.isAbsolute() )
-        {
-            // Get the plugin as a Bundle object
-            String pluginName = extension.getNamespace();
-            Bundle bundle = Platform.getBundle( pluginName );
-        
-            // Find the local URL of the plugin's root
-            // This is expected to be in the "file://" protocol
-            URL root = bundle.getEntry( "/" );
-            URL localRoot;
-            try
-            {
-                localRoot = Platform.asLocalURL( root );
-            }
-            catch ( IOException e )
-            {
-                // TODO: log exception
-                e.printStackTrace( System.err );
-                return;
-            }
-
-			absConfigDir = new File( localRoot.getPath( ), configDir );
-		}
-		odaHomeDirectory = absConfigDir;
-
-		ConfigManager.getInstance( ).addDriver( driverName, absConfigDir );
-	}
-
 }
