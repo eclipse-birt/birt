f5a9bea2073cca10b0308809092763f54b6c3f16
yma <>
2005-02-23 02:57:46 +0000

    Fix the SCR 75132-remove the composite emitter

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/executor/ReportExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/executor/ReportExecutor.java
@@ -26,32 +26,34 @@ import org.eclipse.birt.report.engine.ir.PageSequenceDesign;
 import org.eclipse.birt.report.engine.ir.Report;
 
 /**
- * Captures the (report design to) report instance creation logic, by combining the report 
- * design structure and the data. It acts as an entry point to allow actual data to drive 
- * the contents that appears in a report, i.e., header frames use the first data row, 
- * detail rows are repeated for each data row, etc. The output of the executor, for now, 
- * is a specific output format, i.e., HTML, FO or PDF, with the help of the emitter 
- * extensions. <p>
+ * Captures the (report design to) report instance creation logic, by combining
+ * the report design structure and the data. It acts as an entry point to allow
+ * actual data to drive the contents that appears in a report, i.e., header
+ * frames use the first data row, detail rows are repeated for each data row,
+ * etc. The output of the executor, for now, is a specific output format, i.e.,
+ * HTML, FO or PDF, with the help of the emitter extensions.
+ * <p>
  * 
- * The report instance creation logic is subject to further abstraction, because it
- * is needed in both report generation and report presentation. This is because 
- * report document (not supported for now) does not store each report item instance. As
- * a result, the report item instances need to be created at presentation time too. 
- * For now, report generation and presentation are merged as we do not generate report 
- * documents. The report instance creation logic is therefore run only once. When the 
- * generation and presentation phases are separated, the output of an executor
- * could not only be a specific report output format, but also be a report document. 
- * Data would then come from database in factory engine, and from report document in 
- * the presentation engine. 
+ * The report instance creation logic is subject to further abstraction, because
+ * it is needed in both report generation and report presentation. This is
+ * because report document (not supported for now) does not store each report
+ * item instance. As a result, the report item instances need to be created at
+ * presentation time too. For now, report generation and presentation are merged
+ * as we do not generate report documents. The report instance creation logic is
+ * therefore run only once. When the generation and presentation phases are
+ * separated, the output of an executor could not only be a specific report
+ * output format, but also be a report document. Data would then come from
+ * database in factory engine, and from report document in the presentation
+ * engine.
  * 
- * @version $Revision: #4 $ $Date: 2005/02/01 $
+ * @version $Revision: 1.3 $ $Date: 2005/02/07 02:00:39 $
  */
 public class ReportExecutor
 {
 
-	// for logging 
+	// for logging
 	private static Log log = LogFactory.getLog( ReportExecutor.class );
-	
+
 	// the report execution context
 	private ExecutionContext context;
 
@@ -63,10 +65,12 @@ public class ReportExecutor
 
 	/**
 	 * constructor
-	 *
-	 * @param context the executor context
-	 * @param emitter the report emitter
-	 *            
+	 * 
+	 * @param context
+	 *            the executor context
+	 * @param emitter
+	 *            the report emitter
+	 *  
 	 */
 	public ReportExecutor( ExecutionContext context, IReportEmitter emitter )
 	{
@@ -84,44 +88,46 @@ public class ReportExecutor
 	{
 		context.setReport( report );
 
-		// Exceute scripts defined in included libraries. For each library, executes
-		// first the included scripts, then the initialize method. 
-		// The current release does not supported externally included library files 
+		// Exceute scripts defined in included libraries. For each library,
+		// executes
+		// first the included scripts, then the initialize method.
+		// The current release does not supported externally included library
+		// files
 		// handle global libraries in the future
-		
+
 		// execute scripts defined in include-script element of this report
 		// Parameters are not available at this stage
-		Iterator iter = report.getIncludeScripts().iterator();
-		while (iter.hasNext())
+		Iterator iter = report.getIncludeScripts( ).iterator( );
+		while ( iter.hasNext( ) )
 		{
-			String fileName= (String)iter.next();
-			context.loadScript(fileName);
+			String fileName = (String) iter.next( );
+			context.loadScript( fileName );
 		}
-		
-		// DE needs to support getInitialize() method 
+
+		// DE needs to support getInitialize() method
 		// context.execute(report.getInitialize());
-		
-		// call methods associated with report 
-		context.execute(report.getBeforeFactory());
-		     		
+
+		// call methods associated with report
+		context.execute( report.getBeforeFactory( ) );
+
 		// beforeRender is not supported for now
-		
+
 		// Set up report parameters
 		setParameters( paramValues );
-		
+
 		// Prepare necessary data for this report
-		context.getDataEngine().prepare(report);
+		context.getDataEngine( ).prepare( report );
 
 		// Report documents are not supported for now
 		// context.execute(report.getBeforeOpenDoc());
-		
+
 		emitter.startReport( report );
-		
+
 		// report documents are not supported for now
 		// context.execute(report.getAfterOpenDoc());
-		
+
 		// process page set up information
-		handlePageSetup(report);
+		handlePageSetup( report );
 		context.pushMasterPage( context.getDefaultMasterPage( ) );
 		context.pushMasterPage( null );
 
@@ -129,7 +135,7 @@ public class ReportExecutor
 		emitter.startBody( );
 
 		//assert ( report.getContentCount( ) >= 1 );
-		
+
 		// only top-level elements maybe have the master page reference for now
 		for ( int i = 0; i < report.getContentCount( ); i++ )
 		{
@@ -140,42 +146,48 @@ public class ReportExecutor
 		}
 		//USED TO FIX BUG 74548
 		//FIXME: update the master page handle routines.
-		if (report.getContentCount() > 0)
+		if ( report.getContentCount( ) > 0 )
 		{
 			emitter.getPageSetupEmitter( ).endBody( );
 		}
 		emitter.endBody( );
 		context.popMasterPage( );
 		context.popMasterPage( );
-		
+
 		// Report document is not supported
 		// context.execute(report.getBeforeCloseDoc());
-		
+
 		emitter.endReport( );
 		// close eport document
-		
-		// Report document is not supported 
+
+		// Report document is not supported
 		// context.execute(report.getAfterCloseDoc());
-		
+
 		//call afterFactory method of the report
-		context.execute(report.getAfterFactory());
-		
-		context.getDataEngine().shutdown();
+		context.execute( report.getAfterFactory( ) );
+
+		context.getDataEngine( ).shutdown( );
 	}
 
 	/**
-	 * Handles page setup output. This is mainly for presentation phase, and likely
-	 * for FO only. Allows page setup definitions to be put into the FO output through
-	 * Fo emitter.  
+	 * Handles page setup output. This is mainly for presentation phase, and
+	 * likely for FO only. Allows page setup definitions to be put into the FO
+	 * output through Fo emitter.
 	 * 
-     * @param report the report design
-     */
-    private void handlePageSetup(Report report) {
+	 * @param report
+	 *            the report design
+	 */
+	private void handlePageSetup( Report report )
+	{
 		// first create the master page defined in the report design
 		PageSetupContent pageSetup = new PageSetupContent( report
 				.getPageSetup( ) );
 
-		IPageSetupEmitter pageSetupEmitter = emitter.getPageSetupEmitter();	
+		IPageSetupEmitter pageSetupEmitter = emitter.getPageSetupEmitter( );
+		if ( pageSetupEmitter == null )
+		{
+			return;
+		}
 		pageSetupEmitter.startPageSetup( );
 		for ( int i = 0; i < report.getPageSetup( ).getMasterPageCount( ); i++ )
 		{
@@ -192,7 +204,7 @@ public class ReportExecutor
 				pageSetupEmitter.endMasterPage( );
 			}
 		}
-		
+
 		//then output the page sequence defined in the report define
 		for ( int i = 0; i < report.getPageSetup( ).getPageSequenceCount( ); i++ )
 		{
@@ -213,12 +225,13 @@ public class ReportExecutor
 		context.setPageSetup( pageSetup );
 
 		setDefaultMasterPage( report );
-    }
+	}
 
-    /**
+	/**
 	 * sets the default master page/page sequence of the report.
 	 * 
-	 * @param report entry point for the report design
+	 * @param report
+	 *            entry point for the report design
 	 */
 	private void setDefaultMasterPage( Report report )
 	{
@@ -235,10 +248,11 @@ public class ReportExecutor
 	}
 
 	/**
-	 * @param paramValues values for all the report parameters used for report genration
+	 * @param paramValues
+	 *            values for all the report parameters used for report genration
 	 */
 	protected void setParameters( HashMap paramValues )
 	{
-		context.getParams().putAll(paramValues);
+		context.getParams( ).putAll( paramValues );
 	}
 }
