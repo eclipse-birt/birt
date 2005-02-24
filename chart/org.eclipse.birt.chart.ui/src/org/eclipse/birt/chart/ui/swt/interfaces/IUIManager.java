b1b196decfee49d06f77b3588e47bc7bc30f1d11
mpadhye <>
2005-02-24 03:02:51 +0000

    Updated the UI framework to enable dynamic addition / removal of registered sheets

--- chart/org.eclipse.birt.chart.ui/src/org/eclipse/birt/chart/ui/swt/interfaces/IUIManager.java
+++ chart/org.eclipse.birt.chart.ui/src/org/eclipse/birt/chart/ui/swt/interfaces/IUIManager.java
@@ -19,21 +19,85 @@ import org.eclipse.birt.chart.model.Chart;
  */
 public interface IUIManager
 {
+    /**
+     * Register a collection of Sheets (denoted by their Node Paths) that can be operated on together.
+     * 
+     * @param sCollection unique name for the collection
+     * @param saNodePaths array of node paths associated with each sheet entry in the collection
+     * @return the result of the registration. Will be false if collection is already defined.
+     */
+    public boolean registerSheetCollection(String sCollection, String[] saNodePaths);
+    
+    /**
+     * Get the node names present in the specified collection.
+     *  
+     * @param sCollection name of the collection whose contents are to be fetched
+     * @return array of node paths present in the registered collection. null if collection is not found
+     */
+    public String[] getRegisteredCollectionValue(String sCollection);
+    
+    /**
+     * Add a single instance of all the sheets defined in the specified collection.
+     * 
+     * @param sCollection name of collection whose components are to be added
+     * @return true if addition of all the nodes succeeds. false if any component was not found or if addition of any node fails
+     */
+    public boolean addCollectionInstance(String sCollection);
+    
+    /**
+     * Remove the last instance of each sheet defined in the specified collection.
+     * 
+     * @param sCollection name of collection whose components are to be removed
+     * @return true if removal of all nodes was successful. false if any component was not found or any component could not be removed
+     */
+    public boolean removeCollectionInstance(String sCollection);
 
+    /**
+     * 
+     * @param sheet
+     * @deprecated
+     */
     public void addAxisAttributeSheet(ISheet sheet);
 
+    /**
+     * 
+     * @param sheet
+     * @deprecated
+     */
     public void addAxisDataSheet(ISheet sheet);
 
+    /**
+     * 
+     * @param sheet
+     * @deprecated
+     */
     public void addSeriesAttributeSheet(ISheet sheet);
 
+    /**
+     * 
+     * @param sheet
+     * @deprecated
+     */
     public void addSeriesDataSheet(ISheet sheet);
 
+    /**
+     * @deprecated
+     */
     public void removeAxisAttributeSheet();
 
+    /**
+     * @deprecated
+     */
     public void removeAxisDataSheet();
 
+    /**
+     * @deprecated
+     */
     public void removeSeriesAttributeSheet();
 
+    /**
+     * @deprecated
+     */
     public void removeSeriesDataSheet();
 
     public Chart getCurrentModelState();
