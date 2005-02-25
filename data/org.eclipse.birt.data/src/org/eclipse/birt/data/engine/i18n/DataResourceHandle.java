a6cc01bf43f9bafa941e0651cb58ace0a57f346d
ali <>
2005-02-25 19:07:19 +0000

    - Integrated BIRT i18n framework into odaconsumer manager

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/DataResourceHandle.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/DataResourceHandle.java
@@ -1,5 +1,6 @@
-/*******************************************************************************
- * Copyright (c) 2004 Actuate Corporation.
+/*
+ *************************************************************************
+ * Copyright (c) 2004, 2005 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +8,15 @@
  *
  * Contributors:
  *  Actuate Corporation  - initial API and implementation
- *******************************************************************************/
+ *  
+ *************************************************************************
+ */
 
 package org.eclipse.birt.data.engine.i18n;
 
+import java.util.HashMap;
 import java.util.Locale;
-
+import java.util.Map;
 import org.eclipse.birt.core.i18n.ResourceHandle;
 
 /**
@@ -22,6 +26,7 @@ import org.eclipse.birt.core.i18n.ResourceHandle;
 public class DataResourceHandle extends ResourceHandle
 {
 	private static DataResourceHandle sm_resourceHandle;
+	private static Map sm_localeResourceHandleMap;
 	
 	public static DataResourceHandle getInstance()
 	{
@@ -29,6 +34,28 @@ public class DataResourceHandle extends ResourceHandle
 			sm_resourceHandle = new DataResourceHandle( Locale.getDefault() );
 		
 		return sm_resourceHandle;
+	}
+	
+	public static DataResourceHandle getInstance( Locale locale )
+	{
+		DataResourceHandle ret = 
+			(DataResourceHandle) getLocaleResourceHandleMap().get( locale );
+		
+		if( ret == null )
+		{
+			ret = new DataResourceHandle( locale );
+			getLocaleResourceHandleMap().put( locale, ret );
+		}
+		
+		return ret;
+	}
+	
+	private static Map getLocaleResourceHandleMap()
+	{
+		if( sm_localeResourceHandleMap == null )
+			sm_localeResourceHandleMap = new HashMap();
+		
+		return sm_localeResourceHandleMap;
 	}
 
 	/**
