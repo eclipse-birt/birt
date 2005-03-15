1e8617ca588714bcf7237383d9c6baad2975ffa1
rcolaco <>
2005-03-15 23:57:08 +0000

    Updated the interface for delegating external chart model validation and invoking the expression builder

--- chart/org.eclipse.birt.chart.ui/src/org/eclipse/birt/chart/ui/swt/interfaces/IUIServiceProvider.java
+++ chart/org.eclipse.birt.chart.ui/src/org/eclipse/birt/chart/ui/swt/interfaces/IUIServiceProvider.java
@@ -22,27 +22,27 @@ public interface IUIServiceProvider
     /**
      * Constant indicating a fatal error in the model
      */
-    public static final int FATAL_ERROR = -10;
+    public static final int FATAL_ERRORS = -10;
 
     /**
      * Constant indicating a major (though not fatal) problem in the model
      */
-    public static final int CRITICAL_ERROR = -5;
+    public static final int CRITICAL_ERRORS = -5;
 
     /**
      * Constant indicating a minor error in the model
      */
-    public static final int MINOR_ERROR = -1;
+    public static final int MINOR_ERRORS = -1;
 
     /**
      * Constant indicating no detectable problems exist in the model
      */
-    public static final int NO_PROBLEMS = 0;
+    public static final int NO_ERRORS = 0;
 
     /**
      * Constant indicating possible problems detected in the model
      */
-    public static final int POTENTIAL_PROBLEMS = 1;
+    public static final int POSSIBLE_ERRORS = 1;
 
     /**
      * This method will be used by the Chart Builder UI to invoke the expression builder with any previously defined
@@ -50,7 +50,7 @@ public interface IUIServiceProvider
      * 
      * @param sExpression
      *            the expression to be displayed in the builder (after re-entry)
-     * @param Context
+     * @param oContext
      *            the application-specific context used by the Expression Builder for each invocation
      * 
      * @return The final expression string built by the user in the expression builder
@@ -63,15 +63,9 @@ public interface IUIServiceProvider
      * 
      * @param chartModel
      *            the model to be validated
-     * @return a constant value that will be used by the UI to determine how to proceed
-     */
-    public int validate(Chart chartModel);
-
-    /**
-     * This method will be called by the Chart Builder only after a 'validate' call is made. It returns an array of
-     * user-friendly messages indicating any potential problems or errors in the chart model that was last validated.
-     * 
+     * @param oContext
+     *            the application-specific context associated with the extended chart item 
      * @return an array of user-friendly messages indicating problems with the model
      */
-    public String[] getMessages();
+    public String[] validate(Chart chartModel, Object oContext);
 }
