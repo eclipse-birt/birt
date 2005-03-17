62f7fdc0d47d1672e2c4d71ff8a673c0690700ca
Zhiqiang Qian <zqian@actuate.com>
2005-03-17 02:31:52 +0000

    - SCR(s) Resolved: - Description: 1. Change the behavior of selecting value for property editor filter page the same as data set editor. 2. GUI adjustment for HighlightRuleBuilderDialog
    - Regression ( Yes/No ):No
    
    - Code Owner: Zhiqiang Qian
    
    - Code Reviewers: SHA GUI, Qiangsheng Wang
    
    - Tests: Yes
    
    - Test Automated (Yes/No, if "No" then explain why):No, gui feature
    
    - Case Entries Resolved:
    - Notes to Developers:
    - Notes to QA:
    - Notes to Documentation:
    - Notes to Configuration Management:
    - Notes to Support:
    - Notes to Product Marketing:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/SelectValueDialog.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/SelectValueDialog.java
@@ -28,6 +28,8 @@ import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
 import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.birt.report.model.api.DataSetHandle;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -37,206 +39,231 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
-
 /**
- * This dialog takes an expression and a data set and shows a list of unique values for selection
- * from the data set.
- * It allows both multiple and single selection. The default is single selection.
+ * This dialog takes an expression and a data set and shows a list of unique
+ * values for selection from the data set. It allows both multiple and single
+ * selection. The default is single selection.
  * 
- * @version $Revision: 1.2 $ $Date: 2005/02/24 04:48:32 $
+ * @version $Revision: 1.3 $ $Date: 2005/03/09 00:43:59 $
  */
 public class SelectValueDialog extends BaseDialog
 {
-    private DataSetHandle dataSetHandle = null;
-    private String expression = null;
-    private boolean multipleSelection = false;
-    
-    private List selectValueList = null;
 
-    /**
-     * @param parentShell
-     * @param title
-     */
-    public SelectValueDialog(Shell parentShell, String title)
-    {
-        super(parentShell, title, false);
-    }
+	private DataSetHandle dataSetHandle = null;
+	private String expression = null;
+	private boolean multipleSelection = false;
 
-    /**
-     * @return Returns the dataSetHandle.
-     */
-    public DataSetHandle getDataSetHandle()
-    {
-        return dataSetHandle;
-    }
+	private List selectValueList = null;
 
-    /**
-     * @param dataSetHandle The dataSetHandle to set.
-     */
-    public void setDataSetHandle(DataSetHandle dataSetHandle)
-    {
-        this.dataSetHandle = dataSetHandle;
-    }
+	/**
+	 * @param parentShell
+	 * @param title
+	 */
+	public SelectValueDialog( Shell parentShell, String title )
+	{
+		super( parentShell, title, false );
+	}
 
-    /**
-     * @return Returns the expression.
-     */
-    public String getExpression()
-    {
-        return expression;
-    }
+	/**
+	 * @return Returns the dataSetHandle.
+	 */
+	public DataSetHandle getDataSetHandle( )
+	{
+		return dataSetHandle;
+	}
 
-    /**
-     * @param expression The expression to set.
-     */
-    public void setExpression(String expression)
-    {
-        this.expression = expression;
-    }
+	/**
+	 * @param dataSetHandle
+	 *            The dataSetHandle to set.
+	 */
+	public void setDataSetHandle( DataSetHandle dataSetHandle )
+	{
+		this.dataSetHandle = dataSetHandle;
+	}
 
-    /**
-     * @return Returns the multipleSelection.
-     */
-    public boolean isMultipleSelection()
-    {
-        return multipleSelection;
-    }
+	/**
+	 * @return Returns the expression.
+	 */
+	public String getExpression( )
+	{
+		return expression;
+	}
 
-    /**
-     * @param multipleSelection The multipleSelection to set.
-     */
-    public void setMultipleSelection(boolean multipleSelection)
-    {
-        this.multipleSelection = multipleSelection;
-    }
+	/**
+	 * @param expression
+	 *            The expression to set.
+	 */
+	public void setExpression( String expression )
+	{
+		this.expression = expression;
+	}
 
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
-     */
-    protected Control createDialogArea(Composite parent)
-    {
-        Composite composite = new Composite(parent, SWT.NONE);
-        GridLayout layout = new GridLayout();
-        composite.setLayout(layout);
-        Label label = new Label(composite, SWT.NONE);
-        label.setText(Messages.getString("SelectValueDialog.selectValue")); //$NON-NLS-1$
-        
-        selectValueList = new List(composite, isMultipleSelection()?SWT.MULTI:SWT.SINGLE | SWT.V_SCROLL | SWT.H_SCROLL);
-        GridData data = new GridData(GridData.FILL_BOTH);
-        data.heightHint = 250;
-        data.widthHint = 300;
-        selectValueList.setLayoutData(data);
-        selectValueList.add(Messages.getString("SelectValueDialog.retrieving")); //$NON-NLS-1$
-        
-        PlatformUI.getWorkbench( ).getDisplay( ).asyncExec(
-        new Runnable()
-        {
+	/**
+	 * @return Returns the multipleSelection.
+	 */
+	public boolean isMultipleSelection( )
+	{
+		return multipleSelection;
+	}
 
-            public void run()
-            {
-                populateList();
-            }
-            
-        });
-        
-        return composite;
-    }
-    
-    /* (non-Javadoc)
-     * @see org.eclipse.jface.dialogs.Dialog#okPressed()
-     */
-    protected void okPressed()
-    {
-        setResult(selectValueList.getSelection());
-        super.okPressed();
-    }
-    
-    
-    public String getSelectedValue()
-    {
-        String[] result = (String[])getResult();
-        return (result != null && result.length > 0)?result[0]:null;
-    }
-    
-    public String[] getSelectedValues()
-    {
-        String[] result = (String[])getResult();
-        return (result != null && result.length > 0)?result:null;
-    }
-    
-    private void populateList()
-    {
-        try
-        {
-            //Execute the query and populate this list
-            BaseQueryDefinition query = (BaseQueryDefinition)DataSetManager.getCurrentInstance().getPreparedQuery(getDataSetHandle(), true, false).getReportQueryDefn();
-            ScriptExpression expression = new ScriptExpression(getExpression());
-            GroupDefinition defn = new GroupDefinition();
-            defn.setKeyExpression(getExpression());
-            query.setUsesDetails(false);
-            query.addGroup(defn);
-            query.addExpression(expression, BaseTransform.BEFORE_FIRST_ROW);
-            
-            IPreparedQuery preparedQuery = DataSetManager.getCurrentInstance().getEngine().prepare((IQueryDefinition) query);
-            IQueryResults results = preparedQuery.execute(null);
-            selectValueList.removeAll();
-            if(results != null)
-            {
-                IResultIterator iter = results.getResultIterator();
-                if(iter != null)
-                {
-                    while(iter.next())
-                    {
-                        selectValueList.add(iter.getString(expression));
-                    }
-                }
-                
-                results.close();
-            }
-        }
-        catch (Exception e)
-        {
-            ExceptionHandler.handle(e);
-        }
-    }
+	/**
+	 * @param multipleSelection
+	 *            The multipleSelection to set.
+	 */
+	public void setMultipleSelection( boolean multipleSelection )
+	{
+		this.multipleSelection = multipleSelection;
+	}
 
-    private void populateList1()
-    {
-        try
-        {
-            //Execute the query and populate this list
-            OdaDataSetDesign design = (OdaDataSetDesign)DataSetManager.getCurrentInstance().getDataSetDesign(getDataSetHandle(), true, false);
-            design.addComputedColumn(new ComputedColumn("selectValue", getExpression()));
-            QueryDefinition query = DataSetManager.getCurrentInstance().getQueryDefinition(design);
-            ScriptExpression expression = new ScriptExpression("row[\"selectValue\"]");
-            
-            GroupDefinition defn = new GroupDefinition();
-            defn.setKeyExpression(expression.getText());
-            query.setUsesDetails(false);
-            query.addGroup(defn);
-            query.addExpression(expression, BaseTransform.BEFORE_FIRST_ROW);
-            
-            IPreparedQuery preparedQuery = DataSetManager.getCurrentInstance().getPreparedQuery(query);
-            IQueryResults results = preparedQuery.execute(null);
-            selectValueList.removeAll();
-            if(results != null)
-            {
-                IResultIterator iter = results.getResultIterator();
-                if(iter != null)
-                {
-                    while(iter.next())
-                    {
-                        selectValueList.add(iter.getString(expression));
-                    }
-                }
-                
-                results.close();
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-            ExceptionHandler.handle(e);
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	protected Control createDialogArea( Composite parent )
+	{
+		Composite composite = new Composite( parent, SWT.NONE );
+		GridLayout layout = new GridLayout( );
+		composite.setLayout( layout );
+		Label label = new Label( composite, SWT.NONE );
+		label.setText( Messages.getString( "SelectValueDialog.selectValue" ) ); //$NON-NLS-1$
+
+		selectValueList = new List( composite,
+				isMultipleSelection( ) ? SWT.MULTI : SWT.SINGLE
+						| SWT.V_SCROLL
+						| SWT.H_SCROLL );
+		GridData data = new GridData( GridData.FILL_BOTH );
+		data.heightHint = 250;
+		data.widthHint = 300;
+		selectValueList.setLayoutData( data );
+		selectValueList.add( Messages.getString( "SelectValueDialog.retrieving" ) ); //$NON-NLS-1$
+		selectValueList.addMouseListener( new MouseAdapter( ) {
+
+			public void mouseDoubleClick( MouseEvent e )
+			{
+				if ( selectValueList.getSelectionCount( ) > 0 )
+				{
+					okPressed( );
+				}
+			}
+		} );
+
+		PlatformUI.getWorkbench( ).getDisplay( ).asyncExec( new Runnable( ) {
+
+			public void run( )
+			{
+				populateList( );
+			}
+
+		} );
+
+		return composite;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#okPressed()
+	 */
+	protected void okPressed( )
+	{
+		setResult( selectValueList.getSelection( ) );
+		super.okPressed( );
+	}
+
+	public String getSelectedValue( )
+	{
+		String[] result = (String[]) getResult( );
+		return ( result != null && result.length > 0 ) ? result[0] : null;
+	}
+
+	public String[] getSelectedValues( )
+	{
+		String[] result = (String[]) getResult( );
+		return ( result != null && result.length > 0 ) ? result : null;
+	}
+
+	private void populateList( )
+	{
+		try
+		{
+			//Execute the query and populate this list
+			BaseQueryDefinition query = (BaseQueryDefinition) DataSetManager.getCurrentInstance( )
+					.getPreparedQuery( getDataSetHandle( ), true, false )
+					.getReportQueryDefn( );
+			ScriptExpression expression = new ScriptExpression( getExpression( ) );
+			GroupDefinition defn = new GroupDefinition( );
+			defn.setKeyExpression( getExpression( ) );
+			query.setUsesDetails( false );
+			query.addGroup( defn );
+			query.addExpression( expression, BaseTransform.BEFORE_FIRST_ROW );
+
+			IPreparedQuery preparedQuery = DataSetManager.getCurrentInstance( )
+					.getEngine( )
+					.prepare( (IQueryDefinition) query );
+			IQueryResults results = preparedQuery.execute( null );
+			selectValueList.removeAll( );
+			if ( results != null )
+			{
+				IResultIterator iter = results.getResultIterator( );
+				if ( iter != null )
+				{
+					while ( iter.next( ) )
+					{
+						selectValueList.add( iter.getString( expression ) );
+					}
+				}
+
+				results.close( );
+			}
+		}
+		catch ( Exception e )
+		{
+			ExceptionHandler.handle( e );
+		}
+	}
+
+	private void populateList1( )
+	{
+		try
+		{
+			//Execute the query and populate this list
+			OdaDataSetDesign design = (OdaDataSetDesign) DataSetManager.getCurrentInstance( )
+					.getDataSetDesign( getDataSetHandle( ), true, false );
+			design.addComputedColumn( new ComputedColumn( "selectValue",
+					getExpression( ) ) );
+			QueryDefinition query = DataSetManager.getCurrentInstance( )
+					.getQueryDefinition( design );
+			ScriptExpression expression = new ScriptExpression( "row[\"selectValue\"]" );
+
+			GroupDefinition defn = new GroupDefinition( );
+			defn.setKeyExpression( expression.getText( ) );
+			query.setUsesDetails( false );
+			query.addGroup( defn );
+			query.addExpression( expression, BaseTransform.BEFORE_FIRST_ROW );
+
+			IPreparedQuery preparedQuery = DataSetManager.getCurrentInstance( )
+					.getPreparedQuery( query );
+			IQueryResults results = preparedQuery.execute( null );
+			selectValueList.removeAll( );
+			if ( results != null )
+			{
+				IResultIterator iter = results.getResultIterator( );
+				if ( iter != null )
+				{
+					while ( iter.next( ) )
+					{
+						selectValueList.add( iter.getString( expression ) );
+					}
+				}
+
+				results.close( );
+			}
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace( );
+			ExceptionHandler.handle( e );
+		}
+	}
 }
