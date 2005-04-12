bc8e04234b61892cee75d795a11870761866d499
xli <>
2005-04-12 09:35:41 +0000

    *** empty log message ***

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/core/IDesignElement.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/core/IDesignElement.java
@@ -12,6 +12,7 @@
 package org.eclipse.birt.report.model.api.core;
 
 import org.eclipse.birt.report.model.api.DesignElementHandle;
+import org.eclipse.birt.report.model.api.ReportDesignHandle;
 import org.eclipse.birt.report.model.api.metadata.IElementDefn;
 import org.eclipse.birt.report.model.elements.ReportDesign;
 
@@ -41,4 +42,26 @@ public interface IDesignElement extends Cloneable
 	 */
 
 	public DesignElementHandle getHandle( ReportDesign design );
+	
+	/**
+	 * Generates a clone copy of this element. When a report element is cloned,
+	 * the basic principle is just copying the property value into the clone,
+	 * the other things, like container references, child list references,
+	 * listener references will not be cloned; that is, the clone is isolated
+	 * from the design tree until it is added into a target design tree.
+	 * 
+	 * <p>
+	 * When inserting the cloned element into the design tree, user needs to
+	 * care about the element name confliction; that is, the client needs to
+	 * call the method
+	 * <code>{@link ReportDesignHandle#rename( DesignElementHandle )}</code> to
+	 * change the element names.
+	 * 
+	 * @return Object the cloned design element.
+	 * @throws CloneNotSupportedException
+	 *             if clone is not supported.
+	 *  
+	 */
+
+	public Object clone( ) throws CloneNotSupportedException;
 }
