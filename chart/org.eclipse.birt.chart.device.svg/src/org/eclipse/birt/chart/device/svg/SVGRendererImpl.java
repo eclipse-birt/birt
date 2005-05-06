77832b2b1894dc5f6158beff94b5fbc03e80ae79
sdsouza <>
2005-05-06 18:26:25 +0000

    Initial

---
+++ chart/org.eclipse.birt.chart.device.svg/src/org/eclipse/birt/chart/device/svg/SVGRendererImpl.java
@@ -0,0 +1,17 @@
+/*
+ * Created on May 6, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.eclipse.birt.chart.device.svg;
+
+/**
+ * @author sadsouza
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class SVGRendererImpl {
+
+}
