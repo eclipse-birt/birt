a4c36627f4493524aba988d6e139a17eda53a021
stwang <>
2005-05-07 03:56:50 +0000

    Report Engine: Handled the OutOfMemory error properly

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/RunAndRenderTask.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/RunAndRenderTask.java
@@ -155,6 +155,11 @@ public class RunAndRenderTask extends EngineTask implements IRunAndRenderTask
 		{
 			log.log(Level.SEVERE, "An error happened while running the report. Cause:", ex); //$NON-NLS-1$
 		}
+		catch (OutOfMemoryError err)
+		{
+			log.log(Level.SEVERE, "An OutOfMemory error happened while running the report."); //$NON-NLS-1$
+			throw err;
+		}
 	}
 
 	/**
