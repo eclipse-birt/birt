a49c5855e994534c38bb1d867132fd230c9a7555
xli <>
2005-05-08 02:05:09 +0000

    Update some default value in rom.def and validation of NamePropertyType.

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/metadata/NamePropertyType.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/metadata/NamePropertyType.java
@@ -1,56 +1,112 @@
 /*******************************************************************************
-* Copyright (c) 2004 Actuate Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*  Actuate Corporation  - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.model.metadata;
 
+import org.eclipse.birt.report.model.api.metadata.PropertyValueException;
+import org.eclipse.birt.report.model.api.util.StringUtil;
+import org.eclipse.birt.report.model.elements.ReportDesign;
 
 /**
- * Element name property type. Represents the name
- * of an element.
- *
+ * Element name property type. Represents the name of an element.
+ *  
  */
 
 public class NamePropertyType extends TextualPropertyType
 {
-    /**
+
+	/**
 	 * Display name key.
 	 */
-	
+
 	private static final String DISPLAY_NAME_KEY = "Property.name"; //$NON-NLS-1$
 
 	/**
 	 * Constructor.
 	 */
-	
+
 	public NamePropertyType( )
 	{
-	    super( DISPLAY_NAME_KEY );
+		super( DISPLAY_NAME_KEY );
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.birt.report.model.design.metadata.PropertyType#getTypeCode()
 	 */
-	
+
 	public int getTypeCode( )
 	{
 		return NAME_TYPE;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.birt.report.model.design.metadata.PropertyType#getXmlName()
 	 */
-	
+
 	public String getName( )
 	{
 		return NAME_TYPE_NAME;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.report.model.metadata.PropertyType#validateValue(org.eclipse.birt.report.model.elements.ReportDesign,
+	 *      org.eclipse.birt.report.model.metadata.PropertyDefn,
+	 *      java.lang.Object)
+	 */
+
+	public Object validateValue( ReportDesign design, PropertyDefn defn,
+			Object value ) throws PropertyValueException
+	{
+		assert defn != null;
+		if ( value == null )
+		{
+			if ( defn.getValueType( ) == PropertyDefn.STRUCT_PROPERTY )
+				throw new PropertyValueException( value,
+						PropertyValueException.DESIGN_EXCEPTION_VALUE_REQUIRED,
+						NAME_TYPE );
+			return null;
+		}
+		if ( value instanceof String )
+		{
+			if ( StringUtil.isBlank( (String) value )
+					&& defn.getValueType( ) == PropertyDefn.STRUCT_PROPERTY )
+				throw new PropertyValueException( value,
+						PropertyValueException.DESIGN_EXCEPTION_INVALID_VALUE,
+						NAME_TYPE );
+			return StringUtil.trimString( (String) value );
+		}
+		throw new PropertyValueException( value,
+				PropertyValueException.DESIGN_EXCEPTION_INVALID_VALUE,
+				NAME_TYPE );
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.report.model.metadata.PropertyType#validateXml(org.eclipse.birt.report.model.elements.ReportDesign,
+	 *      org.eclipse.birt.report.model.metadata.PropertyDefn,
+	 *      java.lang.String)
+	 */
+
+	public Object validateXml( ReportDesign design, PropertyDefn defn,
+			String value ) throws PropertyValueException
+	{
+		if ( value == null )
+			return null;
+		return StringUtil.trimString( value );
+	}
 }
