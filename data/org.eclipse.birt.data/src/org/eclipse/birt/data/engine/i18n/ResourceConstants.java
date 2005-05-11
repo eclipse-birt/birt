74c1db663f00186e9601b206de04f6717f4f7e9a
Lin Zhu <lzhu@actuate.com>
2005-05-11 07:07:08 +0000

    Fix #SCR 77966

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
@@ -25,6 +25,7 @@ public class ResourceConstants
 	public final static String NOT_END_GROUP  = "data.engine.EndOfGroupOnly";
 	public final static String INVALID_AGGR_GROUP_LEVEL  = "data.engine.InvalidAggrGroup";
 	public final static String INVALID_AGGR_PARAMETER  = "data.engine.WrongNumAggrArgs";
+	public final static String INVALID_AGGR_GROUP_EXPRESSION ="data.engine.InvalidAggrGroupExpression";
 
 	public final static String DUPLICATE_PROPERTY_NAME = "data.engine.DupPropertyName";
 	public final static String SUBQUERY_NOT_FOUND = "data.engine.NoSubQueryName";
