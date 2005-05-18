2c93d428372f00cf281969e6f56dc4609c7183f5
stwang <>
2005-05-18 20:57:31 +0000

    Added more message constants, mostly for displaying in HTML and PDF inline stream.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/i18n/MessageConstants.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/i18n/MessageConstants.java
@@ -27,5 +27,26 @@ public class MessageConstants
 	public static final String  INVALID_DESIGN_FILE_EXCEPTION = "Error.InvalidDesignFile";  				//$NON-NLS-1$
 	public static final String  CANNOT_CREATE_EMITTER_EXCEPTION = "Error.CannotCreateExtensionInstance";  	//$NON-NLS-1$
 	public static final String  MISSING_COMPUTED_COLUMN_EXPRESSION_EXCEPTION = "Error.MissingComputedColumnExpression";	//$NON-NLS-1$
+
+	// Page Errors
+	public static final String  ERROR = "Error.Error";	//$NON-NLS-1$
+	public static final String  ERRORS_ON_PAGE = "Error.ErrorsOnPage";	//$NON-NLS-1$
+	public static final String  SCRIPT_FILE_LOAD_ERROR = "Error.ScriptFileLoadError";	//$NON-NLS-1$
+	public static final String  SCRIPT_EVALUATION_ERROR = "Error.ScriptEvaluationError";	//$NON-NLS-1$
+	public static final String  TEXT_PROCESSING_ERROR = "Error.TextualItemProcessingError";	//$NON-NLS-1$	
+	public static final String  TABLE_PROCESSING_ERROR = "Error.TableItemProcessingError";	//$NON-NLS-1$	
+	public static final String  EXTENDED_ITEM_GENERATION_ERROR = "Error.ExtendedItemGenerationError";	//$NON-NLS-1$	
+	public static final String  EXTENDED_ITEM_RENDERING_ERROR = "Error.ExtendedItemRenderingError";	//$NON-NLS-1$	
+	public static final String  GRID_PROCESSING_ERROR = "Error.GridItemProcessingError";	//$NON-NLS-1$
+	public static final String  MISSING_IMAGE_FILE_ERROR = "Error.MissingImageFileError";	//$NON-NLS-1$
+	public static final String  INVALID_IMAGE_SOURCE_TYPE_ERROR = "Error.InvalidImageSourceError";	//$NON-NLS-1$	
+	public static final String  DATABASE_IMAGE__ERROR = "Error.InvalidDatabaseImageError";	//$NON-NLS-1$
+	public static final String  EMBEDDED_EXPRESSION_ERROR = "Error.EmbeddedExpressionError";	//$NON-NLS-1$
+	public static final String  EMBEDDED_IMAGE_ERROR = "Error.EmbeddedImageError";	//$NON-NLS-1$
+	public static final String  HTML_IMAGE_ERROR = "Error.HTMLImageError";	//$NON-NLS-1$
+
 }
+
+
+
 
