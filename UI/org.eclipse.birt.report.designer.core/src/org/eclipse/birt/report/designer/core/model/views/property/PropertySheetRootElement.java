3064cc824dd964d1471186fb5e7b558300bda4b7
qwang <>
2005-06-16 07:40:48 +0000

    - SCR(s) Resolved:#  74033  Property sheet does not provide a hint of what is selected.
    - Description: Add root element on properties view. That root element show the type of what is selected.
    
    - Regression ( Yes/No ):No
    
    - Code Owner: WangQS
    
    - Code Reviewers: Dazhen Gao, Yueqian Wang
    
    - Tests: manual
    
    - Test Automated (Yes/No, if No? then explain why):no, gui feature.
    
    - Branches Involved: BIRT 1.0
    
    - Case Entries Resolved:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Notes to Configuration Management:
    
    - Notes to Support:
    
    - Notes to Product Marketing:

---
+++ UI/org.eclipse.birt.report.designer.core/src/org/eclipse/birt/report/designer/core/model/views/property/PropertySheetRootElement.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Actuate Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.birt.report.designer.core.model.views.property;
+
+
+/**
+ * Provide the vitual root of properties view.
+ */
+public class PropertySheetRootElement
+{
+
+	private Object model;
+	private String displayName;
+
+	/**
+	 * Constructuor
+	 * @param model, selected element
+	 */
+	public PropertySheetRootElement(Object model)
+	{
+		this.model = model;
+	}
+	
+	/**
+	 * @return model
+	 */
+	public Object getModel()
+	{
+		return model;
+	}
+	
+	/**
+	 * @return display name of root element
+	 */
+	public String getDisplayName()
+	{
+		return displayName;
+	}
+	
+	/**
+	 * Set display name
+	 * @param displayName
+	 */
+	public void setDisplayName(String displayName)
+	{
+		this.displayName = displayName;
+	}
+}
