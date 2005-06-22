51cbbf1df6c563c9141fd5ec2277c32346c1835a
Zhiqiang Qian <zqian@actuate.com>
2005-06-22 02:57:29 +0000

    - SCR(s) Resolved: scr#79129 Error occurs when set "Attributes->Y Axis->Minor Grid->Ticks->style" to "Above"
    - Description:
    allow above/below only when orientation is horizontal.
    allow left/right only when oritentation is vertical.
    
    - Regression ( Yes/No ):No
    
    - Code Owner: Zhiqiang Qian
    
    - Code Reviewers: SHA GUI
    
    - Tests: Yes
    
    - Test Automated (Yes/No, if "No" then explain why):No, gui feature
    
    - Case Entries Resolved:
    - Notes to Developers:
    - Notes to QA:
    - Notes to Documentation:
    - Notes to Configuration Management:
    - Notes to Support:
    - Notes to Product Marketing:

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/GridAttributesComposite.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/GridAttributesComposite.java
@@ -14,6 +14,7 @@ package org.eclipse.birt.chart.ui.swt.composites;
 import java.util.Vector;
 
 import org.eclipse.birt.chart.model.attribute.ColorDefinition;
+import org.eclipse.birt.chart.model.attribute.Orientation;
 import org.eclipse.birt.chart.model.attribute.TickStyle;
 import org.eclipse.birt.chart.model.component.Grid;
 import org.eclipse.birt.chart.ui.extension.i18n.Messages;
@@ -35,279 +36,303 @@ import org.eclipse.swt.widgets.Listener;
  * @author Actuate Corporation
  *  
  */
-public class GridAttributesComposite extends Composite implements SelectionListener, Listener
+public class GridAttributesComposite extends Composite implements
+		SelectionListener,
+		Listener
 {
 
-    private transient Group grpTicks = null;
+	private transient Group grpTicks = null;
 
-    private transient LineAttributesComposite liacLines = null;
+	private transient LineAttributesComposite liacLines = null;
 
-    private transient LineAttributesComposite liacTicks = null;
+	private transient LineAttributesComposite liacTicks = null;
 
-    private transient Composite cmpContent = null;
+	private transient Composite cmpContent = null;
 
-    private transient Composite cmpLines = null;
+	private transient Composite cmpLines = null;
 
-    private transient Label lblStyle = null;
+	private transient Label lblStyle = null;
 
-    private transient Combo cmbTickStyle = null;
+	private transient Combo cmbTickStyle = null;
 
-    private transient Grid grid = null;
+	private transient Grid grid = null;
 
-    private transient Vector vListeners = null;
+	private transient Vector vListeners = null;
 
-    // Grid Attribute Change Events
-    public static final int LINE_STYLE_CHANGED_EVENT = 1;
+	private int orientation;
 
-    public static final int LINE_WIDTH_CHANGED_EVENT = 2;
+	// Grid Attribute Change Events
+	public static final int LINE_STYLE_CHANGED_EVENT = 1;
 
-    public static final int LINE_COLOR_CHANGED_EVENT = 3;
+	public static final int LINE_WIDTH_CHANGED_EVENT = 2;
 
-    public static final int LINE_VISIBILITY_CHANGED_EVENT = 4;
+	public static final int LINE_COLOR_CHANGED_EVENT = 3;
 
-    public static final int TICK_STYLE_CHANGED_EVENT = 5;
+	public static final int LINE_VISIBILITY_CHANGED_EVENT = 4;
 
-    public static final int TICK_COLOR_CHANGED_EVENT = 6;
+	public static final int TICK_STYLE_CHANGED_EVENT = 5;
 
-    public static final int TICK_VISIBILITY_CHANGED_EVENT = 7;
+	public static final int TICK_COLOR_CHANGED_EVENT = 6;
 
-    /**
-     * @param parent
-     * @param style
-     */
-    public GridAttributesComposite(Composite parent, int style, Grid grid)
-    {
-        super(parent, style);
-        init(grid);
-        placeComponents();
-    }
+	public static final int TICK_VISIBILITY_CHANGED_EVENT = 7;
 
-    /**
-     *  
-     */
-    private void init(Grid grid)
-    {
-        this.setSize(getParent().getClientArea().width, getParent().getClientArea().height);
-        this.grid = grid;
-        this.vListeners = new Vector();
-    }
+	/**
+	 * @param parent
+	 * @param style
+	 */
+	public GridAttributesComposite( Composite parent, int style, Grid grid,
+			int orientation )
+	{
+		super( parent, style );
+		this.orientation = orientation;
+		init( grid );
+		placeComponents( );
+	}
 
-    /**
-     *  
-     */
-    private void placeComponents()
-    {
-        // Layout for entire composite
-        FillLayout flMain = new FillLayout();
-        flMain.marginHeight = 4;
-        flMain.marginWidth = 2;
+	/**
+	 *  
+	 */
+	private void init( Grid grid )
+	{
+		this.setSize( getParent( ).getClientArea( ).width,
+				getParent( ).getClientArea( ).height );
+		this.grid = grid;
+		this.vListeners = new Vector( );
+	}
 
-        // Layout for content composite
-        GridLayout glContent = new GridLayout();
-        glContent.verticalSpacing = 5;
-        glContent.horizontalSpacing = 5;
-        glContent.marginHeight = 0;
-        glContent.marginWidth = 0;
-        glContent.numColumns = 2;
+	/**
+	 *  
+	 */
+	private void placeComponents( )
+	{
+		// Layout for entire composite
+		FillLayout flMain = new FillLayout( );
+		flMain.marginHeight = 4;
+		flMain.marginWidth = 2;
 
-        // Layout for Grid Lines group
-        FillLayout flLines = new FillLayout();
-        flLines.marginHeight = 1;
-        flLines.marginWidth = 1;
+		// Layout for content composite
+		GridLayout glContent = new GridLayout( );
+		glContent.verticalSpacing = 5;
+		glContent.horizontalSpacing = 5;
+		glContent.marginHeight = 0;
+		glContent.marginWidth = 0;
+		glContent.numColumns = 2;
 
-        // Layout for Ticks group
-        GridLayout glTicks = new GridLayout();
-        glTicks.marginHeight = 4;
-        glTicks.marginWidth = 4;
-        glTicks.verticalSpacing = 2;
-        glTicks.horizontalSpacing = 5;
-        glTicks.numColumns = 2;
+		// Layout for Grid Lines group
+		FillLayout flLines = new FillLayout( );
+		flLines.marginHeight = 1;
+		flLines.marginWidth = 1;
 
-        this.setLayout(flMain);
+		// Layout for Ticks group
+		GridLayout glTicks = new GridLayout( );
+		glTicks.marginHeight = 4;
+		glTicks.marginWidth = 4;
+		glTicks.verticalSpacing = 2;
+		glTicks.horizontalSpacing = 5;
+		glTicks.numColumns = 2;
 
-        // Content Composite
-        cmpContent = new Composite(this, SWT.NONE);
-        cmpContent.setLayout(glContent);
+		this.setLayout( flMain );
 
-        // Grid Lines group
-        cmpLines = new Composite(cmpContent, SWT.NONE);
-        GridData gdCMPLines = new GridData(GridData.FILL_BOTH);
-        gdCMPLines.horizontalSpan = 2;
-        cmpLines.setLayoutData(gdCMPLines);
-        cmpLines.setLayout(flLines);
+		// Content Composite
+		cmpContent = new Composite( this, SWT.NONE );
+		cmpContent.setLayout( glContent );
 
-        // Line Attributes for Grid Lines
-        liacLines = new LineAttributesComposite(cmpLines, SWT.NONE, grid.getLineAttributes(), true, true, true);
-        GridData gdLIACLines = new GridData(GridData.FILL_BOTH);
-        liacLines.setLayoutData(gdLIACLines);
-        liacLines.addListener(this);
+		// Grid Lines group
+		cmpLines = new Composite( cmpContent, SWT.NONE );
+		GridData gdCMPLines = new GridData( GridData.FILL_BOTH );
+		gdCMPLines.horizontalSpan = 2;
+		cmpLines.setLayoutData( gdCMPLines );
+		cmpLines.setLayout( flLines );
 
-        // Ticks group
-        grpTicks = new Group(cmpContent, SWT.NONE);
-        GridData gdGRPTicks = new GridData(GridData.FILL_BOTH);
-        gdGRPTicks.horizontalSpan = 2;
-        grpTicks.setLayoutData(gdGRPTicks);
-        grpTicks.setLayout(glTicks);
-        grpTicks.setText(Messages.getString("GridAttributesComposite.Lbl.Ticks")); //$NON-NLS-1$
+		// Line Attributes for Grid Lines
+		liacLines = new LineAttributesComposite( cmpLines,
+				SWT.NONE,
+				grid.getLineAttributes( ),
+				true,
+				true,
+				true );
+		GridData gdLIACLines = new GridData( GridData.FILL_BOTH );
+		liacLines.setLayoutData( gdLIACLines );
+		liacLines.addListener( this );
 
-        // Line Attributes for Ticks
-        liacTicks = new LineAttributesComposite(grpTicks, SWT.NONE, grid.getTickAttributes(), false, false, true);
-        GridData gdLIACTicks = new GridData(GridData.FILL_HORIZONTAL);
-        gdLIACTicks.horizontalSpan = 2;
-        liacTicks.setLayoutData(gdLIACTicks);
-        liacTicks.addListener(this);
+		// Ticks group
+		grpTicks = new Group( cmpContent, SWT.NONE );
+		GridData gdGRPTicks = new GridData( GridData.FILL_BOTH );
+		gdGRPTicks.horizontalSpan = 2;
+		grpTicks.setLayoutData( gdGRPTicks );
+		grpTicks.setLayout( glTicks );
+		grpTicks.setText( Messages.getString( "GridAttributesComposite.Lbl.Ticks" ) ); //$NON-NLS-1$
 
-        // Tick Styles
-        lblStyle = new Label(grpTicks, SWT.NONE);
-        GridData gdLBLStyle = new GridData();
-        gdLBLStyle.horizontalIndent = 4;
-        lblStyle.setLayoutData(gdLBLStyle);
-        lblStyle.setText(Messages.getString("GridAttributesComposite.Lbl.Style")); //$NON-NLS-1$
-        lblStyle.setEnabled(grid.getTickAttributes().isVisible());
+		// Line Attributes for Ticks
+		liacTicks = new LineAttributesComposite( grpTicks,
+				SWT.NONE,
+				grid.getTickAttributes( ),
+				false,
+				false,
+				true );
+		GridData gdLIACTicks = new GridData( GridData.FILL_HORIZONTAL );
+		gdLIACTicks.horizontalSpan = 2;
+		liacTicks.setLayoutData( gdLIACTicks );
+		liacTicks.addListener( this );
 
-        cmbTickStyle = new Combo(grpTicks, SWT.DROP_DOWN | SWT.READ_ONLY);
-        GridData gdCMBTickStyle = new GridData(GridData.FILL_HORIZONTAL);
-        cmbTickStyle.setLayoutData(gdCMBTickStyle);
-        cmbTickStyle.addSelectionListener(this);
-        cmbTickStyle.setEnabled(grid.getTickAttributes().isVisible());
+		// Tick Styles
+		lblStyle = new Label( grpTicks, SWT.NONE );
+		GridData gdLBLStyle = new GridData( );
+		gdLBLStyle.horizontalIndent = 4;
+		lblStyle.setLayoutData( gdLBLStyle );
+		lblStyle.setText( Messages.getString( "GridAttributesComposite.Lbl.Style" ) ); //$NON-NLS-1$
+		lblStyle.setEnabled( grid.getTickAttributes( ).isVisible( ) );
 
-        populateLists();
-        setDefaultSelections();
-    }
+		cmbTickStyle = new Combo( grpTicks, SWT.DROP_DOWN | SWT.READ_ONLY );
+		GridData gdCMBTickStyle = new GridData( GridData.FILL_HORIZONTAL );
+		cmbTickStyle.setLayoutData( gdCMBTickStyle );
+		cmbTickStyle.addSelectionListener( this );
+		cmbTickStyle.setEnabled( grid.getTickAttributes( ).isVisible( ) );
 
-    private void populateLists()
-    {
-        this.cmbTickStyle.add(TickStyle.ACROSS_LITERAL.getName());
-        this.cmbTickStyle.add(TickStyle.ABOVE_LITERAL.getName());
-        this.cmbTickStyle.add(TickStyle.BELOW_LITERAL.getName());
-        this.cmbTickStyle.add(TickStyle.LEFT_LITERAL.getName());
-        this.cmbTickStyle.add(TickStyle.RIGHT_LITERAL.getName());
-    }
+		populateLists( );
+		setDefaultSelections( );
+	}
 
-    private void setDefaultSelections()
-    {
-        if (grid.getTickStyle() == null)
-        {
-            cmbTickStyle.select(0);
-            return;
-        }
-        String sTickStyle = grid.getTickStyle().getName();
-        for (int iC = 0; iC < cmbTickStyle.getItemCount(); iC++)
-        {
-            if (cmbTickStyle.getItem(iC).equals(sTickStyle))
-            {
-                cmbTickStyle.select(iC);
-            }
-        }
-    }
+	private void populateLists( )
+	{
+		this.cmbTickStyle.add( TickStyle.ACROSS_LITERAL.getName( ) );
 
-    public void addListener(Listener listener)
-    {
-        vListeners.add(listener);
-    }
+		if ( orientation == Orientation.HORIZONTAL )
+		{
+			this.cmbTickStyle.add( TickStyle.ABOVE_LITERAL.getName( ) );
+			this.cmbTickStyle.add( TickStyle.BELOW_LITERAL.getName( ) );
+		}
+		else if ( orientation == Orientation.VERTICAL )
+		{
+			this.cmbTickStyle.add( TickStyle.LEFT_LITERAL.getName( ) );
+			this.cmbTickStyle.add( TickStyle.RIGHT_LITERAL.getName( ) );
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
-     */
-    public void widgetSelected(SelectionEvent e)
-    {
-        if (e.getSource().equals(cmbTickStyle))
-        {
-            Event eGrid = new Event();
-            eGrid.widget = this;
-            eGrid.type = TICK_STYLE_CHANGED_EVENT;
-            TickStyle tsGrid = TickStyle.get(cmbTickStyle.getText());
-            eGrid.data = tsGrid;
-            fireEvent(eGrid);
-        }
-    }
+	private void setDefaultSelections( )
+	{
+		if ( grid.getTickStyle( ) == null )
+		{
+			cmbTickStyle.select( 0 );
+			return;
+		}
+		String sTickStyle = grid.getTickStyle( ).getName( );
+		for ( int iC = 0; iC < cmbTickStyle.getItemCount( ); iC++ )
+		{
+			if ( cmbTickStyle.getItem( iC ).equals( sTickStyle ) )
+			{
+				cmbTickStyle.select( iC );
+			}
+		}
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
-     */
-    public void widgetDefaultSelected(SelectionEvent e)
-    {
-        // TODO Auto-generated method stub
+	public void addListener( Listener listener )
+	{
+		vListeners.add( listener );
+	}
 
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
+	public void widgetSelected( SelectionEvent e )
+	{
+		if ( e.getSource( ).equals( cmbTickStyle ) )
+		{
+			Event eGrid = new Event( );
+			eGrid.widget = this;
+			eGrid.type = TICK_STYLE_CHANGED_EVENT;
+			TickStyle tsGrid = TickStyle.get( cmbTickStyle.getText( ) );
+			eGrid.data = tsGrid;
+			fireEvent( eGrid );
+		}
+	}
 
-    public Point getPreferredSize()
-    {
-        return new Point(230, 240);
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+	 */
+	public void widgetDefaultSelected( SelectionEvent e )
+	{
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
-     */
-    public void handleEvent(Event event)
-    {
-        Event eGrid = new Event();
-        eGrid.widget = this;
-        if (event.widget.equals(liacLines))
-        {
-            if (event.type == LineAttributesComposite.STYLE_CHANGED_EVENT)
-            {
-                eGrid.type = LINE_STYLE_CHANGED_EVENT;
-                eGrid.data = event.data;
-                //				grid.getStyle().setStyle((LineStyle) event.data);
-            }
-            else if (event.type == LineAttributesComposite.WIDTH_CHANGED_EVENT)
-            {
-                eGrid.type = LINE_WIDTH_CHANGED_EVENT;
-                eGrid.data = event.data;
-                //				grid.getStyle().setThickness(((Integer)
-                // event.data).intValue());
-            }
-            else if (event.type == LineAttributesComposite.COLOR_CHANGED_EVENT)
-            {
-                eGrid.type = LINE_COLOR_CHANGED_EVENT;
-                eGrid.data = event.data;
-                //				grid.getStyle().setColor((ColorDefinition) event.data);
-            }
-            else
-            {
-                eGrid.type = LINE_VISIBILITY_CHANGED_EVENT;
-                eGrid.data = event.data;
-                //				grid.getStyle().setVisible(((Boolean)
-                // event.data).booleanValue());
-            }
-        }
-        else if (event.widget.equals(liacTicks))
-        {
-            if (event.type == LineAttributesComposite.COLOR_CHANGED_EVENT)
-            {
-                eGrid.type = TICK_COLOR_CHANGED_EVENT;
-                eGrid.data = event.data;
-                grid.getTickAttributes().setColor((ColorDefinition) event.data);
-            }
-            else
-            {
-                eGrid.type = TICK_VISIBILITY_CHANGED_EVENT;
-                eGrid.data = event.data;
-                grid.getTickAttributes().setVisible(((Boolean) event.data).booleanValue());
-                lblStyle.setEnabled(((Boolean) event.data).booleanValue());
-                cmbTickStyle.setEnabled(((Boolean) event.data).booleanValue());
-            }
-        }
-        else
-        {
-            // Unknown event
-            return;
-        }
-        fireEvent(eGrid);
-    }
+	public Point getPreferredSize( )
+	{
+		return new Point( 230, 240 );
+	}
 
-    private void fireEvent(Event event)
-    {
-        for (int i = 0; i < vListeners.size(); i++)
-        {
-            ((Listener) vListeners.get(i)).handleEvent(event);
-        }
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
+	 */
+	public void handleEvent( Event event )
+	{
+		Event eGrid = new Event( );
+		eGrid.widget = this;
+		if ( event.widget.equals( liacLines ) )
+		{
+			if ( event.type == LineAttributesComposite.STYLE_CHANGED_EVENT )
+			{
+				eGrid.type = LINE_STYLE_CHANGED_EVENT;
+				eGrid.data = event.data;
+				//				grid.getStyle().setStyle((LineStyle) event.data);
+			}
+			else if ( event.type == LineAttributesComposite.WIDTH_CHANGED_EVENT )
+			{
+				eGrid.type = LINE_WIDTH_CHANGED_EVENT;
+				eGrid.data = event.data;
+				//				grid.getStyle().setThickness(((Integer)
+				// event.data).intValue());
+			}
+			else if ( event.type == LineAttributesComposite.COLOR_CHANGED_EVENT )
+			{
+				eGrid.type = LINE_COLOR_CHANGED_EVENT;
+				eGrid.data = event.data;
+				//				grid.getStyle().setColor((ColorDefinition) event.data);
+			}
+			else
+			{
+				eGrid.type = LINE_VISIBILITY_CHANGED_EVENT;
+				eGrid.data = event.data;
+				//				grid.getStyle().setVisible(((Boolean)
+				// event.data).booleanValue());
+			}
+		}
+		else if ( event.widget.equals( liacTicks ) )
+		{
+			if ( event.type == LineAttributesComposite.COLOR_CHANGED_EVENT )
+			{
+				eGrid.type = TICK_COLOR_CHANGED_EVENT;
+				eGrid.data = event.data;
+				grid.getTickAttributes( )
+						.setColor( (ColorDefinition) event.data );
+			}
+			else
+			{
+				eGrid.type = TICK_VISIBILITY_CHANGED_EVENT;
+				eGrid.data = event.data;
+				grid.getTickAttributes( )
+						.setVisible( ( (Boolean) event.data ).booleanValue( ) );
+				lblStyle.setEnabled( ( (Boolean) event.data ).booleanValue( ) );
+				cmbTickStyle.setEnabled( ( (Boolean) event.data ).booleanValue( ) );
+			}
+		}
+		else
+		{
+			// Unknown event
+			return;
+		}
+		fireEvent( eGrid );
+	}
+
+	private void fireEvent( Event event )
+	{
+		for ( int i = 0; i < vListeners.size( ); i++ )
+		{
+			( (Listener) vListeners.get( i ) ).handleEvent( event );
+		}
+	}
 }
