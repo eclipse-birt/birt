1e728bcd5bcf50b4939e259c5f891c9a579fe7f5
Zhiqiang Qian <zqian@actuate.com>
2005-07-07 07:53:16 +0000

    - SCR(s) Resolved: bugzilla 102966 Some bars are lost when the chart is transposed.
    - Description:
    This is caused by a strict math calculation precision check. Now use a wider precision check to avoid the incorrect rendering cases.
    
    - Regression ( Yes/No ):No
    
    - Code Owner: Zhiqiang Qian
    
    - Code Reviewers: SHA GUI
    
    - Tests: Yes
    
    - Test Automated (Yes/No, if "No" then explain why):No, gui feature
    
    - Case Entries Resolved:
    - Notes to Developers:
    - Notes to QA:
    - Notes to Documentation:
    - Notes to Configuration Management:
    - Notes to Support:
    - Notes to Product Marketing:

--- chart/org.eclipse.birt.chart.engine/src/org/eclipse/birt/chart/util/ChartUtil.java
+++ chart/org.eclipse.birt.chart.engine/src/org/eclipse/birt/chart/util/ChartUtil.java
@@ -22,6 +22,12 @@ public class ChartUtil
 {
 
 	/**
+	 * Precision for chart rendering. Increase this to avoid unnecessary
+	 * precision check.
+	 */
+	private static final double EPS = 1E-10;
+
+	/**
 	 * Returns if the given color definition is totally transparent. e.g.
 	 * transparency==0.
 	 * 
@@ -43,6 +49,45 @@ public class ChartUtil
 	public static final boolean isShadowDefined( Label la )
 	{
 		return !isColorTransparent( la.getShadowColor( ) );
+	}
+
+	/**
+	 * Returns if the given two double values are equal within a small
+	 * precision, e.g. EPS=1E-10.
+	 * 
+	 * @param v1
+	 * @param v2
+	 * @return
+	 */
+	public static final boolean mathEqual( double v1, double v2 )
+	{
+		return Math.abs( v1 - v2 ) < EPS;
+	}
+
+	/**
+	 * Returns if the given left double value is less than the given right value
+	 * within a small precision, e.g. EPS=1E-10.
+	 * 
+	 * @param v1
+	 * @param v2
+	 * @return
+	 */
+	public static final boolean mathLT( double lv, double rv )
+	{
+		return ( rv - lv ) > EPS;
+	}
+
+	/**
+	 * Returns if the given left double value is greater than the given right
+	 * value within a small precision, e.g. EPS=1E-10.
+	 * 
+	 * @param v1
+	 * @param v2
+	 * @return
+	 */
+	public static final boolean mathGT( double lv, double rv )
+	{
+		return ( lv - rv ) > EPS;
 	}
 
 }
