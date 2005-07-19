06c72eb7a3005a63bbe971e6978d2c9e6d624dd8
Zhiqiang Qian <zqian@actuate.com>
2005-07-19 01:50:46 +0000

    - SCR(s) Resolved: bugzilla103168 X Axis data is lost when transposed.
    - Description:
    There is one bug in java.awt.font.TextLine.getBounds(), when the line string is " " (e.g. blank string only with space), it will return a random negative result. that causes the abnormal drawing result. Added code to double check and handle this case.
    
    - Regression ( Yes/No ):No
    
    - Code Owner: Zhiqiang Qian
    
    - Code Reviewers: SHA GUI
    
    - Tests: Yes
    
    - Test Automated (Yes/No, if "No" then explain why):No, gui feature
    
    - Case Entries Resolved:
    - Notes to Developers:
    - Notes to QA:
    - Notes to Documentation:
    - Notes to Configuration Management:
    - Notes to Support:
    - Notes to Product Marketing:

--- chart/org.eclipse.birt.chart.device.extension/src/org/eclipse/birt/chart/device/swing/SwingTextMetrics.java
+++ chart/org.eclipse.birt.chart.device.extension/src/org/eclipse/birt/chart/device/swing/SwingTextMetrics.java
@@ -28,236 +28,252 @@ import org.eclipse.birt.chart.model.attribute.Insets;
 import org.eclipse.birt.chart.model.component.Label;
 
 /**
- * Provides metrics for a label containing either one or multiple lines
- * of text.
+ * Provides metrics for a label containing either one or multiple lines of text.
  */
 public final class SwingTextMetrics extends TextAdapter
 {
-    private int iLineCount = 0;
 
-    private Object oText = null;
+	private int iLineCount = 0;
 
-    private Graphics2D g2d = null;
+	private Object oText = null;
 
-    private FontMetrics fm = null;
+	private Graphics2D g2d = null;
 
-    private TextLayout[] tla = null;
+	private FontMetrics fm = null;
 
-    private transient Object bi = null;
+	private TextLayout[] tla = null;
 
-    private Label la = null;
+	private transient Object bi = null;
 
-    private final IDisplayServer xs;
+	private Label la = null;
 
-    private Insets ins = null;
+	private final IDisplayServer xs;
 
-    /**
-     * The constructor initializes a tiny image that provides a graphics context capable of performing computations in
-     * the absence of a visual component
-     * 
-     * @param _xs
-     * @param _la
-     */
-    public SwingTextMetrics(IDisplayServer _xs, Label _la)
-    {
-        if (bi == null)
-        {
-            bi = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
-            g2d = (Graphics2D) ((BufferedImage) bi).getGraphics();
-            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        }
-        else
-        {
-            g2d = (Graphics2D) ((BufferedImage) bi).getGraphics();
-        }
-        xs = _xs;
-        la = _la;
-        reuse(la);
-    }
+	private Insets ins = null;
 
-    /**
-     * Allows reuse of the multi-line text element for computing bounds of changed font/text content
-     * 
-     * @param fd
-     */
-    public final void reuse(Label la)
-    {
-        final Font f = (Font) xs.createFont(la.getCaption().getFont());
-        fm = g2d.getFontMetrics(f);
-        final FontRenderContext frc = g2d.getFontRenderContext();
+	/**
+	 * The constructor initializes a tiny image that provides a graphics context
+	 * capable of performing computations in the absence of a visual component
+	 * 
+	 * @param _xs
+	 * @param _la
+	 */
+	public SwingTextMetrics( IDisplayServer _xs, Label _la )
+	{
+		if ( bi == null )
+		{
+			bi = new BufferedImage( 1, 1, BufferedImage.TYPE_INT_RGB );
+			g2d = (Graphics2D) ( (BufferedImage) bi ).getGraphics( );
+			g2d.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING,
+					RenderingHints.VALUE_TEXT_ANTIALIAS_ON );
+			g2d.setRenderingHint( RenderingHints.KEY_FRACTIONALMETRICS,
+					RenderingHints.VALUE_FRACTIONALMETRICS_ON );
+		}
+		else
+		{
+			g2d = (Graphics2D) ( (BufferedImage) bi ).getGraphics( );
+		}
+		xs = _xs;
+		la = _la;
+		reuse( la );
+	}
 
-        String s = la.getCaption().getValue();
-        if (s == null)
-        {
-            s = IConstants.NULL_STRING;
-        }
-        else if (s.length() == 0) // TextLayout DOESN'T LIKE EMPTY STRINGS
-        {
-            s = IConstants.ONE_SPACE;
-        }
-        String[] sa = splitOnHardBreaks(s);
-        if (sa == null)
-        {
-            iLineCount = 1;
-            oText = s;
-            tla = new TextLayout[1];
-            tla[0] = new TextLayout(s, f.getAttributes(), frc);
-        }
-        else
-        {
-            iLineCount = sa.length;
-            oText = sa;
-            tla = new TextLayout[iLineCount];
-            for (int i = 0; i < iLineCount; i++)
-            {
-                tla[i] = new TextLayout(sa[i], f.getAttributes(), frc);
-            }
-        }
-        ins = la.getInsets().scaledInstance(pointsToPixels());
-    }
+	/**
+	 * Allows reuse of the multi-line text element for computing bounds of
+	 * changed font/text content
+	 * 
+	 * @param fd
+	 */
+	public final void reuse( Label la )
+	{
+		final Font f = (Font) xs.createFont( la.getCaption( ).getFont( ) );
+		fm = g2d.getFontMetrics( f );
+		final FontRenderContext frc = g2d.getFontRenderContext( );
 
-    /**
-     * Disposal of the internal image
-     */
-    public final void finalize()
-    {
-        dispose();
-    }
+		String s = la.getCaption( ).getValue( );
+		if ( s == null )
+		{
+			s = IConstants.NULL_STRING;
+		}
+		else if ( s.length( ) == 0 ) // TextLayout DOESN'T LIKE EMPTY STRINGS
+		{
+			s = IConstants.ONE_SPACE;
+		}
+		String[] sa = splitOnHardBreaks( s );
+		if ( sa == null )
+		{
+			iLineCount = 1;
+			oText = s;
+			tla = new TextLayout[1];
+			tla[0] = new TextLayout( s, f.getAttributes( ), frc );
+		}
+		else
+		{
+			iLineCount = sa.length;
+			oText = sa;
+			tla = new TextLayout[iLineCount];
+			for ( int i = 0; i < iLineCount; i++ )
+			{
+				tla[i] = new TextLayout( sa[i], f.getAttributes( ), frc );
+			}
+		}
+		ins = la.getInsets( ).scaledInstance( pointsToPixels( ) );
+	}
 
-    /**
-     * 
-     * @param fm
-     * @return
-     */
-    public final double getHeight()
-    {
-        return fm.getHeight();
-    }
+	/**
+	 * Disposal of the internal image
+	 */
+	public final void finalize( )
+	{
+		dispose( );
+	}
 
-    /**
-     * 
-     * @param fm
-     * @return
-     */
-    public final double getDescent()
-    {
-        return fm.getDescent();
-    }
+	/**
+	 * 
+	 * @param fm
+	 * @return
+	 */
+	public final double getHeight( )
+	{
+		return fm.getHeight( );
+	}
 
-    /**
-     * 
-     * @param fm
-     * @return The width of the line containing the maximum width (if multiline split by hard breaks) or the width of
-     *         the single line of text
-     */
-    private final double stringWidth()
-    {
-        Rectangle2D r2d;
+	/**
+	 * 
+	 * @param fm
+	 * @return
+	 */
+	public final double getDescent( )
+	{
+		return fm.getDescent( );
+	}
 
-        if (iLineCount > 1)
-        {
-            double dWidth, dMaxWidth = 0;
-            for (int i = 0; i < iLineCount; i++)
-            {
-                r2d = tla[i].getBounds();
-                dWidth = r2d.getWidth();
-                if (dWidth > dMaxWidth)
-                {
-                    dMaxWidth = dWidth;
-                }
-            }
-            return dMaxWidth;
-        }
-        else
-        {
-            //return fm.stringWidth((String) oText);
-            return tla[0].getBounds().getWidth();
-        }
-    }
+	/**
+	 * 
+	 * @param fm
+	 * @return The width of the line containing the maximum width (if multiline
+	 *         split by hard breaks) or the width of the single line of text
+	 */
+	private final double stringWidth( )
+	{
+		Rectangle2D r2d;
 
-    final double pointsToPixels()
-    {
-        return (xs.getDpiResolution() / 72d);
-    }
+		if ( iLineCount > 1 )
+		{
+			double dWidth, dMaxWidth = 0;
+			for ( int i = 0; i < iLineCount; i++ )
+			{
+				r2d = tla[i].getBounds( );
+				dWidth = r2d.getWidth( );
+				if ( dWidth > dMaxWidth )
+				{
+					dMaxWidth = dWidth;
+				}
+			}
 
-    public final double getFullHeight()
-    {
-        return getHeight() * getLineCount() + (ins.getTop() + ins.getBottom());
-    }
+			/**
+			 * Fixed for java.awt.font.TextLine.getBounds() bug, when string is
+			 * blank, e.g. " ", it will return an negative result.
+			 */
+			return Math.max( 0, dMaxWidth );
+		}
+		else
+		{
+			double w = tla[0].getBounds( ).getWidth( );
 
-    public final double getFullWidth()
-    {
-        return stringWidth() + (ins.getLeft() + ins.getRight());
-    }
+			/**
+			 * Fixed for java.awt.font.TextLine.getBounds() bug, when string is
+			 * blank, e.g. " ", it will return an negative result.
+			 */
+			return Math.max( 0, w );
+		}
+	}
 
-    /**
-     * 
-     * @return The number of lines created due to the hard breaks inserted
-     */
-    public final int getLineCount()
-    {
-        return iLineCount;
-    }
+	final double pointsToPixels( )
+	{
+		return ( xs.getDpiResolution( ) / 72d );
+	}
 
-    /**
-     * 
-     * @return The line requested for
-     */
-    public final String getLine(int iIndex)
-    {
-        return (iLineCount > 1) ? ((String[]) oText)[iIndex] : (String) oText;
-    }
+	public final double getFullHeight( )
+	{
+		return getHeight( )
+				* getLineCount( )
+				+ ( ins.getTop( ) + ins.getBottom( ) );
+	}
 
-    public final TextLayout getLayout(int iIndex)
-    {
-        return (iLineCount > 1) ? ((TextLayout[]) tla)[iIndex] : (TextLayout) tla[0];
-    }
+	public final double getFullWidth( )
+	{
+		return stringWidth( ) + ( ins.getLeft( ) + ins.getRight( ) );
+	}
 
-    /**
-     * 
-     * @param s
-     * @return
-     */
-    private static String[] splitOnHardBreaks(String s)
-    {
-        final ArrayList al = new ArrayList();
-        int i = 0, j;
-        do
-        {
-            j = s.indexOf('\n', i);
-            if (j == -1)
-                j = s.length();
-            al.add(s.substring(i, j));
-            i = j + 1;
-        }
-        while (j != -1 && j < s.length());
+	/**
+	 * 
+	 * @return The number of lines created due to the hard breaks inserted
+	 */
+	public final int getLineCount( )
+	{
+		return iLineCount;
+	}
 
-        final int n = al.size();
-        if (n == 1)
-            return null;
+	/**
+	 * 
+	 * @return The line requested for
+	 */
+	public final String getLine( int iIndex )
+	{
+		return ( iLineCount > 1 ) ? ( (String[]) oText )[iIndex]
+				: (String) oText;
+	}
 
-        final String[] sa = new String[n];
-        for (i = 0; i < al.size(); i++)
-        {
-            sa[i] = (String) al.get(i);
-        }
-        return sa;
-    }
+	public final TextLayout getLayout( int iIndex )
+	{
+		return ( iLineCount > 1 ) ? ( (TextLayout[]) tla )[iIndex]
+				: (TextLayout) tla[0];
+	}
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.birt.chart.device.ITextMetrics#dispose()
-     */
-    public void dispose()
-    {
-        if (bi != null)
-        {
-            ((BufferedImage) bi).flush();
-            bi = null;
-            g2d.dispose();
-            g2d = null;
-        }
-    }
+	/**
+	 * 
+	 * @param s
+	 * @return
+	 */
+	private static String[] splitOnHardBreaks( String s )
+	{
+		final ArrayList al = new ArrayList( );
+		int i = 0, j;
+		do
+		{
+			j = s.indexOf( '\n', i );
+			if ( j == -1 )
+				j = s.length( );
+			al.add( s.substring( i, j ) );
+			i = j + 1;
+		} while ( j != -1 && j < s.length( ) );
+
+		final int n = al.size( );
+		if ( n == 1 )
+			return null;
+
+		final String[] sa = new String[n];
+		for ( i = 0; i < al.size( ); i++ )
+		{
+			sa[i] = (String) al.get( i );
+		}
+		return sa;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.chart.device.ITextMetrics#dispose()
+	 */
+	public void dispose( )
+	{
+		if ( bi != null )
+		{
+			( (BufferedImage) bi ).flush( );
+			bi = null;
+			g2d.dispose( );
+			g2d = null;
+		}
+	}
 }
