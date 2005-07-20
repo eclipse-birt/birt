cfdc85539153eddaf0be5bc06d8d8dd597523635
Zhiqiang Qian <zqian@actuate.com>
2005-07-20 06:31:41 +0000

    - SCR(s) Resolved: bugzilla 104450 If plot background is set, data set binding is lost.
    - Description:
    When exception thrown from the chart dialog message loop, the loop will be broken, but this case is not well handled, so caused abnormal behavior. added code to handle these cases.
    
    - Regression ( Yes/No ):No
    
    - Code Owner: Zhiqiang Qian
    
    - Code Reviewers: SHA GUI
    
    - Tests: Yes
    
    - Test Automated (Yes/No, if "No" then explain why):No, gui feature
    
    - Case Entries Resolved:
    - Notes to Developers:
    - Notes to QA:
    - Notes to Documentation:
    - Notes to Configuration Management:
    - Notes to Support:
    - Notes to Product Marketing:

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/FillCanvas.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/FillCanvas.java
@@ -17,6 +17,8 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.eclipse.birt.chart.log.ILogger;
+import org.eclipse.birt.chart.log.Logger;
 import org.eclipse.birt.chart.model.attribute.ColorDefinition;
 import org.eclipse.birt.chart.model.attribute.Fill;
 import org.eclipse.birt.chart.model.attribute.Gradient;
@@ -33,136 +35,188 @@ import org.eclipse.swt.widgets.Display;
 
 /**
  * @author Administrator
- *  
+ * 
  */
 class FillCanvas extends Canvas implements PaintListener
 {
 
-    Fill fCurrent = null;
+	private static ILogger logger = Logger.getLogger( "org.eclipse.birt.chart.ui.extension/swt.composites" ); //$NON-NLS-1$
 
-    public FillCanvas(Composite parent, int iStyle)
-    {
-        super(parent, iStyle);
-        this.addPaintListener(this);
-    }
+	Fill fCurrent = null;
 
-    public void setFill(Fill fill)
-    {
-        this.fCurrent = fill;
-    }
+	public FillCanvas( Composite parent, int iStyle )
+	{
+		super( parent, iStyle );
+		this.addPaintListener( this );
+	}
 
-    public void paintControl(PaintEvent pe)
-    {
-        Color cBackground = null;
-        Color clrTransparencyBackground = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
-        GC gc = pe.gc;
-        if (!this.isEnabled())
-        {
-            gc.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
-            Color cFore = Display.getCurrent().getSystemColor(SWT.COLOR_DARK_GRAY);
-            gc.setForeground(cFore);
-            if (fCurrent == null || fCurrent instanceof ColorDefinition
-                && ((ColorDefinition) fCurrent).getTransparency() == 0)
-            {
-                gc.fillRectangle(0, 0, this.getSize().x, this.getSize().y);
-                gc.drawText(Messages.getString("FillCanvas.Transparent"), 2, 2); //$NON-NLS-1$
-            }
-            else
-            {
-                gc.fillRectangle(0, 0, this.getSize().x, this.getSize().y);
-                gc.setBackground(cFore);
-                gc.fillRectangle(2, 2, this.getSize().x - 4, this.getSize().y - 4);
-            }
-        }
-        else
-        {
-            if (fCurrent == null || fCurrent instanceof ColorDefinition
-                && ((ColorDefinition) fCurrent).getTransparency() == 0)
-            {
-                gc.setBackground(clrTransparencyBackground);
-                gc.fillRectangle(0, 0, this.getSize().x, this.getSize().y);
-                Color cText = new Color(this.getDisplay(), 0, 0, 0);
-                gc.setForeground(cText);
-                gc.drawText(Messages.getString("FillCanvas.Transparent"), 2, 2); //$NON-NLS-1$
-                cText.dispose();
-            }
-            else
-            {
-                if (fCurrent instanceof ColorDefinition)
-                {
-                    cBackground = new Color(Display.getDefault(), ((ColorDefinition) fCurrent).getRed(),
-                        ((ColorDefinition) fCurrent).getGreen(), ((ColorDefinition) fCurrent).getBlue());
-                    gc.setBackground(cBackground);
-                    gc.fillRectangle(2, 2, this.getSize().x - 4, this.getSize().y - 4);
-                }
-                else if (fCurrent instanceof Image)
-                {
-                    gc.fillRectangle(2, 2, getSize().x - 4, this.getSize().y - 4);
-                    gc.drawImage(getSWTImage((Image) fCurrent), 2, 2);
-                }
-                else if (fCurrent instanceof Gradient)
-                {
-                    if (((Gradient) fCurrent).getStartColor() == null && ((Gradient) fCurrent).getEndColor() == null)
-                    {
-                        return;
-                    }
-                    Color clrStart = null;
-                    Color clrEnd = null;
-                    if (((Gradient) fCurrent).getStartColor() != null)
-                    {
-                        clrStart = new Color(Display.getDefault(), ((Gradient) fCurrent).getStartColor().getRed(),
-                            ((Gradient) fCurrent).getStartColor().getGreen(), ((Gradient) fCurrent).getStartColor()
-                                .getBlue());
-                        gc.setForeground(clrStart);
-                    }
-                    if (((Gradient) fCurrent).getEndColor() != null)
-                    {
-                        clrEnd = new Color(Display.getDefault(), ((Gradient) fCurrent).getEndColor().getRed(),
-                            ((Gradient) fCurrent).getEndColor().getGreen(), ((Gradient) fCurrent).getEndColor()
-                                .getBlue());
-                        gc.setBackground(clrEnd);
-                    }
-                    gc.fillGradientRectangle(2, 2, this.getSize().x - 4, this.getSize().y - 4, false);
-                }
-            }
-        }
-        if (cBackground != null)
-        {
-            cBackground.dispose();
-        }
-    }
+	public void setFill( Fill fill )
+	{
+		this.fCurrent = fill;
+	}
 
-    private org.eclipse.swt.graphics.Image getSWTImage(Image modelImage)
-    {
-        org.eclipse.swt.graphics.Image img = null;
-        try
-        {
-            try
-            {
-                img = new org.eclipse.swt.graphics.Image(Display.getCurrent(), new URL(modelImage.getURL())
-                    .openStream());
-            }
-            catch (MalformedURLException e1 )
-            {
-                img = new org.eclipse.swt.graphics.Image(Display.getCurrent(), new FileInputStream(modelImage.getURL()));
-            }
-        }
-        catch (FileNotFoundException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        catch (IOException e )
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return img;
-    }
+	public void paintControl( PaintEvent pe )
+	{
+		Color cBackground = null;
 
-    public void setEnabled(boolean bState)
-    {
-        super.setEnabled(bState);
-        redraw();
-    }
+		try
+		{
+			Color clrTransparencyBackground = Display.getCurrent( )
+					.getSystemColor( SWT.COLOR_LIST_BACKGROUND );
+			GC gc = pe.gc;
+			if ( !this.isEnabled( ) )
+			{
+				gc.setBackground( Display.getCurrent( )
+						.getSystemColor( SWT.COLOR_WIDGET_BACKGROUND ) );
+				Color cFore = Display.getCurrent( )
+						.getSystemColor( SWT.COLOR_DARK_GRAY );
+				gc.setForeground( cFore );
+				if ( fCurrent == null
+						|| fCurrent instanceof ColorDefinition
+						&& ( (ColorDefinition) fCurrent ).getTransparency( ) == 0 )
+				{
+					gc.fillRectangle( 0,
+							0,
+							this.getSize( ).x,
+							this.getSize( ).y );
+					gc.drawText( Messages.getString( "FillCanvas.Transparent" ), 2, 2 ); //$NON-NLS-1$
+				}
+				else
+				{
+					gc.fillRectangle( 0,
+							0,
+							this.getSize( ).x,
+							this.getSize( ).y );
+					gc.setBackground( cFore );
+					gc.fillRectangle( 2,
+							2,
+							this.getSize( ).x - 4,
+							this.getSize( ).y - 4 );
+				}
+			}
+			else
+			{
+				if ( fCurrent == null
+						|| fCurrent instanceof ColorDefinition
+						&& ( (ColorDefinition) fCurrent ).getTransparency( ) == 0 )
+				{
+					gc.setBackground( clrTransparencyBackground );
+					gc.fillRectangle( 0,
+							0,
+							this.getSize( ).x,
+							this.getSize( ).y );
+					Color cText = new Color( this.getDisplay( ), 0, 0, 0 );
+					gc.setForeground( cText );
+					gc.drawText( Messages.getString( "FillCanvas.Transparent" ), 2, 2 ); //$NON-NLS-1$
+					cText.dispose( );
+				}
+				else
+				{
+					if ( fCurrent instanceof ColorDefinition )
+					{
+						cBackground = new Color( Display.getDefault( ),
+								( (ColorDefinition) fCurrent ).getRed( ),
+								( (ColorDefinition) fCurrent ).getGreen( ),
+								( (ColorDefinition) fCurrent ).getBlue( ) );
+						gc.setBackground( cBackground );
+						gc.fillRectangle( 2,
+								2,
+								this.getSize( ).x - 4,
+								this.getSize( ).y - 4 );
+					}
+					else if ( fCurrent instanceof Image )
+					{
+						gc.fillRectangle( 2,
+								2,
+								getSize( ).x - 4,
+								this.getSize( ).y - 4 );
+						gc.drawImage( getSWTImage( (Image) fCurrent ), 2, 2 );
+					}
+					else if ( fCurrent instanceof Gradient )
+					{
+						if ( ( (Gradient) fCurrent ).getStartColor( ) == null
+								&& ( (Gradient) fCurrent ).getEndColor( ) == null )
+						{
+							return;
+						}
+						Color clrStart = null;
+						Color clrEnd = null;
+						if ( ( (Gradient) fCurrent ).getStartColor( ) != null )
+						{
+							clrStart = new Color( Display.getDefault( ),
+									( (Gradient) fCurrent ).getStartColor( )
+											.getRed( ),
+									( (Gradient) fCurrent ).getStartColor( )
+											.getGreen( ),
+									( (Gradient) fCurrent ).getStartColor( )
+											.getBlue( ) );
+							gc.setForeground( clrStart );
+						}
+						if ( ( (Gradient) fCurrent ).getEndColor( ) != null )
+						{
+							clrEnd = new Color( Display.getDefault( ),
+									( (Gradient) fCurrent ).getEndColor( )
+											.getRed( ),
+									( (Gradient) fCurrent ).getEndColor( )
+											.getGreen( ),
+									( (Gradient) fCurrent ).getEndColor( )
+											.getBlue( ) );
+							gc.setBackground( clrEnd );
+						}
+						gc.fillGradientRectangle( 2,
+								2,
+								this.getSize( ).x - 4,
+								this.getSize( ).y - 4,
+								false );
+					}
+				}
+			}
+		}
+		catch ( Exception ex )
+		{
+			logger.log( ex );
+		}
+		finally
+		{
+			if ( cBackground != null )
+			{
+				cBackground.dispose( );
+			}
+		}
+	}
+
+	private org.eclipse.swt.graphics.Image getSWTImage( Image modelImage )
+	{
+		org.eclipse.swt.graphics.Image img = null;
+		try
+		{
+			try
+			{
+				img = new org.eclipse.swt.graphics.Image( Display.getCurrent( ),
+						new URL( modelImage.getURL( ) ).openStream( ) );
+			}
+			catch ( MalformedURLException e1 )
+			{
+				img = new org.eclipse.swt.graphics.Image( Display.getCurrent( ),
+						new FileInputStream( modelImage.getURL( ) ) );
+			}
+		}
+		catch ( FileNotFoundException ex )
+		{
+			logger.log( ex );
+			ex.printStackTrace( );
+		}
+		catch ( IOException ex )
+		{
+			logger.log( ex );
+			ex.printStackTrace( );
+		}
+		return img;
+	}
+
+	public void setEnabled( boolean bState )
+	{
+		super.setEnabled( bState );
+		redraw( );
+	}
 }
