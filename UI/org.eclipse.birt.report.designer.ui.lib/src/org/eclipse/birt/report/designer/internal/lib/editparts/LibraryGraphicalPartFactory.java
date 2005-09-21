2fdf6fc990086ce7db87d5a6706d03713e25b21d
Dazheng Gao <dgao@actuate.com>
2005-09-21 07:10:47 +0000

    - SCR(s) Resolved:
    - Description: Fix the bug of the library editor.
    
    - Regression ( Yes/No ): no
    
    - Code Owner: DaZhen Gao
    
    - Code Reviewers:  GUI SHA,Hongxia Wang, Sissi Zhu
    
    - Tests:  Yes
    
    - Test Automated (Yes/No, if no then explain why): No, gui feature
    
    - Branches Involved:  Head
    
    - Case Entries Resolved:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Notes to Configuration Management:
    
    - Notes to Support:
    
    - Notes to Product Marketing:

--- UI/org.eclipse.birt.report.designer.ui.lib/src/org/eclipse/birt/report/designer/internal/lib/editparts/LibraryGraphicalPartFactory.java
+++ UI/org.eclipse.birt.report.designer.ui.lib/src/org/eclipse/birt/report/designer/internal/lib/editparts/LibraryGraphicalPartFactory.java
@@ -1,29 +1,31 @@
 /*******************************************************************************
-* Copyright (c) 2004 Actuate Corporation .
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*  Actuate Corporation  - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2004 Actuate Corporation .
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.designer.internal.lib.editparts;
 
+import org.eclipse.birt.report.designer.core.model.views.outline.ReportElementModel;
 import org.eclipse.birt.report.designer.internal.ui.editors.schematic.editparts.DummyEditpart;
 import org.eclipse.birt.report.designer.internal.ui.editors.schematic.editparts.GraphicalPartFactory;
 import org.eclipse.birt.report.model.api.LibraryHandle;
 import org.eclipse.birt.report.model.api.MasterPageHandle;
+import org.eclipse.birt.report.model.api.SimpleMasterPageHandle;
 import org.eclipse.gef.EditPart;
-
 
 /**
  * Factory to populate the edit part for given model type
- * 
+ *  
  */
 public class LibraryGraphicalPartFactory extends GraphicalPartFactory
 {
+
 	public EditPart createEditPart( EditPart context, Object model )
 	{
 		//default edit part
@@ -32,25 +34,30 @@ public class LibraryGraphicalPartFactory extends GraphicalPartFactory
 		{
 			return new LibraryReportDesignEditPart( model );
 		}
-	
-		if (ignoreModel(model))
+
+		if ( ignoreModel( model ) )
 		{
-			editPart = new EmptyEditPart(model);
+			editPart = new EmptyEditPart( model );
 		}
-		if (editPart != null)
+		if ( editPart != null )
 		{
 			return editPart;
 		}
-		editPart =  super.createEditPart(context, model);
-		if (editPart == null || editPart instanceof DummyEditpart)
+		editPart = super.createEditPart( context, model );
+		if ( editPart == null || editPart instanceof DummyEditpart )
 		{
-			editPart = new EmptyEditPart(model);
+			editPart = new EmptyEditPart( model );
 		}
 		return editPart;
 	}
-	
-	private boolean ignoreModel(Object model)
+
+	private boolean ignoreModel( Object model )
 	{
+		if ( model instanceof ReportElementModel
+				&& ( ( (ReportElementModel) model ).getElementHandle( ) instanceof SimpleMasterPageHandle ) )
+		{
+			return true;
+		}
 		return model instanceof MasterPageHandle;
 	}
 }
