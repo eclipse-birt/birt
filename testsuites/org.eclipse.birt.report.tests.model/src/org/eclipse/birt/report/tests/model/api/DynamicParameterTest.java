cb189aacc96190897a0accb13dd3f3b73880c13d
Sissi Zhu <szhu@actuate.com>
2005-10-24 06:06:30 +0000

    Add test case for Model M2

---
+++ testsuites/org.eclipse.birt.report.tests.model/src/org/eclipse/birt/report/tests/model/api/DynamicParameterTest.java
@@ -0,0 +1,111 @@
+package org.eclipse.birt.report.tests.model.api;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.birt.report.model.api.DesignFileException;
+import org.eclipse.birt.report.model.api.ElementFactory;
+import org.eclipse.birt.report.model.api.ScalarParameterHandle;
+import org.eclipse.birt.report.model.api.DataSetHandle;
+import org.eclipse.birt.report.model.api.SlotHandle;
+import org.eclipse.birt.report.model.api.command.NameException;
+import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
+import org.eclipse.birt.report.model.elements.ReportDesign;
+import org.eclipse.birt.report.tests.model.BaseTestCase;
+
+public class DynamicParameterTest extends BaseTestCase
+{
+	static final String INPUT_FILE_NAME = "DynamicParameterTest.xml"; //$NON-NLS-1$
+	static final String OUTPUT_FILE_NAME = "DynamicParameterTest_out.xml"; //$NON-NLS-1$
+	static final String GOLDEN_FILE_NAME = "DynamicParameterTest_golden.xml";
+	
+	public DynamicParameterTest(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	 public static Test suite()
+	    {
+			
+			return new TestSuite(DynamicParameterTest.class);
+		}
+		protected void setUp( ) throws Exception
+		{
+			super.setUp( );
+			openDesign( INPUT_FILE_NAME ); //$NON-NLS-1$
+		}
+		
+		public void testPropertiesOfDynamicParameter( ) throws Exception
+		{
+			
+			SlotHandle params = designHandle.getParameters( );
+			ScalarParameterHandle handle = (ScalarParameterHandle) params.get( 0 );
+			SlotHandle dataset = designHandle.getDataSets();
+			DataSetHandle ds = (DataSetHandle)dataset.get(0);
+			assertEquals("dataset1",ds.getName());
+			assertEquals("dataset2",dataset.get(1).getName());
+			
+				
+		   // test on the 1st param
+			assertEquals(DesignChoiceConstants.PARAM_VALUE_TYPE_DYNAMIC,handle.getValueType());
+			
+			assertEquals(DesignChoiceConstants.PARAM_TYPE_DATETIME,handle.getDataType());
+			
+			handle.setPromptText("abc");
+			assertEquals("abc",handle.getPromptText());
+			
+			handle.setDataSetName("dataset1");
+			handle.setValueExpr("row[\"ORDERDATE\"]");
+			assertEquals("row[\"ORDERDATE\"]",handle.getValueExpr());
+
+			handle.setControlType(DesignChoiceConstants.PARAM_CONTROL_LIST_BOX);
+			assertEquals(DesignChoiceConstants.PARAM_CONTROL_LIST_BOX,handle.getControlType());
+			
+			handle.setDefaultValue("10/15/2005");
+			assertEquals("10/15/2005",handle.getDefaultValue());
+			
+			handle.setFormat(DesignChoiceConstants.DATETIEM_FORMAT_TYPE_LONG_DATE);
+			assertEquals(DesignChoiceConstants.DATETIEM_FORMAT_TYPE_LONG_DATE,handle.getFormat());
+			
+			handle.setListlimit(20);
+			assertEquals(20,handle.getListlimit());
+			
+			assertTrue(handle.isMustMatch());
+			assertTrue(handle.isFixedOrder());
+			
+			//test on the 2nd param
+			handle = (ScalarParameterHandle) params.get( 1 );
+			
+			assertEquals(DesignChoiceConstants.PARAM_VALUE_TYPE_DYNAMIC,handle.getValueType());
+			assertEquals(DesignChoiceConstants.PARAM_TYPE_FLOAT,handle.getDataType());
+			
+			handle.setDataSetName("dataset2");
+			handle.setValueExpr("row[\"Amount\"]");
+			assertEquals("row[\"Amount\"]",handle.getValueExpr());
+			
+			assertEquals(DesignChoiceConstants.PARAM_CONTROL_LIST_BOX,handle.getControlType());
+	
+			assertFalse(handle.isHidden());
+			assertEquals("scalar para help",handle.getHelpText());
+			assertFalse(handle.isConcealValue());
+			assertFalse(handle.allowBlank());
+			assertTrue(handle.allowNull());
+			assertEquals(DesignChoiceConstants.SCALAR_PARAM_ALIGN_LEFT,handle.getAlignment());
+			assertEquals("##,###.##",handle.getFormat());
+			assertEquals(5,handle.getListlimit());
+			
+			//test on params on duplicated name
+			try{
+				openDesign( "DynamicParamterTest1.xml" );
+			   }
+			catch (DesignFileException e)
+			{}
+			
+			super.saveAs(OUTPUT_FILE_NAME);
+			assertTrue( compareTextFile( GOLDEN_FILE_NAME, OUTPUT_FILE_NAME ));
+
+		}
+}
+
+
+
---
+++ testsuites/org.eclipse.birt.report.tests.model/src/org/eclipse/birt/report/tests/model/api/ModuleUtilTest.java
@@ -0,0 +1,75 @@
+package org.eclipse.birt.report.tests.model.api;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.birt.core.framework.Platform;
+import org.eclipse.birt.report.model.api.DesignEngine;
+import org.eclipse.birt.report.model.api.ModuleUtil;
+import org.eclipse.birt.report.model.api.SessionHandle;
+import org.eclipse.birt.report.tests.model.BaseTestCase;
+
+public class ModuleUtilTest extends BaseTestCase {
+	private final String reportName = "ModuleUtilTest_report.xml"; //$NON-NLS-1$
+	private final String libraryName = "ModuleUtilTest_report.xml"; //$NON-NLS-1$
+	private final String invalidreportName = "ModuleUtilTest_report_invalid.xml"; //$NON-NLS-1$
+	private final String invalidlibraryName = "ModuleUtilTest_report_invalid.xml"; //$NON-NLS-1$
+	protected static final String pluginpath = System.getProperty("eclipse.home")+"/plugins/"+ PLUGIN_NAME +"/bin/";
+		public ModuleUtilTest(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static Test suite() {
+
+		return new TestSuite(ModuleUtilTest.class);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+			
+	}
+	public void testReportValidation() throws Exception 
+	{
+		//test a valid report design
+		
+		openDesign( reportName );
+		File file = new File(pluginpath+getClassFolder( ) + INPUT_FOLDER + reportName );
+		InputStream is = new FileInputStream( file );
+		SessionHandle session = DesignEngine.newSession( null );
+		designHandle = session.openDesign( file.toString() );
+		assertTrue(ModuleUtil.isValidDesign(session,reportName,is));
+		
+		//test a invalid report design
+		File file2 = new File(pluginpath+getClassFolder( ) + INPUT_FOLDER + invalidreportName );
+		InputStream is2 = new FileInputStream( file2 );
+		SessionHandle session2 = DesignEngine.newSession( null );
+		assertFalse(ModuleUtil.isValidDesign(session2,invalidreportName,is2));
+		
+	}	
+	public void testLibraryValidation() throws Exception 
+	{
+		//test a valid library 
+		openDesign( libraryName );
+		File file = new File(pluginpath+getClassFolder( ) + INPUT_FOLDER + libraryName );
+		InputStream is = new FileInputStream( file );
+		SessionHandle session = DesignEngine.newSession( null );
+		designHandle = session.openDesign(file.toString());
+		assertTrue(ModuleUtil.isValidDesign(session,libraryName,is));
+		
+		//test a invalid library
+		File file2 = new File(pluginpath+getClassFolder( ) + INPUT_FOLDER + invalidlibraryName );
+		InputStream is2 = new FileInputStream( file2 );
+		SessionHandle session2 = DesignEngine.newSession( null );
+		assertFalse(ModuleUtil.isValidDesign(session2,invalidlibraryName,is2));
+	}		
+	
+		
+	
+	
+}
