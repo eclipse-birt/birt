a7399780c10a57feba70f9393e11f42d223efdf4
qwang <>
2005-12-05 09:56:09 +0000

    - Summary: Refactir package name to following eclipse naming convention.
    - Bugzilla Bug (s) Resolved:
    
    - Description: Change the intenral package name of IDE editor to following eclipse naming convention.
    
    - Tests Description: manual
    
    - Files Edited:
    
    - Files Added:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

---
+++ UI/org.eclipse.birt.report.designer.ui.ide/src/org/eclipse/birt/report/designer/internal/ui/ide/adapters/LibraryProvider.java
@@ -0,0 +1,218 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.internal.ui.ide.adapters;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
+import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
+import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
+import org.eclipse.birt.report.designer.internal.ui.views.ILibraryProvider;
+import org.eclipse.birt.report.designer.nls.Messages;
+import org.eclipse.birt.report.designer.ui.IReportGraphicConstants;
+import org.eclipse.birt.report.designer.ui.ReportPlatformUIImages;
+import org.eclipse.birt.report.designer.ui.ReportPlugin;
+import org.eclipse.birt.report.model.api.DesignFileException;
+import org.eclipse.birt.report.model.api.LibraryHandle;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+
+/**
+ * Implement of ILibraryProvider Return libraries defined in preference
+ * and libraries in the same project with the report file.
+ */
+
+public class LibraryProvider implements ILibraryProvider
+{
+
+	private static final String MSG_OPEN_DEFINED_LIBRARY_ERROR_TITLE = Messages.getString( "LibraryProvider.openDefinedLibrary.error.dialog.title" ); //$NON-NLS-1$
+	private static final String MSG_OPEN_DEFINED_LIBRARY_ERROR_MSG = Messages.getString( "LibraryProvider.openDefinedLibrary.error.dialog.message" ); //$NON-NLS-1$
+
+	private List projectLibrarys = new ArrayList();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.report.designer.internal.ui.views.lib.ILibraryProvider#getLibrarys()
+	 */
+	public LibraryHandle[] getLibraries( )
+	{
+		ArrayList libList = new ArrayList( );
+		try
+		{
+			libList.addAll( getProjectLibraryList( ) );
+		}
+		catch ( CoreException e1 )
+		{
+		}
+		libList.addAll( getPreferenceLibraries( ) );
+		return (LibraryHandle[]) libList.toArray( new LibraryHandle[libList.size( )] );
+	}
+
+	private ArrayList getProjectLibraryList( ) throws CoreException
+	{
+		IEditorPart editor = UIUtil.getActiveEditor( true );
+		if ( editor != null )
+		{
+			IFile file = ( (IFileEditorInput) editor.getEditorInput( ) ).getFile( );
+			IProject project = file.getProject( );
+			IResource[] resources = project.members( );
+			return buildLibraryList(resources);
+		}
+		return null;
+	}
+
+	private ArrayList buildLibraryList( IResource[] resources )
+	{
+		ArrayList libList = new ArrayList( );
+		for ( int i = 0; i < resources.length; i++ )
+		{
+			IResource resource = resources[i];
+			if ( resource.getType( ) == IResource.FILE )
+			{
+				LibraryHandle handle = null;
+				try
+				{
+					handle = SessionHandleAdapter.getInstance( )
+							.getSessionHandle( )
+							.openLibrary( resource.getLocation( ).toOSString( ) );
+				}
+				catch ( Exception e )
+				{
+					continue;
+				}
+				libList.add( handle );
+				projectLibrarys.add( handle.getFileName());
+			}
+			else if ( resource.getType( ) == IResource.FOLDER )
+			{
+				try
+				{
+					libList.addAll( buildLibraryList( ( (IFolder) resource ).members( ) ) );
+				}
+				catch ( CoreException e )
+				{
+				}
+			}
+		}
+		return libList;
+	}
+
+	private ArrayList getPreferenceLibraries( )
+	{
+		String[] predefinedLibrarys = ReportPlugin.getDefault( )
+				.getLibraryPreference( );
+		ArrayList libList = new ArrayList( );
+
+		for ( int i = 0; i < predefinedLibrarys.length; i++ )
+		{
+			try
+			{
+				LibraryHandle handle = SessionHandleAdapter.getInstance( )
+						.getSessionHandle( )
+						.openLibrary( predefinedLibrarys[i] );
+				if ( !isLibExist( libList, handle ) )
+					libList.add( handle );
+			}
+			catch ( DesignFileException e )
+			{
+				ExceptionHandler.handle( e,
+						MSG_OPEN_DEFINED_LIBRARY_ERROR_TITLE,
+						MSG_OPEN_DEFINED_LIBRARY_ERROR_MSG );
+			}
+		}
+		return libList;
+	}
+
+//	private ArrayList getFolderLibraries( File folder )
+//	{
+//		ArrayList libList = new ArrayList( );
+//		File[] libs = folder.listFiles( new FileFilter( ) {
+//
+//			public boolean accept( File pathname )
+//			{
+//				return pathname.getPath( ).indexOf( ".rptlibrary" ) == pathname.getPath( ) //$NON-NLS-1$
+//						.length( ) - 11;
+//			}
+//
+//		} );
+//		for ( int i = 0; i < libs.length; i++ )
+//		{
+//			File lib = libs[i];
+//			if ( lib.isFile( ) )
+//			{
+//				LibraryHandle handle = null;
+//				try
+//				{
+//					handle = SessionHandleAdapter.getInstance( )
+//							.getSessionHandle( )
+//							.openLibrary( lib.getAbsolutePath( ) );
+//				}
+//				catch ( DesignFileException e )
+//				{
+//					continue;
+//				}
+//				if ( !isLibExist( libList, handle ) )
+//					libList.add( handle );
+//			}
+//			else
+//			{
+//				libList.addAll( getFolderLibraries( lib ) );
+//			}
+//		}
+//		return libList;
+//	}
+//
+//	private File getInputForlder( )
+//	{
+//		IEditorPart editor = UIUtil.getActiveEditor( true );
+//		if ( editor != null )
+//		{
+//			IFile file = ( (IFileEditorInput) editor.getEditorInput( ) ).getFile( );
+//			IProject project = file.getProject( );
+//			return project.getProjectRelativePath( ).toFile( );
+//		}
+//		return null;
+//	}
+
+	private boolean isLibExist( List list, LibraryHandle handle )
+	{
+		for ( Iterator iter = list.iterator( ); iter.hasNext( ); )
+		{
+			LibraryHandle element = (LibraryHandle) iter.next( );
+			if ( element.getFileName( ).equals( handle.getFileName( ) ) )
+				return true;
+		}
+		return false;
+	}
+
+	private boolean isInProjectFolder( final LibraryHandle handle )
+	{
+		return projectLibrarys.contains(handle.getFileName());
+	}
+
+	public Image getDisplayIcon( LibraryHandle handle )
+	{
+		if ( !isInProjectFolder( handle ) )
+		{
+			return ReportPlatformUIImages.getImage( IReportGraphicConstants.ICON_ELEMENT_LIBRARY_REFERENCED );
+		}
+		return null;
+	}
+}
---
+++ UI/org.eclipse.birt.report.designer.ui.ide/src/org/eclipse/birt/report/designer/internal/ui/ide/adapters/NewLibraryWizardAdapterFactory.java
@@ -0,0 +1,227 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.internal.ui.ide.adapters;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URL;
+
+import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
+import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
+import org.eclipse.birt.report.designer.nls.Messages;
+import org.eclipse.birt.report.designer.ui.ReportPlugin;
+import org.eclipse.birt.report.designer.ui.editors.LibraryReportEditor;
+import org.eclipse.birt.report.designer.ui.editors.ReportFileEditorInput;
+import org.eclipse.birt.report.designer.ui.wizards.INewLibraryCreationPage;
+import org.eclipse.birt.report.designer.ui.wizards.NewLibraryWizard;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
+
+/**
+ * Add INewLibraryCreationPage adaptable to NewLibraryWizard. 
+ */
+
+public class NewLibraryWizardAdapterFactory implements IAdapterFactory
+{
+
+	public Object getAdapter( Object adaptableObject, Class adapterType )
+	{
+		NewLibraryWizard wizard = (NewLibraryWizard) adaptableObject;
+
+		return new NewLibraryCreationPage( "", wizard.getSelection( ) );
+	}
+
+	public Class[] getAdapterList( )
+	{
+		return new Class[]{
+			INewLibraryCreationPage.class
+		};
+	}
+
+}
+
+class NewLibraryCreationPage extends WizardNewFileCreationPage implements
+		INewLibraryCreationPage
+{
+
+	private static final String OPENING_FILE_FOR_EDITING = Messages.getString( "NewLibraryWizard.text.OpenFileForEditing" ); //$NON-NLS-1$
+	private static final String CREATING = Messages.getString( "NewLibraryWizard.text.Creating" ); //$NON-NLS-1$
+	
+//	private static final String NEW_REPORT_FILE_NAME_PREFIX = Messages.getString( "NewLibraryWizard.displayName.NewReportFileNamePrefix" ); //$NON-NLS-1$
+//	private static final String NEW_REPORT_FILE_EXTENSION = Messages.getString( "NewLibraryWizard.displayName.NewReportFileExtension" ); //$NON-NLS-1$
+//	private static final String NEW_REPORT_FILE_NAME = NEW_REPORT_FILE_NAME_PREFIX
+//			+ NEW_REPORT_FILE_EXTENSION;
+//	private static final String CREATE_A_NEW_REPORT = Messages.getString( "NewLibraryWizard.text.CreateReport" ); //$NON-NLS-1$
+//	private static final String REPORT = Messages.getString( "NewLibraryWizard.title.Report" ); //$NON-NLS-1$
+//	private static final String WIZARDPAGE = Messages.getString( "NewLibraryWizard.title.WizardPage" ); //$NON-NLS-1$
+//	private static final String NEW = Messages.getString( "NewLibraryWizard.title.New" ); //$NON-NLS-1$
+
+	public NewLibraryCreationPage( String pageName,
+			IStructuredSelection selection )
+	{
+		super( pageName, selection );
+	}
+
+	public boolean performFinish( )
+	{
+		final IPath containerName = getContainerFullPath( );
+		String fn = getFileName( );
+		final String fileName;
+		if ( !fn.endsWith( ".rptlibrary" ) ) //$NON-NLS-1$
+		{
+			fileName = fn + ".rptlibrary"; //$NON-NLS-1$
+		}
+		else
+		{
+			fileName = fn;
+		}
+		InputStream streamFromPage = null;
+		URL url = Platform.find( Platform.getBundle( ReportPlugin.REPORT_UI ),
+				new Path( "/templates/blank_library.rptlibrary" ) );
+		if ( url != null )
+		{
+			try
+			{
+				streamFromPage = url.openStream( );
+			}
+			catch ( IOException e1 )
+			{
+				// ignore.
+			}
+		}
+		final InputStream stream = streamFromPage;
+		IRunnableWithProgress op = new IRunnableWithProgress( ) {
+
+			public void run( IProgressMonitor monitor )
+					throws InvocationTargetException
+			{
+				try
+				{
+					doFinish( containerName, fileName, stream, monitor );
+				}
+				catch ( CoreException e )
+				{
+					throw new InvocationTargetException( e );
+				}
+				finally
+				{
+					monitor.done( );
+				}
+			}
+		};
+		try
+		{
+			getContainer( ).run( true, false, op );
+		}
+		catch ( InterruptedException e )
+		{
+			return false;
+		}
+		catch ( InvocationTargetException e )
+		{
+			Throwable realException = e.getTargetException( );
+			ExceptionHandler.handle( realException );
+			return false;
+		}
+		return true;
+	}
+
+	private void doFinish( IPath containerName, String fileName,
+			InputStream stream, IProgressMonitor monitor ) throws CoreException
+	{
+		// create a sample file
+		monitor.beginTask( CREATING + fileName, 2 );
+		IResource resource = (IContainer) ResourcesPlugin.getWorkspace( )
+				.getRoot( )
+				.findMember( containerName );
+		IContainer container = null;
+		if ( resource == null
+				|| !resource.exists( )
+				|| !( resource instanceof IContainer ) )
+		{
+			// create folder if not exist
+			IFolder folder = createFolderHandle( containerName );
+			UIUtil.createFolder( folder, monitor );
+			container = folder;
+		}
+		else
+		{
+			container = (IContainer) resource;
+		}
+		final IFile file = container.getFile( new Path( fileName ) );
+		try
+		{
+			if ( file.exists( ) )
+			{
+				file.setContents( stream, true, true, monitor );
+			}
+			else
+			{
+				file.create( stream, true, monitor );
+			}
+			stream.close( );
+
+		}
+		catch ( IOException e )
+		{
+		}
+		monitor.worked( 1 );
+		monitor.setTaskName( OPENING_FILE_FOR_EDITING );
+		getShell( ).getDisplay( ).asyncExec( new Runnable( ) {
+
+			public void run( )
+			{
+				IWorkbench workbench = PlatformUI.getWorkbench( );
+				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow( );
+
+				IWorkbenchPage page = window.getActivePage( );
+				try
+				{
+					page.openEditor( new ReportFileEditorInput( file ),
+							LibraryReportEditor.EDITOR_ID,
+							true );
+				}
+				catch ( Exception e )
+				{
+					ExceptionHandler.handle( e );
+				}
+			}
+		} );
+
+		monitor.worked( 1 );
+	}
+
+	protected IFolder createFolderHandle( IPath folderPath )
+	{
+		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace( )
+				.getRoot( );
+		return workspaceRoot.getFolder( folderPath );
+	}
+}
