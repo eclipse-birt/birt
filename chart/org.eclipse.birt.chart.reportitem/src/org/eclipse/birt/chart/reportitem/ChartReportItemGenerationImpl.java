0ff25a54fbf844ab84379d7c9198f27f1617d8d5
dmichonneau <>
2005-12-08 23:44:02 +0000

    Fix #119332 NPE when running attahced report.

--- chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemGenerationImpl.java
+++ chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemGenerationImpl.java
@@ -11,6 +11,12 @@
 
 package org.eclipse.birt.chart.reportitem;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.data.engine.api.IBaseExpression;
+import org.eclipse.birt.report.engine.extension.IRowSet;
 import org.eclipse.birt.report.engine.extension.ReportItemGenerationBase;
 
 /**
@@ -18,6 +24,37 @@ import org.eclipse.birt.report.engine.extension.ReportItemGenerationBase;
  */
 public class ChartReportItemGenerationImpl extends ReportItemGenerationBase
 {
-
-
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.report.engine.extension.IReportItemGeneration#onRowSets(org.eclipse.birt.report.engine.extension.IRowSet[])
+	 */
+	public void onRowSets(IRowSet[] rowSets) throws BirtException
+	{
+		// check
+		if ( rowSets == null
+				|| rowSets.length != 1
+				|| rowSets[0] == null 
+				|| queries == null
+				|| queries[0] == null )
+		{
+			// if the Data rows are null/empty, do nothing.
+			return ;
+		}
+		else
+		{
+			// Evaluate the expressions so that they are registered by the DtE in the Report
+			// Document
+			IRowSet rowSet = rowSets[0];
+			Collection expressions = queries[0].getRowExpressions();
+			while (rowSet.next())
+			{
+				for (Iterator iter = expressions.iterator(); iter.hasNext();)
+				{
+					rowSet.evaluate((IBaseExpression)iter.next());
+				}
+			}
+		}
+		
+	}
 }
