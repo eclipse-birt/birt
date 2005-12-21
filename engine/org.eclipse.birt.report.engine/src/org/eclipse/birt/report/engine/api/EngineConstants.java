1df9f1f993ee09fba55e403f390118b95040eef9
Gang Liu <gliu@actuate.com>
2005-12-21 02:33:50 +0000

    fix Bugzilla Bug 119804--[Smoke]Drill-through link doesn't work in BIRT PDF Emitter.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/EngineConstants.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/EngineConstants.java
@@ -15,7 +15,8 @@ package org.eclipse.birt.report.engine.api;
  * Defines various constants that engine host may need to use
  */
 public class EngineConstants {
-	public final static String APPCONTEXT_HTML_RENDER_CONTEXT = "HTML_RENDER_CONTEXT";
-	public final static String APPCONTEXT_CHART_PRINT_RESOLUTION = "CHART_PRINT_RESOLUTION";	
-	public final static String APPCONTEXT_DATASET_CACHE_OPTION = "DATASET_CACHE_OPTION";
+	public final static String APPCONTEXT_HTML_RENDER_CONTEXT = "HTML_RENDER_CONTEXT"; //$NON-NLS-1$
+	public final static String APPCONTEXT_PDF_RENDER_CONTEXT = "PDF_RENDER_CONTEXT"; //$NON-NLS-1$
+	public final static String APPCONTEXT_CHART_PRINT_RESOLUTION = "CHART_PRINT_RESOLUTION";	 //$NON-NLS-1$
+	public final static String APPCONTEXT_DATASET_CACHE_OPTION = "DATASET_CACHE_OPTION"; //$NON-NLS-1$
 }
