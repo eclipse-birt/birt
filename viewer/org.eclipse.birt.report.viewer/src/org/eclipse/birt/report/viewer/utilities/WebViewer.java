cc7f7e86464dbd33fd5214e31b422bd3bb2a965a
Jun Zhai <jzhai@actuate.com>
2006-01-13 20:55:33 +0000

    Bugzilla Bug 123723 I18n Problem

--- viewer/org.eclipse.birt.report.viewer/src/org/eclipse/birt/report/viewer/utilities/WebViewer.java
+++ viewer/org.eclipse.birt.report.viewer/src/org/eclipse/birt/report/viewer/utilities/WebViewer.java
@@ -142,7 +142,7 @@ public class WebViewer
 			+  ( "run".equalsIgnoreCase( servletName )? "" : "&__document=" + encodedDocumentName )  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 			+ "&__format=" + format //$NON-NLS-1$
 			+ "&__svg=" +  String.valueOf( bSVGFlag ) //$NON-NLS-1$
-			+ "&__locale=" + LocaleTable.get(locale) //$NON-NLS-1$
+			+ ( LocaleTable.containsKey( locale )? "&__locale=" + LocaleTable.get( locale ) : "" ) //$NON-NLS-1$ //$NON-NLS-2$
 			+ "&__designer=true" //$NON-NLS-1$
 			+ "&__masterpage=" + String.valueOf( bMasterPageContent ); //$NON-NLS-1$
 		
