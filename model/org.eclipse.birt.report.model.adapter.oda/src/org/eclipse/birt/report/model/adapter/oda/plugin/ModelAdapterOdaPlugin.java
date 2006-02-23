8d26128588464be63222076023273bb147a4104c
Rick Lu <rlu@actuate.com>
2006-02-23 03:46:58 +0000

    Enhancements in Model DTP Adapter.

---
+++ model/org.eclipse.birt.report.model.adapter.oda/src/org/eclipse/birt/report/model/adapter/oda/plugin/ModelAdapterOdaPlugin.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.adapter.oda.plugin;
+
+import org.eclipse.birt.core.plugin.BIRTPlugin;
+
+/**
+ * The class to use the eclipse tracing facilities.
+ */
+
+public class ModelAdapterOdaPlugin extends BIRTPlugin
+{
+}
