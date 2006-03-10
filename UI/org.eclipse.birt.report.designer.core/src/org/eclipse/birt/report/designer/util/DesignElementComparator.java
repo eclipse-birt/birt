ba1d6ef151ca2d8b32cc0cb6678fb06bd1c73b31
xizhang <>
2006-03-10 08:35:24 +0000

    -Summary:    Library outline view - Sort the Report Item names
    - Bugzilla Bug (s) Resolved:
      126670
    
    - Description:
       In Library outline view, in the Report Items category,
    the report items should be sorted by name.
    
    - Tests Description:
       manual
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    - Files Edited:

---
+++ UI/org.eclipse.birt.report.designer.core/src/org/eclipse/birt/report/designer/util/DesignElementComparator.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2004 - 2006 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.util;
+
+import java.util.Comparator;
+import org.eclipse.birt.report.model.api.DesignElementHandle;
+import com.ibm.icu.text.Collator;
+
+/**
+ * 
+ */
+
+public class DesignElementComparator implements Comparator
+{
+
+	private boolean ascending = true;
+	private int ret = 0;
+
+	public int compare( Object o1, Object o2 )
+	{
+		String name1 = null;
+		String name2 = null;
+
+		if ( o1 instanceof DesignElementHandle
+				&& o2 instanceof DesignElementHandle )
+		{
+			name1 = ( (DesignElementHandle) o1 ).getDefn( ).getName( );
+			name2 = ( (DesignElementHandle) o2 ).getDefn( ).getName( );
+
+			if ( ascending )
+			{
+				ret = Collator.getInstance( ).compare( name1, name2 );
+			}
+			else
+			{
+				ret = Collator.getInstance( ).compare( name2, name1 );
+			}
+			if ( ret != 0 )
+			{
+				return ret;
+			}
+
+			// if ret == 0
+			AlphabeticallyComparator comparator = new AlphabeticallyComparator( );
+			return comparator.compare( o1, o2 );
+
+		}
+
+		return 0;
+	}
+
+}
