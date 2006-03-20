37d8c08b104b2f087343dc7807945ae6e2788965
Wei Yan <wyan@actuate.com>
2006-03-20 11:01:27 +0000

    rename the adapter to TableGroupEventAdapter

---
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/script/eventadapter/TableGroupEventAdapter.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.birt.report.engine.api.script.eventadapter;
+
+import org.eclipse.birt.report.engine.api.script.IReportContext;
+import org.eclipse.birt.report.engine.api.script.element.ITableGroup;
+import org.eclipse.birt.report.engine.api.script.eventhandler.ITableGroupEventHandler;
+
+public class TableGroupEventAdapter implements ITableGroupEventHandler
+{
+
+	public void onPrepare( ITableGroup tableGroup, IReportContext context )
+	{
+		
+	}
+
+}
