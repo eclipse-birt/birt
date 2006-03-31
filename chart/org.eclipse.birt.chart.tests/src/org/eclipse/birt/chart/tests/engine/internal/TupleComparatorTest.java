ccb408c37d65187712860e300730d338d02743ed
Zhiqiang Qian <zqian@actuate.com>
2006-03-31 06:21:49 +0000

    - Summary: fix bug134192:Thai3.2: Thai data sorting does not work in the BIRT chart. - Bugzilla Bug (s) Resolved: 134192 - Description: Change to use icu Collator to sort localed strings. - Tests Description  : Manual test - Files Edited:
    - Files Added:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

--- chart/org.eclipse.birt.chart.tests/src/org/eclipse/birt/chart/tests/engine/internal/TupleComparatorTest.java
+++ chart/org.eclipse.birt.chart.tests/src/org/eclipse/birt/chart/tests/engine/internal/TupleComparatorTest.java
@@ -28,29 +28,29 @@ public class TupleComparatorTest extends TestCase {
 
 	public void testCompareObjects() {
 
-		assertEquals(0, TupleComparator.compareObjects(null, null) );
-		assertEquals(-1, TupleComparator.compareObjects(null, "abc") ); //$NON-NLS-1$
-		assertEquals(1, TupleComparator.compareObjects("abc", null) ); //$NON-NLS-1$
+		assertEquals(0, TupleComparator.compareObjects(null, null, null) );
+		assertEquals(-1, TupleComparator.compareObjects(null, "abc", null) ); //$NON-NLS-1$
+		assertEquals(1, TupleComparator.compareObjects("abc", null, null) ); //$NON-NLS-1$
 		
-		assertEquals(1, TupleComparator.compareObjects("abc", "a") ); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(-1, TupleComparator.compareObjects("ab", "b") ); //$NON-NLS-1$ //$NON-NLS-2$
-		assertEquals(0, TupleComparator.compareObjects("abc", "abc") ); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(1, TupleComparator.compareObjects("abc", "a", null) ); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(-1, TupleComparator.compareObjects("ab", "b", null) ); //$NON-NLS-1$ //$NON-NLS-2$
+		assertEquals(0, TupleComparator.compareObjects("abc", "abc", null) ); //$NON-NLS-1$ //$NON-NLS-2$
 		
 		Integer n1 = new Integer(80);
 		Integer n2 = new Integer(60);
-		assertEquals(1, TupleComparator.compareObjects(n1, n2) );
-		assertEquals(-1, TupleComparator.compareObjects(n2, n1) );
+		assertEquals(1, TupleComparator.compareObjects(n1, n2, null) );
+		assertEquals(-1, TupleComparator.compareObjects(n2, n1, null) );
 		
 		Date a = new Date((long)8E8);
 		Date b = new Date((long)7E9);	
-		assertEquals(-1, TupleComparator.compareObjects(a, b) );
-		assertEquals(1, TupleComparator.compareObjects(b, a) );
+		assertEquals(-1, TupleComparator.compareObjects(a, b, null) );
+		assertEquals(1, TupleComparator.compareObjects(b, a, null) );
 		
         Calendar c1 = Calendar.getInstance();
         c1.set(1999,2,5);
         Calendar c2 = Calendar.getInstance();
         c2.set(2000,1,23);
-		assertEquals(-1, TupleComparator.compareObjects(c1, c2) );
-		assertEquals(1, TupleComparator.compareObjects(c2, c1) );
+		assertEquals(-1, TupleComparator.compareObjects(c1, c2, null) );
+		assertEquals(1, TupleComparator.compareObjects(c2, c1, null) );
 	}
 }
