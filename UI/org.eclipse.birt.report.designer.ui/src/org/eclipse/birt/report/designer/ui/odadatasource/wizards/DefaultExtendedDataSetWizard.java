a005ae9dfacf5020eef5317794949059375f108e
xma <>
2006-04-04 05:26:45 +0000

    Fix Bugzilla Bug 134639 BIRT 2.0 ODA Designer plugin no longer works in BIRT 2.1 Build

---
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/odadatasource/wizards/DefaultExtendedDataSetWizard.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Actuate Corporation. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Actuate Corporation - initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.birt.report.designer.ui.odadatasource.wizards;
+
+/**
+ * @deprecated As of BIRT 2.1, replaced by
+ *             {@link org.eclipse.datatools.connectivity.oda.design.ui 
+ *             org.eclipse.datatools.connectivity.oda.design.ui } .
+ * 
+ * @version $Revision: 1.2 $ $Date: 2006/03/30 04:36:07 $
+ */
+
+public abstract class DefaultExtendedDataSetWizard
+		extends
+			org.eclipse.birt.report.designer.data.ui.dataset.DefaultExtendedDataSetWizard
+
+{
+}
