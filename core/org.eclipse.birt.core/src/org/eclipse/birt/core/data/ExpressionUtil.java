0300c1ef72e7bd3dafa7eb819ac77e81de5e4eea
Lin Zhu <lzhu@actuate.com>
2006-04-05 07:21:01 +0000

    CheckIn: Add a translator method that translate the old "row" style expression to new "dataSetRow" style expression.

--- core/org.eclipse.birt.core/src/org/eclipse/birt/core/data/ExpressionUtil.java
+++ core/org.eclipse.birt.core/src/org/eclipse/birt/core/data/ExpressionUtil.java
@@ -41,4 +41,103 @@ public final class ExpressionUtil
 	{
 		return ROW_INDICATOR + "[" + index + "]";
 	}
+	
+	/**
+	 * Translate the old expression with "row" as indicator to new expression using
+	 * "dataSetRow" as indicator.
+	 * 
+	 * @param oldExpression
+	 * @return
+	 */	
+	public static String translateOldExpression( String oldExpression )
+	{
+		if( oldExpression == null )
+			return null;
+		char[] chars = oldExpression.toCharArray( );
+		
+		//5 is the minium length of expression that can cantain a row expression
+		if( chars.length < 5)
+			return oldExpression;
+		else
+		{
+			boolean candidateKey = true;
+			boolean omitNextQuote = false;
+			int retrieveSize = 0;
+			for( int i = 0; i < chars.length; i++ )
+			{
+				if( chars[i] == '/')
+				{
+					if( i > 0 && chars[i-1] == '/')
+					{
+						retrieveSize++;
+						while( i < chars.length )
+						{
+							i++;
+							retrieveSize++;
+							if( chars[i]=='\n')
+							{
+								break;
+							}	
+						}
+						retrieveSize++;
+						i++;
+					}
+				}else if( chars[i] == '*'  )
+				{
+					if( i > 0 && chars[i-1] == '/' )
+					{
+						i++;
+						retrieveSize = retrieveSize+2;
+						while( i < chars.length )
+						{
+							i++;
+							retrieveSize++;
+							if( chars[i-1] == '*' && chars[i] == '/')
+							{
+								break;
+							}
+						}
+						retrieveSize++;
+						i++;
+					}
+				}
+				
+				if( (!omitNextQuote) && chars[i] == '"')
+					candidateKey = !candidateKey;
+				if( chars[i] == '\\')
+					omitNextQuote = true;
+				else
+					omitNextQuote = false;
+				if( i >= retrieveSize + 3 ){
+					if ( candidateKey
+							&& chars[i - retrieveSize - 3] == 'r'
+							&& chars[i - retrieveSize - 2] == 'o'
+							&& chars[i - retrieveSize - 1] == 'w' )
+					{
+						if( i-retrieveSize-4 <=0 || isValidProceeding(chars[i - retrieveSize - 4]))
+						{
+							if(chars[i] == ' '||chars[i] == '.'||chars[i]=='[')
+							{
+								String firstPart = oldExpression.substring( 0, i-retrieveSize-3 );
+								String secondPart = translateOldExpression( oldExpression.substring( i-retrieveSize ));
+								return firstPart+"dataSetRow"+ secondPart;
+							}
+						}
+					}
+				}
+			}
+		}
+		return oldExpression;
+	}
+	
+	//Test whether the char immediately before the candidate "row" key is valid.
+	private static boolean isValidProceeding( char operator )
+	{
+		if ( (operator >= 'A' && operator <= 'Z')
+			 || (operator >='a' && operator <= 'z')
+			 || operator == '_')
+			return false;
+		
+		return true;
+	}
 }
