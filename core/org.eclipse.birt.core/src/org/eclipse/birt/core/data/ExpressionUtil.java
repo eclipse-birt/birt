eb50f2527eddae577cd926635b2483c39a18e486
Lin Zhu <lzhu@actuate.com>
2006-04-06 07:16:08 +0000

    Add expression util according to Model's request.

--- core/org.eclipse.birt.core/src/org/eclipse/birt/core/data/ExpressionUtil.java
+++ core/org.eclipse.birt.core/src/org/eclipse/birt/core/data/ExpressionUtil.java
@@ -63,119 +63,121 @@ public final class ExpressionUtil
 	}
 	
 	/**
-	 * Translate the old expression with "row" as indicator to new expression using
-	 * "dataSetRow" as indicator.
+	 * Translate the old expression with "row" as indicator to new expression
+	 * using "dataSetRow" as indicator in xml file.
 	 * 
-	 * @param oldExpression
+	 * NOTE: "&quot;" will not be parse as '"'
+	 * 
+	 * @param xmlString
 	 * @return
-	 */	
-	public static String toNewExpression( String oldExpression )
+	 */
+	public static List extractColumnExpressionsFromXML( String xmlString )
 	{
-		if ( oldExpression == null )
+		List result = new ArrayList( );
+		if ( xmlString == null )
 			return null;
-		
-		char[] chars = oldExpression.toCharArray( );
 
-		// 5 is the minium length of expression that can cantain a row
-		// expression
-		if ( chars.length < 5 )
-			return oldExpression;
-		else
+		char[] chars = xmlString.toCharArray( );
+
+		int retrieveSize = 0;
+		for ( int i = 0; i < chars.length; i++ )
 		{
-			//candidateKey1 is used to mark the status of double quote
-			boolean candidateKey1 = true;
-			//candidateKey2 is used to mark the status of 
-			boolean candidateKey2 = true;
-			
-			boolean omitNextQuote = false;
-			int retrieveSize = 0;
-			for ( int i = 0; i < chars.length; i++ )
+			retrieveSize = 0;
+			if ( chars[i] == '-' )
 			{
-				retrieveSize = 0;
-				if ( chars[i] == '/' )
+				if ( i > 1 && chars[i - 1] == '!' && chars[i - 2] == '<' )
 				{
-					if ( i > 0 && chars[i - 1] == '/' )
-					{
-						retrieveSize++;
-						while ( i < chars.length )
-						{
-							i++;
-							retrieveSize++;
-							if ( chars[i] == '\n' )
-							{
-								break;
-							}
-						}
-						retrieveSize++;
-						i++;
-					}
-				}
-				else if ( chars[i] == '*' )
-				{
-					if ( i > 0 && chars[i - 1] == '/' )
+					i++;
+					retrieveSize = retrieveSize + 3;
+					while ( i < chars.length )
 					{
 						i++;
-						retrieveSize = retrieveSize + 2;
-						while ( i < chars.length )
-						{
-							i++;
-							retrieveSize++;
-							if ( chars[i - 1] == '*' && chars[i] == '/' )
-							{
-								break;
-							}
-						}
 						retrieveSize++;
-						i++;
+						if ( chars[i - 2] == '-'
+								&& chars[i - 1] == '-' && chars[i] == '>' )
+						{
+							break;
+						}
 					}
+					retrieveSize++;
+					i++;
 				}
+			}
 
-				if ( ( !omitNextQuote ) && chars[i] == '"' )
+			if ( i >= retrieveSize + 3 )
+			{
+				if ( chars[i - retrieveSize - 3] == 'r'
+					 && chars[i - retrieveSize - 2] == 'o'
+					 && chars[i - retrieveSize - 1] == 'w' )
 				{
-					candidateKey1 = !candidateKey1;
-					if( candidateKey1 )
-						candidateKey2 = true;
-				}
-				if ( ( !omitNextQuote ) && chars[i] == '\'')
-				{	
-					candidateKey2 = !candidateKey2;
-					if( candidateKey2)
-						candidateKey1 = true;
-				}
-				if ( chars[i] == '\\' )
-					omitNextQuote = true;
-				else
-					omitNextQuote = false;
-				if ( i >= retrieveSize + 3 )
-				{
-					if ( candidateKey1
-							&& chars[i - retrieveSize - 3] == 'r'
-							&& chars[i - retrieveSize - 2] == 'o'
-							&& chars[i - retrieveSize - 1] == 'w' )
+					if ( i - retrieveSize - 4 <= 0
+							|| isValidProceeding( chars[i - retrieveSize - 4] ) )
 					{
-						if ( i - retrieveSize - 4 <= 0
-								|| isValidProceeding( chars[i
-										- retrieveSize - 4] ) )
+						if ( chars[i] == '.' )
 						{
-							if ( chars[i] == ' '
-									|| chars[i] == '.' || chars[i] == '[' )
+							i++;
+							int j = i;
+							while ( chars[i] != ' '
+									&& chars[i] != '<' && chars[i] != '>' )
 							{
-								String firstPart = oldExpression.substring( 0,
-										i - retrieveSize - 3 );
-								String secondPart = toNewExpression( oldExpression.substring( i
-										- retrieveSize ) );
-								String newExpression = firstPart
-										+ "dataSetRow" + secondPart;
-								return newExpression;
+								i++;
+								if ( i >= chars.length )
+									return result;
+							}
+							result.add( xmlString.substring( j, i ) );
+						}
+						else if ( chars[i] == '[' )
+						{
+							i++;
+							int j = i;
+							boolean push = true;
+							while ( chars[i] != ']' || chars[i - 1] == '\\' )
+							{
+								i++;
+								if ( i >= chars.length )
+									return result;
+								if ( chars[i] == '<' || chars[i] == '>' )
+								{
+									push = false;
+									break;
+								}
+							}
+							if ( push )
+							{
+								addCandidateColumnToList( xmlString, result, i, j );
 							}
 						}
 					}
 				}
 			}
-			
 		}
-		
-		return oldExpression;
+
+		return result;
+	}
+
+	/**
+	 * @param xmlString
+	 * @param result
+	 * @param i
+	 * @param j
+	 */
+	private static void addCandidateColumnToList( String xmlString, List result, int i, int j )
+	{
+		Object candidate = xmlString.substring( j,i ).trim( );
+		if ( candidate.toString( ).startsWith( "\"" )
+				&& candidate.toString( ).endsWith( "\"" ) )
+			candidate = candidate.toString( ).substring( 1,	candidate.toString( ).length( ) - 1 ); 
+		else
+		{
+			try{
+				candidate = new Integer( candidate.toString( ) );
+			}catch (Exception e)
+			{
+				candidate = null;
+			}
+		}
+		if( candidate!= null )
+			result.add( candidate );
 	}
 	
 	/**
@@ -194,5 +196,6 @@ public final class ExpressionUtil
 		
 		return true;
 	}
+	
 	
 }
