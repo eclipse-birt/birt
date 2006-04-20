b1370319f2d79cca3eab3e44278dccd12ec3dc99
Liwen Chen <lchen@actuate.com>
2006-04-20 07:56:24 +0000

    Add case in RunAndRenderTaskTest to test cancel function

--- testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/api/RunAndRenderTaskTest.java
+++ testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/api/RunAndRenderTaskTest.java
@@ -14,12 +14,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 
-import org.eclipse.birt.report.engine.api.EngineConfig;
 import org.eclipse.birt.report.engine.api.EngineException;
+import org.eclipse.birt.report.engine.api.HTMLRenderOption;
 import org.eclipse.birt.report.engine.api.IReportRunnable;
+import org.eclipse.birt.report.engine.api.IRunAndRenderTask;
 import org.eclipse.birt.report.engine.api.RenderOptionBase;
-import org.eclipse.birt.report.engine.api.ReportEngine;
-import org.eclipse.birt.report.engine.api.impl.RunAndRenderTask;
 import org.eclipse.birt.report.tests.engine.EngineCase;
 
 public class RunAndRenderTaskTest extends EngineCase
@@ -31,15 +30,16 @@ public class RunAndRenderTaskTest extends EngineCase
 	public void testRunAndRenderTask( ) throws EngineException
 	{
 
-		String input = getClassFolder( ) + System.getProperty( "file.separator" )
-				+ INPUT_FOLDER + System.getProperty( "file.separator" )
+		String input = getClassFolder( )
+				+ System.getProperty( "file.separator" ) + INPUT_FOLDER
+				+ System.getProperty( "file.separator" )
 				+ "report_engine.rptdesign";
 
 		try
 		{
 			IReportRunnable runnable = engine
 					.openReportDesign( new FileInputStream( new File( input ) ) );
-			RunAndRenderTask task = new RunAndRenderTask( engine, runnable );
+			IRunAndRenderTask task = engine.createRunAndRenderTask( runnable );
 			// validateParameters
 			assertTrue( task.validateParameters( ) );
 			// set/getRenderOption
@@ -57,14 +57,87 @@ public class RunAndRenderTaskTest extends EngineCase
 			task.setParameterValue( "p1", "p1value" );
 			assertEquals( "Set/getParameterValues fail", task
 					.getParameterValues( ).get( "p1" ), "p1value" );
-
-			// shut down engine
-			engine.destroy( );
 		}
 		catch ( FileNotFoundException e )
 		{
 			e.printStackTrace( );
 		}
+	}
+
+	/*
+	 * Cancel RunAndRenderTask in another thread with cancel method.
+	 */
+	public void testCancelRunAndRenderTask( )
+	{
+		String input = getClassFolder( )
+				+ System.getProperty( "file.separator" ) + INPUT_FOLDER
+				+ System.getProperty( "file.separator" ) + "pages9.rptdesign";
+		long bTime,eTime,timeSpan1,timeSpan2;
+		try
+		{
+			IReportRunnable runnable = engine
+					.openReportDesign( new FileInputStream( new File( input ) ) );
+			IRunAndRenderTask task = engine.createRunAndRenderTask( runnable );
+			HTMLRenderOption option=new HTMLRenderOption();
+			option.setOutputFormat( "html" );
+			task.setRenderOption( option );
+			task.setAppContext( new HashMap() );
+			
+			CancelTask cancelThread = new CancelTask( "cancelThread", task );
+			cancelThread.start( );
+			bTime=System.currentTimeMillis( );
+			task.run( );
+			eTime=System.currentTimeMillis( );
+			task.close( );
+			timeSpan1=eTime-bTime;
+
+			task=engine.createRunAndRenderTask( runnable );
+			task.setRenderOption( option );
+			task.setAppContext( new HashMap() );
+			bTime=System.currentTimeMillis( );
+			task.run( );
+			eTime=System.currentTimeMillis( );
+			task.close( );
+			timeSpan2=eTime-bTime;
+
+			assertTrue("RunAndRenderTask.cancel() failed!",timeSpan2>timeSpan1);
+		}
+		catch ( Exception e )
+		{
+			e.printStackTrace( );
+			fail("RunAndRenderTask.cancel() failed!");
+		}
+
+	}
+
+	/*
+	 * A new thread to cancel existed runTask
+	 */
+	private class CancelTask extends Thread
+	{
+
+		private IRunAndRenderTask runTask;
+
+		public CancelTask( String threadName, IRunAndRenderTask task )
+		{
+			super( threadName );
+			runTask = task;
+		}
+
+		public void run( )
+		{
+			try
+			{
+				Thread.currentThread( ).sleep( 100 );
+				runTask.cancel( );
+			}
+			catch ( Exception e )
+			{
+				e.printStackTrace( );
+				fail( "RunAndRenderTask.cancel() failed!" );
+			}
+		}
+
 	}
 
 }
