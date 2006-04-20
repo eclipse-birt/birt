c59fcd461383594737868b0e5eb4cf6a07040bd6
Dazheng Gao <dgao@actuate.com>
2006-04-20 07:55:37 +0000

    - Summary:Change the Copyright infomation.
    - Bugzilla Bug (s) Resolved:
    
    - Description: Change the Copyright infomation.
    
    - Tests Description  :
    None
    - Files Edited:
    
    - Files Added:
    
    - Notes to Build Team:
    None
    
    - Notes to Developers:
    None
    
    - Notes to QA:
    None
    
    - Notes to Documentation:

--- UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/internal/ui/launcher/util/DebugUtil.java
+++ UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/internal/ui/launcher/util/DebugUtil.java
@@ -1,9 +1,13 @@
-/*
- * Created on 2005-11-11
+/*******************************************************************************
+ * Copyright (c) 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.debug.internal.ui.launcher.util;
 
--- UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/ui/launching/ReportAdvancedLauncherTab.java
+++ UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/ui/launching/ReportAdvancedLauncherTab.java
@@ -1,7 +1,13 @@
-// Decompiled by Jad v1.5.8f. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) 
-// Source File Name:   ReportAdvancedLauncherTab.java
+/*******************************************************************************
+ * Copyright (c) 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.debug.ui.launching;
 
