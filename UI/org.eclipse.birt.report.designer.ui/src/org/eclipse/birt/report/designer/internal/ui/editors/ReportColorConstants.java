647fa940300df0d92fcf0aa46a273a8c16e9f666
qwang <>
2006-05-08 07:56:25 +0000

    - Summary:Replace CPL wiht EPL statements
    - Bugzilla Bug (s) Resolved:None
    
    - Description:
    	The following files still contain the Common Public License and point to the CPL license file.  They should be changed to use Eclipse Public License and point to the EPL file.
    
    - Tests Description : code review.
    
    - Files Edited:
    
    - Files Added:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/editors/ReportColorConstants.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/editors/ReportColorConstants.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Actuate Coporation - Copy code to BIRT package and define BIRT colors
  *******************************************************************************/
 
 package org.eclipse.birt.report.designer.internal.ui.editors;
--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/editors/schematic/tools/FigureCellEditorLocator.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/editors/schematic/tools/FigureCellEditorLocator.java
@@ -1,10 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2004 Actuate Corporation. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Common Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/cpl-v10.html
+ * Copyright (c) 2004 IBM Corporation. All rights reserved.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors: IBM Corporation - initial API and implementation
+ *               Actuate Corporation - Change to fit BIRT requirement
  ******************************************************************************/
 
 package org.eclipse.birt.report.designer.internal.ui.editors.schematic.tools;
