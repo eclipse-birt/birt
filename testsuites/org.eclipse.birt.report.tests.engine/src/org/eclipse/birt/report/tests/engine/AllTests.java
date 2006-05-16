2a0bc8de6d65e0dc0a68235e7a731d04a270e239
xili <>
2006-05-16 10:09:22 +0000

    - Summary: integrate tests.engine with daily build - SCR(s) Resolved: part of #141186 - Description: integrate tests.engine with daily build    - add build.xml    - rewrite AllTest.java - Regression ( Yes/No ): No - Code Owner: Xiang Li - Code Reviewers: Chenzhong Yu - Tests: Manual Test

--- testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/AllTests.java
+++ testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/AllTests.java
@@ -9,91 +9,49 @@
 
 package org.eclipse.birt.report.tests.engine;
 
-import java.util.Locale;
-
-// import org.eclipse.birt.report.model.elements.ReportDesign;
-// import org.eclipse.birt.report.model.i18n.ThreadResources;
-// import org.eclipse.birt.report.model.metadata.MetaDataDictionary;
-// import org.eclipse.birt.report.model.metadata.MetaDataParserException;
-// import org.eclipse.birt.report.model.metadata.MetaDataReader;
-
-import org.eclipse.birt.report.tests.engine.api.Bug128854PrompttextTest;
-import org.eclipse.birt.report.tests.engine.api.DefaultStatusHandlerTest;
-import org.eclipse.birt.report.tests.engine.api.EngineConfigTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLActionHandlerTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLCompleteImageHandlerTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLEmitterConfigTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLRenderContextTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLRenderOptionTest;
-import org.eclipse.birt.report.tests.engine.api.HTMLServerImageHandlerTest;
-import org.eclipse.birt.report.tests.engine.api.RenderFolderDocumentTest;
-import org.eclipse.birt.report.tests.engine.api.RenderOptionBaseTest;
-import org.eclipse.birt.report.tests.engine.api.ReportEngineTest;
-import org.eclipse.birt.report.tests.engine.api.ReportParameterConverterTest;
-import org.eclipse.birt.report.tests.engine.api.RunAndRenderTaskTest;
-import org.eclipse.birt.report.tests.engine.api.RunTaskTest;
-import org.eclipse.birt.report.tests.engine.api.RenderTaskTest;
-import org.eclipse.birt.report.tests.engine.api.DataPreviewTaskTest;
-import org.eclipse.birt.report.tests.engine.api.ReportDocumentTest;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Put your comments
- *  
- * @version $Revision: 1.10 $Date: 2005-6-27
+ * All test cases for tests Engine
  */
-public class AllTests extends TestCase
+public class AllTests extends utility.BaseTestCase
 {
-
-	public AllTests( String name )
-	{
-		super( name );
-	}
-
-	protected void setUp( )
-	{
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown( )
-	{
-
-	}
-
 	public static Test suite( )
 	{
-		TestSuite suite = new TestSuite(
-				"Test for org.eclipse.birt.report.tests.engine" );
-		// $JUnit-BEGIN$
-		suite.addTestSuite( DefaultStatusHandlerTest.class );
-		suite.addTestSuite( EngineConfigTest.class );
-		// suite.addTestSuite(HTMLActionHandlerTest.class);
-		suite.addTestSuite( HTMLCompleteImageHandlerTest.class );
-		suite.addTestSuite( HTMLEmitterConfigTest.class );
-		suite.addTestSuite( HTMLRenderContextTest.class );
-		suite.addTestSuite( HTMLRenderOptionTest.class );
-		suite.addTestSuite( HTMLServerImageHandlerTest.class );
-		suite.addTestSuite( RenderOptionBaseTest.class );
-		suite.addTestSuite( ReportEngineTest.class );
-		suite.addTestSuite( ReportParameterConverterTest.class );
-		suite.addTestSuite( RunAndRenderTaskTest.class );
-		// added 12/27
-		suite.addTestSuite( DataPreviewTaskTest.class );
-		suite.addTestSuite( RunTaskTest.class );
-		suite.addTestSuite( ReportDocumentTest.class );
-		suite.addTestSuite( RenderFolderDocumentTest.class );
-		suite.addTestSuite( RenderTaskTest.class );
-		// added 06/02/28
-		suite.addTestSuite( Bug128854PrompttextTest.class );
-		// $JUnit-END$
+		TestSuite suite = new TestSuite( "Test for org.eclipse.birt.report.tests.engine" );
+
+		List classes = new ArrayList( );
+		AllTests allTests = new AllTests( );
+
+		classes.addAll( allTests.getClasses( "org.eclipse.birt.report.tests.engine.api" ) );
+
+		Iterator iter = classes.iterator( );
+
+		while ( iter.hasNext( ) )
+		{
+			String next = ( String ) iter.next( );
+			
+			try
+			{
+				Class c = Class.forName( next );
+				int modifier = c.getModifiers( );
+				if ( Modifier.isPublic( modifier ) && !Modifier.isStatic( modifier ) )
+				{
+					suite.addTestSuite( c );
+				}
+			}
+			catch ( ClassNotFoundException e )
+			{
+				assert false;
+			}
+		}
+
 		return suite;
 	}
 }
