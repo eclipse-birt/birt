bc64423c55cd4c9bea230ccf56a740f95261b555
Dazheng Gao <dgao@actuate.com>
2006-05-24 02:44:41 +0000

    - Summary:Fix bug 142873,TVT3.2:TCT574: DA: Message for empty library truncated.
    - Bugzilla Bug (s) Resolved:142873.
    
    
    
    - Description: Auto layout the text.
    
    
    
    - Tests Description  :
    
    Manual test
    
    - Files Edited:
    
    @changelist
    
    - Files Added:
    
    @addlist
    
    - Files Deleted:
    
    @deletelist
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -          Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui.lib/src/org/eclipse/birt/report/designer/internal/lib/editors/figures/EmptyFigure.java
+++ UI/org.eclipse.birt.report.designer.ui.lib/src/org/eclipse/birt/report/designer/internal/lib/editors/figures/EmptyFigure.java
@@ -1,50 +1,46 @@
 /*******************************************************************************
-* Copyright (c) 2004 Actuate Corporation .
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*  Actuate Corporation  - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2004 Actuate Corporation .
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.designer.internal.lib.editors.figures;
 
 import org.eclipse.birt.report.designer.internal.ui.editors.ReportColorConstants;
-import org.eclipse.draw2d.Figure;
-import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.geometry.Rectangle;
-
+import org.eclipse.birt.report.designer.internal.ui.editors.schematic.figures.LabelFigure;
+import org.eclipse.draw2d.geometry.Dimension;
 
 /**
- * The figure show in the library editor when the seleection object form outline is not a visual element.
+ * The figure show in the library editor when the seleection object form outline
+ * is not a visual element.
  * 
  */
-public class EmptyFigure extends Figure
+public class EmptyFigure extends LabelFigure
 {
-	private String text;
-	
-	public EmptyFigure()
+
+	// private String text;
+
+	public EmptyFigure( )
 	{
-		setBackgroundColor(ReportColorConstants.greyFillColor);
+		setOpaque( true );
+		setBackgroundColor( ReportColorConstants.greyFillColor );
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.birt.report.designer.internal.ui.editors.schematic.figures.ReportElementFigure#paintFigure(org.eclipse.draw2d.Graphics)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.birt.report.designer.internal.ui.editors.schematic.figures.LabelFigure#getPreferredSize(int,
+	 *      int)
 	 */
-	protected void paintFigure( Graphics graphics )
+	public Dimension getPreferredSize( int wHint, int hHint )
 	{
-		super.paintFigure( graphics );
-		graphics.setBackgroundColor(ReportColorConstants.ShadowLineColor);
-		
-		Rectangle rect = getBounds();
-		graphics.fillRectangle(rect);
-		graphics.drawText(text, rect.x,rect.y);
-	}
-	
-	public void setText(String text)
-	{
-		this.text = text;
+		if ( prefSize != null )
+			return prefSize;
+		return getSize( );
 	}
 }
