85487ccc3119a46a9e151ec24b67eb347e833510
xizhang <>
2006-05-25 09:28:22 +0000

    -Summary:   fix 2 bugs    1. Library explorer - Right click menu to Add - Allows user to add library from external folders    2.   	Publish library to BIRT Resource folder does not allow user to BIRT_RESOURCE_PATH or its sub-folder
    - Bugzilla Bug (s) Resolved:
    	141162, 141164
    
    - Description:
       Add a wizard, so that user can select the publish folder, which is BIRT_RESOURCE_PATH or its sub-folder when publishing library or add library.
    
    - Tests Description: Manual
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    - Files Edited:

---
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/wizards/PublishLibraryWizard.java
@@ -0,0 +1,209 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.internal.ui.wizards;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
+import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
+import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
+import org.eclipse.birt.report.designer.nls.Messages;
+import org.eclipse.birt.report.model.api.LibraryHandle;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.Wizard;
+
+
+/**
+ * 
+ */
+
+public class PublishLibraryWizard extends Wizard
+{
+	LibraryHandle handle;
+	
+	private static String windowTitle = Messages.getString("PublishLibraryDialog.ShellText");
+	private static String PAGE_TITLE = Messages.getString("PublishLibraryDialog.TitleArea");
+	private static String PAGE_DESC = Messages.getString( "PublishLibraryDialog.Message" );
+	
+	private static String addLibraryTitle = Messages.getString("PublishLibraryDialog.AddLibrary");
+
+	private String filePath;
+	private String fileName;
+	private String folderName;
+	
+	private WizardLibrarySettingPage page;
+
+	public static int HAVE_HANDLE = 1;
+	public static int HAVE_NO_HANDLE = 0;
+	
+	int type;
+	
+	/**
+	 * 
+	 */
+	public PublishLibraryWizard(LibraryHandle handle ,String fileName, String folderName)
+	{
+		setWindowTitle( windowTitle );
+		this.fileName = fileName;
+		this.folderName = folderName;		
+		this.handle = handle;
+		this.filePath = SessionHandleAdapter.getInstance( )
+		.getReportDesignHandle( )
+		.getFileName( );
+		type = HAVE_HANDLE;
+	}
+	
+	/**
+	 * 
+	 */
+	public PublishLibraryWizard(String folderName)
+	{
+		setWindowTitle( addLibraryTitle );
+		this.fileName = null;
+		this.folderName = folderName;		
+		this.handle = null;
+		type = HAVE_NO_HANDLE;
+	}
+	
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	public void addPages( )
+	{
+		page = new WizardLibrarySettingPage( type );
+		
+		if(type == HAVE_HANDLE)
+		{
+			
+			page.setTitle( PAGE_TITLE );
+			page.setMessage( PAGE_DESC );
+
+			page.setFileName( fileName );
+			page.setfolderName( folderName );						
+		}else
+		if(type == HAVE_NO_HANDLE)
+		{
+			page.setTitle( Messages.getString( "PublishLibraryDialog.AddText" ) );
+			page.setMessage( Messages.getString( "PublishLibraryDialog.AddMessage" ) );
+			page.setfolderName( folderName );			
+		}
+		page.setType( type );
+		addPage( page );
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
+	 */
+	public boolean performFinish( )
+	{
+		// TODO Auto-generated method stub
+		fileName = page.getFileName( );
+		folderName = page.getFolder( );
+		if(type == HAVE_NO_HANDLE)
+		{
+			filePath = page.getSourceFileName( );
+		}
+		return publishiLibrary();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.IWizard#canFinish()
+	 */
+	public boolean canFinish( )
+	{
+		return page.canFinish( );
+	}	
+	
+	private boolean publishiLibrary()
+	{
+		// copy to library folder
+		
+		File targetFolder = new File( folderName );
+		if ( !targetFolder.isDirectory( ) )
+		{
+			ExceptionHandler.openErrorMessageBox( Messages.getString( "PublishLibraryAction.wizard.errorTitle" ), //$NON-NLS-1$
+					Messages.getString( "PublishLibraryAction.wizard.notvalidfolder" ) ); //$NON-NLS-1$
+			return true;
+		}
+		if ( !targetFolder.exists( ) )
+		{
+			targetFolder.mkdirs( );
+		}
+		File targetFile = new File( targetFolder, fileName );
+		if ( new File( filePath ).compareTo( targetFile ) == 0 )
+		{
+			ExceptionHandler.openErrorMessageBox( Messages.getString( "PublishLibraryAction.wizard.errorTitle" ), //$NON-NLS-1$
+					Messages.getString( "PublishLibraryAction.wizard.message" ) ); //$NON-NLS-1$
+			return false;
+		}
+
+		int overwrite = Window.OK;
+		try
+		{
+			if ( targetFile.exists( ) )
+			{
+				String[] buttons = new String[]{
+						IDialogConstants.YES_LABEL,
+						IDialogConstants.NO_LABEL,
+						IDialogConstants.CANCEL_LABEL
+				};
+				String question = Messages.getFormattedString( "SaveAsDialog.overwriteQuestion", //$NON-NLS-1$
+						new Object[]{
+							targetFile.getAbsolutePath( )
+						} );
+				MessageDialog d = new MessageDialog(UIUtil.getDefaultShell( ),
+						Messages.getString( "SaveAsDialog.Question" ), //$NON-NLS-1$
+						null,
+						question,
+						MessageDialog.QUESTION,
+						buttons,
+						0 );
+				overwrite = d.open( );
+			}
+			if ( overwrite == Window.OK
+					&& ( targetFile.exists( ) || ( !targetFile.exists( ) && targetFile.createNewFile( ) ) ) )
+			{
+				copyFile( filePath, targetFile );
+			}
+		}
+		catch ( IOException e )
+		{
+			ExceptionHandler.handle( e );
+		}
+
+		return overwrite != 1;	
+	}
+	
+	private void copyFile( String in, File targetFile ) throws IOException
+	{
+		FileInputStream fis = new FileInputStream( in );
+		FileOutputStream fos = new FileOutputStream( targetFile );
+		byte[] buf = new byte[1024];
+		int i = 0;
+		while ( ( i = fis.read( buf ) ) != -1 )
+		{
+			fos.write( buf, 0, i );
+		}
+		fis.close( );
+		fos.close( );
+	}
+}
--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/actions/PublishLibraryAction.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/actions/PublishLibraryAction.java
@@ -11,33 +11,25 @@
 
 package org.eclipse.birt.report.designer.ui.actions;
 
-
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
-import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
 import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
-import org.eclipse.birt.report.designer.nls.Messages;
+import org.eclipse.birt.report.designer.internal.ui.wizards.PublishLibraryWizard;
 import org.eclipse.birt.report.designer.ui.ReportPlugin;
+import org.eclipse.birt.report.model.api.LibraryHandle;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
-
 
 /**
  * 
  */
 
-public class PublishLibraryAction implements
-		IWorkbenchWindowActionDelegate
+public class PublishLibraryAction implements IWorkbenchWindowActionDelegate
 {
 
 	public void dispose( )
@@ -54,79 +46,29 @@ public class PublishLibraryAction implements
 
 	public void run( IAction action )
 	{
-		if(isEnable() == true)
+
+		if ( isEnable( ) == false )
 		{
-			publishiLibrary();
+			return;
 		}
-
-	}
-
-	private boolean publishiLibrary()
-	{
-		// copy to library folder
-		String libraryFolderPath = ReportPlugin.getDefault( )
-				.getResourcePreference( );
 
 		String filePath = SessionHandleAdapter.getInstance( )
 				.getReportDesignHandle( )
 				.getFileName( );
 		String fileName = filePath.substring( filePath.lastIndexOf( File.separator ) + 1 );
-		File targetFolder = new File( libraryFolderPath );
-		if ( !targetFolder.isDirectory( ) )
-		{
-			ExceptionHandler.openErrorMessageBox( Messages.getString( "PublishLibraryAction.wizard.errorTitle" ), //$NON-NLS-1$
-					Messages.getString( "PublishLibraryAction.wizard.notvalidfolder" ) ); //$NON-NLS-1$
-			return true;
-		}
-		if ( !targetFolder.exists( ) )
-		{
-			targetFolder.mkdirs( );
-		}
-		File targetFile = new File( targetFolder, fileName );
-		if ( new File( filePath ).compareTo( targetFile ) == 0 )
-		{
-			ExceptionHandler.openErrorMessageBox( Messages.getString( "PublishLibraryAction.wizard.errorTitle" ), //$NON-NLS-1$
-					Messages.getString( "PublishLibraryAction.wizard.message" ) ); //$NON-NLS-1$
-			return true;
-		}
 
-		int overwrite = Window.OK;
-		try
-		{
-			if ( targetFile.exists( ) )
-			{
-				String[] buttons = new String[]{
-						IDialogConstants.YES_LABEL,
-						IDialogConstants.NO_LABEL,
-						IDialogConstants.CANCEL_LABEL
-				};
-				String question = Messages.getFormattedString( "SaveAsDialog.overwriteQuestion", //$NON-NLS-1$
-						new Object[]{
-							targetFile.getAbsolutePath( )
-						} );
-				MessageDialog d = new MessageDialog(UIUtil.getDefaultShell( ),
-						Messages.getString( "SaveAsDialog.Question" ), //$NON-NLS-1$
-						null,
-						question,
-						MessageDialog.QUESTION,
-						buttons,
-						0 );
-				overwrite = d.open( );
-			}
-			if ( overwrite == Window.OK
-					&& ( targetFile.exists( ) || ( !targetFile.exists( ) && targetFile.createNewFile( ) ) ) )
-			{
-				copyFile( filePath, targetFile );
-			}
-		}
-		catch ( IOException e )
-		{
-			ExceptionHandler.handle( e );
-		}
+		PublishLibraryWizard publishLibrary = new PublishLibraryWizard( (LibraryHandle) SessionHandleAdapter.getInstance( )
+				.getReportDesignHandle( ),
+				fileName,
+				ReportPlugin.getDefault( ).getResourcePreference( ) );
 
-		return overwrite != 1;	
+		WizardDialog dialog = new WizardDialog( UIUtil.getDefaultShell( ),
+				publishLibrary );
+
+		dialog.setPageSize( 500, 250 );
+		dialog.open( );
 	}
-	
+
 	public void selectionChanged( IAction action, ISelection selection )
 	{
 		action.setEnabled( isEnable( ) ); //$NON-NLS-1$
@@ -143,19 +85,5 @@ public class PublishLibraryAction implements
 		return false;
 
 	}
-	
-	private void copyFile( String in, File targetFile ) throws IOException
-	{
-		FileInputStream fis = new FileInputStream( in );
-		FileOutputStream fos = new FileOutputStream( targetFile );
-		byte[] buf = new byte[1024];
-		int i = 0;
-		while ( ( i = fis.read( buf ) ) != -1 )
-		{
-			fos.write( buf, 0, i );
-		}
-		fis.close( );
-		fos.close( );
-	}
-	
+
 }
