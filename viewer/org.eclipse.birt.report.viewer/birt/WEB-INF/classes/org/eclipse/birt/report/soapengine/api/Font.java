6cc7ab3cc7218a94fce3a9cbf1592592564dcae8
kshen <>
2006-05-26 23:42:09 +0000

    Update soapengine api and soapengine endpoint to reflect the latest changes in WSDL schema ( version 34 )

--- viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/soapengine/api/Font.java
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/soapengine/api/Font.java
@@ -14,6 +14,12 @@ public class Font  implements java.io.Serializable {
     private java.lang.String effect;
     private java.lang.String _super;
     private java.lang.String weight;
+    private java.lang.String color;
+    private java.lang.String backgroundColor;
+    private java.lang.Boolean bold;
+    private java.lang.Boolean italic;
+    private java.lang.Boolean underline;
+    private java.lang.Boolean lineThrough;
 
     public Font() {
     }
@@ -24,13 +30,25 @@ public class Font  implements java.io.Serializable {
            java.lang.Integer size,
            java.lang.String effect,
            java.lang.String _super,
-           java.lang.String weight) {
+           java.lang.String weight,
+           java.lang.String color,
+           java.lang.String backgroundColor,
+           java.lang.Boolean bold,
+           java.lang.Boolean italic,
+           java.lang.Boolean underline,
+           java.lang.Boolean lineThrough) {
            this.family = family;
            this.style = style;
            this.size = size;
            this.effect = effect;
            this._super = _super;
            this.weight = weight;
+           this.color = color;
+           this.backgroundColor = backgroundColor;
+           this.bold = bold;
+           this.italic = italic;
+           this.underline = underline;
+           this.lineThrough = lineThrough;
     }
 
 
@@ -153,6 +171,126 @@ public class Font  implements java.io.Serializable {
         this.weight = weight;
     }
 
+
+    /**
+     * Gets the color value for this Font.
+     * 
+     * @return color
+     */
+    public java.lang.String getColor() {
+        return color;
+    }
+
+
+    /**
+     * Sets the color value for this Font.
+     * 
+     * @param color
+     */
+    public void setColor(java.lang.String color) {
+        this.color = color;
+    }
+
+
+    /**
+     * Gets the backgroundColor value for this Font.
+     * 
+     * @return backgroundColor
+     */
+    public java.lang.String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+
+    /**
+     * Sets the backgroundColor value for this Font.
+     * 
+     * @param backgroundColor
+     */
+    public void setBackgroundColor(java.lang.String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+
+    /**
+     * Gets the bold value for this Font.
+     * 
+     * @return bold
+     */
+    public java.lang.Boolean getBold() {
+        return bold;
+    }
+
+
+    /**
+     * Sets the bold value for this Font.
+     * 
+     * @param bold
+     */
+    public void setBold(java.lang.Boolean bold) {
+        this.bold = bold;
+    }
+
+
+    /**
+     * Gets the italic value for this Font.
+     * 
+     * @return italic
+     */
+    public java.lang.Boolean getItalic() {
+        return italic;
+    }
+
+
+    /**
+     * Sets the italic value for this Font.
+     * 
+     * @param italic
+     */
+    public void setItalic(java.lang.Boolean italic) {
+        this.italic = italic;
+    }
+
+
+    /**
+     * Gets the underline value for this Font.
+     * 
+     * @return underline
+     */
+    public java.lang.Boolean getUnderline() {
+        return underline;
+    }
+
+
+    /**
+     * Sets the underline value for this Font.
+     * 
+     * @param underline
+     */
+    public void setUnderline(java.lang.Boolean underline) {
+        this.underline = underline;
+    }
+
+
+    /**
+     * Gets the lineThrough value for this Font.
+     * 
+     * @return lineThrough
+     */
+    public java.lang.Boolean getLineThrough() {
+        return lineThrough;
+    }
+
+
+    /**
+     * Sets the lineThrough value for this Font.
+     * 
+     * @param lineThrough
+     */
+    public void setLineThrough(java.lang.Boolean lineThrough) {
+        this.lineThrough = lineThrough;
+    }
+
     private java.lang.Object __equalsCalc = null;
     public synchronized boolean equals(java.lang.Object obj) {
         if (!(obj instanceof Font)) return false;
@@ -182,7 +320,25 @@ public class Font  implements java.io.Serializable {
               this._super.equals(other.get_super()))) &&
             ((this.weight==null && other.getWeight()==null) || 
              (this.weight!=null &&
-              this.weight.equals(other.getWeight())));
+              this.weight.equals(other.getWeight()))) &&
+            ((this.color==null && other.getColor()==null) || 
+             (this.color!=null &&
+              this.color.equals(other.getColor()))) &&
+            ((this.backgroundColor==null && other.getBackgroundColor()==null) || 
+             (this.backgroundColor!=null &&
+              this.backgroundColor.equals(other.getBackgroundColor()))) &&
+            ((this.bold==null && other.getBold()==null) || 
+             (this.bold!=null &&
+              this.bold.equals(other.getBold()))) &&
+            ((this.italic==null && other.getItalic()==null) || 
+             (this.italic!=null &&
+              this.italic.equals(other.getItalic()))) &&
+            ((this.underline==null && other.getUnderline()==null) || 
+             (this.underline!=null &&
+              this.underline.equals(other.getUnderline()))) &&
+            ((this.lineThrough==null && other.getLineThrough()==null) || 
+             (this.lineThrough!=null &&
+              this.lineThrough.equals(other.getLineThrough())));
         __equalsCalc = null;
         return _equals;
     }
@@ -211,6 +367,24 @@ public class Font  implements java.io.Serializable {
         }
         if (getWeight() != null) {
             _hashCode += getWeight().hashCode();
+        }
+        if (getColor() != null) {
+            _hashCode += getColor().hashCode();
+        }
+        if (getBackgroundColor() != null) {
+            _hashCode += getBackgroundColor().hashCode();
+        }
+        if (getBold() != null) {
+            _hashCode += getBold().hashCode();
+        }
+        if (getItalic() != null) {
+            _hashCode += getItalic().hashCode();
+        }
+        if (getUnderline() != null) {
+            _hashCode += getUnderline().hashCode();
+        }
+        if (getLineThrough() != null) {
+            _hashCode += getLineThrough().hashCode();
         }
         __hashCodeCalc = false;
         return _hashCode;
@@ -261,6 +435,48 @@ public class Font  implements java.io.Serializable {
         elemField.setFieldName("weight");
         elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "Weight"));
         elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("color");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "Color"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("backgroundColor");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "BackgroundColor"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "string"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("bold");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "Bold"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "boolean"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("italic");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "Italic"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "boolean"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("underline");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "Underline"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "boolean"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("lineThrough");
+        elemField.setXmlName(new javax.xml.namespace.QName("http://schemas.eclipse.org/birt", "LineThrough"));
+        elemField.setXmlType(new javax.xml.namespace.QName("http://www.w3.org/2001/XMLSchema", "boolean"));
         elemField.setMinOccurs(0);
         elemField.setNillable(false);
         typeDesc.addFieldDesc(elemField);
