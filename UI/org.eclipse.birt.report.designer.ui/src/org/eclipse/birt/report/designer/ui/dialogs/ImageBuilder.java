4e29b7effdccc1f3856887090cb7550263bbde03
xizhang <>
2006-06-01 03:04:32 +0000

    -Summary:Tooltips, Edit & DeleteStyle, and Add image in Linux.
    - Bugzilla Bug (s) Resolved:
    134952,143921,144369,144370,144384,144387
    
    - Description:
    134952:[Linux-WS]Add an error embedded image
    143921:[Accessibility]Provide tooltip for B/I/U/S button in Property Editor
    144369:[Accessibility] Expression builder buttons in visibility property/group editor should have tooltips
    144370:[Accessibility] No tooltip for buttons in style editor/property editor->hyperlink
    144384:Modify the style from library is error
    144387:No effect when delete the style from library
    
    
    - Tests Description: Manual
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    - Files Edited:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ImageBuilder.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ImageBuilder.java
@@ -24,6 +24,7 @@ import org.eclipse.birt.report.designer.internal.ui.dialogs.resource.ResourceFil
 import org.eclipse.birt.report.designer.internal.ui.dialogs.resource.ResourceFileLabelProvider;
 import org.eclipse.birt.report.designer.internal.ui.dialogs.resource.ResourceSelectionValidator;
 import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
+import org.eclipse.birt.report.designer.internal.ui.util.HelpContextIds;
 import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
 import org.eclipse.birt.report.designer.internal.ui.util.graphics.BirtImageLoader;
 import org.eclipse.birt.report.designer.internal.ui.util.graphics.ImageCanvas;
@@ -205,6 +206,7 @@ public class ImageBuilder extends BaseDialog
 
 		new Label( topCompostie, SWT.SEPARATOR | SWT.HORIZONTAL ).setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
 
+//		UIUtil.bindHelp( parent,HelpContextIds.IMAGE_BUIDLER_ID ); 
 		return topCompostie;
 	}
 
@@ -536,6 +538,14 @@ public class ImageBuilder extends BaseDialog
 										SWT.ICON_WARNING );
 								return;
 							}
+							
+							if ( checkExtensions( fileName ) == false )
+							{
+								ExceptionHandler.openErrorMessageBox( Messages.getString( "EmbeddedImagesNodeProvider.FileNameError.Title" ),
+										Messages.getString( "EmbeddedImagesNodeProvider.FileNameError.Message" ) );
+								return;
+							}
+							
 							previewCanvas.loadImage( fullPath );
 							BirtImageLoader imageLoader = new BirtImageLoader( );
 							EmbeddedImage image = imageLoader.save( getModuleHandle( ),
@@ -876,4 +886,16 @@ public class ImageBuilder extends BaseDialog
 		return dataSetList;
 	}
 
+	private boolean checkExtensions( String fileName )
+	{
+		for ( int i = 0; i < IMAGE_TYPES.length; i++ )
+		{
+			if ( fileName.endsWith( IMAGE_TYPES[i] ) )
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
