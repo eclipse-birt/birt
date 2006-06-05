6f05b4719e15430728b6ff988769ec4f324505f5
Jun Zhai <jzhai@actuate.com>
2006-06-05 22:04:59 +0000

    Remove unnecessary call to get page count.

--- viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/service/actionhandler/AbstractChangeParameterActionHandler.java
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/service/actionhandler/AbstractChangeParameterActionHandler.java
@@ -4,12 +4,21 @@ package org.eclipse.birt.report.service.actionhandler;
 import java.rmi.RemoteException;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.namespace.QName;
+
+import org.apache.axis.AxisFault;
+import org.eclipse.birt.report.IBirtConstants;
 import org.eclipse.birt.report.context.BaseAttributeBean;
 import org.eclipse.birt.report.context.IContext;
+import org.eclipse.birt.report.resource.BirtResources;
+import org.eclipse.birt.report.resource.ResourceConstants;
 import org.eclipse.birt.report.service.api.InputOptions;
+import org.eclipse.birt.report.service.api.OutputOptions;
 import org.eclipse.birt.report.service.api.ReportServiceException;
 import org.eclipse.birt.report.soapengine.api.GetUpdatedObjectsResponse;
 import org.eclipse.birt.report.soapengine.api.Operation;
+import org.eclipse.birt.report.soapengine.api.Oprand;
 
 public abstract class AbstractChangeParameterActionHandler
 		extends
@@ -78,4 +87,113 @@ public abstract class AbstractChangeParameterActionHandler
 			boolean svgFlag, boolean isMasterContent, boolean useBookmark,
 			String bookmark, Locale locale, boolean isRtl )
 			throws ReportServiceException, RemoteException;
+	
+	/**
+	 * Check whether the page number is valid or not.
+	 * 
+	 * @param pageNumber
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 * @throws ReportServiceException
+	 */
+	protected boolean isValidPageNumber( HttpServletRequest request,
+			long pageNumber, String documentName ) throws RemoteException,
+			ReportServiceException
+	{
+		InputOptions options = new InputOptions( );
+		options.setOption( InputOptions.OPT_REQUEST, request );
+		return pageNumber > 0
+				&& pageNumber <= getReportService( ).getPageCount(
+						documentName, options, new OutputOptions( ) );
+	}
+
+	/**
+	 * Get page number from incoming soap request.
+	 * 
+	 * @param params
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 * @throws ReportServiceException
+	 */
+	protected long getPageNumber( HttpServletRequest request, Oprand[] params,
+			String documentName ) throws RemoteException,
+			ReportServiceException
+	{
+		long pageNumber = -1;
+		if ( params != null && params.length > 0 )
+		{
+			for ( int i = 0; i < params.length; i++ )
+			{
+				if ( IBirtConstants.OPRAND_PAGENO.equalsIgnoreCase( params[i]
+						.getName( ) ) )
+				{
+					try
+					{
+						pageNumber = Integer.parseInt( params[i].getValue( ) );
+					}
+					catch ( NumberFormatException e )
+					{
+						pageNumber = -1;
+					}
+					InputOptions options = new InputOptions( );
+					options.setOption( InputOptions.OPT_REQUEST, request );
+					if ( pageNumber <= 0
+							|| pageNumber > getReportService( )
+									.getPageCount( documentName, options,
+											new OutputOptions( ) ) )
+					{
+						AxisFault fault = new AxisFault( );
+						fault.setFaultCode( new QName(
+								"DocumentProcessor.getPageNumber( )" ) ); //$NON-NLS-1$
+						fault
+								.setFaultString( BirtResources
+										.getString( ResourceConstants.ACTION_EXCEPTION_INVALID_PAGE_NUMBER ) );
+						throw fault;
+					}
+
+					break;
+				}
+			}
+		}
+
+		return pageNumber;
+	}
+
+	/**
+	 * Get page number by bookmark.
+	 * 
+	 * @param params
+	 * @param bean
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 */
+	protected String getBookmark( Oprand[] params, BaseAttributeBean bean )
+	{
+		assert bean != null;
+
+		String bookmark = null;
+		if ( params != null && params.length > 0 )
+		{
+			for ( int i = 0; i < params.length; i++ )
+			{
+				if ( IBirtConstants.OPRAND_BOOKMARK.equalsIgnoreCase( params[i]
+						.getName( ) ) )
+				{
+					bookmark = params[i].getValue( );
+					break;
+				}
+			}
+		}
+
+		// Then use url bookmark.
+		if ( bookmark == null || bookmark.length( ) <= 0 )
+		{
+			bookmark = bean.getBookmark( );
+		}
+
+		return bookmark;
+	}
 }
--- viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/service/actionhandler/AbstractGetPageActionHandler.java
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/service/actionhandler/AbstractGetPageActionHandler.java
@@ -5,7 +5,11 @@ import java.io.ByteArrayOutputStream;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.namespace.QName;
+
 import org.apache.axis.AxisFault;
+import org.eclipse.birt.report.IBirtConstants;
 import org.eclipse.birt.report.context.BaseAttributeBean;
 import org.eclipse.birt.report.context.IContext;
 import org.eclipse.birt.report.resource.BirtResources;
@@ -16,6 +20,7 @@ import org.eclipse.birt.report.service.api.ReportServiceException;
 import org.eclipse.birt.report.soapengine.api.Data;
 import org.eclipse.birt.report.soapengine.api.GetUpdatedObjectsResponse;
 import org.eclipse.birt.report.soapengine.api.Operation;
+import org.eclipse.birt.report.soapengine.api.Oprand;
 import org.eclipse.birt.report.soapengine.api.Page;
 import org.eclipse.birt.report.soapengine.api.Update;
 import org.eclipse.birt.report.soapengine.api.UpdateContent;
@@ -34,6 +39,8 @@ public abstract class AbstractGetPageActionHandler
 
 	protected long __totalPageNumber;
 
+	protected boolean __isCompleted = true;
+	
 	protected boolean __useBookmark = false;
 
 	protected String __bookmark;
@@ -92,6 +99,22 @@ public abstract class AbstractGetPageActionHandler
 		__bean = context.getBean( );
 		__docName = __getReportDocument( );
 		__checkDocumentExists( );
+		
+		// Get total page count.
+		InputOptions getPageCountOptions = new InputOptions( );
+		getPageCountOptions.setOption( InputOptions.OPT_LOCALE, __bean.getLocale( ) );
+		getPageCountOptions.setOption( InputOptions.OPT_RTL, new Boolean( __bean.isRtl( ) ) );
+		getPageCountOptions.setOption( InputOptions.OPT_REQUEST, context.getRequest( ) );
+		OutputOptions outputOptions = new OutputOptions( );
+		__totalPageNumber = getReportService( ).getPageCount( __docName, getPageCountOptions,
+				outputOptions );
+		Boolean isCompleted = ( Boolean ) outputOptions.getOption(
+				OutputOptions.OPT_REPORT_GENERATION_COMPLETED );
+		if ( isCompleted != null )
+		{
+			__isCompleted = isCompleted.booleanValue( );
+		}
+
 		__bookmark = getBookmark( operation.getOprand( ), __bean );
 
 		__pageNumber = getPageNumber( context.getRequest( ), operation
@@ -145,8 +168,6 @@ public abstract class AbstractGetPageActionHandler
 		__activeIds = new ArrayList( );
 		__page = getReportService( ).getPage( __docName, __pageNumber + "", //$NON-NLS-1$
 				options, __activeIds );
-		__totalPageNumber = getReportService( ).getPageCount( __docName,
-				options, new OutputOptions( ) );
 	}
 
 	protected void prepareResponse( ) throws ReportServiceException,
@@ -177,5 +198,106 @@ public abstract class AbstractGetPageActionHandler
 		updateNavbar.setUpdateData( updateData );
 
 		response.setUpdate( new Update[]{updateDocument, updateNavbar} );
+	}
+	
+	/**
+	 * Check whether the page number is valid or not.
+	 * 
+	 * @param pageNumber
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 * @throws ReportServiceException
+	 */
+	protected boolean isValidPageNumber( HttpServletRequest request,
+			long pageNumber, String documentName ) throws RemoteException,
+			ReportServiceException
+	{
+		InputOptions options = new InputOptions( );
+		options.setOption( InputOptions.OPT_REQUEST, request );
+		return pageNumber > 0 && pageNumber <= __totalPageNumber;
+	}
+
+	/**
+	 * Get page number from incoming soap request.
+	 * 
+	 * @param params
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 * @throws ReportServiceException
+	 */
+	protected long getPageNumber( HttpServletRequest request, Oprand[] params,
+			String documentName ) throws RemoteException,
+			ReportServiceException
+	{
+		long pageNumber = -1;
+		if ( params != null && params.length > 0 )
+		{
+			for ( int i = 0; i < params.length; i++ )
+			{
+				if ( IBirtConstants.OPRAND_PAGENO.equalsIgnoreCase( params[i]
+						.getName( ) ) )
+				{
+					try
+					{
+						pageNumber = Integer.parseInt( params[i].getValue( ) );
+					}
+					catch ( NumberFormatException e )
+					{
+						pageNumber = -1;
+					}
+					if ( pageNumber <= 0 || pageNumber > __totalPageNumber )
+					{
+						AxisFault fault = new AxisFault( );
+						fault.setFaultCode( new QName(
+								"DocumentProcessor.getPageNumber( )" ) ); //$NON-NLS-1$
+						fault.setFaultString( BirtResources
+								.getString( ResourceConstants.ACTION_EXCEPTION_INVALID_PAGE_NUMBER ) );
+						throw fault;
+					}
+
+					break;
+				}
+			}
+		}
+
+		return pageNumber;
+	}
+
+	/**
+	 * Get page number by bookmark.
+	 * 
+	 * @param params
+	 * @param bean
+	 * @param document
+	 * @return
+	 * @throws RemoteException
+	 */
+	protected String getBookmark( Oprand[] params, BaseAttributeBean bean )
+	{
+		assert bean != null;
+
+		String bookmark = null;
+		if ( params != null && params.length > 0 )
+		{
+			for ( int i = 0; i < params.length; i++ )
+			{
+				if ( IBirtConstants.OPRAND_BOOKMARK.equalsIgnoreCase( params[i]
+						.getName( ) ) )
+				{
+					bookmark = params[i].getValue( );
+					break;
+				}
+			}
+		}
+
+		// Then use url bookmark.
+		if ( bookmark == null || bookmark.length( ) <= 0 )
+		{
+			bookmark = bean.getBookmark( );
+		}
+
+		return bookmark;
 	}
 }
