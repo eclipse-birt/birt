dbe11c697e497a40645700e59de42f6a039fda31
xli <>
2006-06-08 09:18:44 +0000

    Fix Bugzilla Bug 145749

---
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/presentation/aggregation/layout/ParameterFragment.java
@@ -0,0 +1,34 @@
+/*************************************************************************************
+ * Copyright (c) 2004 Actuate Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Actuate Corporation - Initial implementation.
+ ************************************************************************************/
+
+package org.eclipse.birt.report.presentation.aggregation.layout;
+
+import org.eclipse.birt.report.presentation.aggregation.BirtBaseFragment;
+import org.eclipse.birt.report.presentation.aggregation.control.ProgressBarFragment;
+import org.eclipse.birt.report.presentation.aggregation.dialog.DialogContainerFragment;
+import org.eclipse.birt.report.presentation.aggregation.dialog.ParameterDialogFragment;
+
+/**
+ * Fragment for parameter dialog.
+ * <p>
+ * @see BaseFragment
+ */
+public class ParameterFragment extends BirtBaseFragment
+{
+	/**
+	 * Build fragment by adding parameter dialog fragment as child.
+	 */
+	protected void build( )
+	{
+		addChild( new ProgressBarFragment( ) );
+		addChild( new DialogContainerFragment( new ParameterDialogFragment( ) ) );
+	}
+}
--- viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/presentation/aggregation/layout/RequesterFragment.java
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/presentation/aggregation/layout/RequesterFragment.java
@@ -7,11 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.birt.report.presentation.aggregation.BirtBaseFragment;
-import org.eclipse.birt.report.presentation.aggregation.dialog.DialogContainerFragment;
-import org.eclipse.birt.report.presentation.aggregation.dialog.ExceptionDialogFragment;
-import org.eclipse.birt.report.presentation.aggregation.dialog.ParameterDialogFragment;
-import org.eclipse.birt.report.presentation.aggregation.layout.DocumentFragment;
-
 
 public class RequesterFragment extends BirtBaseFragment
 {
@@ -30,9 +25,7 @@ public class RequesterFragment extends BirtBaseFragment
 	 * Override build method.
 	 */
 	protected void build( )
-	{
-		addChild( new DocumentFragment( ) );
-		addChild( new DialogContainerFragment( new ExceptionDialogFragment( ) ) );
-		addChild( new DialogContainerFragment( new ParameterDialogFragment( ) ) );
+	{		
+		addChild( new ParameterFragment( ) );
 	}
 }
