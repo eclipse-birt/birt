fbf231a1093eb81b52a67fd729e23fda4275d7cb
zwei <>
2006-06-16 08:19:29 +0000

    write document version as *2.1.0* instead of *1.2.1.RC4*

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/ReportDocumentWriter.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/ReportDocumentWriter.java
@@ -315,7 +315,7 @@ public class ReportDocumentWriter implements ReportDocumentConstants
 				IOUtil.writeString( coreStream, REPORT_DOCUMENT_TAG );
 				//in BIRT 2.1.0.RC6 and final, use the document version *2.1.0*
 				//use document version *1.2.1.RC4*, only in 2.1.0.RC5
-				IOUtil.writeString( coreStream, REPORT_DOCUMENT_VERSION_1_2_1_RC4 );
+				IOUtil.writeString( coreStream, REPORT_DOCUMENT_VERSION_2_1_0 );
 				IOUtil.writeString( coreStream, designName );
 				IOUtil.writeMap( coreStream, paramters );
 				IOUtil.writeMap( coreStream, globalVariables );
