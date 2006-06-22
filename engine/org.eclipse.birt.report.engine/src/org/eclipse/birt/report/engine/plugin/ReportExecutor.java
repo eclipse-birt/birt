545b6c78f5a01186748904199c4486fe1745fecb
Wei Yan <wyan@actuate.com>
2006-06-22 01:14:33 +0000

    remove extra arguments added by the eclipse platform

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/plugin/ReportExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/plugin/ReportExecutor.java
@@ -19,9 +19,25 @@ public class ReportExecutor implements IPlatformRunnable
 
 	public Object run( Object args ) throws Exception
 	{
+		// as we know that the last argument must be a report design
+		// or report document, so remove any extra arguments which
+		// added by the eclipse platform (which is started with '-').
 		if ( args instanceof String[] )
 		{
-			new ReportRunner( (String[]) args ).execute( );
+			String[] platformArgs = (String[]) args;
+			int appArgLength = platformArgs.length;
+			for ( ; appArgLength > 0; appArgLength-- )
+			{
+				String arg = platformArgs[appArgLength - 1];
+				if ( arg.charAt( 0 ) != '-' )
+				{
+					break;
+				}
+			}
+
+			String[] appArgs = new String[appArgLength];
+			System.arraycopy( args, 0, appArgs, 0, appArgLength );
+			new ReportRunner( appArgs ).execute( );
 		}
 		return null;
 	}
