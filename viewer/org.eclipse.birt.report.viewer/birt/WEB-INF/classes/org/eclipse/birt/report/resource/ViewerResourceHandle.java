981fe1286c40ec8962cd2e86ad05ee83077b4ce0
Yu Li <yli@actuate.com>
2006-07-25 05:30:39 +0000

    Change the Viewer Birt resources to handle multiple resource locales.

---
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/resource/ViewerResourceHandle.java
@@ -0,0 +1,24 @@
+
+package org.eclipse.birt.report.resource;
+
+import java.util.Locale;
+
+import org.eclipse.birt.core.i18n.ResourceHandle;
+
+public class ViewerResourceHandle extends ResourceHandle
+{
+
+	/**
+	 * Constructs the resource handle with a specific resource bundle, which is
+	 * associated with locale.
+	 * 
+	 * @param locale
+	 *            the locale of <code>ULocale</code> type
+	 */
+
+	public ViewerResourceHandle( Locale locale )
+	{
+		super( locale );
+	}
+
+}
