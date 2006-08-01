ad0d52ad3783a781b07d4921d8016404667706e4
Lin Zhu <lzhu@actuate.com>
2006-08-01 09:32:31 +0000

    Make the Unit Test of data adapter runnable throw build file.

---
+++ data/org.eclipse.birt.report.data.adapter/test/org/eclipse/birt/report/data/adapter/AllTests.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.data.adapter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test suit for data adapter.
+ */
+public class AllTests
+{
+	
+	/**
+	 * @return
+	 */
+	public static Test suite( )
+	{
+		TestSuite suite = new TestSuite( "Test for org.eclipse.birt.report.data.adapter" );
+		
+		suite.addTestSuite( org.eclipse.birt.report.data.adapter.internal.script.DataAdapterTopLevelScopeTest.class );
+		
+		return suite;
+	}
+	
+}
