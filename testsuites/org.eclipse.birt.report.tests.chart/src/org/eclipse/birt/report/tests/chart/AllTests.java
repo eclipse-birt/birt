17a669c830fd076fc958e0adedf999e62334a798
cyu <>
2006-08-01 04:48:17 +0000

    Minor changes

--- testsuites/org.eclipse.birt.report.tests.chart/src/org/eclipse/birt/report/tests/chart/AllTests.java
+++ testsuites/org.eclipse.birt.report.tests.chart/src/org/eclipse/birt/report/tests/chart/AllTests.java
@@ -1,21 +1,24 @@
 
 package org.eclipse.birt.report.tests.chart;
 
-import org.eclipse.birt.report.tests.chart.acceptance.*;
-import org.eclipse.birt.report.tests.chart.smoke.*;
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
-public class AllTests
+/**
+ *
+ *  All test cases for tests chart
+ */
+public class AllTests 
 {
-
-	public static void main( String[] args )
+	public static Test suite( )
 	{
-		AllTests all = new AllTests( );
-	}
-
-	AllTests( )
-	{
+		TestSuite suite = new TestSuite( "Test for org.eclipse.birt.report.tests.chart"); //$NON-NLS-1$
+		suite.addTestSuite( org.eclipse.birt.report.tests.chart.acceptance.LineChart_3D.class );
+		return suite;
+		
+		/*
 		AreaChart_3D st1 = new AreaChart_3D( );
-		LineChart_3D st2 = new LineChart_3D( );
+		
 		OverlayAreaChart_2D st3 = new OverlayAreaChart_2D( );
 		OverlayAreaChart_2DWithDepth st4 = new OverlayAreaChart_2DWithDepth( );
 		OverlayLineChart_2D st5 = new OverlayLineChart_2D( );
@@ -41,42 +44,6 @@ public class AllTests
 		StandardMeterChart st25 = new StandardMeterChart( );
 		StockChart st26 = new StockChart( );
 		SuperimposedMeterChart st27 = new SuperimposedMeterChart( );
-		
-		/*
-		 * Create3DBarChart_1 st31 = new Create3DBarChart_1();
-		 * Create3DBarChart_2 st32 = new Create3DBarChart_2();
-		 * Create3DLineChart_1 st33 = new Create3DLineChart_1();
-		 * Create3DLineChart_2 st34 = new Create3DLineChart_2();
-		 * CreateAreaChart_1 st35 = new CreateAreaChart_1(); CreateAreaChart_2
-		 * st36 = new CreateAreaChart_2(); CreateAreaChart_3 st37 = new
-		 * CreateAreaChart_3(); CreateBarChart_1 st38 = new CreateBarChart_1();
-		 * CreateBarChart_2 st39 = new CreateBarChart_2(); CreateBarChart_3 st40 =
-		 * new CreateBarChart_3(); CreateBarChart_4 st41 = new
-		 * CreateBarChart_4(); CreateCFAreaChart_1 st42 = new
-		 * CreateCFAreaChart_1(); CreateCFAreaChart_2 st43 = new
-		 * CreateCFAreaChart_2(); CreateCFBarChart_1 st44 = new
-		 * CreateCFBarChart_1(); CreateCFBarChart_2 st45 = new
-		 * CreateCFBarChart_2(); CreateCFLineChart_1 st46 = new
-		 * CreateCFLineChart_1(); CreateCFLineChart_2 st47 = new
-		 * CreateCFLineChart_2(); CreateCFLineChart_3 st48 = new
-		 * CreateCFLineChart_3(); CreateCFLineChart_4 st49 = new
-		 * CreateCFLineChart_4(); CreateCFStockChart_1 st50 = new
-		 * CreateCFStockChart_1(); CreateLineChart_1 st51 = new
-		 * CreateLineChart_1(); CreateLineChart_2 st52 = new
-		 * CreateLineChart_2(); CreateLineChart_3 st53 = new
-		 * CreateLineChart_3(); CreateMeterChart_1 st54 = new
-		 * CreateMeterChart_1(); CreateMeterChart_2 st55 = new
-		 * CreateMeterChart_2(); CreateMeterChart_3 st56 = new
-		 * CreateMeterChart_3(); CreatePieChart_1 st57 = new CreatePieChart_1();
-		 * CreatePieChart_2 st58 = new CreatePieChart_2(); CreatePieChart_3 st59 =
-		 * new CreatePieChart_3(); CreateScatterChart_1 st60 = new
-		 * CreateScatterChart_1(); CreateScatterChart_2 st61 = new
-		 * CreateScatterChart_2(); CreateStockChart_1 st62 = new
-		 * CreateStockChart_1(); CreateStockChart_2 st63 = new
-		 * CreateStockChart_2(); LabelStaggering_1 st64 = new
-		 * LabelStaggering_1(); LegendTitle_1 st65 = new LegendTitle_1();
-		 * LegendTitle_2 st66 = new LegendTitle_2(); MarkerShape_1 st67 = new
-		 * MarkerShape_1();
-		 */
+		*/
 	}
 }
