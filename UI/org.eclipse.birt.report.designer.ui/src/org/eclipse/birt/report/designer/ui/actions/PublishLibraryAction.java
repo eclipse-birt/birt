fb6513c80373d4051a54d769ef954b2fdb7aebbd
hlin <>
2006-08-18 03:42:07 +0000

    - Summary: Fix Bugzilla bug 153225-'Publish library in resource folder' should be available even if the library is not opened in editor.
    - Bugzilla Bug (s) Resolved: 153225
    
    - Description:
    Makes the "Publish library in resource folder" option available from File menu when library is selected from the Resource Navigator. And if a library is opened in layout editor, this library will be published when invoking publish library action no matter which library is selected from Navigator then.
    
    - Tests Description: Manual test and code review.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    @addlist
    
    - Files Edited:
    @changelist

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/actions/PublishLibraryAction.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/actions/PublishLibraryAction.java
@@ -14,16 +14,22 @@ package org.eclipse.birt.report.designer.ui.actions;
 import java.io.File;
 
 import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
+import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
 import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
 import org.eclipse.birt.report.designer.internal.ui.wizards.PublishLibraryWizard;
 import org.eclipse.birt.report.designer.ui.ReportPlugin;
 import org.eclipse.birt.report.model.api.LibraryHandle;
+import org.eclipse.birt.report.model.api.ModuleHandle;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.views.navigator.ResourceNavigator;
 
 /**
  * 
@@ -31,6 +37,8 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 public class PublishLibraryAction implements IWorkbenchWindowActionDelegate
 {
+
+	private ResourceNavigator navigator;
 
 	public void dispose( )
 	{
@@ -41,7 +49,8 @@ public class PublishLibraryAction implements IWorkbenchWindowActionDelegate
 	public void init( IWorkbenchWindow window )
 	{
 		// TODO Auto-generated method stub
-
+		IViewPart view = UIUtil.getView( "org.eclipse.ui.views.ResourceNavigator" );
+		navigator = (ResourceNavigator) view;
 	}
 
 	/*
@@ -51,19 +60,66 @@ public class PublishLibraryAction implements IWorkbenchWindowActionDelegate
 	 */
 	public void run( IAction action )
 	{
-
+		String fileName = null;
+		LibraryHandle libHandle = null;
 		if ( isEnable( ) == false )
 		{
 			return;
 		}
 
-		String filePath = SessionHandleAdapter.getInstance( )
-				.getReportDesignHandle( )
-				.getFileName( );
-		String fileName = filePath.substring( filePath.lastIndexOf( File.separator ) + 1 );
+		if ( editLibrary( ) )
+		{
+			String filePath = SessionHandleAdapter.getInstance( )
+					.getReportDesignHandle( )
+					.getFileName( );
+			fileName = filePath.substring( filePath.lastIndexOf( File.separator ) + 1 );
+			libHandle = (LibraryHandle) SessionHandleAdapter.getInstance( )
+					.getReportDesignHandle( );
+		}
+		else if ( selectLibrary( ) )
+		{
+			IFile file = getSelectedFile( navigator );
+			if ( file == null )
+			{
+				return;
+			}
 
-		PublishLibraryWizard publishLibrary = new PublishLibraryWizard( (LibraryHandle) SessionHandleAdapter.getInstance( )
-				.getReportDesignHandle( ),
+			String url = file.getLocation( ).toOSString( );
+			ModuleHandle handle = null;
+			try
+			{
+				handle = SessionHandleAdapter.getInstance( )
+						.getSessionHandle( )
+						.openLibrary( url );
+
+				if ( !( handle instanceof LibraryHandle ) )
+				{
+					action.setEnabled( false );
+					return;
+				}
+
+				String filePath = handle.getFileName( );
+				if ( filePath != null && filePath.length( ) != 0 )
+				{
+					fileName = filePath.substring( filePath.lastIndexOf( File.separator ) + 1 );
+				}
+				libHandle = (LibraryHandle) handle;
+			}
+			catch ( Exception e )
+			{
+				ExceptionHandler.handle( e );
+				return;
+			}
+			finally
+			{
+				if ( handle != null )
+				{
+					handle.close( );
+				}
+			}
+		}
+
+		PublishLibraryWizard publishLibrary = new PublishLibraryWizard( libHandle,
 				fileName,
 				ReportPlugin.getDefault( ).getResourcePreference( ) );
 
@@ -88,13 +144,50 @@ public class PublishLibraryAction implements IWorkbenchWindowActionDelegate
 
 	private boolean isEnable( )
 	{
+		return ( selectLibrary( ) || editLibrary( ) );
+
+	}
+
+	private boolean selectLibrary( )
+	{
+		IFile file = getSelectedFile( navigator );
+		if ( file == null )
+		{
+			return false;
+		}
+
+		if ( file.getFileExtension( ).equals( "rptlibrary" ) )
+			return true;
+		return false;
+	}
+
+	private boolean editLibrary( )
+	{
 		IEditorPart editor = UIUtil.getActiveEditor( true );
 		if ( editor != null )
 		{
 			return ( editor.getEditorInput( ).getName( ).endsWith( ".rptlibrary" ) ); //$NON-NLS-1$
 		}
 		return false;
-
 	}
 
+	/**
+	 * Get selected file.
+	 * 
+	 * @return IFile Selected file
+	 */
+	private IFile getSelectedFile( ResourceNavigator navigator )
+	{
+		if ( navigator != null )
+		{
+			IStructuredSelection selection = (IStructuredSelection) navigator.getTreeViewer( )
+					.getSelection( );
+			if ( selection.size( ) == 1
+					&& selection.getFirstElement( ) instanceof IFile )
+			{
+				return (IFile) selection.getFirstElement( );
+			}
+		}
+		return null;
+	}
 }
