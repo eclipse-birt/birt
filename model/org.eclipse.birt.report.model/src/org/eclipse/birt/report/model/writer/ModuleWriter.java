f0b75666329ff3070dc1ff0bf0f625bf7821340d
jliu <>
2006-08-24 06:20:58 +0000

    Refine fragment search.

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/writer/ModuleWriter.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/writer/ModuleWriter.java
@@ -1046,7 +1046,6 @@ public abstract class ModuleWriter extends ElementVisitor
 		property( obj, Module.UNITS_PROP );
 		property( obj, Module.BASE_PROP );
 		property( obj, Module.INCLUDE_RESOURCE_PROP );
-		property( obj, Module.SYMBOLIC_NAME_PROP );
 
 		resourceKey( obj, Module.TITLE_ID_PROP, Module.TITLE_PROP );
 		property( obj, Module.COMMENTS_PROP );
