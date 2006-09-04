54c40b63d6738b4e5988fe1219039e7ef591a176
Rick Lu <rlu@actuate.com>
2006-09-04 09:24:27 +0000

    Added report version constants.

---
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/ModelConstants.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.api;
+
+import org.eclipse.birt.report.model.parser.DesignSchemaConstants;
+
+/**
+ * Defines various constants.
+ * 
+ */
+
+public class ModelConstants
+{
+
+	/**
+	 * The version of the report design. Whenever the design is parsed or
+	 * serialized, the version number is this value.
+	 */
+
+	public final static String REPORT_VERSION = DesignSchemaConstants.REPORT_VERSION;
+
+}
