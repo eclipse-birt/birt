ae58e54c246fc12a321bdbbcb4bc0845a3715e2c
zwei <>
2006-09-08 05:58:43 +0000

    remove the assert in checking the value expression of drillthrough to be a String instance.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/data/dte/ReportQueryBuilder.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/data/dte/ReportQueryBuilder.java
@@ -99,7 +99,7 @@ import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
  * visit the report design and prepare all report queries and sub-queries to
  * send to data engine
  * 
- * @version $Revision: 1.82 $ $Date: 2006/08/25 03:49:23 $
+ * @version $Revision: 1.82.2.1 $ $Date: 2006/09/07 12:56:57 $
  */
 public class ReportQueryBuilder
 {
@@ -1515,7 +1515,6 @@ public class ReportQueryBuilder
 								while ( ite.hasNext( ) )
 								{
 									Map.Entry entry = (Map.Entry) ite.next( );
-									assert entry.getValue( ) instanceof String;
 									entry.setValue( ( String ) newExpressions.get( expressionIndex++ ) );
 								}
 							}
@@ -1603,7 +1602,6 @@ public class ReportQueryBuilder
 								while ( ite.hasNext( ) )
 								{
 									Map.Entry entry = (Map.Entry) ite.next( );
-									assert entry.getValue( ) instanceof String;
 									expressions.add( entry.getValue( ) );
 								}
 							}
