bf7ee0f9da0abc12734355c5f8aa1350d4eb9ab3
shu <>
2006-09-22 07:38:49 +0000

    update case due to difference source in head and branch2.1.1

--- testsuites/org.eclipse.birt.report.tests.model/src/org/eclipse/birt/report/tests/model/regression/Regression_155509.java
+++ testsuites/org.eclipse.birt.report.tests.model/src/org/eclipse/birt/report/tests/model/regression/Regression_155509.java
@@ -31,32 +31,32 @@ import org.eclipse.birt.report.tests.model.BaseTestCase;
  */
 public class Regression_155509 extends BaseTestCase
 {
+
 	private String filename = "Regression_155509.xml"; //$NON-NLS-1$
 	private String propfile = "Regression_155509"; //$NON-NLS-1$
 	private String jarfile = "input/Regression_155509.jar"; //$NON-NLS-1$
 	private DefaultResourceLocator rl;
 
-
 	/**
-	 * @throws DesignFileException 
+	 * @throws DesignFileException
 	 */
 	public void test_155509( ) throws DesignFileException
 	{
 		openDesign( filename );
 		rl = new DefaultResourceLocator( );
 		sessionHandle.setResourceFolder( getClassFolder( ) );
-		
-		URL jarrsc = rl.findResource( designHandle,jarfile, IResourceLocator.JAR_FILE );
-		assertNotNull(jarrsc);
-		
-		
-		sessionHandle.setResourceFolder( getClassFolder( )+ INPUT_FOLDER );
-		
-		URL messagersc = rl.findResource( designHandle,propfile, IResourceLocator.MESSAGE_FILE  );
-		assertNotNull(messagersc);
-		
-	
-	   
+
+		URL jarrsc = rl.findResource( designHandle, jarfile,
+				IResourceLocator.JAR_FILE );
+		assertNotNull( jarrsc );
+
+		sessionHandle.setResourceFolder( getClassFolder( ) + INPUT_FOLDER );
+
+		//TODO: source are not update in branch2.1.1. 
+		// URL messagersc = rl.findResource( designHandle,propfile,
+		// IResourceLocator.MESSAGE_FILE );
+		// assertNotNull(messagersc);
+
 	}
-	
+
 }
