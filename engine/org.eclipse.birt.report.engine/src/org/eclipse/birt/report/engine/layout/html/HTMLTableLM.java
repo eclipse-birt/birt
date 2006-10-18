297b24f4fa8ed5dabef6d803a2a33acc637b3d25
Jun Ouyang <jouyang@actuate.com>
2006-10-18 03:20:17 +0000

    Fix bug 157411: Output container in the same page as the first child

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/html/HTMLTableLM.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/html/HTMLTableLM.java
@@ -59,7 +59,7 @@ public class HTMLTableLM extends HTMLBlockStackingLM
 					boolean skipPageHint = context.getSkipPageHint( );
 					context.setAllowPageBreak( pageBreak );
 					context.setSkipPageHint( true );
-					engine.layout( header, emitter );
+					engine.layout(this, header, emitter );
 					context.setAllowPageBreak( pageBreak );
 					context.setSkipPageHint( skipPageHint );
 				}
