d303e09d4ca5256aa0d5707ca3d7d773fdfbdd59
wwang <>
2006-10-26 02:38:38 +0000

    first checkin of the plugin *org.eclipse.birt.report.engine.emitter.pdf.tests*

---
+++ engine/org.eclipse.birt.report.engine.emitter.pdf.tests/test/org/eclipse/birt/report/engine/emitter/pdf/PDFEmitterTest.java
@@ -0,0 +1,12 @@
+package org.eclipse.birt.report.engine.emitter.pdf;
+
+import junit.framework.TestCase;
+
+public class PDFEmitterTest extends TestCase 
+{
+	public void test()
+	{
+		assertTrue(true);
+	}
+
+}
