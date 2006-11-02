9ee16233f2f6fb4c115d2753295709b205ec470a
xli <>
2006-11-02 02:53:34 +0000

    Add a fragment project for all model test util classes and resources.

---
+++ model/org.eclipse.birt.report.model.testhelper/test/org/eclipse/birt/report/model/activity/ActivityTestUtil.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.activity;
+
+import java.util.List;
+
+/**
+ * Wrapper to return internal vairable for tests under independent plug-in
+ * "model.tests".
+ */
+
+public class ActivityTestUtil
+{
+
+	/**
+	 * Wrapper method to return internal variable listener in activity stack.
+	 * 
+	 * @param as
+	 *            activity stack
+	 * @return listeners in activity stack.
+	 */
+
+	public static List getActivityListener( ActivityStack as )
+	{
+		return as.listeners;
+	}
+}
