8d68a700e393e47b5e58ebabe178e4e6e45254ec
iliu <>
2007-01-31 02:46:53 +0000

    for fixed the bug 168131.Blank lines missing in PDF

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/PDFTextLM.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/PDFTextLM.java
@@ -29,6 +29,7 @@ import org.eclipse.birt.report.engine.layout.area.IArea;
 import org.eclipse.birt.report.engine.layout.area.impl.AbstractArea;
 import org.eclipse.birt.report.engine.layout.area.impl.AreaFactory;
 import org.eclipse.birt.report.engine.layout.area.impl.ContainerArea;
+import org.eclipse.birt.report.engine.layout.pdf.font.FontHandler;
 import org.eclipse.birt.report.engine.layout.pdf.font.FontInfo;
 import org.eclipse.birt.report.engine.layout.pdf.hyphen.DefaultHyphenationManager;
 import org.eclipse.birt.report.engine.layout.pdf.hyphen.DefaultWordRecognizer;
@@ -64,6 +65,8 @@ public class PDFTextLM extends PDFLeafItemLM implements ITextLayoutManager
 	private Compositor comp = null;
 
 	private ITextContent textContent = null;
+	
+	private static final char MaxHighChar ='P'; 
 	
 	public PDFTextLM( PDFLayoutEngineContext context, PDFStackingLM parent,
 			IContent content, IReportItemExecutor executor )
@@ -268,8 +271,8 @@ public class PDFTextLM extends PDFLeafItemLM implements ITextLayoutManager
 
 		private boolean hasMore()
 		{
-			if (cg.hasMore())
-				return true;
+			if (cg.hasMore())							
+				return true;				
 			else if ( null == chunk )
 				return false;
 			else
@@ -308,6 +311,14 @@ public class PDFTextLM extends PDFLeafItemLM implements ITextLayoutManager
 					chunk = cg.getNext();
 					if (chunk == Chunk.HARD_LINE_BREAK)
 					{
+						FontHandler handler = new FontHandler(content);
+						handler.selectFont(MaxHighChar);
+						ContainerArea con = (ContainerArea)createInlineContainer(content, false, true);
+						con.setWidth(0);
+						con.setHeight( (int)(handler.getFontInfo().getWordHeight()*PDFConstants.LAYOUT_TO_PDF_RATIO)
+								+ topBorder + topPadding + bottomBorder + bottomPadding);
+						PDFTextLM.this.addSpaceHolder(con);
+						
 						currentPos = chunk.getText().length();
 						PDFTextLM.this.newLine();
 						vestigeIndex = -1;
