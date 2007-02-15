83f3bdca7e7c1c45434a54ba463773dcaffc86f1
Jun Ouyang <jouyang@actuate.com>
2007-02-15 03:02:51 +0000

    Fix bugzilla bug  132508: Mouse Click->URL Redirect does not work on Legend in PDF.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/PDFImageLM.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/PDFImageLM.java
@@ -15,15 +15,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.birt.report.engine.content.Dimension;
+import org.eclipse.birt.report.engine.content.IContainerContent;
 import org.eclipse.birt.report.engine.content.IContent;
+import org.eclipse.birt.report.engine.content.IHyperlinkAction;
 import org.eclipse.birt.report.engine.content.IImageContent;
+import org.eclipse.birt.report.engine.content.impl.ActionContent;
+import org.eclipse.birt.report.engine.content.impl.ReportContent;
 import org.eclipse.birt.report.engine.executor.IReportItemExecutor;
 import org.eclipse.birt.report.engine.ir.ExtendedItemDesign;
 import org.eclipse.birt.report.engine.layout.ILineStackingLayoutManager;
+import org.eclipse.birt.report.engine.layout.area.IImageArea;
 import org.eclipse.birt.report.engine.layout.area.impl.AreaFactory;
+import org.eclipse.birt.report.engine.layout.area.impl.BlockContainerArea;
 import org.eclipse.birt.report.engine.layout.area.impl.ContainerArea;
 import org.eclipse.birt.report.engine.layout.area.impl.ImageArea;
 import org.eclipse.birt.report.model.api.IResourceLocator;
@@ -107,6 +116,10 @@ public class PDFImageLM extends PDFLeafItemLM
 
 	protected ContainerArea root;
 
+	private Dimension instrinsic;
+
+	private static final String BOOKMARK_PREFIX = "javascript:catchBookmark('";
+
 	public PDFImageLM( PDFLayoutEngineContext context, PDFStackingLM parent,
 			IContent content, IReportItemExecutor executor )
 	{
@@ -181,7 +194,6 @@ public class PDFImageLM extends PDFLeafItemLM
 	protected Dimension getSpecifiedDimension( IImageContent content )
 	{
 		Dimension dim = new Dimension( DEFAULT_WIDHT, DEFAULT_HEIGHT );
-		Dimension instrinsic = null;
 		try
 		{
 			instrinsic = getIntrinsicDimension( content );
@@ -279,8 +291,10 @@ public class PDFImageLM extends PDFLeafItemLM
 			}
 			else
 			{
-				//change the root height to make sure image can be put into parent.
-				root.setAllocatedHeight( lineParent.getCurrentMaxContentHeight( ) );
+				// change the root height to make sure image can be put into
+				// parent.
+				root.setAllocatedHeight( lineParent
+						.getCurrentMaxContentHeight( ) );
 				parent.addArea( root, false, false );
 				return false;
 			}
@@ -332,11 +346,201 @@ public class PDFImageLM extends PDFLeafItemLM
 		imageArea.setWidth( contentDimension.getWidth( ) );
 		imageArea.setHeight( contentDimension.getHeight( ) );
 		root.addChild( imageArea );
-
 		imageArea.setPosition( root.getContentX( ), root.getContentY( ) );
+
+		processChartLegend( image, imageArea );
 		root.setContentWidth( contentDimension.getWidth( ) );
 		root.setContentHeight( Math.min( context.getMaxHeight( ),
 				contentDimension.getHeight( ) ) );
+	}
+
+	/**
+	 * Creates legend for chart.
+	 * 
+	 * @param imageContent
+	 *            the image content of the chart.
+	 * @param imageArea
+	 *            the imageArea of the chart.
+	 */
+	private void processChartLegend( IImageContent imageContent,
+			IImageArea imageArea )
+	{
+		Object imageMapObject = imageContent.getImageMap( );
+		boolean hasImageMap = ( imageMapObject != null )
+				&& ( imageMapObject instanceof String )
+				&& ( ( (String) imageMapObject ).length( ) > 0 );
+		if ( hasImageMap )
+		{
+			createImageMap( (String) imageMapObject, imageArea );
+		}
+	}
+
+	private void createImageMap( String imageMapObject, IImageArea imageArea )
+	{
+		Pattern pattern = Pattern
+				.compile( "<AREA[^<>]*coords=\"([\\d,]*)\" href=\"([^<>\"]*)\" target=\"([^<>\"]*)\"/>" );
+		Matcher matcher = pattern.matcher( imageMapObject );
+		String url = null;
+		String targetWindow = null;
+		ArrayList areas = new ArrayList( );
+		while ( matcher.find( ) )
+		{
+			try
+			{
+				areas.add( getArea( matcher.group( 1 ) ) );
+				if ( url == null )
+					url = matcher.group( 2 );
+				if ( targetWindow == null )
+					targetWindow = matcher.group( 3 );
+			}
+			catch ( NumberFormatException e )
+			{
+				logger.log( Level.WARNING, e.getMessage( ), e );
+			}
+		}
+		if ( url == null )
+		{
+			return;
+		}
+		ActionContent link = new ActionContent( );
+		if ( isBookmark( url ) )
+		{
+			String bookmark = getBookmark( url );
+			link.setBookmark( bookmark );
+		}
+		else
+		{
+			link.setHyperlink( url, targetWindow );
+		}
+		createImageMaps( areas, imageArea, link );
+	}
+
+	private void createImageMaps( ArrayList areas, IImageArea imageArea,
+			IHyperlinkAction link )
+	{
+		for ( int i = 0; i < areas.size( ); i++ )
+		{
+			int[] area = (int[]) areas.get( i );
+			area = getAbsoluteArea( area, imageArea );
+			createImageMapContainer( area[0], area[1], area[2], area[3], link );
+		}
+	}
+
+	/**
+	 * Creates an image map container, which is an empty container with an hyper
+	 * link.
+	 * 
+	 * @param x
+	 *            x cordinat of lower left corner of the container.
+	 * @param y
+	 *            x cordinat of lower left corner of the container.
+	 * @param width
+	 *            width of the container.
+	 * @param height
+	 *            height of the container.
+	 * @param link
+	 *            destination of the hyperlink.
+	 */
+	private void createImageMapContainer( int x, int y, int width, int height,
+			IHyperlinkAction link )
+	{
+		ReportContent reportContent = (ReportContent) image.getReportContent( );
+		IContainerContent mapContent = reportContent.createContainerContent( );
+		mapContent.setHyperlinkAction( link );
+		BlockContainerArea area = (BlockContainerArea) AreaFactory
+				.createBlockContainer( mapContent );
+		area.setPosition( x, y );
+		area.setWidth( width );
+		area.setHeight( height );
+		root.addChild( area );
+	}
+
+	/**
+	 * Caculates the absolute positions of image map when given the position of
+	 * image. The image map position is relative to the left up corner of the
+	 * image.
+	 * 
+	 * The argument and returned value are both 4 length integer area, the four
+	 * value of which are x, y of up left corner, width and height respectively.
+	 * 
+	 * @param area
+	 *            rectangle area of a image map.
+	 * @param imageArea
+	 *            image area of the image in which the image map is.
+	 * @return absolute postion of the image map.
+	 */
+	private int[] getAbsoluteArea( int[] area, IImageArea imageArea )
+	{
+		for ( int i = 0; i < 4; i++ )
+		{
+			area[i] = getTranslatedLength( area[i] );
+		}
+		int[] result = new int[4];
+		int imageX = imageArea.getX( );
+		int imageY = imageArea.getY( );
+		int imageHeight = imageArea.getHeight( );
+		int imageWidth = imageArea.getWidth( );
+		int intrinsicWidth = instrinsic.getWidth( );
+		int intrinsicHeight = instrinsic.getHeight( );
+		float ratio = (float) imageWidth / (float) intrinsicWidth;
+		result[0] = imageX + (int) ( area[0] * ratio );
+		result[2] = (int) ( area[2] * ratio );
+		ratio = (float) imageHeight / (float) intrinsicHeight;
+		result[1] = imageY + (int) ( area[1] * ratio );
+		result[3] = (int) ( area[3] * ratio );
+		return result;
+	}
+
+	private int getTranslatedLength( int length )
+	{
+		return length * 1000 / 192 * 72;
+	}
+
+	/**
+	 * Check if a url is of an internal bookmark.
+	 * 
+	 * @param url
+	 *            the url string.
+	 * @return true if and only if the url is of an internal bookmark.
+	 */
+	private boolean isBookmark( String url )
+	{
+		return url.startsWith( BOOKMARK_PREFIX ) && url.endsWith( "')" );
+	}
+
+	/**
+	 * Parses out bookmark name from a url for interanl bookmark.
+	 * 
+	 * @param url
+	 *            the url string
+	 * @return the bookmark name.
+	 */
+	private String getBookmark( String url )
+	{
+		int start = url.indexOf( BOOKMARK_PREFIX ) + BOOKMARK_PREFIX.length( );
+		int end = url.length( ) - 2;
+		return url.substring( start, end );
+	}
+
+	/**
+	 * Parse the image map postion from a string which is of format "x1, y1, x2,
+	 * y2".
+	 * 
+	 * @param string
+	 *            the postion string.
+	 * @return a array which contains the x, y cordinate of left up corner,
+	 *         width and hegiht in sequence.
+	 * 
+	 */
+	private int[] getArea( String string )
+	{
+		String[] rawDatas = string.split( "," );
+		int[] area = new int[4];
+		area[0] = Integer.parseInt( rawDatas[0] );
+		area[1] = Integer.parseInt( rawDatas[1] );
+		area[2] = Integer.parseInt( rawDatas[4] ) - area[0];
+		area[3] = Integer.parseInt( rawDatas[5] ) - area[1];
+		return area;
 	}
 
 }
