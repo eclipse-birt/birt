87ed25eabaa1b6442c05aa8789660b6255d280fb
hlin <>
2007-03-08 06:00:07 +0000

    - Summary: Fix Bugzilla bug 159505-Quick Start to Report Development
    - Bugzilla Bug (s) Resolved:
    
    - Description:
    
    - Tests Description: Manual test and code review
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted:

---
+++ UI/org.eclipse.birt.report.designer.ui.samples.rcp/src/org/eclipse/birt/report/designer/ui/samples/rcp/action/RCPOpenSampleReportAction.java
@@ -0,0 +1,84 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.ui.samples.rcp.action;
+
+import org.eclipse.birt.report.designer.nls.Messages;
+import org.eclipse.birt.report.designer.ui.samplesview.action.IOpenSampleReportAction;
+import org.eclipse.birt.report.designer.ui.samplesview.util.PlaceResources;
+import org.eclipse.birt.report.designer.ui.samplesview.view.ReportExamples;
+import org.eclipse.birt.report.model.api.ReportDesignHandle;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.action.Action;
+import org.eclipse.swt.widgets.TreeItem;
+
+public class RCPOpenSampleReportAction extends Action implements
+		IOpenSampleReportAction
+{
+
+	private static final String ACTION_TEXT = Messages.getString( "SampleReportsView.Action.openSampleReport" );
+
+	private ReportExamples composite;
+
+	public RCPOpenSampleReportAction( )
+	{
+		super( ACTION_TEXT );
+		setToolTipText( Messages.getString( "SampleReportsView.Action.openSampleReport.toolTipText.rcp" ) );
+	}
+
+	public void setMainComposite( ReportExamples composite )
+	{
+		this.composite = composite;
+	}
+
+	public void run( )
+	{
+		TreeItem item = (TreeItem) composite.getSelectedElement( );
+		Object selectedElement = item.getData( );
+		if ( selectedElement == null
+				|| !( selectedElement instanceof ReportDesignHandle ) )
+		{
+			return;
+		}
+
+		PlaceResources.copy( composite.getShell( ),
+				getDefaultLocation( ),
+				item.getText( ),
+				( (ReportDesignHandle) selectedElement ).getFileName( ) );
+
+		/*
+		 * Copy the inluded libraries if selecting sample report demostrate
+		 * report library feature
+		 */
+		if ( item.getParentItem( ).getText( ).equals( "Libraries" ) )
+		{
+			PlaceResources.copyIncludedLibraries( composite.getShell( ),
+					getDefaultLocation( ) );
+		}
+
+		/*
+		 * Copy the inluded libraries if selecting sample report demostrate
+		 * report library feature
+		 */
+		if ( item.getParentItem( ).getText( ).equals( "XML Data Source" ) )
+		{
+			PlaceResources.copyIncludedPng( composite.getShell( ),
+					getDefaultLocation( ) );
+		}
+	}
+
+	private String getDefaultLocation( )
+	{
+		IPath defaultPath = Platform.getLocation( );
+		return defaultPath.toOSString( );
+	}
+}
