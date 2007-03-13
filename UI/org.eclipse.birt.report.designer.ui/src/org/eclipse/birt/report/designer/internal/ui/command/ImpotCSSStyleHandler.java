58ef08f98d29040e8cbd1600010c2e3f23b941da
xizhang <>
2007-03-13 08:03:15 +0000

    - Summary:  Error occurs in error log Show View when start BIRT
    - Bugzilla Bug (s) Resolved:
     176442
    
    - Description:
     This bugs occurs because ImportCSSStyleHandler is missing in the checkin on March.7 2007.
     Fixed.
    
    - Tests Description  :
    Manual test
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted:
    
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    -  Notes to Documentation:

---
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/command/ImpotCSSStyleHandler.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.internal.ui.command;
+
+import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
+import org.eclipse.birt.report.designer.internal.ui.dialogs.SelectCssStyleWizard;
+import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
+import org.eclipse.birt.report.designer.internal.ui.wizards.PublishTemplateWizard;
+import org.eclipse.birt.report.model.api.ReportDesignHandle;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * 
+ */
+
+public class ImpotCSSStyleHandler extends SelectionHandler
+{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	public Object execute( ExecutionEvent event ) throws ExecutionException
+	{
+		boolean retBoolean = true;
+		super.execute( event );
+
+		Dialog dialog = new WizardDialog( PlatformUI.getWorkbench( )
+				.getDisplay( )
+				.getActiveShell( ), new SelectCssStyleWizard( getSelection() ) );
+		dialog.open( );
+
+		return new Boolean( retBoolean );
+	}
+}
