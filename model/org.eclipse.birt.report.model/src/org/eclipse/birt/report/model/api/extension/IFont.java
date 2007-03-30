838b22eb041f60e7b4873c7c89c8619268a05e56
Rick Lu <rlu@actuate.com>
2007-03-30 08:59:21 +0000

    Two common intefaces for extension elements.

---
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/extension/IFont.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.api.extension;
+
+
+public interface IFont
+{
+
+	boolean isBold( );
+
+	void setIsBold( boolean isBlod );
+
+	boolean isItalic( );
+
+	void setIsItalic( boolean isItalic );
+
+	boolean isUnderline( );
+
+	void setIsUnderline( boolean isUnderline );
+
+	boolean isStrikeThrough( );
+
+	void setIsStrikeThrough( boolean isStrikeThrough );
+
+	String getName( );
+
+	void setName( String name );
+
+	float getSize( );
+
+	void setSize( float size );
+}
