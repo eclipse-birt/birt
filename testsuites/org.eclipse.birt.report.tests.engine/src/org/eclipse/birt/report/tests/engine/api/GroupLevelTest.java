d22ca2718bfa770e51bdca37e4c2d6472fdb5ac7
Liwen Chen <lchen@actuate.com>
2007-04-05 05:55:39 +0000

    Add unit test for getGroupLevel method in GroupContent class

---
+++ testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/api/GroupLevelTest.java
@@ -0,0 +1,47 @@
+
+package org.eclipse.birt.report.tests.engine.api;
+
+import org.eclipse.birt.report.engine.api.EngineException;
+import org.eclipse.birt.report.engine.content.ITableGroupContent;
+import org.eclipse.birt.report.engine.ir.TableGroupDesign;
+import org.eclipse.birt.report.tests.engine.BaseEmitter;
+
+public class GroupLevelTest extends BaseEmitter
+{
+
+	private String reportName = "groupLevelTest.rptdesign";
+
+	protected String getReportName( )
+	{
+		return reportName;
+	}
+
+	public void setUp( ) throws Exception
+	{
+		super.setUp( );
+		removeResource( );
+		copyResource_INPUT( reportName, reportName );
+	}
+
+	public void tearDown( )
+	{
+		removeResource( );
+	}
+
+	public void testGetGroupLevel( ) throws EngineException
+	{
+		runandrender_emitter( EMITTER_HTML, false );
+	}
+
+	public void startTableGroup( ITableGroupContent group )
+	{
+		if ( ( (TableGroupDesign) group.getGenerateBy( ) ).getName( ).equals(
+				"NewTableGroup1" ) )
+			assertEquals( 0, group.getGroupLevel( ) );
+		else
+		{
+			assertEquals( 1, group.getGroupLevel( ) );
+		}
+	}
+
+}
