0532c2cfc4137e3221ef6b7dc76151b1dd93c9a6
jliu <>
2007-04-06 06:08:43 +0000

    Move interface ErrorCodes to api package.

---
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/ErrorCodes.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.api;
+
+import org.eclipse.birt.report.model.i18n.MessageConstants;
+
+/**
+ * Implements to define the error codes that are exposed to api.
+ */
+
+public interface ErrorCodes
+{
+
+	/**
+	 * The report version is unsupported.
+	 */
+
+	public static final String DESIGN_EXCEPTION_UNSUPPORTED_VERSION = MessageConstants.DESIGN_PARSER_EXCEPTION_UNSUPPORTED_VERSION;
+
+}
--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/ErrorDetail.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/ErrorDetail.java
@@ -24,7 +24,6 @@ import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
 import org.eclipse.birt.report.model.core.DesignElement;
 import org.eclipse.birt.report.model.core.Module;
 import org.eclipse.birt.report.model.parser.DesignParserException;
-import org.eclipse.birt.report.model.util.ErrorCodes;
 import org.eclipse.birt.report.model.util.XMLParserException;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
