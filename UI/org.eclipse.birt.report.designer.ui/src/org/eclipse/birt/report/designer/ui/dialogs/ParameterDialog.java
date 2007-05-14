c2cd987b340f241afff7c64c5300eb9ae269ff25
hlin <>
2007-05-14 04:00:48 +0000

    - Summary:Fix Bugzilla bug 186732-Static parameter can not be created[00]
    - Bugzilla Bug (s) Resolved: 186732
    
    - Description:
    Fix possible NPE on determining sortting controls is visible.
    
    - Tests Description: Manual test and code review
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ParameterDialog.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ParameterDialog.java
@@ -1953,7 +1953,7 @@ public class ParameterDialog extends BaseDialog
 						null );
 			}
 
-			if ( sorttingArea.isVisible( ) )
+			if ( sorttingArea != null && sorttingArea.isVisible( ) )
 			{
 				if ( !sortKeyChooser.getText( ).equals( CHOICE_NONE ) )
 				{
