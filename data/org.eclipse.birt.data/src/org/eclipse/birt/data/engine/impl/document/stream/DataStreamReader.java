dc0f571b8a7d3c38b91b5cf0b22ab9d0995169fe
Lin Zhu <lzhu@actuate.com>
2007-05-16 14:39:46 +0000

    CheckIn:Merge Stream "DataSet" and "ResultClass" to reduce document size(2.1.3).

---
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/document/stream/DataStreamReader.java
@@ -0,0 +1,94 @@
+
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.birt.data.engine.impl.document.stream;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.eclipse.birt.core.archive.RAInputStream;
+import org.eclipse.birt.data.engine.api.DataEngineContext;
+import org.eclipse.birt.data.engine.core.DataException;
+import org.eclipse.birt.data.engine.i18n.ResourceConstants;
+
+/**
+ * 
+ */
+
+public class DataStreamReader extends StreamReader
+{
+
+	/**
+	 * 
+	 * @param context
+	 * @param id
+	 * @throws DataException
+	 */
+	public DataStreamReader( DataEngineContext context, StreamID id ) throws DataException
+	{
+		try
+		{
+			this.streamMap = new HashMap();
+			this.id = id;
+			this.context = context;
+			RAInputStream is = context.getInputStream( id.getStartStream( ),
+					id.getSubQueryStream( ),
+					DataEngineContext.DATASET_DATA_STREAM );
+			
+			DataInputStream metaIndexStream = new DataInputStream( is );
+			
+			
+			while ( is.getOffset( ) < is.length( ) )
+			{
+				int type = is.readInt( );
+				int size = is.readInt( );
+				long offset = is.getOffset( );
+				this.streamMap.put( new Integer( type ),
+						new WrapperedRAInputStream( (RAInputStream)context.getInputStream( id.getStartStream( ),
+								id.getSubQueryStream( ),
+								DataEngineContext.DATASET_DATA_STREAM ),offset,size) );
+				is.skip( size );
+				
+
+			}
+			metaIndexStream.close( );
+			
+		}
+		catch ( IOException e )
+		{
+			throw new DataException( e.getLocalizedMessage( ) );
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.birt.data.engine.impl.document.stream.StreamReader#getRAInputStream(int)
+	 */
+	public RAInputStream getRAInputStream( int streamType )
+			throws DataException
+	{
+		Object temp = this.streamMap.get( new Integer( streamType ) );
+		if( temp == null )
+		{
+			throw new DataException( ResourceConstants.DOCUMENT_ERROR_CANNOT_LOAD_STREAM,
+					DataEngineContext.getPath( id.getStartStream( ),
+							id.getSubQueryStream( ),
+							streamType ) );
+		}
+		else
+		{
+			return (RAInputStream)temp;
+
+		}
+	}
+
+}
--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/document/stream/StreamReader.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/document/stream/StreamReader.java
@@ -11,61 +11,20 @@
  *******************************************************************************/
 package org.eclipse.birt.data.engine.impl.document.stream;
 
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.util.HashMap;
 
 import org.eclipse.birt.core.archive.RAInputStream;
-import org.eclipse.birt.core.util.IOUtil;
 import org.eclipse.birt.data.engine.api.DataEngineContext;
 import org.eclipse.birt.data.engine.core.DataException;
-import org.eclipse.birt.data.engine.i18n.ResourceConstants;
 
 /**
  * 
  */
-public class StreamReader
+public abstract class StreamReader
 {
-	private HashMap streamMap;
-	private StreamID id;
-	private DataEngineContext context;
-	
-	/**
-	 * 
-	 * @param context
-	 * @param id
-	 * @throws DataException
-	 */
-	public StreamReader( DataEngineContext context, StreamID id ) throws DataException
-	{
-		try
-		{
-			this.streamMap = new HashMap();
-			this.id = id;
-			this.context = context;
-			RAInputStream is = context.getInputStream( id.getStartStream( ),
-					id.getSubQueryStream( ),
-					DataEngineContext.META_INDEX_STREAM );
-			
-			DataInputStream metaIndexStream = new DataInputStream( is );
-			
-			
-			while( is.getOffset( ) != is.length( ) )
-			{
-				int type = IOUtil.readInt( metaIndexStream );
-				long offset = IOUtil.readLong( metaIndexStream );
-				int size = IOUtil.readInt( metaIndexStream );
-									
-				this.streamMap.put( new Integer( type ), new OffsetInfo( offset, size ) );
-			}
-			
-			metaIndexStream.close( );
-		}
-		catch ( IOException e )
-		{
-			throw new DataException( e.getLocalizedMessage( ) );
-		}
-	}
+	protected HashMap streamMap;
+	protected StreamID id;
+	protected DataEngineContext context;
 	
 	/**
 	 * 
@@ -73,29 +32,7 @@ public class StreamReader
 	 * @return
 	 * @throws DataException
 	 */
-	public RAInputStream getRAInputStream( int streamType ) throws DataException
-	{
-		Object temp = this.streamMap.get( new Integer( streamType ) );
-		if( temp == null )
-		{
-			throw new DataException( ResourceConstants.DOCUMENT_ERROR_CANNOT_LOAD_STREAM,
-					DataEngineContext.getPath( id.getStartStream( ),
-							id.getSubQueryStream( ),
-							streamType ) );	}
-		else
-		{
-			OffsetInfo oi = (OffsetInfo)temp;
-			
-			long offset = oi.offset;
-			int size = oi.size;
-			RAInputStream metaStream = new WrapperedRAInputStream((RAInputStream)context.getInputStream( id.getStartStream( ),
-					id.getSubQueryStream( ),
-					DataEngineContext.META_STREAM ), offset, size);
-			return metaStream;
-
-		}
-		
-	}
+	public abstract RAInputStream getRAInputStream( int streamType ) throws DataException;
 	
 	/**
 	 * 
@@ -107,15 +44,4 @@ public class StreamReader
 		return this.streamMap.get( new Integer(streamType) ) != null;
 	}
 	
-	private class OffsetInfo
-	{
-		private long offset;
-		private int size;
-		
-		public OffsetInfo( long offset, int size )
-		{
-			this.offset = offset;
-			this.size = size;
-		}
-	}
 }
