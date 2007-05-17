b15d5a1b49222d1926c9723df1d12222e2437483
Dazheng Gao <dgao@actuate.com>
2007-05-17 09:29:39 +0000

    - Summary:Fix bug 187499, Cross tab - Function list for measures and totals.
    - Bugzilla Bug (s) Resolved:
    
    
    
    - Description: Fix bug 187499, Cross tab - Function list for measures and totals.
    
    
    
    - Tests Description  :
    
    Manual test
    
    - Files Edited:
    
    @changelist
    
    - Files Added:
    
    @addlist
    
    - Files Deleted:
    
    @deletelist
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -          Notes to Documentation:

--- xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/dialogs/TotalProvider.java
+++ xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/dialogs/TotalProvider.java
@@ -1,14 +1,16 @@
+
 package org.eclipse.birt.report.item.crosstab.internal.ui.dialogs;
 
 import org.eclipse.birt.report.designer.ui.views.attributes.providers.ChoiceSetFactory;
 import org.eclipse.birt.report.designer.util.DEUtil;
-import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
+import org.eclipse.birt.report.model.api.elements.ReportDesignConstants;
 import org.eclipse.birt.report.model.api.metadata.IChoice;
+import org.eclipse.birt.report.model.elements.interfaces.IMeasureModel;
 import org.eclipse.jface.viewers.LabelProvider;
-
 
 public class TotalProvider extends LabelProvider
 {
+
 	public String[] getFunctionDisplayNames( )
 	{
 		IChoice[] choices = getFunctions( );
@@ -38,17 +40,40 @@ public class TotalProvider extends LabelProvider
 		return displayNames;
 	}
 
+	// public String getFunctionDisplayName( String name )
+	// {
+	// return ChoiceSetFactory.getDisplayNameFromChoiceSet( name,
+	// DEUtil.getMetaDataDictionary( )
+	// .getChoiceSet( DesignChoiceConstants.CHOICE_MEASURE_FUNCTION ) );
+	// }
+	//
+	// private IChoice[] getFunctions( )
+	// {
+	// return DEUtil.getMetaDataDictionary( )
+	// .getChoiceSet( DesignChoiceConstants.CHOICE_MEASURE_FUNCTION )
+	// .getChoices( );
+	// }
+
 	public String getFunctionDisplayName( String name )
+
 	{
 		return ChoiceSetFactory.getDisplayNameFromChoiceSet( name,
 				DEUtil.getMetaDataDictionary( )
-						.getChoiceSet( DesignChoiceConstants.CHOICE_MEASURE_FUNCTION ) );
+						.getElement( ReportDesignConstants.MEASURE_ELEMENT )
+						.getProperty( IMeasureModel.FUNCTION_PROP )
+						.getAllowedChoices( ) );
+
 	}
 
 	private IChoice[] getFunctions( )
+
 	{
 		return DEUtil.getMetaDataDictionary( )
-				.getChoiceSet( DesignChoiceConstants.CHOICE_MEASURE_FUNCTION )
+				.getElement( ReportDesignConstants.MEASURE_ELEMENT )
+				.getProperty( IMeasureModel.FUNCTION_PROP )
+				.getAllowedChoices( )
 				.getChoices( );
+
 	}
+
 }
