bbf41bcd56ba58ba9d1dd93a0faca5cbecec1d73
Mingxia Wu <mwu@actuate.com>
2007-06-07 12:11:46 +0000

    CheckIn:Fix bugzilla Bug 190985Enhance the filter condition selection list of crosstab[1102]

--- data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/impl/CubeQueryUtil.java
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/impl/CubeQueryUtil.java
@@ -108,6 +108,11 @@ public class CubeQueryUtil implements ICubeQueryUtil
 							{
 								DimLevel next = OlapExpressionUtil.getTargetDimLevel( aggrOns.get( j+1 )
 										.toString( ) );
+								//If target dim level is on ROW_EDGE, do not add any subtotal expression.
+								if ( getAxisQualifierEdgeType( target, cubeDefn ) == ICubeQueryDefinition.COLUMN_EDGE )
+								{
+									continue;
+								}							
 								if ( getAxisQualifierLevel( next,
 										cubeDefn.getEdge( getAxisQualifierEdgeType( dimLevel,
 												cubeDefn ) ) ) == null )
