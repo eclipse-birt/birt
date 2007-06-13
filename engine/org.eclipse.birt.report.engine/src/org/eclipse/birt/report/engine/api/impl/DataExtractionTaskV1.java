1f1e23952d8e2137c8271845a485e953bad0d117
Gang Liu <gliu@actuate.com>
2007-06-13 04:52:37 +0000

    Fix scr 94397-- Filtering chart data doesn't work after Top N/Top Percent filter on merged colum

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/DataExtractionTaskV1.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/DataExtractionTaskV1.java
@@ -142,6 +142,13 @@ public class DataExtractionTaskV1 extends EngineTask
 		executionContext.setFactoryMode( false );
 		executionContext.setPresentationMode( true );
 
+		// load the informationf rom the report document
+		setParameterValues( reportDocReader.getParameterValues( ) );
+		// setParameterDisplayTexts( reportDoc.getParameterDisplayTexts( ) );
+		usingParameterValues( );
+		executionContext.registerGlobalBeans( reportDocReader
+				.getGlobalVariables( null ) );
+		
 		IDataEngine dataEngine = executionContext.getDataEngine( );
 		dataEngine.prepare( report, appContext );
 	}
