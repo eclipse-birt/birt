8dd3ac88ed26aadfb558034227658743f3a94554
Xiaoying Gu <xgu@actuate.com>
2007-06-19 07:56:12 +0000

    Sync releng tools for 2.2RC4

---
+++ build/org.eclipse.birt.releng.birtbuilder/replaceBuildInfo.sh
@@ -0,0 +1,10 @@
+# Input Arguments:
+# 
+#
+
+rm -f /home/adb/releng/PackageFiles/template/build_info_template_new.txt
+
+sed "s/#build_time#/$1 $2/g" /home/adb/releng/PackageFiles/template/build_info_template.txt > build_info_template_new.txt
+
+sed "s/#univBDate#/$3 $4 $5 $6 $7 $8 $9/g" build_info_template_new.txt > /home/adb/releng/PackageFiles/template/build_info_template_new.txt
+rm -f build_info_template_new.txt
