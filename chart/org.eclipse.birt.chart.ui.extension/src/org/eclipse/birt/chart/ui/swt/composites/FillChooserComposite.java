bf32f37e631a83a6ba8de7ebaee602bca37e3fc7
Yueqian Wang <ywang@actuate.com>
2007-06-19 06:08:11 +0000

    - Summary: Fix Bugzilla Bug 192699.TVT33:TCT685: 'Custom Color' button truncation in palette
    - Bugzilla Bug (s) Resolved: 192699
    
    - Description: Extend the width of the palette to make enough space for the button in Greek
    
    - Tests Description  : Manual Test
    
    - Files Edited:
    
    - Files Added:
    
    - Files Deleted:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/FillChooserComposite.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/FillChooserComposite.java
@@ -1,5 +1,5 @@
 /***********************************************************************
- * Copyright (c) 2004 Actuate Corporation.
+ * Copyright (c) 2004, 2005, 2006, 2007 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -425,7 +425,7 @@ public class FillChooserComposite extends Composite implements
 			return;
 		}
 		int iShellHeight = 170;
-		int iShellWidth = 160;
+		int iShellWidth = 190;
 		// Reduce the height based on which buttons are to be shown.
 		if ( bGradientEnabled )
 		{
