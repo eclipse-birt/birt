2fecd3df040d41dd4ece862b6211bf1642926c19
iliu <>
2007-07-10 01:32:32 +0000

    fixed the bug 191696 WordUtil is getScreenResolution()-dependent

--- engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/WordUtil.java
+++ engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/WordUtil.java
@@ -15,123 +15,141 @@ import org.eclipse.birt.report.engine.content.IStyle;
 import org.eclipse.birt.report.engine.ir.DimensionType;
 
 public class WordUtil
-{	
-	public final static double INCH_PX = java.awt.Toolkit.getDefaultToolkit ( ).getScreenResolution ( );
-	
-	public final static double INCH_PT = 72;
-	
-	public final static double PT_TWIPS = 20;
-	
-	public final static double INCH_TWIPS = INCH_PT * PT_TWIPS;
-	
-	public final static double PX_TWIPS = INCH_TWIPS / INCH_PX;
-	
-	public final static double PX_PT = INCH_PT / INCH_PX;	
-	
-	public static String validBookmarkName(String name)
+{
+
+	private static double Temp_PX;
+	public final static double INCH_PX;
+	static
 	{
-		String bookmark = name.replaceAll ( " ", "_" );
-        bookmark = bookmark.replaceAll("\"", "_");
-        return bookmark;
+
+		try
+		{
+			Temp_PX = java.awt.Toolkit
+					.getDefaultToolkit( )
+					.getScreenResolution( );
+		}
+		catch ( Exception e )
+		{
+			Temp_PX = 96;
+		}
+		INCH_PX = Temp_PX;
 	}
-	
-	public static int convertTo ( DimensionType value, int prefValue )
+
+	public final static double INCH_PT = 72;
+
+	public final static double PT_TWIPS = 20;
+
+	public final static double INCH_TWIPS = INCH_PT * PT_TWIPS;
+
+	public final static double PX_TWIPS = INCH_TWIPS / INCH_PX;
+
+	public final static double PX_PT = INCH_PT / INCH_PX;
+
+	public static String validBookmarkName( String name )
 	{
-		if(value == null)
+		String bookmark = name.replaceAll( " ", "_" );
+		bookmark = bookmark.replaceAll( "\"", "_" );
+		return bookmark;
+	}
+
+	public static int convertTo( DimensionType value, int prefValue )
+	{
+		if ( value == null )
 		{
 			return 0;
-		}	
-		
-		if (DimensionType.UNITS_PERCENTAGE
-				.equalsIgnoreCase ( value.getUnits ( ) ))
-		{
-			return (int) (prefValue * value.getMeasure ( ) / 100);
 		}
 
-		return (int) convertTo ( value );
+		if ( DimensionType.UNITS_PERCENTAGE
+				.equalsIgnoreCase( value.getUnits( ) ) )
+		{
+			return (int) ( prefValue * value.getMeasure( ) / 100 );
+		}
+
+		return (int) convertTo( value );
 	}
 
-	public static double convertTo ( DimensionType value )
+	public static double convertTo( DimensionType value )
 	{
-		if (value == null
-				|| DimensionType.UNITS_PERCENTAGE.equalsIgnoreCase ( value
-						.getUnits ( ) ))
+		if ( value == null
+				|| DimensionType.UNITS_PERCENTAGE.equalsIgnoreCase( value
+						.getUnits( ) ) )
 		{
 			return -1;
 		}
 
-		if (DimensionType.UNITS_PX.equalsIgnoreCase ( value.getUnits ( ) ))
+		if ( DimensionType.UNITS_PX.equalsIgnoreCase( value.getUnits( ) ) )
 		{
-			return value.getMeasure ( ) * PX_TWIPS;
+			return value.getMeasure( ) * PX_TWIPS;
 		}
 
-		double val = value.convertTo ( DimensionType.UNITS_IN );
-		return  val * INCH_TWIPS;
+		double val = value.convertTo( DimensionType.UNITS_IN );
+		return val * INCH_TWIPS;
 	}
-	
-	public static int getWidth(int cw, IStyle style)
+
+	public static int getWidth( int cw, IStyle style )
 	{
-		float left = WordUtil.getPadding ( style.getPaddingLeft ( ));
-		float right = WordUtil.getPadding ( style.getPaddingRight ( ));
-		
-		if(left > cw)
+		float left = WordUtil.getPadding( style.getPaddingLeft( ) );
+		float right = WordUtil.getPadding( style.getPaddingRight( ) );
+
+		if ( left > cw )
 		{
 			left = 0;
 		}
-		
-		if(right > cw)
+
+		if ( right > cw )
 		{
 			right = 0;
 		}
-		
-		if((left + right) > cw) 
+
+		if ( ( left + right ) > cw )
 		{
 			right = 0;
 		}
-		
-		return (int) (cw - left - right);
+
+		return (int) ( cw - left - right );
 	}
-	
-	public static float getPadding(String padding)
+
+	public static float getPadding( String padding )
 	{
 		float value = 0;
-//Percentage value will be omitted		
+		//Percentage value will be omitted		
 		try
 		{
-			value = Float.parseFloat ( padding ) / 50;
+			value = Float.parseFloat( padding ) / 50;
 		}
-		catch(Exception e)
+		catch ( Exception e )
 		{
-			
+
 		}
-		
+
 		return value;
 	}
-	
-	public static double convertImageSize ( DimensionType value, int ref )
+
+	public static double convertImageSize( DimensionType value, int ref )
 	{
-		if(value == null)
+
+		if ( value == null )
 		{
 			return ref * PX_PT;
 		}
-		
-		if (DimensionType.UNITS_PX.equalsIgnoreCase ( value.getUnits ( ) ))
+
+		if ( DimensionType.UNITS_PX.equalsIgnoreCase( value.getUnits( ) ) )
 		{
-			return value.getMeasure ( ) * PX_PT;
+			return value.getMeasure( ) * PX_PT;
 		}
-		else if (DimensionType.UNITS_PERCENTAGE.equalsIgnoreCase ( value
-				.getUnits ( ) ))
+		else if ( DimensionType.UNITS_PERCENTAGE.equalsIgnoreCase( value
+				.getUnits( ) ) )
 		{
-			return (value.getMeasure ( ) / 100) * ref * PX_PT ;
+			return ( value.getMeasure( ) / 100 ) * ref * PX_PT;
 		}
 		else
 		{
-			return value.convertTo ( DimensionType.UNITS_IN ) * INCH_PT ;
+			return value.convertTo( DimensionType.UNITS_IN ) * INCH_PT;
 		}
 	}
-	
-	public static double twipToPt(double t)
+
+	public static double twipToPt( double t )
 	{
-		return  t / PT_TWIPS;		
+		return t / PT_TWIPS;
 	}
 }
