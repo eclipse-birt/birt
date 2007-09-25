7d5246e730941e1567a840e9d340e0da3e1d2052
cchao <>
2007-09-25 08:18:37 +0000

    - Summary: Rollback the commit 203684
    - Bugzilla Bug (s) Resolved:
    
    - Description: Rollback the commit 203684
    
    - Tests Description: Manual test.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    
    - Files Edited:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui.cubebuilder/src/org/eclipse/birt/report/designer/ui/cubebuilder/dialog/LevelPropertyDialog.java
+++ UI/org.eclipse.birt.report.designer.ui.cubebuilder/src/org/eclipse/birt/report/designer/ui/cubebuilder/dialog/LevelPropertyDialog.java
@@ -235,7 +235,17 @@ public class LevelPropertyDialog extends TitleAreaDialog
 
 			displayKeyCombo.setItems( OlapUtil.getDataFieldNames( dataset ) );
 			displayKeyCombo.add( Messages.getString( "LevelPropertyDialog.None" ), 0 ); //$NON-NLS-1$
-			
+			displayKeyCombo.addSelectionListener( new SelectionAdapter( ) {
+
+				public void widgetSelected( SelectionEvent e )
+				{
+					if ( displayKeyCombo.getSelectionIndex( ) > 0 )
+					{
+						displayKeyCombo.setText( DEUtil.getResultSetColumnExpression( displayKeyCombo.getText( ) ) );
+
+					}
+				}
+			} );
 			if ( input.getDisplayColumnName( ) != null )
 			{
 				displayKeyCombo.setText( input.getDisplayColumnName( ) );
@@ -762,17 +772,6 @@ public class LevelPropertyDialog extends TitleAreaDialog
 		gd = new GridData( GridData.FILL_HORIZONTAL );
 		gd.horizontalSpan = 2;
 		displayKeyCombo.setLayoutData( gd );
-		displayKeyCombo.addSelectionListener( new SelectionAdapter( ) {
-
-			public void widgetSelected( SelectionEvent e )
-			{
-				if ( displayKeyCombo.getSelectionIndex( ) > 0 )
-				{
-					displayKeyCombo.setText( DEUtil.getResultSetColumnExpression( displayKeyCombo.getText( ) ) );
-
-				}
-			}
-		} );
 		Button expressionButton = new Button( groupGroup, SWT.PUSH );
 		UIUtil.setExpressionButtonImage( expressionButton );
 		expressionButton.addSelectionListener( new SelectionAdapter( ) {
