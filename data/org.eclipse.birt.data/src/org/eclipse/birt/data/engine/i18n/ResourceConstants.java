69181180790e59d8c78a382f9068cb43b2cfd6d3
Lin Zhu <lzhu@actuate.com>
2007-10-06 06:53:30 +0000

    CheckIn: Fix bugzilla bug 204427. ArrayOutOfBoundsException when using percentile on column with no data

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/i18n/ResourceConstants.java
@@ -65,6 +65,7 @@ public class ResourceConstants
 	public final static String DUPLICATE_COLUMN_NAME = "data.engine.DupColumnName";
 	public final static String DUPLICATE_ALIAS_NAME = "data.engine.DupAlias";
 	public final static String INVALID_FIELD_INDEX = "data.engine.InvalidFieldIndex";
+	public final static String INVALID_PERCENTILE_COLUMN = "data.engine.InvalidPercentileColumn";
 	
 	public final static String MATCH_ERROR = "data.engine.MatchError";
 	public final static String UNSUPPORTED_DATA_TYPE = "data.engine.BadOperandType";
