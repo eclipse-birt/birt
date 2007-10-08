426019779d6160275ad3896be793d53b407aa001
lyu <>
2007-10-08 09:40:54 +0000

    Over write the method doCreateContent of the base class.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ListGroupExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ListGroupExecutor.java
@@ -1,11 +1,18 @@
 
 package org.eclipse.birt.report.engine.internal.document.v4;
 
+import org.eclipse.birt.report.engine.content.IContent;
+
 public class ListGroupExecutor extends GroupExecutor
 {
 
 	protected ListGroupExecutor( ExecutorManager manager )
 	{
 		super( manager, ExecutorManager.LISTGROUPITEM );
+	}
+	
+	protected IContent doCreateContent( )
+	{
+		return report.createListGroupContent( );
 	}
 }
