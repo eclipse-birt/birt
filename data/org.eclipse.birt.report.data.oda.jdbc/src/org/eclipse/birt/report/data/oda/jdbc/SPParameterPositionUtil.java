7c1a678f83f50d06db3e8aef3443275e821ce134
Mingxia Wu <mwu@actuate.com>
2007-10-18 06:29:23 +0000

    CheckIn:Fix several bug on stored procedure with default value setting.

--- data/org.eclipse.birt.report.data.oda.jdbc/src/org/eclipse/birt/report/data/oda/jdbc/SPParameterPositionUtil.java
+++ data/org.eclipse.birt.report.data.oda.jdbc/src/org/eclipse/birt/report/data/oda/jdbc/SPParameterPositionUtil.java
@@ -11,9 +11,12 @@
 
 package org.eclipse.birt.report.data.oda.jdbc;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.birt.report.data.oda.i18n.ResourceConstants;
 import org.eclipse.datatools.connectivity.oda.OdaException;
 
 /**
@@ -22,7 +25,7 @@ import org.eclipse.datatools.connectivity.oda.OdaException;
 public final class SPParameterPositionUtil
 {
 
-	private char[] parameterDefnChars;
+	private int[] position;
 	private char escaper;
 
 	/**
@@ -34,74 +37,129 @@ public final class SPParameterPositionUtil
 	public SPParameterPositionUtil( String sqlTxt, char escaper )
 			throws OdaException
 	{
-		this.escaper = escaper;
-		this.parameterDefnChars = getParameterDefinitionChars( sqlTxt, escaper );
-
-	};
+		try
+		{
+			this.escaper = escaper;
+			String paramTxt = getParameterDefinitionChars( sqlTxt );
+			parseQueryText( paramTxt );
+		}
+		catch ( IOException e )
+		{
+			// do nothing;
+		}
+	}
 
 	/**
-	 * exsit non-default parameter or not
-	 * @return
+	 * 
+	 * @param queryText
+	 * @throws IOException
 	 */
-	public boolean hasNonDefaultParameter( )
+	private void parseQueryText( String queryText ) throws IOException
 	{
-		boolean nextDelimiterValid = true;
-		for ( int i = 0; i < this.parameterDefnChars.length; i++ )
+		StringReader reader = new StringReader( queryText );
+		int i, nextPosition = 1;
+		boolean escaped = false;
+		List result = new ArrayList( );
+		while ( ( i = reader.read( ) ) != -1 )
 		{
-			if ( i > 0 && this.parameterDefnChars[i - 1] == escaper )
+			if ( escaped )
+			{
+				escaped = false;
 				continue;
-			if ( '"' == this.parameterDefnChars[i] )
-			{
-				nextDelimiterValid = !nextDelimiterValid;
 			}
-			if ( nextDelimiterValid )
+			if ( i == this.escaper )
 			{
-				if ( '?' == this.parameterDefnChars[i] )
-				{
-					return true;
-				}
+				escaped = true;
+				continue;
+			}
+			else if ( i == '\"' || i == '\'' )
+			{
+				readNextQuote( reader, i );
+			}
+			else if ( i == '(' )
+			{
+				if ( readNextBracket( reader ) )
+					result.add( new Integer( nextPosition ) );
+			}
+			else if ( i == ',' )
+			{
+				nextPosition++;
+			}
+			else if ( i == '?' )
+			{
+				result.add( new Integer( nextPosition ) );
 			}
 		}
-		return false;
+		position = new int[result.size( )];
+		for ( int k = 0; k < result.size( ); k++ )
+		{
+			position[k] = new Integer( result.get( k ).toString( ) ).intValue( );
+		}
+	}
+
+	/**
+	 * get the quoted string
+	 * 
+	 * @param strBuf
+	 * @param reader
+	 * @return
+	 * @throws IOException
+	 */
+	private void readNextQuote( StringReader reader, int quote )
+			throws IOException
+	{
+		int i = -1;
+		while ( ( i = reader.read( ) ) != -1 )
+		{
+			if ( i != quote )
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param reader
+	 * @throws IOException
+	 */
+	private boolean readNextBracket( StringReader reader )
+			throws IOException
+	{
+		int i = -1;
+		boolean hasPlaceHolder = false;
+		while ( ( i = reader.read( ) ) != -1 )
+		{
+			if ( i == '?' )
+			{
+				hasPlaceHolder = true;
+			}
+			else if ( i != ')' )
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+		return hasPlaceHolder;
 	}
 
 	/**
 	 * get non-default parameters position is sql text
+	 * 
 	 * @return
 	 */
 	public int[] getParameterPositions( )
 	{
-		int nextPosition = 1;
-		List result = new ArrayList( );
-		boolean nextDelimiterValid = true;
-		for ( int i = 0; i < this.parameterDefnChars.length; i++ )
-		{
-			if ( i > 0 && this.parameterDefnChars[i - 1] == escaper )
-				continue;
-			if ( '"' == this.parameterDefnChars[i] )
-			{
-				nextDelimiterValid = !nextDelimiterValid;
-			}
-			if ( nextDelimiterValid )
-			{
-				if ( '?' == this.parameterDefnChars[i] )
-				{
-					result.add( new Integer( nextPosition ) );
-				}
-				if ( ',' == this.parameterDefnChars[i] )
-				{
-					nextPosition++;
-				}
-			}
-		}
-		int[] rt = new int[result.size( )];
-		for ( int i = 0; i < result.size( ); i++ )
-		{
-			rt[i] = new Integer( result.get( i ).toString( ) ).intValue( );
-		}
-		return rt;
+		return this.position == null? new int[0]:this.position;
 	}
-
+	
 	/**
 	 * put sqlText to char array
 	 * @param sqlTxt
@@ -109,23 +167,19 @@ public final class SPParameterPositionUtil
 	 * @return
 	 * @throws OdaException
 	 */
-	private char[] getParameterDefinitionChars( String sqlTxt, char escaper )
+	private String getParameterDefinitionChars( String sqlTxt )
 			throws OdaException
 	{
 		char[] temp = sqlTxt.toCharArray( );
 		int startPoint = -1;
 		int endPoint = -1;
 		boolean validBracket = true;
-		for ( int i = temp.length - 1; i >= 0; i-- )
+		for ( int i = 0; i < temp.length; i++ )
 		{
 			if ( i > 0 && temp[i - 1] == escaper )
 				continue;
 			if ( validBracket )
 			{
-				if ( ')' == temp[i] && endPoint == -1 )
-				{
-					endPoint = i;
-				}
 				if ( '(' == temp[i] )
 				{
 					startPoint = i;
@@ -138,10 +192,30 @@ public final class SPParameterPositionUtil
 			}
 
 		}
+
+		for ( int i = temp.length - 1; i >= 0; i-- )
+		{
+			if ( i > 0 && temp[i - 1] == escaper )
+				continue;
+			if ( validBracket )
+			{
+				if ( ')' == temp[i] )
+				{
+					endPoint = i;
+					break;
+				}
+			}
+			if ( '"' == temp[i] )
+			{
+				validBracket = !validBracket;
+			}
+
+		}
 		if ( startPoint == -1 && endPoint == -1 )
-			return new char[0];
+			return "";
 		else if ( startPoint >= endPoint || startPoint == -1 )
-			throw new OdaException( "Illegal SP call" );
-		return sqlTxt.substring( startPoint, endPoint ).toCharArray( );
+			throw new JDBCException( ResourceConstants.INVALID_STORED_PRECEDURE,
+					ResourceConstants.ERROR_INVALID_STATEMENT );
+		return sqlTxt.substring( startPoint + 1, endPoint );
 	}
 }
