13cb8c5d41e16d93cb99da60af87248e5d3c04d0
xizhang <>
2007-10-22 09:34:23 +0000

    - Summary: Issue about localization page of report parameter in Property editor[0705]
    - Bugzilla Bug (s) Resolved:
    207022
    
    - Description:
     Add another resource key field for help text.
    Fixed.
    
    - Tests Description  :
    Manual test
    
    - Files Edited:
    
    
    - Files Added:
    
    
    - Files Deleted:
    
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    -  Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/ScalarParameterI18nPage.java
+++ UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/ScalarParameterI18nPage.java
@@ -11,6 +11,10 @@
 
 package org.eclipse.birt.report.designer.internal.ui.views.attributes.page;
 
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.ResourceKeyDescriptorProvider;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.LabelSection;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.ResourceKeySection;
+import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.birt.report.model.api.ScalarParameterHandle;
 import org.eclipse.birt.report.model.api.elements.ReportDesignConstants;
 import org.eclipse.swt.widgets.Composite;
@@ -19,14 +23,42 @@ import org.eclipse.swt.widgets.Composite;
  * The i18n page for Scalarparameter
  */
 
-public class ScalarParameterI18nPage extends I18nPage
+public class ScalarParameterI18nPage extends AttributePage
 {
+	private static final String MESSAGE_NOTE = Messages.getString( "I18nPage.text.Note" ); //$NON-NLS-1$
+	public static final String I18N_I18N_PROMPT_TEXT = "I18N_I18N_PROMPT_TEXT";
+	public static final String I18N_I18N_HELP_TEXT = "I18N_I18N_HELP_TEXT";
 
 	public void buildUI( Composite parent  )
 	{
-		elementName = ReportDesignConstants.SCALAR_PARAMETER_ELEMENT;
-		propertyName = ScalarParameterHandle.PROMPT_TEXT_ID_PROP;
 		super.buildUI( parent );
+		container.setLayout( WidgetUtil.createGridLayout( 5 ,15) );
+	
+		buildsections(ScalarParameterHandle.PROMPT_TEXT_ID_PROP,ReportDesignConstants.SCALAR_PARAMETER_ELEMENT,I18N_I18N_PROMPT_TEXT);
+		buildsections(ScalarParameterHandle.HELP_TEXT_KEY_PROP,ReportDesignConstants.SCALAR_PARAMETER_ELEMENT,I18N_I18N_HELP_TEXT);
+		
+		LabelSection labelSection = new LabelSection( MESSAGE_NOTE, container, true );
+		labelSection.setGridPlaceholder( 3, true );
+		labelSection.setWidth( 350 );
+		labelSection.setFillLabel( true );
+		addSection( PageSectionId.I18N_LABEL, labelSection );
+		
+		createSections( );
+		layoutSections( );
+	}
+	
+	protected void buildsections(String propertyName, String elementName, String pageSectionId)
+	{		
+
+		ResourceKeyDescriptorProvider i18nProvider = new ResourceKeyDescriptorProvider( propertyName,
+				elementName );
+		ResourceKeySection i18nSection = new ResourceKeySection( i18nProvider.getDisplayName( ),
+				container,
+				true );
+		i18nSection.setProvider( i18nProvider );
+		i18nSection.setWidth( 350 );
+		i18nSection.setGridPlaceholder( 3, true );
+		addSection( pageSectionId, i18nSection );
 	}
 
 }
