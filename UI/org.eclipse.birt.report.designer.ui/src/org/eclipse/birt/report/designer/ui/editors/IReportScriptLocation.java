a924e85362d8485a1e138d1814bd30d9b952ab60
Zhiqiang Qian <zqian@actuate.com>
2007-11-07 06:21:56 +0000

    - Bugzilla Bug (s) Resolved:
    - Description:
    add interface to support script location

---
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/editors/IReportScriptLocation.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.ui.editors;
+
+/**
+ * IReportScriptLocation
+ */
+public interface IReportScriptLocation
+{
+
+	/**
+	 * @return Returns the full path of associated report file of current script
+	 *         location
+	 */
+	String getReportFileName( );
+
+	/**
+	 * @return Returns the unique ID for current script location in associated
+	 *         report
+	 */
+	String getID( );
+
+	/**
+	 * @return Returns the line number of current script location.
+	 */
+	int getLineNumber( );
+}
