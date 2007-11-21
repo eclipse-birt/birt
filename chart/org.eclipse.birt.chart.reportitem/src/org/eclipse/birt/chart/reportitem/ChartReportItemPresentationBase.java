26c874f1388a5c15bcda52eab9a6b1f942e86683
yulin <>
2007-11-21 06:27:26 +0000

    Check in Bug 210490 Caused by null IPrepareQuery when checking IBaseQueryDefinition's groups. Since DtE can't return IBaseQueryDefinition in web viewer's case and Chart can't get IBaseQueryDefinition from report engine, remove the group logic and complete the group enhancement #208409 later.

--- chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemPresentationBase.java
+++ chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemPresentationBase.java
@@ -425,17 +425,18 @@ public class ChartReportItemPresentationBase extends ReportItemPresentationBase
 	{
 		if ( set instanceof IQueryResultSet )
 		{
-			List groups = ( (IQueryResultSet) set ).getResultIterator( )
-					.getQueryResults( )
-					.getPreparedQuery( )
-					.getReportQueryDefn( )
-					.getGroups( );
-
-			if ( groups == null || groups.size( ) == 0 )
-			{
-				return new BIRTQueryResultSetEvaluator( (IQueryResultSet) set );
-			}
-			return new BIRTGroupedDataRowExpressionEvaluator( (IQueryResultSet) set );
+			//Remove the logic for smoke and complete the group enhancement
+//			List groups = ( (IQueryResultSet) set ).getResultIterator( )
+//					.getQueryResults( )
+//					.getPreparedQuery( )
+//					.getReportQueryDefn( )
+//					.getGroups( );
+//
+//			if ( groups != null && groups.size( ) > 0 )
+//			{
+//				return new BIRTGroupedDataRowExpressionEvaluator( (IQueryResultSet) set );
+//			}
+			return new BIRTQueryResultSetEvaluator( (IQueryResultSet) set );
 		}
 		// if ( set instanceof ICubeResultSet )
 		// {
