fac7b339e68bf1268507df486076876b753c7958
xiazhang <>
2007-12-05 07:05:54 +0000

    Revert the last checkin which is needless.

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/executor/CacheMapManager.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/executor/CacheMapManager.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -49,7 +48,7 @@ class CacheMapManager
 	 * cache directory map for disk based cache( disk cache and incremental
 	 * cache )
 	 */
-	private Map cacheDirMap = new Hashtable( );
+	private Map cacheDirMap = new HashMap( );
 	
 	private String tempDir;
 	/**
