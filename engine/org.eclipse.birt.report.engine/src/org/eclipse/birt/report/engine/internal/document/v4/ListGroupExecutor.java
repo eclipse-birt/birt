529bf61d6b1f5b1c4f403e12246a64ba7c74b80d
Wei Yan <wyan@actuate.com>
2008-01-03 03:12:41 +0000

    214082: missing group in render

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ListGroupExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ListGroupExecutor.java
@@ -1,5 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.engine.internal.document.v4;
+
+import org.eclipse.birt.report.engine.content.IContent;
 
 public class ListGroupExecutor extends GroupExecutor
 {
@@ -7,5 +19,10 @@ public class ListGroupExecutor extends GroupExecutor
 	protected ListGroupExecutor( ExecutorManager manager )
 	{
 		super( manager, ExecutorManager.LISTGROUPITEM );
+	}
+
+	protected IContent doCreateContent( )
+	{
+		return report.createListGroupContent( );
 	}
 }
