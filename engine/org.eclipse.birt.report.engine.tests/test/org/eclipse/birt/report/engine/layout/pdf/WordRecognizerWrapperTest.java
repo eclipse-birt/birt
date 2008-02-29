e69887a2e8b86099eff9c3265b4a18c1ce7e6390
Jun Ouyang <jouyang@actuate.com>
2008-02-29 07:16:09 +0000

    Fixed Bugzilla ¨C Bug 220889 [Automation][Regression]Exception is thrown out when previewing report in PDF

--- engine/org.eclipse.birt.report.engine.tests/test/org/eclipse/birt/report/engine/layout/pdf/WordRecognizerWrapperTest.java
+++ engine/org.eclipse.birt.report.engine.tests/test/org/eclipse/birt/report/engine/layout/pdf/WordRecognizerWrapperTest.java
@@ -8,6 +8,8 @@ import org.eclipse.birt.report.engine.layout.pdf.hyphen.Word;
 public class WordRecognizerWrapperTest extends TestCase
 {
 	final String testStr = " This is a test! 中国 ￥2000 $3446 \"Apple\" ";
+	final String testStr2 = "(";
+	final String testStr3 = ")";
 	
 	public void test( )
 	{
@@ -38,5 +40,12 @@ public class WordRecognizerWrapperTest extends TestCase
 		assertTrue("$3446 ".equals( word.getValue( ) ));
 		word = wr.getNextWord( );
 		assertTrue("\"Apple\" ".equals( word.getValue( ) ));
+		
+		wr = new WordRecognizerWrapper(testStr2, java.util.Locale.ENGLISH);
+		word = wr.getNextWord( );
+		assertTrue("(".equals( word.getValue( ) ));
+		wr = new WordRecognizerWrapper(testStr3, java.util.Locale.ENGLISH);
+		word = wr.getNextWord( );
+		assertTrue(")".equals( word.getValue( ) ));
 	}
 }
