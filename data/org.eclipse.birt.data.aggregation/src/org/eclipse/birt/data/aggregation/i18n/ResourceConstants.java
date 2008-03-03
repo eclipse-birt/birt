5c176d54df008916c040a9beb7fda6b02276a8a2
jili <>
2008-03-03 07:12:50 +0000

    CheckIn: Change source location of plugin org.eclipse.birt.data.aggregation

---
+++ data/org.eclipse.birt.data.aggregation/src/org/eclipse/birt/data/aggregation/i18n/ResourceConstants.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2008 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.data.aggregation.i18n;
+
+/**
+ * 
+ */
+
+public class ResourceConstants
+{
+
+	public final static String UNSUPPORTED_DATA_TYPE = "aggregation.BadOperandType";//$NON-NLS-1$
+	public final static String DATATYPEUTIL_ERROR = "aggregation.DataConversionError";//$NON-NLS-1$
+	public final static String BAD_PARAM_COUNT = "aggregation.BadParameterCount";//$NON-NLS-1$
+	public final static String BAD_PARAM_TYPE = "aggregation.BadParameterType";//$NON-NLS-1$
+	public final static String NO_SOLUTION_FOUND = "aggregation.NoIrrSolution";//$NON-NLS-1$
+	public final static String INVALID_PERCENTILE_COLUMN = "aggregation.InvalidPercentileColumn";//$NON-NLS-1$
+	public final static String INVALID_TOP_BOTTOM_ARGUMENT = "aggregation.InvalidTopBottomArgument";//$NON-NLS-1$
+	public final static String INVALID_TOP_BOTTOM_N_ARGUMENT = "aggregation.InvalidTopBottomNArgument";//$NON-NLS-1$
+	public final static String INVALID_TOP_BOTTOM_PERCENT_ARGUMENT = "aggregation.InvalidTopBottomPercentArgument";//$NON-NLS-1$
+	public final static String INVALID_TOP_BOTTOM_N = "aggregation.InvalidTopBottomN";//$NON-NLS-1$
+	public final static String INVALID_PERCENTILE_ARGUMENT = "aggregation.InvalidPercentileArgument";//$NON-NLS-1$
+	public final static String INVALID_QUARTILE_ARGUMENT = "aggregation.InvalidQuartileArgument";//$NON-NLS-1$
+	public final static String ILLEGAL_PARAMETER_FUN = "aggregation.BadAggrFuncParam";//$NON-NLS-1$
+	public final static String RESET_RATE = "aggregation.ResetRateGuess";//$NON-NLS-1$
+
+}
