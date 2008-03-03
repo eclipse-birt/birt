85e103e32af038d7e9828e74294a0a191b237028
Zhiqiang Qian <zqian@actuate.com>
2008-03-03 06:37:09 +0000

    - Bugzilla Bug (s) Resolved:
    - Description:
    remove local style handling in Crosstab executor, this is handled by report engine now.

--- xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/re/executor/ContentUtil.java
+++ xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/re/executor/ContentUtil.java
@@ -25,16 +25,13 @@ import org.eclipse.birt.report.engine.extension.IBaseResultSet;
 import org.eclipse.birt.report.engine.extension.IExecutorContext;
 import org.eclipse.birt.report.engine.ir.DimensionType;
 import org.eclipse.birt.report.item.crosstab.core.de.AbstractCrosstabItemHandle;
-import org.eclipse.birt.report.item.crosstab.core.de.CrosstabCellHandle;
 import org.eclipse.birt.report.model.api.DimensionHandle;
-import org.eclipse.birt.report.model.api.FactoryPropertyHandle;
 import org.eclipse.birt.report.model.api.HideRuleHandle;
 import org.eclipse.birt.report.model.api.HighlightRuleHandle;
 import org.eclipse.birt.report.model.api.MemberHandle;
 import org.eclipse.birt.report.model.api.ReportElementHandle;
 import org.eclipse.birt.report.model.api.ReportItemHandle;
 import org.eclipse.birt.report.model.api.StructureHandle;
-import org.eclipse.birt.report.model.api.StyleHandle;
 import org.eclipse.birt.report.model.api.TOCHandle;
 import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 import org.eclipse.birt.report.model.api.elements.structures.HighlightRule;
@@ -81,32 +78,39 @@ class ContentUtil
 			return null;
 		}
 
-		IStyle style = reportContent.createStyle( );
+		// !!! local style is processed in reprot engine now, just need process
+		// highlight here
 
-		if ( styleCache != null && handle instanceof CrosstabCellHandle )
-		{
-			// only cache crosstab cell styles
-			IStyle cachedStyle = (IStyle) styleCache.get( modelHandle );
+		// IStyle style = reportContent.createStyle( );
 
-			if ( cachedStyle != null )
-			{
-				style.setProperties( cachedStyle );
-			}
-			else
-			{
-				setupPrivateStyle( modelHandle, style );
+		// if ( styleCache != null && handle instanceof CrosstabCellHandle )
+		// {
+		// // only cache crosstab cell styles
+		// IStyle cachedStyle = (IStyle) styleCache.get( modelHandle );
+		//
+		// if ( cachedStyle != null )
+		// {
+		// style.setProperties( cachedStyle );
+		// }
+		// else
+		// {
+		// setupPrivateStyle( modelHandle, style );
+		//
+		// styleCache.put( modelHandle, style );
+		// }
+		// }
+		// else
+		// {
+		// setupPrivateStyle( modelHandle, style );
+		// }
 
-				styleCache.put( modelHandle, style );
-			}
-		}
-		else
-		{
-			setupPrivateStyle( modelHandle, style );
-		}
+		IStyle style = null;
 
 		// process highlight
 		if ( evaluator != null )
 		{
+			style = reportContent.createStyle( );
+
 			IStyle highlightStyle = reportContent.createStyle( );
 
 			setupHighlightStyle( modelHandle, highlightStyle, evaluator );
@@ -127,8 +131,8 @@ class ContentUtil
 		return style;
 	}
 
-	static void setupHighlightStyle( ReportElementHandle handle, IStyle style,
-			IBaseResultSet evaluator ) throws BirtException
+	private static void setupHighlightStyle( ReportElementHandle handle,
+			IStyle style, IBaseResultSet evaluator ) throws BirtException
 	{
 		Iterator itr = handle.getPrivateStyle( ).highlightRulesIterator( );
 
@@ -258,7 +262,7 @@ class ContentUtil
 		return;
 	}
 
-	static String validExpression( String expr )
+	private static String validExpression( String expr )
 	{
 		if ( expr != null && expr.trim( ).length( ) > 0 )
 		{
@@ -267,7 +271,7 @@ class ContentUtil
 		return null;
 	}
 
-	static ReportItemHandle getReportItemHandle(
+	private static ReportItemHandle getReportItemHandle(
 			AbstractCrosstabItemHandle handle )
 	{
 		if ( handle != null
@@ -278,7 +282,7 @@ class ContentUtil
 		return null;
 	}
 
-	static ReportElementHandle getReportElementHandle(
+	private static ReportElementHandle getReportElementHandle(
 			AbstractCrosstabItemHandle handle )
 	{
 		if ( handle != null
@@ -289,132 +293,146 @@ class ContentUtil
 		return null;
 	}
 
-	static void setupPrivateStyle( ReportElementHandle handle, IStyle style )
-	{
-		// Background
-		style.setBackgroundColor( getElementProperty( handle,
-				Style.BACKGROUND_COLOR_PROP,
-				true ) );
-		style.setBackgroundImage( getElementProperty( handle,
-				Style.BACKGROUND_IMAGE_PROP ) );
-		style.setBackgroundPositionX( getElementProperty( handle,
-				Style.BACKGROUND_POSITION_X_PROP ) );
-		style.setBackgroundPositionY( getElementProperty( handle,
-				Style.BACKGROUND_POSITION_Y_PROP ) );
-		style.setBackgroundRepeat( getElementProperty( handle,
-				Style.BACKGROUND_REPEAT_PROP ) );
+	// private static void setupPrivateStyle( ReportElementHandle handle,
+	// IStyle style )
+	// {
+	// // Background
+	// style.setBackgroundColor( getElementProperty( handle,
+	// Style.BACKGROUND_COLOR_PROP,
+	// true ) );
+	// style.setBackgroundImage( getElementProperty( handle,
+	// Style.BACKGROUND_IMAGE_PROP ) );
+	// style.setBackgroundPositionX( getElementProperty( handle,
+	// Style.BACKGROUND_POSITION_X_PROP ) );
+	// style.setBackgroundPositionY( getElementProperty( handle,
+	// Style.BACKGROUND_POSITION_Y_PROP ) );
+	// style.setBackgroundRepeat( getElementProperty( handle,
+	// Style.BACKGROUND_REPEAT_PROP ) );
+	//
+	// // Text related
+	// style.setTextAlign( getElementProperty( handle, Style.TEXT_ALIGN_PROP )
+	// );
+	// style.setTextIndent( getElementProperty( handle, Style.TEXT_INDENT_PROP )
+	// );
+	//
+	// style.setTextUnderline( getElementProperty( handle,
+	// Style.TEXT_UNDERLINE_PROP ) );
+	//
+	// style.setTextLineThrough( getElementProperty( handle,
+	// Style.TEXT_LINE_THROUGH_PROP ) );
+	// style.setTextOverline( getElementProperty( handle,
+	// Style.TEXT_OVERLINE_PROP ) );
+	//
+	// style.setLetterSpacing( getElementProperty( handle,
+	// Style.LETTER_SPACING_PROP ) );
+	// style.setLineHeight( getElementProperty( handle, Style.LINE_HEIGHT_PROP )
+	// );
+	// style.setOrphans( getElementProperty( handle, Style.ORPHANS_PROP ) );
+	// style.setTextTransform( getElementProperty( handle,
+	// Style.TEXT_TRANSFORM_PROP ) );
+	// style.setVerticalAlign( getElementProperty( handle,
+	// Style.VERTICAL_ALIGN_PROP ) );
+	// style.setWhiteSpace( getElementProperty( handle, Style.WHITE_SPACE_PROP )
+	// );
+	// style.setWidows( getElementProperty( handle, Style.WIDOWS_PROP ) );
+	// style.setWordSpacing( getElementProperty( handle,
+	// Style.WORD_SPACING_PROP ) );
+	//
+	// // Section properties
+	// style.setDisplay( getElementProperty( handle, Style.DISPLAY_PROP ) );
+	// style.setMasterPage( getElementProperty( handle, Style.MASTER_PAGE_PROP )
+	// );
+	// String pageBreakAfter = getElementProperty( handle,
+	// StyleHandle.PAGE_BREAK_AFTER_PROP );
+	// style.setPageBreakAfter( decodePageBreak( pageBreakAfter ) );
+	// String pageBreakBefore = getElementProperty( handle,
+	// StyleHandle.PAGE_BREAK_BEFORE_PROP );
+	// style.setPageBreakBefore( decodePageBreak( pageBreakBefore ) );
+	//
+	// style.setPageBreakInside( getElementProperty( handle,
+	// Style.PAGE_BREAK_INSIDE_PROP ) );
+	//
+	// // Font related
+	// style.setFontFamily( getElementProperty( handle, Style.FONT_FAMILY_PROP )
+	// );
+	// style.setColor( getElementProperty( handle, Style.COLOR_PROP, true ) );
+	// style.setFontSize( getElementProperty( handle, Style.FONT_SIZE_PROP ) );
+	// style.setFontStyle( getElementProperty( handle, Style.FONT_STYLE_PROP )
+	// );
+	// style.setFontWeight( getElementProperty( handle, Style.FONT_WEIGHT_PROP )
+	// );
+	// style.setFontVariant( getElementProperty( handle,
+	// Style.FONT_VARIANT_PROP ) );
+	//
+	// // Border
+	// style.setBorderBottomColor( getElementProperty( handle,
+	// Style.BORDER_BOTTOM_COLOR_PROP,
+	// true ) );
+	// style.setBorderBottomStyle( getElementProperty( handle,
+	// Style.BORDER_BOTTOM_STYLE_PROP ) );
+	// style.setBorderBottomWidth( getElementProperty( handle,
+	// Style.BORDER_BOTTOM_WIDTH_PROP ) );
+	// style.setBorderLeftColor( getElementProperty( handle,
+	// Style.BORDER_LEFT_COLOR_PROP,
+	// true ) );
+	// style.setBorderLeftStyle( getElementProperty( handle,
+	// Style.BORDER_LEFT_STYLE_PROP ) );
+	// style.setBorderLeftWidth( getElementProperty( handle,
+	// Style.BORDER_LEFT_WIDTH_PROP ) );
+	// style.setBorderRightColor( getElementProperty( handle,
+	// Style.BORDER_RIGHT_COLOR_PROP,
+	// true ) );
+	// style.setBorderRightStyle( getElementProperty( handle,
+	// Style.BORDER_RIGHT_STYLE_PROP ) );
+	// style.setBorderRightWidth( getElementProperty( handle,
+	// Style.BORDER_RIGHT_WIDTH_PROP ) );
+	// style.setBorderTopColor( getElementProperty( handle,
+	// Style.BORDER_TOP_COLOR_PROP,
+	// true ) );
+	// style.setBorderTopStyle( getElementProperty( handle,
+	// Style.BORDER_TOP_STYLE_PROP ) );
+	// style.setBorderTopWidth( getElementProperty( handle,
+	// Style.BORDER_TOP_WIDTH_PROP ) );
+	//
+	// // Margin
+	// style.setMarginTop( getElementProperty( handle, Style.MARGIN_TOP_PROP )
+	// );
+	// style.setMarginLeft( getElementProperty( handle, Style.MARGIN_LEFT_PROP )
+	// );
+	// style.setMarginBottom( getElementProperty( handle,
+	// Style.MARGIN_BOTTOM_PROP ) );
+	// style.setMarginRight( getElementProperty( handle,
+	// Style.MARGIN_RIGHT_PROP ) );
+	//
+	// // Padding
+	// style.setPaddingTop( getElementProperty( handle, Style.PADDING_TOP_PROP )
+	// );
+	// style.setPaddingLeft( getElementProperty( handle,
+	// Style.PADDING_LEFT_PROP ) );
+	// style.setPaddingBottom( getElementProperty( handle,
+	// Style.PADDING_BOTTOM_PROP ) );
+	// style.setPaddingRight( getElementProperty( handle,
+	// Style.PADDING_RIGHT_PROP ) );
+	//
+	// // Data Formatting
+	// style.setNumberAlign( getElementProperty( handle,
+	// Style.NUMBER_ALIGN_PROP ) );
+	// style.setDateFormat( getElementProperty( handle,
+	// Style.DATE_TIME_FORMAT_PROP ) );
+	// style.setNumberFormat( getElementProperty( handle,
+	// Style.NUMBER_FORMAT_PROP ) );
+	// style.setStringFormat( getElementProperty( handle,
+	// Style.STRING_FORMAT_PROP ) );
+	//
+	// // Others
+	// style.setCanShrink( getElementProperty( handle, Style.CAN_SHRINK_PROP )
+	// );
+	// style.setShowIfBlank( getElementProperty( handle,
+	// Style.SHOW_IF_BLANK_PROP ) );
+	// }
 
-		// Text related
-		style.setTextAlign( getElementProperty( handle, Style.TEXT_ALIGN_PROP ) );
-		style.setTextIndent( getElementProperty( handle, Style.TEXT_INDENT_PROP ) );
-
-		style.setTextUnderline( getElementProperty( handle,
-				Style.TEXT_UNDERLINE_PROP ) );
-
-		style.setTextLineThrough( getElementProperty( handle,
-				Style.TEXT_LINE_THROUGH_PROP ) );
-		style.setTextOverline( getElementProperty( handle,
-				Style.TEXT_OVERLINE_PROP ) );
-
-		style.setLetterSpacing( getElementProperty( handle,
-				Style.LETTER_SPACING_PROP ) );
-		style.setLineHeight( getElementProperty( handle, Style.LINE_HEIGHT_PROP ) );
-		style.setOrphans( getElementProperty( handle, Style.ORPHANS_PROP ) );
-		style.setTextTransform( getElementProperty( handle,
-				Style.TEXT_TRANSFORM_PROP ) );
-		style.setVerticalAlign( getElementProperty( handle,
-				Style.VERTICAL_ALIGN_PROP ) );
-		style.setWhiteSpace( getElementProperty( handle, Style.WHITE_SPACE_PROP ) );
-		style.setWidows( getElementProperty( handle, Style.WIDOWS_PROP ) );
-		style.setWordSpacing( getElementProperty( handle,
-				Style.WORD_SPACING_PROP ) );
-
-		// Section properties
-		style.setDisplay( getElementProperty( handle, Style.DISPLAY_PROP ) );
-		style.setMasterPage( getElementProperty( handle, Style.MASTER_PAGE_PROP ) );
-		String pageBreakAfter = getElementProperty( handle,
-				StyleHandle.PAGE_BREAK_AFTER_PROP );
-		style.setPageBreakAfter( decodePageBreak( pageBreakAfter ) );
-		String pageBreakBefore = getElementProperty( handle,
-				StyleHandle.PAGE_BREAK_BEFORE_PROP );
-		style.setPageBreakBefore( decodePageBreak( pageBreakBefore ) );
-
-		style.setPageBreakInside( getElementProperty( handle,
-				Style.PAGE_BREAK_INSIDE_PROP ) );
-
-		// Font related
-		style.setFontFamily( getElementProperty( handle, Style.FONT_FAMILY_PROP ) );
-		style.setColor( getElementProperty( handle, Style.COLOR_PROP, true ) );
-		style.setFontSize( getElementProperty( handle, Style.FONT_SIZE_PROP ) );
-		style.setFontStyle( getElementProperty( handle, Style.FONT_STYLE_PROP ) );
-		style.setFontWeight( getElementProperty( handle, Style.FONT_WEIGHT_PROP ) );
-		style.setFontVariant( getElementProperty( handle,
-				Style.FONT_VARIANT_PROP ) );
-
-		// Border
-		style.setBorderBottomColor( getElementProperty( handle,
-				Style.BORDER_BOTTOM_COLOR_PROP,
-				true ) );
-		style.setBorderBottomStyle( getElementProperty( handle,
-				Style.BORDER_BOTTOM_STYLE_PROP ) );
-		style.setBorderBottomWidth( getElementProperty( handle,
-				Style.BORDER_BOTTOM_WIDTH_PROP ) );
-		style.setBorderLeftColor( getElementProperty( handle,
-				Style.BORDER_LEFT_COLOR_PROP,
-				true ) );
-		style.setBorderLeftStyle( getElementProperty( handle,
-				Style.BORDER_LEFT_STYLE_PROP ) );
-		style.setBorderLeftWidth( getElementProperty( handle,
-				Style.BORDER_LEFT_WIDTH_PROP ) );
-		style.setBorderRightColor( getElementProperty( handle,
-				Style.BORDER_RIGHT_COLOR_PROP,
-				true ) );
-		style.setBorderRightStyle( getElementProperty( handle,
-				Style.BORDER_RIGHT_STYLE_PROP ) );
-		style.setBorderRightWidth( getElementProperty( handle,
-				Style.BORDER_RIGHT_WIDTH_PROP ) );
-		style.setBorderTopColor( getElementProperty( handle,
-				Style.BORDER_TOP_COLOR_PROP,
-				true ) );
-		style.setBorderTopStyle( getElementProperty( handle,
-				Style.BORDER_TOP_STYLE_PROP ) );
-		style.setBorderTopWidth( getElementProperty( handle,
-				Style.BORDER_TOP_WIDTH_PROP ) );
-
-		// Margin
-		style.setMarginTop( getElementProperty( handle, Style.MARGIN_TOP_PROP ) );
-		style.setMarginLeft( getElementProperty( handle, Style.MARGIN_LEFT_PROP ) );
-		style.setMarginBottom( getElementProperty( handle,
-				Style.MARGIN_BOTTOM_PROP ) );
-		style.setMarginRight( getElementProperty( handle,
-				Style.MARGIN_RIGHT_PROP ) );
-
-		// Padding
-		style.setPaddingTop( getElementProperty( handle, Style.PADDING_TOP_PROP ) );
-		style.setPaddingLeft( getElementProperty( handle,
-				Style.PADDING_LEFT_PROP ) );
-		style.setPaddingBottom( getElementProperty( handle,
-				Style.PADDING_BOTTOM_PROP ) );
-		style.setPaddingRight( getElementProperty( handle,
-				Style.PADDING_RIGHT_PROP ) );
-
-		// Data Formatting
-		style.setNumberAlign( getElementProperty( handle,
-				Style.NUMBER_ALIGN_PROP ) );
-		style.setDateFormat( getElementProperty( handle,
-				Style.DATE_TIME_FORMAT_PROP ) );
-		style.setNumberFormat( getElementProperty( handle,
-				Style.NUMBER_FORMAT_PROP ) );
-		style.setStringFormat( getElementProperty( handle,
-				Style.STRING_FORMAT_PROP ) );
-
-		// Others
-		style.setCanShrink( getElementProperty( handle, Style.CAN_SHRINK_PROP ) );
-		style.setShowIfBlank( getElementProperty( handle,
-				Style.SHOW_IF_BLANK_PROP ) );
-	}
-
-	static IStyle setupRuleStyle( StructureHandle highlight, IStyle style )
+	private static IStyle setupRuleStyle( StructureHandle highlight,
+			IStyle style )
 	{
 		String value;
 
@@ -651,7 +669,7 @@ class ContentUtil
 		return style;
 	}
 
-	static int toDteFilterOperator( String modelOpr )
+	private static int toDteFilterOperator( String modelOpr )
 	{
 		if ( modelOpr.equals( DesignChoiceConstants.FILTER_OPERATOR_EQ ) )
 			return IConditionalExpression.OP_EQ;
@@ -695,69 +713,73 @@ class ContentUtil
 		return IConditionalExpression.OP_NONE;
 	}
 
-	static String decodePageBreak( String pageBreak )
-	{
-		if ( pageBreak == null )
-		{
-			return null;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_AFTER_ALWAYS.equals( pageBreak ) )
-		{
-			return IStyle.CSS_ALWAYS_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_AFTER_ALWAYS_EXCLUDING_LAST.equals( pageBreak ) )
-		{
-			return IStyle.CSS_ALWAYS_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_AFTER_AUTO.equals( pageBreak ) )
-		{
-			return IStyle.CSS_AUTO_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_AFTER_AVOID.equals( pageBreak ) )
-		{
-			return IStyle.CSS_AVOID_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_ALWAYS.equals( pageBreak ) )
-		{
-			return IStyle.CSS_ALWAYS_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_ALWAYS_EXCLUDING_FIRST.equals( pageBreak ) )
-		{
-			return IStyle.CSS_ALWAYS_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_AUTO.equals( pageBreak ) )
-		{
-			return IStyle.CSS_AUTO_VALUE;
-		}
-		if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_AVOID.equals( pageBreak ) )
-		{
-			return IStyle.CSS_AVOID_VALUE;
-		}
-		return IStyle.CSS_AUTO_VALUE;
-	}
+	// private static String decodePageBreak( String pageBreak )
+	// {
+	// if ( pageBreak == null )
+	// {
+	// return null;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_AFTER_ALWAYS.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_ALWAYS_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_AFTER_ALWAYS_EXCLUDING_LAST.equals(
+	// pageBreak ) )
+	// {
+	// return IStyle.CSS_ALWAYS_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_AFTER_AUTO.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_AUTO_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_AFTER_AVOID.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_AVOID_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_ALWAYS.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_ALWAYS_VALUE;
+	// }
+	// if (
+	// DesignChoiceConstants.PAGE_BREAK_BEFORE_ALWAYS_EXCLUDING_FIRST.equals(
+	// pageBreak ) )
+	// {
+	// return IStyle.CSS_ALWAYS_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_AUTO.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_AUTO_VALUE;
+	// }
+	// if ( DesignChoiceConstants.PAGE_BREAK_BEFORE_AVOID.equals( pageBreak ) )
+	// {
+	// return IStyle.CSS_AVOID_VALUE;
+	// }
+	// return IStyle.CSS_AUTO_VALUE;
+	// }
 
-	static String getElementProperty( ReportElementHandle handle, String name )
-	{
-		return getElementProperty( handle, name, false );
-	}
+	// private static String getElementProperty( ReportElementHandle handle,
+	// String name )
+	// {
+	// return getElementProperty( handle, name, false );
+	// }
 
-	static String getElementProperty( ReportElementHandle handle, String name,
-			boolean isColorProperty )
-	{
-		FactoryPropertyHandle prop = handle.getFactoryPropertyHandle( name );
-		if ( prop != null && prop.isSet( ) )
-		{
-			if ( isColorProperty )
-			{
-				return prop.getColorValue( );
-			}
+	// private static String getElementProperty( ReportElementHandle handle,
+	// String name, boolean isColorProperty )
+	// {
+	// FactoryPropertyHandle prop = handle.getFactoryPropertyHandle( name );
+	// if ( prop != null && prop.isSet( ) )
+	// {
+	// if ( isColorProperty )
+	// {
+	// return prop.getColorValue( );
+	// }
+	//
+	// return prop.getStringValue( );
+	// }
+	// return null;
+	// }
 
-			return prop.getStringValue( );
-		}
-		return null;
-	}
-
-	static String getMemberProperty( StructureHandle handle, String name )
+	private static String getMemberProperty( StructureHandle handle, String name )
 	{
 		MemberHandle prop = handle.getMember( name );
 		if ( prop != null )
