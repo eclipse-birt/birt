f942a405492aa9aa985a017f309327409711ab1e
cchao <>
2008-03-04 04:46:10 +0000

    - Summary: The Width units of cell should be selected twice
    - Bugzilla Bug (s) Resolved: 221112
    
    - Description: The Width units of cell should be selected twice
    
    - Tests Description: Manual test.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    
    - Files Edited:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/views/property/widgets/DimensionCellEditor.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/views/property/widgets/DimensionCellEditor.java
@@ -27,7 +27,6 @@ import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
 
 /**
