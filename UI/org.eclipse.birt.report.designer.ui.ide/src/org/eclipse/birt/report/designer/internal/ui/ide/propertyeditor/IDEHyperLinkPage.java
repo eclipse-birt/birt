45b8a86e4d057ee4af492a1c08550ebffc64ba62
cchao <>
2008-03-24 08:35:25 +0000

    - Summary: [AS5] The "Hyperlink Options" dialogue will not pop up until click the button the second time [00]
    - Bugzilla Bug (s) Resolved: 223470
    
    - Description: A peculiar bug, more details please see bugzilla comment.
    
    - Tests Description: Manual test.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    
    - Files Edited:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui.ide/src/org/eclipse/birt/report/designer/internal/ui/ide/propertyeditor/IDEHyperLinkPage.java
+++ UI/org.eclipse.birt.report.designer.ui.ide/src/org/eclipse/birt/report/designer/internal/ui/ide/propertyeditor/IDEHyperLinkPage.java
@@ -1,33 +1,51 @@
+
 package org.eclipse.birt.report.designer.internal.ui.ide.propertyeditor;
 
-import org.eclipse.birt.report.designer.internal.ui.views.attributes.page.HyperLinkPage;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.page.AttributePage;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.page.PageSectionId;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.page.WidgetUtil;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.HyperLinkDescriptorProvider;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.TextAndButtonSection;
+import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Composite;
 
-
-public class IDEHyperLinkPage extends HyperLinkPage
+public class IDEHyperLinkPage extends AttributePage
 {
-	private TextAndButtonSection section;
+
+	private TextAndButtonSection hyperLinkSection;
 	private HyperLinkDescriptorProvider hyperLinkProvider;
 
-	public void applyCustomSections(){
-		hyperLinkProvider = new IDEHyperLinkDescriptorProvider();
-		section = (TextAndButtonSection)getSection( PageSectionId.HYPERLINK_HYPERLINK );
-		section.setProvider( hyperLinkProvider );
-		section.addSelectionListener( new SelectionAdapter( ) {
+	public void buildUI( Composite parent )
+	{
+		super.buildUI( parent );
+		container.setLayout( WidgetUtil.createGridLayout( 3, 15 ) );
+
+		hyperLinkProvider = new IDEHyperLinkDescriptorProvider( );
+		hyperLinkSection = new TextAndButtonSection( hyperLinkProvider.getDisplayName( ),
+				container,
+				true );
+		hyperLinkSection.setProvider( hyperLinkProvider );
+		hyperLinkSection.addSelectionListener( new SelectionAdapter( ) {
 
 			public void widgetSelected( SelectionEvent e )
 			{
 				if ( hyperLinkProvider.hyperLinkSelected( ) )
-					section.load( );
+					hyperLinkSection.load( );
 			}
 
 		} );
+		hyperLinkSection.setWidth( 300 );
+		hyperLinkSection.setButtonText( "..." ); //$NON-NLS-1$
+		hyperLinkSection.setButtonTooltipText( Messages.getString( "HyperLinkPage.toolTipText.Button" ) ); //$NON-NLS-1$
+		hyperLinkSection.setButtonIsComputeSize( true );
+		addSection( PageSectionId.HYPERLINK_HYPERLINK, hyperLinkSection );
+
+		createSections( );
+		layoutSections( );
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -36,10 +54,10 @@ public class IDEHyperLinkPage extends HyperLinkPage
 	public void refresh( )
 	{
 		super.refresh( );
-		if ( section != null &&
-				section.getButtonControl( ) != null )
+		if ( hyperLinkSection != null
+				&& hyperLinkSection.getButtonControl( ) != null )
 		{
-			section.getButtonControl( )
+			hyperLinkSection.getButtonControl( )
 					.setEnabled( hyperLinkProvider.isEnable( ) );
 		}
 	}
