e6689a9e06065cac9acaef8c2b2712e65b808a7c
Zhiqiang Qian <zqian@actuate.com>
2008-03-31 07:01:25 +0000

    - Bugzilla Bug (s) Resolved: 224505 - Description: According to engine change, move Crosstab empty value processing code to preparation phase

--- xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/re/CrosstabReportItemQuery.java
+++ xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/re/CrosstabReportItemQuery.java
@@ -15,7 +15,6 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.eclipse.birt.core.data.ExpressionUtil;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.data.engine.api.IDataQueryDefinition;
 import org.eclipse.birt.report.engine.extension.ReportItemQueryBase;
@@ -27,15 +26,8 @@ import org.eclipse.birt.report.item.crosstab.core.de.DimensionViewHandle;
 import org.eclipse.birt.report.item.crosstab.core.de.LevelViewHandle;
 import org.eclipse.birt.report.item.crosstab.core.de.MeasureViewHandle;
 import org.eclipse.birt.report.item.crosstab.core.i18n.Messages;
-import org.eclipse.birt.report.model.api.DataItemHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
-import org.eclipse.birt.report.model.api.PropertyHandle;
 import org.eclipse.birt.report.model.api.ReportElementHandle;
-import org.eclipse.birt.report.model.api.StructureFactory;
-import org.eclipse.birt.report.model.api.StyleHandle;
-import org.eclipse.birt.report.model.api.activity.SemanticException;
-import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
-import org.eclipse.birt.report.model.api.elements.structures.MapRule;
 import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
 
 /**
@@ -92,13 +84,11 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 				true,
 				true );
 
-		String emptyValue = crosstabItem.getEmptyCellValue( );
-
 		// build child element query
 		if ( context != null )
 		{
 			// process crosstab header
-			processChildQuery( cubeQuery, crosstabItem.getHeader( ), emptyValue );
+			processChildQuery( cubeQuery, crosstabItem.getHeader( ) );
 
 			// process measure
 			for ( int i = 0; i < crosstabItem.getMeasureCount( ); i++ )
@@ -106,14 +96,12 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 				// TODO check visibility?
 				MeasureViewHandle mv = crosstabItem.getMeasure( i );
 
-				processChildQuery( cubeQuery, mv.getCell( ), emptyValue );
-				processChildQuery( cubeQuery, mv.getHeader( ), emptyValue );
+				processChildQuery( cubeQuery, mv.getCell( ) );
+				processChildQuery( cubeQuery, mv.getHeader( ) );
 
 				for ( int j = 0; j < mv.getAggregationCount( ); j++ )
 				{
-					processChildQuery( cubeQuery,
-							mv.getAggregationCell( j ),
-							emptyValue );
+					processChildQuery( cubeQuery, mv.getAggregationCell( j ) );
 				}
 			}
 
@@ -130,10 +118,8 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 					{
 						LevelViewHandle lv = dv.getLevel( j );
 
-						processChildQuery( cubeQuery, lv.getCell( ), emptyValue );
-						processChildQuery( cubeQuery,
-								lv.getAggregationHeader( ),
-								emptyValue );
+						processChildQuery( cubeQuery, lv.getCell( ) );
+						processChildQuery( cubeQuery, lv.getAggregationHeader( ) );
 					}
 				}
 
@@ -152,10 +138,8 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 					{
 						LevelViewHandle lv = dv.getLevel( j );
 
-						processChildQuery( cubeQuery, lv.getCell( ), emptyValue );
-						processChildQuery( cubeQuery,
-								lv.getAggregationHeader( ),
-								emptyValue );
+						processChildQuery( cubeQuery, lv.getCell( ) );
+						processChildQuery( cubeQuery, lv.getAggregationHeader( ) );
 					}
 				}
 
@@ -163,11 +147,9 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 
 			// process grandtotal header
 			processChildQuery( cubeQuery,
-					crosstabItem.getGrandTotal( ROW_AXIS_TYPE ),
-					emptyValue );
+					crosstabItem.getGrandTotal( ROW_AXIS_TYPE ) );
 			processChildQuery( cubeQuery,
-					crosstabItem.getGrandTotal( COLUMN_AXIS_TYPE ),
-					emptyValue );
+					crosstabItem.getGrandTotal( COLUMN_AXIS_TYPE ) );
 		}
 
 		return new IDataQueryDefinition[]{
@@ -176,38 +158,13 @@ public class CrosstabReportItemQuery extends ReportItemQueryBase implements
 	}
 
 	private void processChildQuery( IDataQueryDefinition parent,
-			CrosstabCellHandle cell, String emptyVlaue )
+			CrosstabCellHandle cell )
 	{
 		if ( cell != null )
 		{
 			for ( Iterator itr = cell.getContents( ).iterator( ); itr.hasNext( ); )
 			{
 				ReportElementHandle handle = (ReportElementHandle) itr.next( );
-
-				// handle empty value mapping
-				if ( emptyVlaue != null && handle instanceof DataItemHandle )
-				{
-					DataItemHandle dataHandle = (DataItemHandle) handle;
-
-					MapRule rule = StructureFactory.createMapRule( );
-
-					rule.setTestExpression( ExpressionUtil.createJSDataExpression( dataHandle.getResultSetColumn( ) ) );
-					rule.setOperator( DesignChoiceConstants.MAP_OPERATOR_NULL );
-					rule.setDisplay( emptyVlaue );
-
-					PropertyHandle mapHandle = dataHandle.getPropertyHandle( StyleHandle.MAP_RULES_PROP );
-
-					try
-					{
-						mapHandle.addItem( rule );
-					}
-					catch ( SemanticException e )
-					{
-						logger.log( Level.SEVERE,
-								Messages.getString( "CrosstabReportItemQuery.error.register.empty.cell.value" ), //$NON-NLS-1$
-								e );
-					}
-				}
 
 				context.createQuery( parent, handle );
 			}
--- xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/script/internal/handler/CrosstabPreparationHandler.java
+++ xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/script/internal/handler/CrosstabPreparationHandler.java
@@ -12,7 +12,10 @@
 package org.eclipse.birt.report.item.crosstab.core.script.internal.handler;
 
 import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.eclipse.birt.core.data.ExpressionUtil;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.report.engine.extension.IPreparationContext;
 import org.eclipse.birt.report.item.crosstab.core.ICrosstabConstants;
@@ -22,12 +25,21 @@ import org.eclipse.birt.report.item.crosstab.core.de.CrosstabReportItemHandle;
 import org.eclipse.birt.report.item.crosstab.core.de.DimensionViewHandle;
 import org.eclipse.birt.report.item.crosstab.core.de.LevelViewHandle;
 import org.eclipse.birt.report.item.crosstab.core.de.MeasureViewHandle;
+import org.eclipse.birt.report.item.crosstab.core.i18n.Messages;
 import org.eclipse.birt.report.item.crosstab.core.script.ICrosstab;
 import org.eclipse.birt.report.item.crosstab.core.script.ICrosstabCell;
 import org.eclipse.birt.report.item.crosstab.core.script.internal.CrosstabCellImpl;
 import org.eclipse.birt.report.item.crosstab.core.script.internal.CrosstabImpl;
+import org.eclipse.birt.report.model.api.DataItemHandle;
 import org.eclipse.birt.report.model.api.DesignElementHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
+import org.eclipse.birt.report.model.api.PropertyHandle;
+import org.eclipse.birt.report.model.api.ReportElementHandle;
+import org.eclipse.birt.report.model.api.StructureFactory;
+import org.eclipse.birt.report.model.api.StyleHandle;
+import org.eclipse.birt.report.model.api.activity.SemanticException;
+import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
+import org.eclipse.birt.report.model.api.elements.structures.MapRule;
 
 /**
  * CrosstabPreparationHandler
@@ -35,6 +47,8 @@ import org.eclipse.birt.report.model.api.ExtendedItemHandle;
 public class CrosstabPreparationHandler extends BaseCrosstabEventHandler implements
 		ICrosstabConstants
 {
+
+	private static Logger logger = Logger.getLogger( CrosstabPreparationHandler.class.getName( ) );
 
 	private CrosstabScriptHandler handler;
 	private CrosstabReportItemHandle crosstab;
@@ -48,14 +62,14 @@ public class CrosstabPreparationHandler extends BaseCrosstabEventHandler impleme
 		String javaClass = modelHandle.getEventHandlerClass( );
 		String script = modelHandle.getOnPrepare( );
 
+		this.crosstab = crosstab;
+		this.context = context;
+
 		if ( ( javaClass == null || javaClass.trim( ).length( ) == 0 )
 				&& ( script == null || script.trim( ).length( ) == 0 ) )
 		{
 			return;
 		}
-
-		this.crosstab = crosstab;
-		this.context = context;
 
 		handler = createScriptHandler( modelHandle,
 				ICrosstabReportItemConstants.ON_PREPARE_METHOD,
@@ -66,24 +80,24 @@ public class CrosstabPreparationHandler extends BaseCrosstabEventHandler impleme
 
 	public void handle( ) throws BirtException
 	{
-		if ( handler == null )
+		if ( handler != null )
 		{
-			return;
+			ICrosstab crosstabItem = new CrosstabImpl( crosstab );
+
+			handler.callFunction( CrosstabScriptHandler.ON_PREPARE_CROSSTAB,
+					crosstabItem,
+					context );
 		}
-
-		ICrosstab crosstabItem = new CrosstabImpl( crosstab );
-
-		handler.callFunction( CrosstabScriptHandler.ON_PREPARE_CROSSTAB,
-				crosstabItem,
-				context );
 
 		handleChildren( );
 	}
 
 	private void handleChildren( ) throws BirtException
 	{
+		String emptyValue = crosstab.getEmptyCellValue( );
+
 		// process crosstab header
-		handleCell( crosstab.getHeader( ) );
+		handleCell( crosstab.getHeader( ), emptyValue );
 
 		// process column edge
 		if ( crosstab.getDimensionCount( COLUMN_AXIS_TYPE ) > 0 )
@@ -98,15 +112,15 @@ public class CrosstabPreparationHandler extends BaseCrosstabEventHandler impleme
 				{
 					LevelViewHandle lv = dv.getLevel( j );
 
-					handleCell( lv.getCell( ) );
-					handleCell( lv.getAggregationHeader( ) );
+					handleCell( lv.getCell( ), emptyValue );
+					handleCell( lv.getAggregationHeader( ), emptyValue );
 				}
 			}
 
 		}
 
 		// process column grandtotal header
-		handleCell( crosstab.getGrandTotal( COLUMN_AXIS_TYPE ) );
+		handleCell( crosstab.getGrandTotal( COLUMN_AXIS_TYPE ), emptyValue );
 
 		// process row edge
 		if ( crosstab.getDimensionCount( ROW_AXIS_TYPE ) > 0 )
@@ -121,15 +135,15 @@ public class CrosstabPreparationHandler extends BaseCrosstabEventHandler impleme
 				{
 					LevelViewHandle lv = dv.getLevel( j );
 
-					handleCell( lv.getCell( ) );
-					handleCell( lv.getAggregationHeader( ) );
+					handleCell( lv.getCell( ), emptyValue );
+					handleCell( lv.getAggregationHeader( ), emptyValue );
 				}
 			}
 
 		}
 
 		// process row grandtotal header
-		handleCell( crosstab.getGrandTotal( ROW_AXIS_TYPE ) );
+		handleCell( crosstab.getGrandTotal( ROW_AXIS_TYPE ), emptyValue );
 
 		// process measure
 		for ( int i = 0; i < crosstab.getMeasureCount( ); i++ )
@@ -137,34 +151,67 @@ public class CrosstabPreparationHandler extends BaseCrosstabEventHandler impleme
 			// TODO check visibility?
 			MeasureViewHandle mv = crosstab.getMeasure( i );
 
-			handleCell( mv.getHeader( ) );
-			handleCell( mv.getCell( ) );
+			handleCell( mv.getHeader( ), emptyValue );
+			handleCell( mv.getCell( ), emptyValue );
 
 			for ( int j = 0; j < mv.getAggregationCount( ); j++ )
 			{
-				handleCell( mv.getAggregationCell( j ) );
+				handleCell( mv.getAggregationCell( j ), emptyValue );
 			}
 		}
 	}
 
 	@SuppressWarnings("unchecked")
-	private void handleCell( CrosstabCellHandle cell ) throws BirtException
+	private void handleCell( CrosstabCellHandle cell, String emptyVlaue )
+			throws BirtException
 	{
 		if ( cell == null )
 		{
 			return;
 		}
 
-		ICrosstabCell cellItem = new CrosstabCellImpl( cell );
+		if ( handler != null )
+		{
+			ICrosstabCell cellItem = new CrosstabCellImpl( cell );
 
-		handler.callFunction( CrosstabScriptHandler.ON_PREPARE_CELL,
-				cellItem,
-				context );
+			handler.callFunction( CrosstabScriptHandler.ON_PREPARE_CELL,
+					cellItem,
+					context );
+		}
 
 		// prepare contents
 		for ( Iterator itr = cell.getContents( ).iterator( ); itr.hasNext( ); )
 		{
-			context.prepare( (DesignElementHandle) itr.next( ) );
+
+			ReportElementHandle handle = (ReportElementHandle) itr.next( );
+
+			context.prepare( handle );
+
+			// handle empty value mapping, this is done by adding an extra
+			// mapping rule to related data item.
+			if ( emptyVlaue != null && handle instanceof DataItemHandle )
+			{
+				DataItemHandle dataHandle = (DataItemHandle) handle;
+
+				MapRule rule = StructureFactory.createMapRule( );
+
+				rule.setTestExpression( ExpressionUtil.createJSDataExpression( dataHandle.getResultSetColumn( ) ) );
+				rule.setOperator( DesignChoiceConstants.MAP_OPERATOR_NULL );
+				rule.setDisplay( emptyVlaue );
+
+				PropertyHandle mapHandle = dataHandle.getPropertyHandle( StyleHandle.MAP_RULES_PROP );
+
+				try
+				{
+					mapHandle.addItem( rule );
+				}
+				catch ( SemanticException e )
+				{
+					logger.log( Level.SEVERE,
+							Messages.getString( "CrosstabReportItemQuery.error.register.empty.cell.value" ), //$NON-NLS-1$
+							e );
+				}
+			}
 		}
 	}
 }
