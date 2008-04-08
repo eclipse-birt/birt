4cd9faa126e9ecd4ee4108724ba926f8095d99bf
Rick Lu <rlu@actuate.com>
2008-04-08 08:42:52 +0000

    Revert last check for changes in Engine codes. see bug 224064.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/script/element/IGrid.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/script/element/IGrid.java
@@ -2,6 +2,7 @@
 package org.eclipse.birt.report.engine.api.script.element;
 
 import org.eclipse.birt.report.engine.api.script.ScriptException;
+import org.eclipse.birt.report.model.api.activity.SemanticException;
 
 /**
  * Represents a the design of a Grid in the scripting environment
@@ -29,43 +30,9 @@ public interface IGrid extends IReportItem
 	 * 
 	 * @param summary
 	 *            the summary
-	 * @throws ScriptException
+	 * @throws SemanticException
+	 *             if this property is locked.
 	 */
 	void setSummary( String summary ) throws ScriptException;
 
-	/**
-	 * Returns the caption text of this grid.
-	 * 
-	 * @return the caption text
-	 */
-
-	String getCaption( );
-
-	/**
-	 * Sets the caption text of this grid.
-	 * 
-	 * @param caption
-	 *            the caption text
-	 * @throws ScriptException
-	 */
-
-	void setCaption( String caption ) throws ScriptException;
-
-	/**
-	 * Returns the resource key of the caption.
-	 * 
-	 * @return the resource key of the caption
-	 */
-
-	String getCaptionKey( );
-
-	/**
-	 * Sets the resource key of the caption.
-	 * 
-	 * @param captionKey
-	 *            the resource key of the caption
-	 * @throws ScriptException
-	 */
-
-	void setCaptionKey( String captionKey ) throws ScriptException;
 }
