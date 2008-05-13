c78863ef4fa9acf9619b5fa815701be584518377
Linda Chan <lchan@actuate.com>
2008-05-13 21:58:20 +0000

    [231247] handle null connection profile instance; externalized strings

---
+++ data/org.eclipse.birt.report.data.oda.jdbc.dbprofile/src/org/eclipse/birt/report/data/oda/jdbc/dbprofile/nls/Messages.java
@@ -0,0 +1,36 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2008 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.birt.report.data.oda.jdbc.dbprofile.nls;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS
+{
+    private static final String BUNDLE_NAME = "org.eclipse.birt.report.data.oda.jdbc.dbprofile.nls.messages"; //$NON-NLS-1$
+
+    public static String connection_nullProfile;
+    public static String connection_openFailed;
+    public static String dBProfileStatement_nullPropertyNameArg;
+
+    static
+    {
+        // initialize resource bundle
+        NLS.initializeMessages( BUNDLE_NAME, Messages.class );
+    }
+
+    private Messages()
+    {
+    }
+}
