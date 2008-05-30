5f9d13b805abd4ba57eb4c6c0f919b2ed23c1d03
Jun Ouyang <jouyang@actuate.com>
2008-05-30 06:57:44 +0000

    Fix failed test.

--- engine/org.eclipse.birt.report.engine.tests/test/org/eclipse/birt/report/engine/data/dte/ResultSetIndexTest.java
+++ engine/org.eclipse.birt.report.engine.tests/test/org/eclipse/birt/report/engine/data/dte/ResultSetIndexTest.java
@@ -18,11 +18,12 @@ public class ResultSetIndexTest extends TestCase
 
 	void setupIndex( ResultSetIndex index )
 	{
-		index.addResultSet( "master-page", null, -1, "master-page" );
-		index.addResultSet( "query", null, -1, "query" );
-		index.addResultSet( "nest", "query", 0, "nest1" );
-		index.addResultSet( "nest", "query", 10, "nest2" );
-		index.addResultSet( "nest", "query", 12, "nest3" );
+		index.addResultSet( "master-page", null, "-1", "master-page" );
+		index.addResultSet( "query", null, "-1", "query" );
+		index.addResultSet( "nest", "query", "0", "nest1" );
+		index.addResultSet( "nest", "query", "10", "nest2" );
+		index.addResultSet( "nest", "query", "12", "nest3" );
+		index.addResultSet( "nest", "query", "String", "nest4" );
 	}
 
 	public void testResultSet( )
@@ -30,11 +31,12 @@ public class ResultSetIndexTest extends TestCase
 		ResultSetIndex index = new ResultSetIndex( );
 		setupIndex( index );
 		assertEquals( "master-page", index.getResultSet( "master-page", null,
-				-1 ) );
-		assertEquals( "query", index.getResultSet( "query", null, -1 ) );
-		assertEquals( "nest1", index.getResultSet( "nest", "query", 0 ) );
-		assertEquals( "nest1", index.getResultSet( "nest", "query", 9 ) );
-		assertEquals( "nest2", index.getResultSet( "nest", "query", 10 ) );
-		assertEquals( "nest3", index.getResultSet( "nest", "query", 12 ) );
+				"-1" ) );
+		assertEquals( "query", index.getResultSet( "query", null, "-1" ) );
+		assertEquals( "nest1", index.getResultSet( "nest", "query", "0" ) );
+		assertEquals( "nest1", index.getResultSet( "nest", "query", "9" ) );
+		assertEquals( "nest2", index.getResultSet( "nest", "query", "10" ) );
+		assertEquals( "nest3", index.getResultSet( "nest", "query", "12" ) );
+		assertEquals( "nest4", index.getResultSet( "nest", "query", "String" ) );
 	}
 }
