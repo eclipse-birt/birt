901ad59501fe2d65586dd656eec777bcb823a9f4
jcheng <>
2008-06-10 03:53:21 +0000

    Integrated patch from Snjezana Peco which fixes the installation part. Added fix for the BIRT runtime import.

---
+++ viewer/org.eclipse.birt.integration.wtp.ui/src/org/eclipse/birt/integration/wtp/ui/project/facet/BirtFacetUtilFactory.java
@@ -0,0 +1,24 @@
+package org.eclipse.birt.integration.wtp.ui.project.facet;
+
+import org.eclipse.jst.javaee.web.WebApp;
+import org.eclipse.jst.javaee.web.WebAppVersionType;
+
+
+public class BirtFacetUtilFactory
+{
+
+	public static IBirtFacetUtil getInstance(Object webApp) {
+		if (isWebApp25(webApp)) {
+			return new BirtFacetUtil25();
+		}
+		return new BirtFacetUtil();
+	}
+
+	public static boolean isWebApp25(final Object webApp) {
+		if (webApp instanceof WebApp
+				&& ((WebApp) webApp).getVersion() == WebAppVersionType._25_LITERAL)
+			return true;
+		return false;
+	}
+
+}
