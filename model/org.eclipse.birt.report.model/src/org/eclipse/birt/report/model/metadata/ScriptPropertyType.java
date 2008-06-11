33c284b3d6f9240ebca7c1cae67420309261dcf5
Rick Lu <rlu@actuate.com>
2008-06-11 06:53:35 +0000

    - Summary:Model trim the script input.
    - Bugzilla Bug (s) Resolved: bug 236559
    
    - Description:
    
    revised the code in scriptpropertytype.java and  updated test case. see bug 236559
    
    
    
    - Tests Description  :
    
    JUnit test
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    - Files Deleted:
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -  Notes to Documentation:

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/metadata/ScriptPropertyType.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/metadata/ScriptPropertyType.java
@@ -1,41 +1,43 @@
 /*******************************************************************************
-* Copyright (c) 2004 Actuate Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*  Actuate Corporation  - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.model.metadata;
 
+import org.eclipse.birt.report.model.api.metadata.PropertyValueException;
+import org.eclipse.birt.report.model.api.util.StringUtil;
 import org.eclipse.birt.report.model.core.Module;
 
 /**
- * Represents script property type. Scripts are stored as strings.
- * Scripts are not validated in the model; BIRT relies on run-time validation
- * in the scripting engine.
+ * Represents script property type. Scripts are stored as strings. Scripts are
+ * not validated in the model; BIRT relies on run-time validation in the
+ * scripting engine.
  */
 
 public class ScriptPropertyType extends TextualPropertyType
 {
-    /**
+
+	/**
 	 * Display name key.
 	 */
-	
+
 	private static final String DISPLAY_NAME_KEY = "Property.script"; //$NON-NLS-1$
 
 	/**
 	 * Constructor.
 	 */
-	
+
 	public ScriptPropertyType( )
 	{
-	    super( DISPLAY_NAME_KEY );
+		super( DISPLAY_NAME_KEY );
 	}
-	
 
 	/*
 	 * (non-Javadoc)
@@ -57,13 +59,44 @@ public class ScriptPropertyType extends TextualPropertyType
 		return SCRIPT_TYPE_NAME;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.birt.report.model.design.metadata.PropertyType#toString(java.lang.Object)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.birt.report.model.design.metadata.PropertyType#toString(java
+	 * .lang.Object)
 	 */
-	
+
 	public String toString( Module module, PropertyDefn defn, Object value )
 	{
 		return (String) value;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.TextualPropertyType#validateValue
+	 * (org.eclipse.birt.report.model.core.Module,
+	 * org.eclipse.birt.report.model.metadata.PropertyDefn, java.lang.Object)
+	 */
+	public Object validateValue( Module module, PropertyDefn defn, Object value )
+			throws PropertyValueException
+	{
+		if ( value == null )
+			return null;
+		if ( value instanceof String )
+		{
+			if ( ( (String) value ).length( ) > 0 )
+			{
+				return value;
+			}
+			return null;
+
+		}
+		throw new PropertyValueException( value,
+				PropertyValueException.DESIGN_EXCEPTION_INVALID_VALUE,
+				getTypeCode( ) );
 	}
 
 }
