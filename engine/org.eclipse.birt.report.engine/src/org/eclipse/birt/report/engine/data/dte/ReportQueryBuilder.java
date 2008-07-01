966d691aac449bcb715a5aa4e82649059c688d5f
jili <>
2008-07-01 05:42:05 +0000

    CheckIn: Fix part of bugzilla bug 238719. [Automation][Regression]Stack over flow error thrown out when preview report in HTML238719

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/data/dte/ReportQueryBuilder.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/data/dte/ReportQueryBuilder.java
@@ -1293,12 +1293,24 @@ public class ReportQueryBuilder
 					String argument = argumentHandle.getValue( );
 					if ( argument != null )
 					{
-						binding.addArgument( argumentHandle.getName(), new ScriptExpression( argument ) );
+						binding.addArgument( adaptArgumentName( argumentHandle ), new ScriptExpression( argument ) );
 					}
 				}
 			}
 			binding.setDataType( dbType );
 			transfer.addBinding( binding );
+		}
+
+		/**
+		 * 
+		 * @param argumentHandle
+		 * @return
+		 */
+		private String adaptArgumentName(
+				AggregationArgumentHandle argumentHandle )
+		{
+			final String name = argumentHandle.getName( );
+			return "Data Field".equals( name ) ? "Expression" : name;
 		}
 
 		/**
