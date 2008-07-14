daec6124ef1c0e3d0298c36baf6ef7fe122f709e
xizhang <>
2008-07-14 02:12:09 +0000

    - Summary: Add Reload CSS File menu to Styles node[0303]
    - Bugzilla Bug (s) Resolved:
    240119
    
    - Description:
    This action is added.
    
    
    - Tests Description  :
    Manual test
    
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    
    - Files Deleted:
    
    
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -  Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/outline/providers/StylesNodeProvider.java
+++ UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/outline/providers/StylesNodeProvider.java
@@ -19,6 +19,7 @@ import java.util.List;
 import org.eclipse.birt.report.designer.internal.ui.views.DefaultNodeProvider;
 import org.eclipse.birt.report.designer.internal.ui.views.actions.ImportCSSStyleAction;
 import org.eclipse.birt.report.designer.internal.ui.views.actions.InsertAction;
+import org.eclipse.birt.report.designer.internal.ui.views.actions.ReloadCssStyleAction;
 import org.eclipse.birt.report.designer.internal.ui.views.actions.UseCssStyleAction;
 import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.birt.report.designer.ui.IReportGraphicConstants;
@@ -69,6 +70,8 @@ public class StylesNodeProvider extends DefaultNodeProvider
 		menu.insertAfter( IWorkbenchActionConstants.MB_ADDITIONS,
 				new ImportCSSStyleAction( object ) ); //$NON-NLS-1$
 		
+		menu.insertAfter( IWorkbenchActionConstants.MB_ADDITIONS,
+				new ReloadCssStyleAction( object ) );
 		menu.insertAfter( IWorkbenchActionConstants.MB_ADDITIONS,new UseCssStyleAction(object) );
 
 	}
