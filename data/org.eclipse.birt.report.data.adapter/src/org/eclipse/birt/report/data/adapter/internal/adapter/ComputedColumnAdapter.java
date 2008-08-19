3df926d92c7157204a9729aaf92a87a0ca02e4be
Lin Zhu <lzhu@actuate.com>
2008-08-19 07:48:46 +0000

    CheckIn: Fix bugzilla bug 235095 The error should not be "Invalid bound column name: customernumber" [0705]

--- data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/internal/adapter/ComputedColumnAdapter.java
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/internal/adapter/ComputedColumnAdapter.java
@@ -14,11 +14,17 @@
 package org.eclipse.birt.report.data.adapter.internal.adapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.birt.data.engine.api.aggregation.AggregationManager;
+import org.eclipse.birt.data.engine.api.aggregation.IAggrFunction;
+import org.eclipse.birt.data.engine.api.aggregation.IParameterDefn;
 import org.eclipse.birt.data.engine.api.querydefn.ComputedColumn;
 import org.eclipse.birt.data.engine.api.querydefn.ScriptExpression;
+import org.eclipse.birt.data.engine.core.DataException;
 import org.eclipse.birt.report.data.adapter.api.AdapterException;
 import org.eclipse.birt.report.model.api.AggregationArgumentHandle;
 import org.eclipse.birt.report.model.api.ComputedColumnHandle;
@@ -31,28 +37,66 @@ public class ComputedColumnAdapter extends ComputedColumn
 	public ComputedColumnAdapter ( ComputedColumnHandle modelCmptdColumn ) throws AdapterException
 	{
 		
-		
 		super( modelCmptdColumn.getName( ),
 				modelCmptdColumn.getExpression( ),
 				org.eclipse.birt.report.data.adapter.api.DataAdapterUtil.adaptModelDataType( modelCmptdColumn.getDataType( ) ),
-				org.eclipse.birt.report.data.adapter.api.DataAdapterUtil.adaptModelAggregationType( modelCmptdColumn.getAggregateFunction( )),
-				modelCmptdColumn.getFilterExpression( ) == null ? null:new ScriptExpression( modelCmptdColumn.getFilterExpression( )),
-				populateArgument(modelCmptdColumn));
+				modelCmptdColumn.getAggregateFunction( ),
+				modelCmptdColumn.getFilterExpression( ) == null
+						? null
+						: new ScriptExpression( modelCmptdColumn.getFilterExpression( ) ),
+				populateArgument( modelCmptdColumn ) );
 	}
 	
+	/**
+	 * Populate the arguments to a List by the order of the IAggrFunction saved
+	 * 
+	 * @param modelCmptdColumn
+	 * @return
+	 */
 	private static List populateArgument( ComputedColumnHandle modelCmptdColumn )
 	{
-		List argument = new ArrayList();
-		Iterator it = modelCmptdColumn.argumentsIterator( );
-		while( it!= null && it.hasNext( ))
+		Map argumentList = new HashMap( );
+		Iterator argumentIter = modelCmptdColumn.argumentsIterator( );
+		while ( argumentIter.hasNext( ) )
 		{
-			AggregationArgumentHandle arg = (AggregationArgumentHandle)it.next( );
-			if( arg.getValue()!= null )
-				argument.add( new ScriptExpression( arg.getValue( ) ));
-			else
-				argument.add( new ScriptExpression( null ));
+			AggregationArgumentHandle handle = (AggregationArgumentHandle) argumentIter.next( );
+			argumentList.put( handle.getName( ),
+					new ScriptExpression( handle.getValue( ) ) );
 		}
-		return argument;
+
+		List orderedArgument = new ArrayList( );
+		if ( modelCmptdColumn.getAggregateFunction( ) != null )
+		{
+			IAggrFunction info = null;
+			try
+			{
+				info = AggregationManager.getInstance( )
+						.getAggregation( modelCmptdColumn.getAggregateFunction( ) );
+			}
+			catch ( DataException e )
+			{
+				e.printStackTrace();
+			}
+			if ( info != null )
+			{
+				IParameterDefn[] parameters = info.getParameterDefn( );
+
+				if ( parameters != null )
+				{
+					for ( int i = 0; i < parameters.length; i++ )
+					{
+						IParameterDefn pInfo = parameters[i];
+						if ( argumentList.get( pInfo.getName( ) ) != null )
+						{
+							orderedArgument.add( argumentList.get( pInfo.getName( ) ) );
+						}
+					}
+				}
+			}
+		}
+		return orderedArgument;
+
 	}
+	
 
 }
