db1e26d9250b7b5fdd17c9e3ebd639e3631be827
cchao <>
2008-08-28 09:42:05 +0000

    - Summary: [Regression]Unhandled exception occurs when using jar file in report design[0801]
    - Bugzilla Bug (s) Resolved: 245492
    
    - Description: Use URL toString method to replace getFile method
    
    - Tests Description: Manual test.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    
    - Files Edited:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/resourcelocator/ResourceLocator.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/resourcelocator/ResourceLocator.java
@@ -64,7 +64,7 @@ public class ResourceLocator
 
 	public static String relativize( URL url )
 	{
-		String path = url.getFile( );
+		String path = url.toString( );
 		if ( url.getProtocol( ).equals( "file" ) ) //$NON-NLS-1$
 		{
 			return relativize( path );
