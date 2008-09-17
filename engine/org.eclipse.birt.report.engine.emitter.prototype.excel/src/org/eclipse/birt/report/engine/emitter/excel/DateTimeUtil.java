21297cf01841693a3670286e7884652aa38c0899
Jun Ouyang <jouyang@actuate.com>
2008-09-17 05:37:24 +0000

    Fix bugzilla bug 247588:genReport a report including DATE type column will lead excel output failed to open

--- engine/org.eclipse.birt.report.engine.emitter.prototype.excel/src/org/eclipse/birt/report/engine/emitter/excel/DateTimeUtil.java
+++ engine/org.eclipse.birt.report.engine.emitter.prototype.excel/src/org/eclipse/birt/report/engine/emitter/excel/DateTimeUtil.java
@@ -12,10 +12,10 @@ public class DateTimeUtil
 
 	static
 	{
-		locale2Code.put( new Locale( "sq_", "" ), "[$-41C]" );
+		locale2Code.put( new Locale( "sq", "" ), "[$-41C]" );
 		locale2Code.put( new Locale( "sq", "AL" ), "[$-41C]" );
 
-		locale2Code.put( new Locale( "ar_", "" ), "[$-2010401]" );
+		locale2Code.put( new Locale( "ar", "" ), "[$-2010401]" );
 		locale2Code.put( new Locale( "ar", "DZ" ), "[$-2010401]" );
 		locale2Code.put( new Locale( "ar", "BH" ), "[$-2010401]" );
 		locale2Code.put( new Locale( "ar", "EG" ), "[$-2010401]" );
@@ -34,34 +34,34 @@ public class DateTimeUtil
 		locale2Code.put( new Locale( "ar", "AE" ), "[$-2010401]" );
 		locale2Code.put( new Locale( "ar", "YE" ), "[$-2010401]" );
 
-		locale2Code.put( new Locale( "be_", "" ), "[$-423]" );
+		locale2Code.put( new Locale( "be", "" ), "[$-423]" );
 		locale2Code.put( new Locale( "be", "BY" ), "[$-423]" );
 
-		locale2Code.put( new Locale( "bg_", "" ), "[$-402]" );
+		locale2Code.put( new Locale( "bg", "" ), "[$-402]" );
 		locale2Code.put( new Locale( "bg", "BG" ), "[$-402]" );
 
-		locale2Code.put( new Locale( "ca_", "" ), "[$-403]" );
+		locale2Code.put( new Locale( "ca", "" ), "[$-403]" );
 		locale2Code.put( new Locale( "ca", "ES" ), "[$-403]" );
 
-		locale2Code.put( new Locale( "zh_", "" ), "[$-804]" );
+		locale2Code.put( new Locale( "zh", "" ), "[$-804]" );
 		locale2Code.put( new Locale( "zh", "CN" ), "[$-804]" );
 		locale2Code.put( new Locale( "zh", "HK" ), "[$-804]" );
 		locale2Code.put( new Locale( "zh", "TW" ), "[$-804]" );
 
-		locale2Code.put( new Locale( "hr_", "" ), "[$-41A]" );
+		locale2Code.put( new Locale( "hr", "" ), "[$-41A]" );
 		locale2Code.put( new Locale( "hr", "HR" ), "[$-41A]" );
 
-		locale2Code.put( new Locale( "cs_", "" ), "[$-405]" );
+		locale2Code.put( new Locale( "cs", "" ), "[$-405]" );
 		locale2Code.put( new Locale( "cs", "CZ" ), "[$-405]" );
 
-		locale2Code.put( new Locale( "da_", "" ), "[$-406]" );
+		locale2Code.put( new Locale( "da", "" ), "[$-406]" );
 		locale2Code.put( new Locale( "da", "DK" ), "[$-406]" );
 
-		locale2Code.put( new Locale( "nl_", "" ), "[$-413]" );
+		locale2Code.put( new Locale( "nl", "" ), "[$-413]" );
 		locale2Code.put( new Locale( "nl", "BE" ), "[$-413]" );
 		locale2Code.put( new Locale( "nl", "NL" ), "[$-413]" );
 
-		locale2Code.put( new Locale( "en_", "" ), "[$-409]" );
+		locale2Code.put( new Locale( "en", "" ), "[$-409]" );
 		locale2Code.put( new Locale( "en", "AU" ), "[$-C09]" );
 		locale2Code.put( new Locale( "en", "CA" ), "[$-1009]" );
 		locale2Code.put( new Locale( "en", "IN" ), "[$-409]" );
@@ -71,81 +71,81 @@ public class DateTimeUtil
 		locale2Code.put( new Locale( "en", "GB" ), "[$-809]" );
 		locale2Code.put( new Locale( "en", "US" ), "[$-409]" );
 
-		locale2Code.put( new Locale( "et_", "" ), "[$-425]" );
+		locale2Code.put( new Locale( "et", "" ), "[$-425]" );
 		locale2Code.put( new Locale( "et", "EE" ), "[$-425]" );
 
-		locale2Code.put( new Locale( "fi_", "" ), "[$-40B]" );
+		locale2Code.put( new Locale( "fi", "" ), "[$-40B]" );
 		locale2Code.put( new Locale( "fi", "FI" ), "[$-40B]" );
 
-		locale2Code.put( new Locale( "fr_", "" ), "[$-40C]" );
+		locale2Code.put( new Locale( "fr","","" ), "[$-40C]" );
 		locale2Code.put( new Locale( "fr", "BE" ), "[$-40C]" );
 		locale2Code.put( new Locale( "fr", "CA" ), "[$-C0C]" );
 		locale2Code.put( new Locale( "fr", "FR" ), "[$-40C]" );
 		locale2Code.put( new Locale( "fr", "LU" ), "[$-40C]" );
 		locale2Code.put( new Locale( "fr", "CH" ), "[$-40C]" );
 
-		locale2Code.put( new Locale( "de_", "" ), "[$-407]" );
+		locale2Code.put( new Locale( "de", "" ), "[$-407]" );
 		locale2Code.put( new Locale( "de", "AT" ), "[$-C07]" );
 		locale2Code.put( new Locale( "de", "DE" ), "[$-407]" );
 		locale2Code.put( new Locale( "de", "LU" ), "[$-407]" );
 		locale2Code.put( new Locale( "de", "CH" ), "[$-807]" );
 
-		locale2Code.put( new Locale( "el_", "" ), "[$-408]" );
+		locale2Code.put( new Locale( "el", "" ), "[$-408]" );
 		locale2Code.put( new Locale( "el", "GR" ), "[$-408]" );
 
-		locale2Code.put( new Locale( "iw_", "" ), "[$-40D]" );
+		locale2Code.put( new Locale( "iw", "" ), "[$-40D]" );
 		locale2Code.put( new Locale( "iw", "IL" ), "[$-40D]" );
 
 		locale2Code.put( new Locale( "hi", "IN" ), "[$-3010439]" );
 
-		locale2Code.put( new Locale( "hu_", "" ), "[$-40E]" );
+		locale2Code.put( new Locale( "hu", "" ), "[$-40E]" );
 		locale2Code.put( new Locale( "hu", "HU" ), "[$-40E]" );
 
-		locale2Code.put( new Locale( "is_", "" ), "[$-40F]" );
+		locale2Code.put( new Locale( "is", "" ), "[$-40F]" );
 		locale2Code.put( new Locale( "is", "IS" ), "[$-40F]" );
 
-		locale2Code.put( new Locale( "it_", "" ), "[$-410]" );
+		locale2Code.put( new Locale( "it", "" ), "[$-410]" );
 		locale2Code.put( new Locale( "it", "IT" ), "[$-410]" );
 		locale2Code.put( new Locale( "it", "CH" ), "[$-410]" );
 
-		locale2Code.put( new Locale( "ja_", "" ), "[$-411]" );
+		locale2Code.put( new Locale( "ja", "" ), "[$-411]" );
 		locale2Code.put( new Locale( "ja", "JP" ), "[$-411]" );
 
-		locale2Code.put( new Locale( "ko_", "" ), "[$-412]" );
+		locale2Code.put( new Locale( "ko", "" ), "[$-412]" );
 		locale2Code.put( new Locale( "ko", "KR" ), "[$-412]" );
 
-		locale2Code.put( new Locale( "lv_", "" ), "[$-426]" );
+		locale2Code.put( new Locale( "lv", "" ), "[$-426]" );
 		locale2Code.put( new Locale( "lv", "LV" ), "[$-426]" );
 
-		locale2Code.put( new Locale( "lt_", "" ), "[$-427]" );
+		locale2Code.put( new Locale( "lt", "" ), "[$-427]" );
 		locale2Code.put( new Locale( "lt", "LT" ), "[$-427]" );
 
-		locale2Code.put( new Locale( "mk_", "" ), "[$-42F]" );
+		locale2Code.put( new Locale( "mk", "" ), "[$-42F]" );
 		locale2Code.put( new Locale( "mk", "MK" ), "[$-42F]" );
 
-		locale2Code.put( new Locale( "no_", "" ), "[$-414]" );
+		locale2Code.put( new Locale( "no", "" ), "[$-414]" );
 		locale2Code.put( new Locale( "no", "NO" ), "[$-414]" );
 
-		locale2Code.put( new Locale( "pl_", "" ), "[$-415]" );
+		locale2Code.put( new Locale( "pl", "" ), "[$-415]" );
 		locale2Code.put( new Locale( "pl", "PL" ), "[$-415]" );
 
-		locale2Code.put( new Locale( "pt_", "" ), "[$-816]" );
+		locale2Code.put( new Locale( "pt", "" ), "[$-816]" );
 		locale2Code.put( new Locale( "pt", "BR" ), "[$-416]" );
 		locale2Code.put( new Locale( "pt", "PT" ), "[$-816]" );
 
-		locale2Code.put( new Locale( "ro_", "" ), "[$-418]" );
+		locale2Code.put( new Locale( "ro", "" ), "[$-418]" );
 		locale2Code.put( new Locale( "ro", "RO" ), "[$-418]" );
 
-		locale2Code.put( new Locale( "ru_", "" ), "[$-419]" );
+		locale2Code.put( new Locale( "ru", "" ), "[$-419]" );
 		locale2Code.put( new Locale( "ru", "RU" ), "[$-419]" );
 
-		locale2Code.put( new Locale( "sk_", "" ), "[$-41B]" );
+		locale2Code.put( new Locale( "sk", "" ), "[$-41B]" );
 		locale2Code.put( new Locale( "sk", "SK" ), "[$-41B]" );
 
-		locale2Code.put( new Locale( "sl_", "" ), "[$-424]" );
+		locale2Code.put( new Locale( "sl", "" ), "[$-424]" );
 		locale2Code.put( new Locale( "sl", "SI" ), "[$-424]" );
 
-		locale2Code.put( new Locale( "es_", "" ), "[$-C0A]" );
+		locale2Code.put( new Locale( "es", "" ), "[$-C0A]" );
 		locale2Code.put( new Locale( "es", "AR" ), "[$-C0A]" );
 		locale2Code.put( new Locale( "es", "BO" ), "[$-C0A]" );
 		locale2Code.put( new Locale( "es", "CL" ), "[$-C0A]" );
@@ -166,24 +166,30 @@ public class DateTimeUtil
 		locale2Code.put( new Locale( "es", "UY" ), "[$-C0A]" );
 		locale2Code.put( new Locale( "es", "VE" ), "[$-C0A]" );
 
-		locale2Code.put( new Locale( "sv_", "" ), "[$-41D]" );
+		locale2Code.put( new Locale( "sv", "" ), "[$-41D]" );
 		locale2Code.put( new Locale( "sv", "SE" ), "[$-41D]" );
 
-		locale2Code.put( new Locale( "th_", "" ), "[$-41E]" );
+		locale2Code.put( new Locale( "th", "" ), "[$-41E]" );
 		locale2Code.put( new Locale( "th", "TH" ), "[$-41E]" );
 
-		locale2Code.put( new Locale( "tr_", "" ), "[$-41F]" );
+		locale2Code.put( new Locale( "tr", "" ), "[$-41F]" );
 		locale2Code.put( new Locale( "tr", "TR" ), "[$-41F]" );
 
-		locale2Code.put( new Locale( "uk_", "" ), "[$-422]" );
+		locale2Code.put( new Locale( "uk", "" ), "[$-422]" );
 		locale2Code.put( new Locale( "uk", "UA" ), "[$-422]" );
 
-		locale2Code.put( new Locale( "vi_", "" ), "[$-042A]" );
+		locale2Code.put( new Locale( "vi", "" ), "[$-042A]" );
 		locale2Code.put( new Locale( "vi", "VN" ), "[$-042A]" );
 	}
 
 	public static String formatDateTime( String format, Locale locale )
 	{
-		return locale2Code.get( locale ) + format;
+		String language = locale.getLanguage( );
+		String code = locale2Code.get( locale );
+		if ( code == null )
+		{
+			code = locale2Code.get( new Locale( language ) );
+		}
+		return code + format;
 	}
 }
