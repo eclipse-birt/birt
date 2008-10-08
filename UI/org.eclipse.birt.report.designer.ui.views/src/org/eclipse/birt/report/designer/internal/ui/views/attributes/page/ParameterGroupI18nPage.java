4e5b59108d22005f50095d6446f7efbb7d581104
xizhang <>
2008-10-08 06:15:33 +0000

    - Summary: Parameter Group Display Name Not Being Localized
    - Bugzilla Bug (s) Resolved:
    247163
    
    - Description:
    Supported prompt text in "Localization" property page for
    ParameterGroup and CascadingParameterGroup.
    Checked in head and 2.3.2 branch.
    
    
    - Tests Description  :
    Manual test
    
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    
    - Files Deleted:
    
    
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -  Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/ParameterGroupI18nPage.java
+++ UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/ParameterGroupI18nPage.java
@@ -11,6 +11,10 @@
 
 package org.eclipse.birt.report.designer.internal.ui.views.attributes.page;
 
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.ResourceKeyDescriptorProvider;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.LabelSection;
+import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.ResourceKeySection;
+import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.birt.report.model.api.ParameterGroupHandle;
 import org.eclipse.birt.report.model.api.elements.ReportDesignConstants;
 import org.eclipse.swt.widgets.Composite;
@@ -19,12 +23,42 @@ import org.eclipse.swt.widgets.Composite;
  * 
  */
 
-public class ParameterGroupI18nPage extends I18nPage
+public class ParameterGroupI18nPage extends AttributePage
 {
+	private static final String MESSAGE_NOTE = Messages.getString( "I18nPage.text.Note" ); //$NON-NLS-1$
+	public static final String I18N_I18N_PROMPT_TEXT = "I18N_I18N_PROMPT_TEXT"; //$NON-NLS-1$
+	public static final String I18N_I18N_HELP_TEXT = "I18N_I18N_HELP_TEXT"; //$NON-NLS-1$
+
 	public void buildUI( Composite parent  )
 	{
-		elementName = ReportDesignConstants.PARAMETER_GROUP_ELEMENT;
-		propertyName = ParameterGroupHandle.HELP_TEXT_KEY_PROP;
 		super.buildUI( parent );
+		container.setLayout( WidgetUtil.createGridLayout( 5 ,15) );
+	
+		buildsections(ParameterGroupHandle.PROMPT_TEXT_ID_PROP,ReportDesignConstants.PARAMETER_GROUP_ELEMENT,I18N_I18N_PROMPT_TEXT);
+		buildsections(ParameterGroupHandle.HELP_TEXT_KEY_PROP,ReportDesignConstants.PARAMETER_GROUP_ELEMENT,I18N_I18N_HELP_TEXT);
+		
+		LabelSection labelSection = new LabelSection( MESSAGE_NOTE, container, true );
+		labelSection.setGridPlaceholder( 3, true );
+		labelSection.setWidth( 350 );
+		labelSection.setFillLabel( true );
+		addSection( PageSectionId.I18N_LABEL, labelSection );
+		
+		createSections( );
+		layoutSections( );
 	}
+	
+	protected void buildsections(String propertyName, String elementName, String pageSectionId)
+	{		
+
+		ResourceKeyDescriptorProvider i18nProvider = new ResourceKeyDescriptorProvider( propertyName,
+				elementName );
+		ResourceKeySection i18nSection = new ResourceKeySection( i18nProvider.getDisplayName( ),
+				container,
+				true );
+		i18nSection.setProvider( i18nProvider );
+		i18nSection.setWidth( 350 );
+		i18nSection.setGridPlaceholder( 3, true );
+		addSection( pageSectionId, i18nSection );
+	}
+
 }
