e7bb56be0a4929ef2273fa700ccb25bbfc865668
xizhang <>
2008-10-27 08:25:54 +0000

    - Summary: Cannot insert Aggregation to the bottom of xtab total's cell.[1102]
    - Bugzilla Bug (s) Resolved:
    251803
    
    - Description:
    Fixed and checked into both head and 2.3.2 branch.
    
    - Tests Description  :
    Manual test
    
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    
    - Files Deleted:
    
    
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -  Notes to Documentation:

--- xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/AggregationDropAdapter.java
+++ xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/AggregationDropAdapter.java
@@ -11,6 +11,7 @@
 
 package org.eclipse.birt.report.item.crosstab.internal.ui;
 
+import org.eclipse.birt.report.designer.core.DesignerConstants;
 import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
 import org.eclipse.birt.report.designer.internal.ui.dialogs.DataColumnBindingDialog;
 import org.eclipse.birt.report.designer.internal.ui.dnd.DNDLocation;
@@ -19,6 +20,7 @@ import org.eclipse.birt.report.designer.internal.ui.dnd.IDropAdapter;
 import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
 import org.eclipse.birt.report.designer.ui.newelement.DesignElementFactory;
 import org.eclipse.birt.report.designer.util.DEUtil;
+import org.eclipse.birt.report.item.crosstab.core.ICrosstabCellConstants;
 import org.eclipse.birt.report.item.crosstab.core.de.CrosstabCellHandle;
 import org.eclipse.birt.report.item.crosstab.internal.ui.editors.editparts.CrosstabCellEditPart;
 import org.eclipse.birt.report.item.crosstab.internal.ui.editors.model.CrosstabCellAdapter;
@@ -26,6 +28,9 @@ import org.eclipse.birt.report.item.crosstab.internal.ui.editors.model.ICrosstab
 import org.eclipse.birt.report.model.api.CellHandle;
 import org.eclipse.birt.report.model.api.CommandStack;
 import org.eclipse.birt.report.model.api.DataItemHandle;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.requests.CreateRequest;
 import org.eclipse.jface.window.Window;
 
 /**
@@ -60,6 +65,11 @@ public class AggregationDropAdapter implements IDropAdapter
 	public boolean performDrop( Object transfer, Object target, int operation,
 			DNDLocation location )
 	{
+		
+		if(target instanceof EditPart)
+		{
+			EditPart editPart = (EditPart)target;
+		
 		CommandStack stack = SessionHandleAdapter.getInstance( )
 				.getCommandStack( );
 		stack.startTrans( "Add Aggregation" ); //$NON-NLS-1$
@@ -78,9 +88,30 @@ public class AggregationDropAdapter implements IDropAdapter
 
 			if ( dialog.open( ) == Window.OK )
 			{
-				dataHandle.setResultSetColumn( dialog.getBindingColumn( )
-						.getName( ) );
-				stack.commit( );
+				cellHandle.getModelHandle( ).getPropertyHandle( ICrosstabCellConstants.CONTENT_PROP ).removeItem( dataHandle );
+				CreateRequest request = new CreateRequest( );
+
+				request.getExtendedData( )
+						.put( DesignerConstants.KEY_NEWOBJECT, dataHandle );
+				request.setLocation( location.getPoint( ) );				
+				
+				Command command = editPart.getCommand( request );
+				if ( command != null && command.canExecute( ) )
+				{
+					dataHandle.setResultSetColumn( dialog.getBindingColumn( )
+							.getName( ) );		
+					
+					editPart.getViewer( )
+					.getEditDomain( )
+					.getCommandStack( )
+					.execute( command );
+					
+					stack.commit( );
+				}else
+				{
+					stack.rollback( );
+				}
+		
 			}
 			else
 			{
@@ -92,8 +123,73 @@ public class AggregationDropAdapter implements IDropAdapter
 			stack.rollback( );
 			ExceptionHandler.handle( e );
 		}
-
+		}
 		return true;
 	}
 
+//	public boolean performDrop( Object transfer, Object target, int operation,
+//			DNDLocation location )
+//	{
+//		
+//		if(target instanceof EditPart)
+//		{
+//			EditPart editPart = (EditPart)target;
+//		
+//		CommandStack stack = SessionHandleAdapter.getInstance( )
+//				.getCommandStack( );
+//		stack.startTrans( "Add Aggregation" ); //$NON-NLS-1$
+//
+//		DataItemHandle dataHandle = DesignElementFactory.getInstance( )
+//				.newDataItem( null );
+//
+//		CrosstabCellHandle cellHandle = ( (CrosstabCellAdapter) ( (CrosstabCellEditPart) target ).getModel( ) ).getCrosstabCellHandle( );
+//		try
+//		{
+//			cellHandle.addContent( dataHandle, CellHandle.CONTENT_SLOT );
+//
+//			DataColumnBindingDialog dialog = new DataColumnBindingDialog( true );
+//			dialog.setInput( dataHandle, null, cellHandle );
+//			dialog.setAggreate( true );
+//
+//			if ( dialog.open( ) == Window.OK )
+//			{
+//				cellHandle.getModelHandle( ).getPropertyHandle( ICrosstabCellConstants.CONTENT_PROP ).removeItem( dataHandle );
+//				CreateRequest request = new CreateRequest( );
+//
+//				request.getExtendedData( )
+//						.put( DesignerConstants.KEY_NEWOBJECT, dataHandle );
+//				request.setLocation( location.getPoint( ) );				
+//				
+//				Command command = editPart.getCommand( request );
+//				if ( command != null && command.canExecute( ) )
+//				{
+//					dataHandle.setResultSetColumn( dialog.getBindingColumn( )
+//							.getName( ) );		
+//					
+//					editPart.getViewer( )
+//					.getEditDomain( )
+//					.getCommandStack( )
+//					.execute( command );
+//					
+//					stack.commit( );
+//				}else
+//				{
+//					stack.rollback( );
+//				}
+//		
+//			}
+//			else
+//			{
+//				stack.rollback( );
+//			}
+//		}
+//		catch ( Exception e )
+//		{
+//			stack.rollback( );
+//			ExceptionHandler.handle( e );
+//		}
+//		}
+//		return true;
+//	}
+	
 }
