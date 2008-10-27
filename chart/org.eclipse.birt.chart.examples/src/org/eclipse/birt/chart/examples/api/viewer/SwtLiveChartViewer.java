e1030219ff99dc7fccf82ca41c1de8d2760fed7a
yulin <>
2008-10-27 09:05:47 +0000

    Fix Bug#248324 on behalf of Xingsheng Zhu: Example SWTLiveViewer crashes after a few minutes.

--- chart/org.eclipse.birt.chart.examples/src/org/eclipse/birt/chart/examples/api/viewer/SwtLiveChartViewer.java
+++ chart/org.eclipse.birt.chart.examples/src/org/eclipse/birt/chart/examples/api/viewer/SwtLiveChartViewer.java
@@ -62,8 +62,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-public class SwtLiveChartViewer extends Composite implements
-PaintListener
+public class SwtLiveChartViewer extends Composite implements PaintListener
 {
 
 	private IDeviceRenderer idr = null;
@@ -80,9 +79,8 @@ PaintListener
 	 */
 	private boolean bFirstPaint = true;
 
-
-
 	private static ILogger logger = Logger.getLogger( JavaScriptViewer.class.getName( ) );
+
 	/**
 	 * execute application
 	 * 
@@ -91,7 +89,7 @@ PaintListener
 	public static void main( String[] args )
 	{
 		Display display = Display.getDefault( );
-		Shell shell = new Shell( display );
+		Shell shell = new Shell( display, SWT.CLOSE );
 		shell.setSize( 600, 400 );
 		shell.setLayout( new GridLayout( ) );
 
@@ -127,8 +125,7 @@ PaintListener
 			logger.log( pex );
 		}
 
-		cm = createLiveChart( );		
-
+		cm = createLiveChart( );
 
 	}
 
@@ -136,14 +133,14 @@ PaintListener
 	{
 		ChartWithAxes cwaBar = ChartWithAxesImpl.create( );
 
-		//Plot
+		// Plot
 		cwaBar.getBlock( ).setBackground( ColorDefinitionImpl.WHITE( ) );
 		Plot p = cwaBar.getPlot( );
 		p.getClientArea( ).setBackground( ColorDefinitionImpl.create( 255,
 				255,
 				225 ) );
 
-		//Legend
+		// Legend
 		Legend lg = cwaBar.getLegend( );
 		LineAttributes lia = lg.getOutline( );
 		lg.getText( ).getFont( ).setSize( 16 );
@@ -151,13 +148,13 @@ PaintListener
 		lg.getInsets( ).setLeft( 10 );
 		lg.getInsets( ).setRight( 10 );
 
-		//Title
+		// Title
 		cwaBar.getTitle( )
-		.getLabel( )
-		.getCaption( )
-		.setValue( "Live Chart Demo" );//$NON-NLS-1$
+				.getLabel( )
+				.getCaption( )
+				.setValue( "Live Chart Demo" );//$NON-NLS-1$
 
-		//X-Axis
+		// X-Axis
 		Axis xAxisPrimary = cwaBar.getPrimaryBaseAxes( )[0];
 
 		xAxisPrimary.setType( AxisType.TEXT_LITERAL );
@@ -165,8 +162,8 @@ PaintListener
 		xAxisPrimary.getOrigin( ).setType( IntersectionType.MIN_LITERAL );
 
 		xAxisPrimary.getTitle( )
-		.getCaption( )
-		.setValue( "Category Text X-Axis" );//$NON-NLS-1$
+				.getCaption( )
+				.setValue( "Category Text X-Axis" );//$NON-NLS-1$
 		xAxisPrimary.setTitlePosition( Position.BELOW_LITERAL );
 
 		xAxisPrimary.getLabel( ).getCaption( ).getFont( ).setRotation( 75 );
@@ -174,14 +171,14 @@ PaintListener
 
 		xAxisPrimary.getMajorGrid( ).setTickStyle( TickStyle.BELOW_LITERAL );
 		xAxisPrimary.getMajorGrid( )
-		.getLineAttributes( )
-		.setStyle( LineStyle.DOTTED_LITERAL );
+				.getLineAttributes( )
+				.setStyle( LineStyle.DOTTED_LITERAL );
 		xAxisPrimary.getMajorGrid( )
-		.getLineAttributes( )
-		.setColor( ColorDefinitionImpl.create( 64, 64, 64 ) );
+				.getLineAttributes( )
+				.setColor( ColorDefinitionImpl.create( 64, 64, 64 ) );
 		xAxisPrimary.getMajorGrid( ).getLineAttributes( ).setVisible( true );
 
-		//Y-Axis
+		// Y-Axis
 		Axis yAxisPrimary = cwaBar.getPrimaryOrthogonalAxis( xAxisPrimary );
 
 		yAxisPrimary.getLabel( ).getCaption( ).setValue( "Price Axis" );//$NON-NLS-1$
@@ -195,26 +192,26 @@ PaintListener
 
 		yAxisPrimary.getMajorGrid( ).setTickStyle( TickStyle.LEFT_LITERAL );
 		yAxisPrimary.getMajorGrid( )
-		.getLineAttributes( )
-		.setStyle( LineStyle.DOTTED_LITERAL );
+				.getLineAttributes( )
+				.setStyle( LineStyle.DOTTED_LITERAL );
 		yAxisPrimary.getMajorGrid( )
-		.getLineAttributes( )
-		.setColor( ColorDefinitionImpl.RED( ) );
+				.getLineAttributes( )
+				.setColor( ColorDefinitionImpl.RED( ) );
 		yAxisPrimary.getMajorGrid( ).getLineAttributes( ).setVisible( true );
 
-		//X-Series
+		// X-Series
 		Series seCategory = SeriesImpl.create( );
 		SeriesDefinition sdX = SeriesDefinitionImpl.create( );
 		xAxisPrimary.getSeriesDefinitions( ).add( sdX );
 		sdX.getSeries( ).add( seCategory );
 
-		//Y-Series (1)
+		// Y-Series (1)
 		BarSeries bs1 = (BarSeries) BarSeriesImpl.create( );
 		bs1.setSeriesIdentifier( "Unit Price" );//$NON-NLS-1$
 		bs1.setRiserOutline( null );
 		bs1.setRiser( RiserType.RECTANGLE_LITERAL );
 
-		//Y-Series (2)
+		// Y-Series (2)
 		LineSeries ls1 = (LineSeries) LineSeriesImpl.create( );
 		ls1.setSeriesIdentifier( "Quantity" );//$NON-NLS-1$
 		ls1.getLineAttributes( ).setColor( ColorDefinitionImpl.GREEN( ) );
@@ -245,39 +242,46 @@ PaintListener
 		// X-Axis
 		Axis xAxisPrimary = cwaBar.getPrimaryBaseAxes( )[0];
 		SeriesDefinition sdX = (SeriesDefinition) xAxisPrimary.getSeriesDefinitions( )
-		.get( 0 );
+				.get( 0 );
 		( (Series) sdX.getSeries( ).get( 0 ) ).setDataSet( categoryValues );
 
 		// Y-Axis
 		Axis yAxisPrimary = cwaBar.getPrimaryOrthogonalAxis( xAxisPrimary );
 		SeriesDefinition sdY = (SeriesDefinition) yAxisPrimary.getSeriesDefinitions( )
-		.get( 0 );
+				.get( 0 );
 		( (Series) sdY.getSeries( ).get( 0 ) ).setDataSet( seriesOneValues );
 		( (Series) sdY.getSeries( ).get( 1 ) ).setDataSet( seriesTwoValues );
-	}	
-	//Live Date Set 
+	}
+	// Live Date Set
 	private static final String[] sa = {
-		"One", "Two", "Three", "Four", "Five",//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$//$NON-NLS-5$
-		"Six", "Seven", "Eight", "Nine", "Ten"};//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$//$NON-NLS-5$
+			"One", "Two", "Three", "Four", "Five",//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$//$NON-NLS-5$
+			"Six", "Seven", "Eight", "Nine", "Ten"};//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$//$NON-NLS-5$
 	private static final double[] da1 = {
-		56.99,
-		352.95,
-		-201.95,
-		299.95,
-		-95.95,
-		25.45,
-		129.33,
-		-26.5,
-		43.5,
-		122
+			56.99,
+			352.95,
+			-201.95,
+			299.95,
+			-95.95,
+			25.45,
+			129.33,
+			-26.5,
+			43.5,
+			122
 	};
 
 	private static final double[] da2 = {
-		20, 35, 59, 105, 150, -37, -65, -99, -145, -185
-	};	
+			20, 35, 59, 105, 150, -37, -65, -99, -145, -185
+	};
+
+	private Image imgChart;
+
+	private GC gcImage;
+
+	private Bounds bo;
+
 	static final void scrollData( ChartWithAxes cwa )
 	{
-		//Scroll the bar (Y) series
+		// Scroll the bar (Y) series
 		double dTemp = da1[0];
 		for ( int i = 0; i < da1.length - 1; i++ )
 		{
@@ -285,7 +289,7 @@ PaintListener
 		}
 		da1[da1.length - 1] = dTemp;
 
-		//Scroll the line (Y) series
+		// Scroll the line (Y) series
 		dTemp = da2[0];
 		for ( int i = 0; i < da2.length - 1; i++ )
 		{
@@ -293,7 +297,7 @@ PaintListener
 		}
 		da2[da2.length - 1] = dTemp;
 
-		//Scroll X series
+		// Scroll X series
 		String sTemp = sa[0];
 		for ( int i = 0; i < sa.length - 1; i++ )
 		{
@@ -302,106 +306,80 @@ PaintListener
 		sa[sa.length - 1] = sTemp;
 
 		updateDataSet( cwa );
-	}	
+	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.swt.events.PaintListener#paintControl(org.eclipse.swt.events.PaintEvent)
+	 * @see
+	 * org.eclipse.swt.events.PaintListener#paintControl(org.eclipse.swt.events
+	 * .PaintEvent)
 	 */
 	public final void paintControl( PaintEvent e )
 	{
 		Rectangle d = this.getClientArea( );
-		Image imgChart = new Image( this.getDisplay( ), d );
-		GC gcImage = new GC( imgChart );
-		idr.setProperty( IDeviceRenderer.GRAPHICS_CONTEXT, gcImage );
+		if ( bFirstPaint )
+		{
+			imgChart = new Image( this.getDisplay( ), d );
+			gcImage = new GC( imgChart );
+			idr.setProperty( IDeviceRenderer.GRAPHICS_CONTEXT, gcImage );
 
-		Bounds bo = BoundsImpl.create( 0, 0, d.width, d.height );
-		bo.scale( 72d / idr.getDisplayServer( ).getDpiResolution( ) );
+			bo = BoundsImpl.create( 0, 0, d.width, d.height );
+			bo.scale( 72d / idr.getDisplayServer( ).getDpiResolution( ) );
+		}
 
 		Generator gr = Generator.instance( );
 
 		try
 		{
-			gcs = gr.build( idr.getDisplayServer( ),
-					cm,
-					bo,
-					null,
-					null,
-					null );
+			gcs = gr.build( idr.getDisplayServer( ), cm, bo, null, null, null );
+			gr.render( idr, gcs );
+			GC gc = e.gc;
+			gc.drawImage( imgChart, d.x, d.y );
 		}
 		catch ( ChartException ce )
 		{
 			ce.printStackTrace( );
 		}
 
+		bFirstPaint = false;
 
+		Display.getDefault( ).timerExec( 500, new Runnable( ) {
 
-
-
-		if ( bFirstPaint )
-		{
-			bFirstPaint = false;
-
-
-			new Thread(
-					new Runnable() {
-						public void run() {
-							while (true) {
-								try { Thread.sleep(500); } catch (Exception e) { }
-								Display.getDefault().asyncExec(
-										new Runnable() {
-											public void run() {
-												chartRefresh();
-											}
-										}
-								);
-							}
-						}
-					}
-			).start();
-
-
-
-		}
-		try
-		{
-			gr.render( idr, gcs );
-			GC gc = e.gc;
-			gc.drawImage( imgChart, d.x, d.y );			
-		}
-		catch ( ChartException ex )
-		{
-			ex.printStackTrace( );
-		}		
-
-
+			public void run( )
+			{
+				chartRefresh( );
+			}
+		} );
 
 	}
-
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+	 * @see
+	 * org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse
+	 * .swt.events.SelectionEvent)
 	 */
-	private void chartRefresh()
+	private void chartRefresh( )
 	{
-
-
-		final Generator gr = Generator.instance( );
-		scrollData( (ChartWithAxes) cm );
-
-		// Refresh
-		try
+		if ( !isDisposed( ) )
 		{
-			gr.refresh( gcs );
+			final Generator gr = Generator.instance( );
+			scrollData( (ChartWithAxes) cm );
+
+			// Refresh
+			try
+			{
+				gr.refresh( gcs );
+			}
+			catch ( ChartException ex )
+			{
+				ex.printStackTrace( );
+			}
+			redraw( );
 		}
-		catch ( ChartException ex )
-		{
-			ex.printStackTrace( );
-		}
-		redraw();
 
 	}
+
 }
