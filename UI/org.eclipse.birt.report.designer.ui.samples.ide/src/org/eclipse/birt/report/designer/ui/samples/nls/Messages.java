a33930f018559c137ec666b642d19ca11297e4ea
xizhang <>
2008-11-06 06:03:07 +0000

    - Summary: Correct package name of project org.eclipse.birt.report.designer.ui.samples.ide
    - Bugzilla Bug (s) Resolved:
     N/A
    
    - Description:
    "org.eclipse.brit.report.designer.ui.samples.nls" should be "org.eclipse.birt.report.designer.ui.samples.nls".
    Checked in head and 2.3.2 branch.
    
    
    - Tests Description  :
    Manual test
    
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    
    - Files Deleted:
    
    
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -  Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui.samples.ide/src/org/eclipse/birt/report/designer/ui/samples/nls/Messages.java
+++ UI/org.eclipse.birt.report.designer.ui.samples.ide/src/org/eclipse/birt/report/designer/ui/samples/nls/Messages.java
@@ -23,7 +23,7 @@ import java.util.ResourceBundle;
 public class Messages
 {
 
-	private static final String BUNDLE_NAME = "org.eclipse.brit.report.designer.ui.samples.nls.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.birt.report.designer.ui.samples.nls.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
 			.getBundle( BUNDLE_NAME );
