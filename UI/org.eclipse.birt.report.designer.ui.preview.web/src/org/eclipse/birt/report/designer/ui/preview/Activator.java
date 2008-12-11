b1c8eba08e9ea03e7709fbe198d0619cbcfeed50
Zhiqiang Qian <zqian@actuate.com>
2008-12-11 10:03:52 +0000

    - Bugzilla Bug (s) Resolved: 245641 - Description: set the URI root to web app root path when start the viewer plugin.

--- UI/org.eclipse.birt.report.designer.ui.preview.web/src/org/eclipse/birt/report/designer/ui/preview/Activator.java
+++ UI/org.eclipse.birt.report.designer.ui.preview.web/src/org/eclipse/birt/report/designer/ui/preview/Activator.java
@@ -11,8 +11,10 @@
 
 package org.eclipse.birt.report.designer.ui.preview;
 
-import org.eclipse.ui.plugin.*;
+import org.eclipse.birt.report.designer.util.ImageManager;
+import org.eclipse.birt.report.viewer.ViewerPlugin;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -21,7 +23,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin
 {
 
-	//The shared instance.
+	// The shared instance.
 	private static Activator plugin;
 
 	public static final String ID = "org.eclipse.birt.report.designer.ui.preview.web"; //$NON-NLS-1$
@@ -40,6 +42,16 @@ public class Activator extends AbstractUIPlugin
 	public void start( BundleContext context ) throws Exception
 	{
 		super.start( context );
+
+		// ensure viewer plugin already started
+		ViewerPlugin dummy = ViewerPlugin.getDefault( );
+
+		// set the URI root path to ImageManager
+		String appRootPath = System.getProperty( ViewerPlugin.BIRT_VIEWER_ROOT_PATH );
+		if ( appRootPath != null )
+		{
+			ImageManager.getInstance( ).setURIRootPath( appRootPath );
+		}
 	}
 
 	/**
@@ -53,7 +65,7 @@ public class Activator extends AbstractUIPlugin
 
 	/**
 	 * Returns the shared instance.
-	 *
+	 * 
 	 * @return the shared instance.
 	 */
 	public static Activator getDefault( )
@@ -62,10 +74,11 @@ public class Activator extends AbstractUIPlugin
 	}
 
 	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path.
-	 *
-	 * @param path the path
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path.
+	 * 
+	 * @param path
+	 *            the path
 	 * @return the image descriptor
 	 */
 	public static ImageDescriptor getImageDescriptor( String path )
