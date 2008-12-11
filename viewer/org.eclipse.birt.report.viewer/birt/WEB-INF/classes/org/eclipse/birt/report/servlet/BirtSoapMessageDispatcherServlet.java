2d8225765e16ee9ca1fe16dc4c0e1aa0ac61e82c
vpetry <>
2008-12-11 08:07:30 +0000

    Fixed missing servlet path bug

--- viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/servlet/BirtSoapMessageDispatcherServlet.java
+++ viewer/org.eclipse.birt.report.viewer/birt/WEB-INF/classes/org/eclipse/birt/report/servlet/BirtSoapMessageDispatcherServlet.java
@@ -109,6 +109,9 @@ abstract public class BirtSoapMessageDispatcherServlet extends AxisServlet
 		if ( req.getCharacterEncoding( ) == null )
 			req.setCharacterEncoding( IBirtConstants.DEFAULT_ENCODE );
 
+		// workaround for Jetty
+		req.setAttribute( "ServletPath", ((HttpServletRequest)req).getServletPath( ) ); //$NON-NLS-1$		
+		
 		super.service( req, res );
 	}
 
