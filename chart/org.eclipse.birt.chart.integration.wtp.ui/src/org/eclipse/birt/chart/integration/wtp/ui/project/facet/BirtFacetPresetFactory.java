6c6e5498ac00dfe76794173f90103dcb678a2a41
xzhu <>
2009-02-17 05:20:34 +0000

    Update Chart runtime component version to 2.5.0

--- chart/org.eclipse.birt.chart.integration.wtp.ui/src/org/eclipse/birt/chart/integration/wtp/ui/project/facet/BirtFacetPresetFactory.java
+++ chart/org.eclipse.birt.chart.integration.wtp.ui/src/org/eclipse/birt/chart/integration/wtp/ui/project/facet/BirtFacetPresetFactory.java
@@ -56,8 +56,7 @@ public class BirtFacetPresetFactory implements IPresetFactory
 			final IProjectFacet birtFacet = ProjectFacetsManager
 					.getProjectFacet( "birt.chart.runtime" ); //$NON-NLS-1$
 
-			final IProjectFacetVersion chartFacetVersion = birtFacet
-					.getVersion( "2.3.0" ); //$NON-NLS-1$
+			final IProjectFacetVersion chartFacetVersion = birtFacet.getVersion( "2.5.0" ); //$NON-NLS-1$
 
 			facets.add( chartFacetVersion );
 			facets.add( webFacetVersion );
