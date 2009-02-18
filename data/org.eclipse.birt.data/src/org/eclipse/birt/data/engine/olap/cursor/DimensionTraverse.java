fce8ef50645476e1cc4b37dce518e3d4303666f7
Mingxia Wu <mwu@actuate.com>
2009-02-18 06:01:27 +0000

    CheckIn:Fix bugzilla Bug 265126 Part of data lost in the xtab which has set show empty row

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/cursor/DimensionTraverse.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/cursor/DimensionTraverse.java
@@ -28,7 +28,6 @@ class DimensionTraverse
 	private EdgeDimensionRelation relationMap;
 	private ResultSetFetcher fetcher;
 	int[] dimensionCursorPosition;
-	private int[] startPosition, endPosition;
 	private int edgeStart, edgeEnd;
 
 	/**
@@ -45,9 +44,8 @@ class DimensionTraverse
 		this.beforeFirst( );
 		this.fetcher = relationMap.fetcher;
 		dimensionCursorPosition = findDimensionPosition( -1 );
-		endPosition = findDimensionPosition( this.relationMap.traverseLength - 1 );
-		edgeStart = 0;
-		edgeEnd = this.relationMap.traverseLength -1;
+		this.edgeStart = 0;
+		this.edgeEnd = this.relationMap.traverseLength -1;
 	}
 	
 	/**
@@ -66,7 +64,6 @@ class DimensionTraverse
 		this.edgeStart = edgeStart;
 		this.edgeEnd = edgeEnd;
 		dimensionCursorPosition = findDimensionPosition( edgeStart -1 );
-		endPosition = findDimensionPosition( edgeEnd );
 	}
 	
 	/**
@@ -574,17 +571,14 @@ class DimensionTraverse
 		}
 		for ( int k = relationMap.mirrorStartPosition; k <= dimAxisIndex; k++ )
 		{
-			int offset = 1;
+			int offset = this.dimensionCursorPosition[k];
+			if ( !isStart && k == dimAxisIndex )
+			{
+				offset = this.dimensionCursorPosition[k] + 1;
+			}
 			for ( int i = k + 1; i < this.dimAxis.length; i++ )
 			{
-				if ( k == dimAxisIndex && !isStart )
-					offset = offset *
-							( this.dimensionCursorPosition[k] + 1 ) *
-							relationMap.mirrorLength[i];
-				else
-					offset = offset *
-							this.dimensionCursorPosition[k] *
-							relationMap.mirrorLength[i];
+				offset = offset * relationMap.mirrorLength[i];
 			}
 			if ( k + 1 < this.dimAxis.length )
 				start += offset;
