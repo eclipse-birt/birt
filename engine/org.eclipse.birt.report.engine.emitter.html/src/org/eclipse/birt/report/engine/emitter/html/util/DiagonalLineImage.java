f44bf25a658e26e2fda88f8553ebf9493b7fb0c6
lyu <>
2009-03-04 09:44:01 +0000

    Implement the diagonal line width and part of the diagonal styel for the bugzilla bug 243259.

--- engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/util/DiagonalLineImage.java
+++ engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/util/DiagonalLineImage.java
@@ -11,8 +11,10 @@
 
 package org.eclipse.birt.report.engine.emitter.html.util;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -70,6 +72,8 @@ public class DiagonalLineImage
 	 * Image DPI.
 	 */
 	protected int imageDpi = -1;
+	
+	Stroke originStroke = null;
 
 	/**
 	 * Default image pixel width.
@@ -139,6 +143,17 @@ public class DiagonalLineImage
 		{
 			imagePXHeight = DEFAULT_IMAGE_PX_HEIGHT;
 		}
+		
+		int diagonalPXWidth = HTMLEmitterUtil.getDimensionPixelValue( diagonalWidth, imageDpi );
+		if ( diagonalPXWidth <= 0 )
+		{
+			diagonalPXWidth = 1;
+		}
+		int antidiagonalPXWidth = HTMLEmitterUtil.getDimensionPixelValue( antidiagonalWidth, imageDpi );
+		if ( antidiagonalPXWidth <= 0 )
+		{
+			antidiagonalPXWidth = 1;
+		}
 
 		// Create a buffered image in which to draw
 		BufferedImage bufferedImage = new BufferedImage( imagePXWidth,
@@ -146,6 +161,7 @@ public class DiagonalLineImage
 				BufferedImage.TYPE_INT_ARGB );
 		// Create a graphics contents on the buffered image
 		Graphics2D g2d = bufferedImage.createGraphics( );
+		originStroke = g2d.getStroke( );
 
 		try
 		{
@@ -155,60 +171,228 @@ public class DiagonalLineImage
 				g2d.setColor( color );
 			}
 
-			// FIXME continue: implement the line width and style.
 			// Draw diagonal line.
-			if ( diagonalNumber == 1 )
+			// FIXME continue: Double style hasn't been implemented yet, and it
+			// will be treated as solid style. The double style should be
+			// implemented in the future.
+			/*if ( "double".equalsIgnoreCase( diagonalStyle )
+					&& ( diagonalPXWidth > 2 ) )
 			{
-				g2d.drawLine( 0, 0, imagePXWidth - 1, imagePXHeight - 1 );
+				// Double line has the same effect with the solid line when the
+				// line width equal 1px or 2px.
 			}
-			else if ( diagonalNumber == 2 )
+			else if ( "dotted".equalsIgnoreCase( diagonalStyle ) )*/
+			if ( "dotted".equalsIgnoreCase( diagonalStyle ) )
 			{
-				g2d.drawLine( ( imagePXWidth / 3 ) - 1,
-						0,
-						imagePXWidth - 1,
-						imagePXHeight - 1 );
-				g2d.drawLine( 0,
-						( imagePXHeight / 3 ) - 1,
-						imagePXWidth - 1,
-						imagePXHeight - 1 );
+				float dash[] = {
+						1, ( diagonalPXWidth * 2 ) - 1 + ( diagonalPXWidth % 2 )
+				};
+				Stroke stroke = new BasicStroke( diagonalPXWidth,
+						BasicStroke.CAP_ROUND,
+						BasicStroke.JOIN_ROUND,
+						10.0f,
+						dash,
+						( diagonalPXWidth * 2 ) - ( diagonalPXWidth / 2 ) );
+				g2d.setStroke( stroke );
+
+				if ( diagonalNumber == 1 )
+				{
+					g2d.drawLine( 0,
+							diagonalPXWidth / 2,
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+				}
+				else if ( diagonalNumber == 2 )
+				{
+					g2d.drawLine( ( imagePXWidth / 3 ) - 1,
+							diagonalPXWidth / 2,
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+					g2d.drawLine( 0,
+							( imagePXHeight / 3 ) - 1 + ( diagonalPXWidth / 2 ),
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+				}
+				else if ( diagonalNumber >= 3 )
+				{
+					g2d.drawLine( ( imagePXWidth / 2 ) - 1,
+							diagonalPXWidth / 2,
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+					g2d.drawLine( 0,
+							diagonalPXWidth / 2,
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+					g2d.drawLine( 0,
+							( imagePXHeight / 2 ) - 1 + ( diagonalPXWidth / 2 ),
+							imagePXWidth - 1,
+							imagePXHeight - 1 + ( diagonalPXWidth / 2 ) );
+				}
 			}
-			else if ( diagonalNumber >= 3 )
+			else
 			{
-				g2d.drawLine( ( imagePXWidth / 2 ) - 1,
-						0,
-						imagePXWidth - 1,
-						imagePXHeight - 1 );
-				g2d.drawLine( 0, 0, imagePXWidth - 1, imagePXHeight - 1 );
-				g2d.drawLine( 0,
-						( imagePXHeight / 2 ) - 1,
-						imagePXWidth - 1,
-						imagePXHeight - 1 );
+				if ( "dashed".equalsIgnoreCase( diagonalStyle ) )
+				{
+					float dash[] = {
+						3 * diagonalPXWidth
+					};
+					Stroke stroke = new BasicStroke( diagonalPXWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_MITER,
+							10.0f,
+							dash,
+							0.0f );
+					g2d.setStroke( stroke );
+				}
+				else
+				{
+					// Solid is the default value
+					// Use the default stroke when the diagonalPXWidth is 1.
+					if ( diagonalPXWidth > 1 )
+					{
+						g2d.setStroke( new BasicStroke( diagonalPXWidth ) );
+					}
+					
+				}
+				
+				if ( diagonalNumber == 1 )
+				{
+					g2d.drawLine( 0, 0, imagePXWidth - 1, imagePXHeight - 1 );
+				}
+				else if ( diagonalNumber == 2 )
+				{
+					g2d.drawLine( ( imagePXWidth / 3 ) - 1,
+							0,
+							imagePXWidth - 1,
+							imagePXHeight - 1 );
+					g2d.drawLine( 0,
+							( imagePXHeight / 3 ) - 1,
+							imagePXWidth - 1,
+							imagePXHeight - 1 );
+				}
+				else if ( diagonalNumber >= 3 )
+				{
+					g2d.drawLine( ( imagePXWidth / 2 ) - 1,
+							0,
+							imagePXWidth - 1,
+							imagePXHeight - 1 );
+					g2d.drawLine( 0, 0, imagePXWidth - 1, imagePXHeight - 1 );
+					g2d.drawLine( 0,
+							( imagePXHeight / 2 ) - 1,
+							imagePXWidth - 1,
+							imagePXHeight - 1 );
+				}
 			}
 
 			// Draw antidiagonal line.
-			if ( antidiagonalNumber == 1 )
+			// FIXME continue: Double style hasn't been implemented yet, and it
+			// will be treated as solid style. The double style should be
+			// implemented in the future.
+			/*if ( "double".equalsIgnoreCase( diagonalStyle ) && ( antidiagonalPXWidth > 2 ) )
 			{
-				g2d.drawLine( imagePXWidth - 1, 0, 0, imagePXHeight - 1 );
+				// Double line has the same effect with the solid line when the
+				// line width equal 1px or 2px.
+				// FIXME continue: implement the left double part.
 			}
-			else if ( antidiagonalNumber == 2 )
+			else if ( "dotted".equalsIgnoreCase( diagonalStyle ) )*/
+			if ( "dotted".equalsIgnoreCase( diagonalStyle ) )
 			{
-				g2d.drawLine( ( imagePXWidth * 2 / 3 ) - 1,
-						0,
-						0,
-						imagePXHeight - 1 );
-				g2d.drawLine( imagePXWidth - 1,
-						( imagePXHeight / 3 ) - 1,
-						0,
-						imagePXHeight - 1 );
+				float dash[] = {
+						1, ( antidiagonalPXWidth * 2 ) - 1 + ( antidiagonalPXWidth % 2 )
+				};
+				Stroke stroke = new BasicStroke( antidiagonalPXWidth,
+						BasicStroke.CAP_ROUND,
+						BasicStroke.JOIN_ROUND,
+						10.0f,
+						dash,
+						( antidiagonalPXWidth * 2 ) - ( antidiagonalPXWidth / 2 ) );
+				g2d.setStroke( stroke );
+
+				if ( antidiagonalNumber == 1 )
+				{
+					g2d.drawLine( imagePXWidth - 1,
+							antidiagonalPXWidth / 2,
+							0,
+							imagePXHeight - 1 + ( antidiagonalPXWidth / 2 ) );
+				}
+				else if ( antidiagonalNumber == 2 )
+				{
+					g2d.drawLine( ( imagePXWidth * 2 / 3 ) - 1,
+							antidiagonalPXWidth / 2,
+							0,
+							imagePXHeight - 1 + ( antidiagonalPXWidth / 2 ) );
+					g2d.drawLine( imagePXWidth - 1, ( imagePXHeight / 3 )
+							- 1 + ( antidiagonalPXWidth / 2 ), 0, imagePXHeight
+							- 1 + ( antidiagonalPXWidth / 2 ) );
+				}
+				else if ( antidiagonalNumber >= 3 )
+				{
+					g2d.drawLine( ( imagePXWidth / 2 ) - 1,
+							antidiagonalPXWidth / 2,
+							0,
+							imagePXHeight - 1 + ( antidiagonalPXWidth / 2 ) );
+					g2d.drawLine( imagePXWidth - 1,
+							antidiagonalPXWidth / 2,
+							0,
+							imagePXHeight - 1 + ( antidiagonalPXWidth / 2 ) );
+					g2d.drawLine( imagePXWidth - 1, ( imagePXHeight / 2 )
+							- 1 + ( antidiagonalPXWidth / 2 ), 0, imagePXHeight
+							- 1 + ( antidiagonalPXWidth / 2 ) );
+				}
 			}
-			else if ( antidiagonalNumber >= 3 )
+			else
 			{
-				g2d.drawLine( ( imagePXWidth / 2 ) - 1, 0, 0, imagePXHeight - 1 );
-				g2d.drawLine( imagePXWidth - 1, 0, 0, imagePXHeight - 1 );
-				g2d.drawLine( imagePXWidth - 1,
-						( imagePXHeight / 2 ) - 1,
-						0,
-						imagePXHeight - 1 );
+				if ( "dashed".equalsIgnoreCase( diagonalStyle ) )
+				{
+					float dash[] = {
+						3 * antidiagonalPXWidth
+					};
+					Stroke stroke = new BasicStroke( antidiagonalPXWidth,
+							BasicStroke.CAP_BUTT,
+							BasicStroke.JOIN_MITER,
+							10.0f,
+							dash,
+							0.0f );
+					g2d.setStroke( stroke );
+				}
+				else
+				{
+					// Solid is the default value
+					if ( antidiagonalPXWidth > 1 )
+					{
+						g2d.setStroke( new BasicStroke( antidiagonalPXWidth ) );
+					}
+					else
+					{
+						// Use the default stroke when the diagonalPXWidth is 1.
+						g2d.setStroke( originStroke );
+					}
+				}
+				
+				if ( antidiagonalNumber == 1 )
+				{
+					g2d.drawLine( imagePXWidth - 1, 0, 0, imagePXHeight - 1 );
+				}
+				else if ( antidiagonalNumber == 2 )
+				{
+					g2d.drawLine( ( imagePXWidth * 2 / 3 ) - 1,
+							0,
+							0,
+							imagePXHeight - 1 );
+					g2d.drawLine( imagePXWidth - 1,
+							( imagePXHeight / 3 ) - 1,
+							0,
+							imagePXHeight - 1 );
+				}
+				else if ( antidiagonalNumber >= 3 )
+				{
+					g2d.drawLine( ( imagePXWidth / 2 ) - 1, 0, 0, imagePXHeight - 1 );
+					g2d.drawLine( imagePXWidth - 1, 0, 0, imagePXHeight - 1 );
+					g2d.drawLine( imagePXWidth - 1,
+							( imagePXHeight / 2 ) - 1,
+							0,
+							imagePXHeight - 1 );
+				}
 			}
 		}
 		finally
@@ -228,10 +412,5 @@ public class DiagonalLineImage
 		imageStream.close( );
 		
 		return resultImageByteArray;
-
-		//if ( resultImageByteArray != null )
-		//{
-		//	image = new Image( resultImageByteArray, null, ".png" );
-		//}
 	}
 }
