b5c76bf135a844903a2b86aebc040d27e2b32953
Rick Lu <rlu@actuate.com>
2009-03-06 05:32:42 +0000

    revised code to enable all textual property types trim string according to the trim option

--- model/org.eclipse.birt.report.model.tests/test/org/eclipse/birt/report/model/metadata/StringPropertyTypeTest.java
+++ model/org.eclipse.birt.report.model.tests/test/org/eclipse/birt/report/model/metadata/StringPropertyTypeTest.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
-* Copyright (c) 2004 Actuate Corporation.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*  Actuate Corporation  - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipse.birt.report.model.metadata;
 
@@ -22,12 +22,14 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 
 	StringPropertyType type = new StringPropertyType( );
 
-	PropertyDefn propDefn = new PropertyDefnFake( );
+	PropertyDefnFake propDefn = new PropertyDefnFake( );
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testGetTypeCode()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testGetTypeCode
+	 * ()
 	 */
 	public void testGetTypeCode( )
 	{
@@ -37,7 +39,8 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testGetName()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testGetName()
 	 */
 	public void testGetName( )
 	{
@@ -47,13 +50,18 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testValidateValue()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testValidateValue
+	 * ()
 	 */
 	public void testValidateValue( ) throws PropertyValueException
 	{
-        assertEquals( null, type.validateValue( design, propDefn, null ) );
-        assertEquals( null, type.validateValue( design, propDefn, "" ) ); //$NON-NLS-1$
-		assertEquals( "", type.validateValue( design, propDefn, "    " ) ); //$NON-NLS-1$//$NON-NLS-2$
+		int optionValue = TextualPropertyType.TRIM_SPACE_VALUE
+				| TextualPropertyType.TRIM_EMPTY_TO_NULL_VALUE;
+		propDefn.setTrimOption( optionValue );
+		assertEquals( null, type.validateValue( design, propDefn, null ) );
+		assertEquals( null, type.validateValue( design, propDefn, "" ) ); //$NON-NLS-1$
+		assertNull( type.validateValue( design, propDefn, "    " ) ); //$NON-NLS-1$
 		assertEquals( "abc", type.validateValue( design, propDefn, "abc" ) ); //$NON-NLS-1$//$NON-NLS-2$
 		assertEquals(
 				"123", type.validateValue( design, propDefn, new Integer( 123 ) ) ); //$NON-NLS-1$
@@ -67,7 +75,8 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testValidateInputString()
+	 * @seeorg.eclipse.birt.report.model.metadata.PropertyTypeTestCase#
+	 * testValidateInputString()
 	 */
 	public void testValidateInputString( ) throws PropertyValueException
 	{
@@ -77,13 +86,18 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testValidateXml()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testValidateXml
+	 * ()
 	 */
 	public void testValidateXml( ) throws PropertyValueException
 	{
-        assertEquals( null, type.validateValue( design, propDefn, null ) );
-        assertEquals( null, type.validateValue( design, propDefn, "" ) ); //$NON-NLS-1$
-		assertEquals( "", type.validateValue( design, propDefn, "    " ) );  //$NON-NLS-1$//$NON-NLS-2$
+		int optionValue = TextualPropertyType.TRIM_SPACE_VALUE
+				| TextualPropertyType.TRIM_EMPTY_TO_NULL_VALUE;
+		propDefn.setTrimOption( optionValue );
+		assertEquals( null, type.validateValue( design, propDefn, null ) );
+		assertEquals( null, type.validateValue( design, propDefn, "" ) ); //$NON-NLS-1$
+		assertNull( type.validateValue( design, propDefn, "    " ) ); //$NON-NLS-1$
 		assertEquals(
 				"any-input", type.validateXml( design, propDefn, "any-input" ) ); //$NON-NLS-1$//$NON-NLS-2$
 	}
@@ -91,7 +105,9 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToDouble()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToDouble
+	 * ()
 	 */
 	public void testToDouble( )
 	{
@@ -101,18 +117,21 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToInteger()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToInteger
+	 * ()
 	 */
 	public void testToInteger( )
 	{
 		assertEquals( 123, type.toInteger( design, "123" ) ); //$NON-NLS-1$
-		assertEquals( 0, type.toInteger( design, null ) ); 
+		assertEquals( 0, type.toInteger( design, null ) );
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToXml()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToXml()
 	 */
 	public void testToXml( )
 	{
@@ -122,7 +141,9 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToString()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToString
+	 * ()
 	 */
 	public void testToString( )
 	{
@@ -133,7 +154,8 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToDisplayString()
+	 * @seeorg.eclipse.birt.report.model.metadata.PropertyTypeTestCase#
+	 * testToDisplayString()
 	 */
 	public void testToDisplayString( )
 	{
@@ -144,7 +166,9 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToNumber()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToNumber
+	 * ()
 	 */
 	public void testToNumber( )
 	{
@@ -153,7 +177,9 @@ public class StringPropertyTypeTest extends PropertyTypeTestCase
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToBoolean()
+	 * @see
+	 * org.eclipse.birt.report.model.metadata.PropertyTypeTestCase#testToBoolean
+	 * ()
 	 */
 	public void testToBoolean( )
 	{
