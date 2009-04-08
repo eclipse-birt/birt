4a0b06cf8e0f57d3f03524baf04e0bab9e89cbd7
Jun Ouyang <jouyang@actuate.com>
2009-04-08 06:09:32 +0000

    Fix bugzilla bug 270220:org.eclipse.birt.report.engine.emitter.prototype.excel.tests plugin can't run in 2.5.0 birt-test-suite package.

--- engine/org.eclipse.birt.report.engine.emitter.prototype.excel.tests/test/org/eclipse/birt/report/engine/emitter/prototype/excel/tests/Activator.java
+++ engine/org.eclipse.birt.report.engine.emitter.prototype.excel.tests/test/org/eclipse/birt/report/engine/emitter/prototype/excel/tests/Activator.java
@@ -1,50 +1,22 @@
 package org.eclipse.birt.report.engine.emitter.prototype.excel.tests;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
-public class Activator extends AbstractUIPlugin {
-
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.birt.report.engine.emitter.prototype.excel.tests";
-
-	// The shared instance
-	private static Activator plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+public class Activator implements BundleActivator {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
 	}
 
 }
