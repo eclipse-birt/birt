0956173b65d3595a64eae620c30a3a98526f6346
Xiaoying Gu <xgu@actuate.com>
2009-04-09 05:29:20 +0000

    fix the failure of the case

--- testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/api/IColumnTest.java
+++ testsuites/org.eclipse.birt.report.tests.engine/src/org/eclipse/birt/report/tests/engine/api/IColumnTest.java
@@ -65,7 +65,7 @@ public class IColumnTest extends BaseEmitter
 		assertEquals( "rgb(128, 128, 128)", cell
 				.getStyle( )
 				.getBackgroundColor( ) );
-		assertEquals( "style_1", cell.getStyleClass( ) );
+		assertEquals( "related-container-style, column_style", cell.getStyleClass( ) );
 
 	}
 
