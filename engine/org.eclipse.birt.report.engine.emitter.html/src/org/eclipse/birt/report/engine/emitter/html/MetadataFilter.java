d1bccc7be22b2213c8956af30d2b5ba7a1c3824e
lyu <>
2009-04-28 05:16:17 +0000

    Remove useless comment.

--- engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/MetadataFilter.java
+++ engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/MetadataFilter.java
@@ -212,7 +212,6 @@ public class MetadataFilter implements IMetadataFilter
 		}
 		else if ( elementHandle instanceof DataItemHandle )
 		{
-			// FIXME Leon: data item text;
 			Boolean chacheResult = (Boolean) dataItemCache.get( elementHandle );
 			if ( chacheResult == null )
 			{
