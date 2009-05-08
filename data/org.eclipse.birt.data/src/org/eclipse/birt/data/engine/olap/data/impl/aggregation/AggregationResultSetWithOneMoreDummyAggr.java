353d146032a8f85fb5588a26685f5337b37f4aa4
wtu <>
2009-05-08 02:57:56 +0000

    CheckIn: avoid NPE because getAggregationDataType() of AggregationResult returns null if that aggregationResult is empty

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/data/impl/aggregation/AggregationResultSetWithOneMoreDummyAggr.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/olap/data/impl/aggregation/AggregationResultSetWithOneMoreDummyAggr.java
@@ -12,6 +12,7 @@
 package org.eclipse.birt.data.engine.olap.data.impl.aggregation;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,6 +25,7 @@ import org.eclipse.birt.data.engine.olap.data.api.DimLevel;
 import org.eclipse.birt.data.engine.olap.data.api.IAggregationResultRow;
 import org.eclipse.birt.data.engine.olap.data.api.IAggregationResultSet;
 import org.eclipse.birt.data.engine.olap.data.impl.AggregationDefinition;
+import org.eclipse.birt.data.engine.olap.data.util.DataType;
 import org.eclipse.birt.data.engine.script.ScriptConstants;
 import org.eclipse.birt.data.engine.script.ScriptEvalUtil;
 import org.mozilla.javascript.Context;
@@ -83,9 +85,16 @@ public class AggregationResultSetWithOneMoreDummyAggr implements IAggregationRes
 
 	public int[] getAggregationDataType( )
 	{
-		int[] types = new int[ars.getAggregationDataType( ).length + 1];
-		System.arraycopy( ars.getAggregationDataType( ), 
-				0, types, 0, ars.getAggregationDataType( ).length );
+		int[] types = new int[ars.getAggregationCount( ) + 1];
+		if ( ars.getAggregationDataType( ) != null )
+		{
+			System.arraycopy( ars.getAggregationDataType( ), 
+					0, types, 0, ars.getAggregationDataType( ).length );
+		}
+		else
+		{
+			Arrays.fill( types, DataType.UNKNOWN_TYPE );
+		}
 		types[types.length - 1] = addedAggrExpression.getDataType( ); 
 		return types;
 	}
