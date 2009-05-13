d8892c58fa60cadfb281b51370c00f5420985eaa
Linda Chan <lchan@actuate.com>
2009-05-13 18:22:04 +0000

    [275962] Replace call to use latest method signature

--- UI/org.eclipse.birt.report.designer.ui.data/src/org/eclipse/birt/report/designer/data/ui/actions/ExportElementToSourceCPStoreAction.java
+++ UI/org.eclipse.birt.report.designer.ui.data/src/org/eclipse/birt/report/designer/data/ui/actions/ExportElementToSourceCPStoreAction.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2004 Actuate Corporation.
+ * Copyright (c) 2009 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Actuate Corporation  - initial API and implementation
+ *  Actuate Corporation - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.birt.report.designer.data.ui.actions;
@@ -99,11 +99,11 @@ public class ExportElementToSourceCPStoreAction extends AbstractViewAction
 					DesignSessionRequest designSessionRequest = DTPUtil.getInstance( )
 							.createDesignSessionRequest( (OdaDataSourceHandle) selection );
 					DataSourceDesignSession.convertDesignToLinkedProfile( designSessionRequest,
+					        null, false,    // TODO UI enhancement to prompt user for their values
 							( (Boolean) dialog.getResult( ) ).booleanValue( ),
 							PlatformUI.getWorkbench( )
 									.getDisplay( )
-									.getActiveShell( ) )
-							.getResponseDataSourceDesign( );
+									.getActiveShell( ) );
 				}
 				catch ( Exception ex )
 				{
