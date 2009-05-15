6e008744848d4839ff750e8f1780097b176338af
Wei Yan <wyan@actuate.com>
2009-05-15 09:35:11 +0000

    202438 [Improve Page Break Management] Create a total page number per group

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/script/internal/instance/AutoTextInstance.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/script/internal/instance/AutoTextInstance.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004 Actuate Corporation.
+ * Copyright (c) 2004,2009 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,16 +15,32 @@ import org.eclipse.birt.report.engine.api.script.instance.IAutoTextInstance;
 import org.eclipse.birt.report.engine.content.IAutoTextContent;
 import org.eclipse.birt.report.engine.executor.ExecutionContext;
 
-
-public class AutoTextInstance extends ReportElementInstance implements IAutoTextInstance
+public class AutoTextInstance extends ReportElementInstance
+		implements
+			IAutoTextInstance
 {
 
 	IAutoTextContent autoText;
-	
+
 	public AutoTextInstance( IAutoTextContent autoText,
 			ExecutionContext context, RunningState runningState )
 	{
 		super( autoText, context, runningState );
 		this.autoText = autoText;
-	}	
+	}
+
+	public void setText( String text )
+	{
+		if ( runningState != RunningState.RENDER )
+		{
+			throw new UnsupportedOperationException(
+					"setText can only be invoked in onRender" );
+		}
+		autoText.setText( text );
+	}
+
+	public String getText( )
+	{
+		return autoText.getText( );
+	}
 }
