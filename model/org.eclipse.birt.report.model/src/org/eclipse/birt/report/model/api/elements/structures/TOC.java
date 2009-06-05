df88c915340ed00607897366e6458a3a016a079a
Rick Lu <rlu@actuate.com>
2009-06-05 06:57:16 +0000

    Reverted changes of "dataType" for VariableElement/TOC. see bug 278735.

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/elements/structures/TOC.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/elements/structures/TOC.java
@@ -78,11 +78,6 @@ public class TOC extends PropertyStructure
 	public static final String TEXT_DIRECTION_MEMBER = Style.TEXT_DIRECTION_PROP; // bidi_hcg
 	
 	/**
-	 * Name of the data type member
-	 */
-	public static final String DATA_TYPE_MEMBER = "dataType"; //$NON-NLS-1$
-	
-	/**
 	 * The reference to a style.
 	 */
 
