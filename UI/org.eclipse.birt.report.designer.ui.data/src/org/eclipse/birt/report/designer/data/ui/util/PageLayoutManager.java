5b683e812827f4b6deb1e876392f91d0f6d0b1ac
Lin Zhu <lzhu@actuate.com>
2009-07-10 09:42:55 +0000

    CheckIn: Fix ted issue 15332 Project 1189 - Union data set dialog enlarge when Select Union Data Set page

---
+++ UI/org.eclipse.birt.report.designer.ui.data/src/org/eclipse/birt/report/designer/data/ui/util/PageLayoutManager.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2009 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.designer.data.ui.util;
+
+import java.util.HashSet;
+
+
+public class PageLayoutManager
+{
+
+	private static HashSet<String> registeredSet = new HashSet<String>( );
+
+	public static void registerPage( String pageId )
+	{
+		if ( pageId != null )
+		{
+			registeredSet.add( pageId );
+		}
+	}
+
+	public static boolean isRegisteredPage( String pageId )
+	{
+		return pageId != null && registeredSet.contains( pageId );
+	}
+
+}
