5b13e8b10c3a20622b1650431f0a9812a9cf54fc
Xiaoying Gu <xgu@actuate.com>
2009-09-02 08:04:54 +0000

    Sync cvsTag.sh with the one on build server

--- build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag.sh
+++ build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag.sh
@@ -5,21 +5,26 @@ echo starting tag BIRT...
 CVS_RSH=ssh
 export CVS_RSH
 
-echo "================== Tagging third party =======================" > cvsTag.log
-ant -f src/fetch_org.apache.derby.core.xml -propertyfile cvsTag.properties >> cvsTag.log
-ant -f src/fetch_com.lowagie.itext.xml -propertyfile cvsTag.properties >> cvsTag.log
-ant -f src/fetch_org.apache.commons.codec.xml -propertyfile cvsTag.properties >> cvsTag.log
-ant -f src/fetch_org.mozilla.rhino.xml -propertyfile cvsTag.properties >> cvsTag.log
-ant -f src/fetch_org.w3c.sac.xml -propertyfile cvsTag.properties >> cvsTag.log
+#echo "================== Tagging third party ======================="
+#ant -f src/fetch_org.eclipse.birt.thirdparty.xml -propertyfile cvsTag.properties > third.log
 
-echo "================== Tagging sdk feature =======================" >> cvsTag.log
-ant -f src/fetch_org.eclipse.birt.sdk.xml -propertyfile cvsTag.properties >> cvsTag.log
+echo "================== Tagging sdk feature ======================="
+ant -f src/fetch_org.eclipse.birt.sdk.xml -propertyfile cvsTag.properties > tagSDK.log
 
-echo "================== Tagging rcp feature =======================" >> cvsTag.log
-ant -f src/fetch_org.eclipse.birt.rcp.xml -propertyfile cvsTag.properties >> cvsTag.log
+echo "================== Tagging rcp feature ======================="
+ant -f src/fetch_org.eclipse.birt.report.designer.ui.rcp.xml -propertyfile cvsTag.properties > cvsRCP.log
 
-echo "================== Tagging tests feature =======================" >> cvsTag.log
-ant -f src/fetch_org.eclipse.birt.tests.xml -propertyfile cvsTag.properties >> cvsTag.log
+echo "================== Tagging tests feature ======================="
+ant -f src/fetch_org.eclipse.birt.tests.xml -propertyfile cvsTag.properties > cvsTest.log
 
-echo "================== Tagging birt finished!! =======================" >> cvsTag.log
+echo "================== Tagging chart.viewer feature ======================="
+
+ant -f src/fetch_org.eclipse.birt.chart.viewer.xml -propertyfile cvsTag.properties > cvsChartViewer.log
+
+echo "================== Tagging wtp integration feature ======================="
+ant -f src/fetch_org.eclipse.birt.integration.wtp.sdk.xml -propertyfile cvsTag.properties > cvsWTP.log
+echo "================== Tagging rcp feature ======================="
+
+ant -f src/fetch_org.eclipse.pde.build.container.feature.xml -propertyfile cvsTag.properties > cvsRCPFeature.log
+echo "================== Tagging birt finished!! ======================="
 
