5718b11f2ab3550701fa62964384f985e4b0c4aa
Xiaoying Gu <xgu@actuate.com>
2009-09-08 12:44:00 +0000

    update dtp tagging script

--- build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag-dtp.sh
+++ build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag-dtp.sh
@@ -4,19 +4,19 @@ echo starting tag datatools...
 
 CVS_RSH=ssh
 export CVS_RSH
-
+export src.dir=/home/adb/farrah/cvsTag/src
 
 #1.6
-#ant -f src/fetch_org.eclipse.datatools.enablement-all.feature.xml -propertyfile cvsTag.properties > cvsTag.log
+#ant -f $src.dir/fetch_org.eclipse.datatools.enablement-all.feature.xml -propertyfile cvsTag.properties > cvsTag.log
 
 #1.7
-ant -f src/fetch_org.eclipse.datatools.enablement.sdk.feature.xml -propertyfile cvsTag.properties > cvsTag.log
+ant -f $src.dir/fetch_org.eclipse.datatools.enablement.sdk.feature.xml -propertyfile cvsTag.properties > cvsTag.log
 
 
 
-ant -f src/fetch_org.eclipse.datatools.sdk-all.feature.xml  -propertyfile cvsTag.properties >> cvsTag.log
-#ant -f src/fetch_org.eclipse.datatools.sdk.feature.xml  -propertyfile cvsTag.properties >> cvsTag.log
+ant -f $src.dir/fetch_org.eclipse.datatools.sdk-all.feature.xml  -propertyfile cvsTag.properties >> cvsTag.log
+#ant -f $src.dir/fetch_org.eclipse.datatools.sdk.feature.xml  -propertyfile cvsTag.properties >> cvsTag.log
 
-ant -f src/fetch_org.eclipse.datatools.enablement.oda.ecore.feature.xml -propertyfile cvsTag.properties >> cvsTag.log
-ant -f src/fetch_org.eclipse.datatools.enablement.oda.ecore.sdk.feature.xml -propertyfile cvsTag.properties >> cvsTag.log
+ant -f $src.dir/fetch_org.eclipse.datatools.enablement.oda.ecore.feature.xml -propertyfile cvsTag.properties >> cvsTag.log
+ant -f $src.dir/fetch_org.eclipse.datatools.enablement.oda.ecore.sdk.feature.xml -propertyfile cvsTag.properties >> cvsTag.log
 
