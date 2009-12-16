ecbfa2b4016fe1cc5a0dd4b8fd51e31e415f40a3
cchao <>
2009-12-16 06:51:47 +0000

    - Summary: report save status issue when new cube in chart builder [1301]
    - Bugzilla Bug (s) Resolved: 287773
    
    - Description: Use  persistent transaction in NewCubeAction.
    
    - Tests Description: Manual test.
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:
    
    - Files Added:
    
    
    - Files Edited:
    
    
    - Files Deleted:

--- UI/org.eclipse.birt.report.designer.ui.cubebuilder/src/org/eclipse/birt/report/designer/ui/cubebuilder/action/NewCubeAction.java
+++ UI/org.eclipse.birt.report.designer.ui.cubebuilder/src/org/eclipse/birt/report/designer/ui/cubebuilder/action/NewCubeAction.java
@@ -89,7 +89,7 @@ public class NewCubeAction extends Action
 		List existingCubes = getCubes( );
 
 		CommandStack stack = getActionStack( );
-		stack.startTrans( "Create Cube" ); //$NON-NLS-1$
+		stack.startPersistentTrans( Messages.getString("NewCubeAction.trans.cube.new") );  //$NON-NLS-1$
 
 		TabularCubeHandle newCube = DesignElementFactory.getInstance( )
 				.newTabularCube( Messages.getString("NewCubeAction.DataCube") ); //$NON-NLS-1$
