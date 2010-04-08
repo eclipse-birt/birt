a1af7ad5a26c2fac955b8e7ba25734bc247b83b0
yulin <>
2010-04-08 08:32:05 +0000

    - Summary: Proj 1175: failed to preserve Yseries grouping in a chart that using xtab with multiple shared dimensions as data source
     - TED Bug (s) Resolved: 24085
    
     - Description:
    @desc
    TED#24085
    Use designer's Data Service to register session before evaluating data cube.
    @desc
    
    - Tests Description:  Manual Test
    
    - Notes to Build Team: N/A
    
    - Notes to Developers: N/A
    
    - Notes to QA:  N/A
    
    - Notes to Documentation:  N/A
    
    - Files Edited:
    @changelist
    
    - Files Added:
    @addlist
    
    - Files Deleted
    @deletelist

--- chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ReportDataServiceProvider.java
+++ chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ReportDataServiceProvider.java
@@ -89,6 +89,7 @@ import org.eclipse.birt.report.data.adapter.api.DataSessionContext;
 import org.eclipse.birt.report.data.adapter.api.IModelAdapter;
 import org.eclipse.birt.report.designer.data.ui.util.DataSetProvider;
 import org.eclipse.birt.report.designer.data.ui.util.DummyEngineTask;
+import org.eclipse.birt.report.designer.internal.ui.data.DataService;
 import org.eclipse.birt.report.designer.internal.ui.expressions.IExpressionConverter;
 import org.eclipse.birt.report.designer.internal.ui.util.DataUtil;
 import org.eclipse.birt.report.designer.internal.ui.util.ExpressionUtility;
@@ -1935,7 +1936,8 @@ public class ReportDataServiceProvider implements IDataServiceProvider
 
 		if ( needDefineCube( cube ) )
 		{
-			session.defineCube( cube );
+			DataService.getInstance( ).registerSession( cube, session );
+			session.defineCube( cube );	
 		}
 		
 		dteAdapter.populateApplicationContext( cube, session );
