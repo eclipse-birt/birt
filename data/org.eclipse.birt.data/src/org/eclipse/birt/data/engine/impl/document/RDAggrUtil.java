f32ea1fba2fabc0f89c072b26653ab2168a15ee6
Mingxia Wu <mwu@actuate.com>
2010-04-15 02:57:58 +0000

    throw out the exception when type conversion fails

--- data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/document/RDAggrUtil.java
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/document/RDAggrUtil.java
@@ -126,6 +126,10 @@ public class RDAggrUtil
 			if ( this.contains( aggrName ) )
 			{
 				Object value = holders.get( aggrName ).get( groupInstanceIndex );
+
+				if ( value != null && value instanceof BirtException )
+					throw (BirtException) value;
+				
 				if ( qd != null && qd.getBindings( ).containsKey( aggrName ))
 				{
 					IBinding b = (IBinding)qd.getBindings( ).get( aggrName );
@@ -144,7 +148,7 @@ public class RDAggrUtil
 		}
 		catch ( BirtException e )
 		{
-			throw new DataException( e.getLocalizedMessage( ), e );
+			throw new DataException( e.getErrorCode( ), e );
 		}
 	}
 
