d835b77af37e4a132f071eed1dc4f3c10f07c02e
jili <>
2010-05-11 00:56:17 +0000

    25691 - java.util.EmptyStackException exception error when adding bookmarks from .rptdocument.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ReportletBodyExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/internal/document/v4/ReportletBodyExecutor.java
@@ -16,6 +16,11 @@ import java.util.ArrayList;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.report.engine.content.IContent;
 import org.eclipse.birt.report.engine.content.IStyle;
+import org.eclipse.birt.report.engine.content.impl.AbstractBandContent;
+import org.eclipse.birt.report.engine.content.impl.CellContent;
+import org.eclipse.birt.report.engine.content.impl.RowContent;
+import org.eclipse.birt.report.engine.content.impl.TableContent;
+import org.eclipse.birt.report.engine.content.impl.TableGroupContent;
 import org.eclipse.birt.report.engine.css.dom.StyleDeclaration;
 import org.eclipse.birt.report.engine.css.engine.CSSEngine;
 import org.eclipse.birt.report.engine.css.engine.StyleConstants;
@@ -75,9 +80,27 @@ public class ReportletBodyExecutor implements IReportItemExecutor
 				{
 					if ( docExt.getIndex( ) == offset )
 					{
-						bodyContent = content;
-						childExecutor = executor;
-						break;
+						if ( docExt.getIndex( ) == offset )
+						{
+							if ( content instanceof TableGroupContent
+									|| content instanceof RowContent
+									|| content instanceof CellContent
+									|| content instanceof AbstractBandContent )
+							{
+								do
+								{
+									content = (IContent) content.getParent( );
+									// wrap parent executor which has only one child
+									WrappedExecutor parentExecutor = new WrappedExecutor( executor.getParent( ),
+											content,
+											executor );
+									executor = parentExecutor;
+								} while ( !( content instanceof TableContent ) );
+							}
+							bodyContent = content;
+							childExecutor = executor;
+							break;
+						}
 					}
 				}
 			}
@@ -171,5 +194,83 @@ public class ReportletBodyExecutor implements IReportItemExecutor
 	public void setParent( IReportItemExecutor parent )
 	{
 
+	}
+	
+	static class WrappedExecutor implements IReportItemExecutor
+	{
+		IContent content;
+		IReportItemExecutor executor;
+		IReportItemExecutor childExecutor;
+
+		WrappedExecutor( IReportItemExecutor executor, IContent content,
+				IReportItemExecutor childExecutor )
+		{
+			this.content = content;
+			this.executor = executor;
+			this.childExecutor = childExecutor;
+		}
+
+		public void close( ) throws BirtException
+		{
+		}
+
+		
+		public IContent execute( )
+		{
+			return content;
+		}
+
+		public IContent getContent( )
+		{
+			return content;
+		}
+
+		public IExecutorContext getContext( )
+		{
+			return executor.getContext( );
+		}
+
+		public Object getModelObject( )
+		{
+			return executor.getModelObject( );
+		}
+
+		public IReportItemExecutor getNextChild( )
+		{
+			if ( childExecutor != null )
+			{
+				IReportItemExecutor executor = childExecutor;
+				childExecutor = null;
+				return executor;
+			}
+			return null;
+		}
+
+		public IReportItemExecutor getParent( )
+		{
+			return executor.getParent( );
+		}
+
+		public IBaseResultSet[] getQueryResults( )
+		{
+			return executor.getQueryResults( );
+		}
+
+		public boolean hasNextChild( )
+		{
+			return childExecutor != null;
+		}
+
+		public void setContext( IExecutorContext context )
+		{
+		}
+
+		public void setModelObject( Object handle )
+		{
+		}
+
+		public void setParent( IReportItemExecutor parent )
+		{
+		}
 	}
 }
