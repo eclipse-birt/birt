0d0dfb1c944a23602f1dd2890550a07294a5a7d8
yuchen <>
2010-05-19 03:47:11 +0000

    Bug 312763 - [regression] Masterpage disappeared in PDF when header/footer height unit is %

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/RegionArea.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/RegionArea.java
@@ -12,6 +12,8 @@
 package org.eclipse.birt.report.engine.nLayout.area.impl;
 
 import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.report.engine.content.IContent;
+import org.eclipse.birt.report.engine.ir.DimensionType;
 import org.eclipse.birt.report.engine.nLayout.area.IContainerArea;
 
 public class RegionArea extends BlockContainerArea implements IContainerArea
@@ -31,7 +33,20 @@ public class RegionArea extends BlockContainerArea implements IContainerArea
 		calculateSpecifiedWidth( content );
 		calculateSpecifiedHeight( content );
 	}
-
+	
+	// override this method because for region area, the reference height for
+	// percentage calculating can be retrieved in advance.
+	protected void calculateSpecifiedHeight( IContent content )
+	{
+		if ( content == null )
+			return;
+		DimensionType height = content.getHeight( );
+		if ( height != null )
+		{
+			specifiedHeight = getDimensionValue( content, height, parent.getHeight( ) );
+		}
+	}
+	
 	public void close( ) throws BirtException
 	{
 		if ( context.isFixedLayout( ) )
