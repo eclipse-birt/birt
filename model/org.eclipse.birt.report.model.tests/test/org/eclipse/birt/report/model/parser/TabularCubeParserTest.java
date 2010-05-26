ae88dacc0e6fd23af534257490cbe431e6de1963
Yueqian Wang <ywang@actuate.com>
2010-05-26 04:16:11 +0000

    - Summary: Update test cases
    -Bug (s) Resolved:
    
    - Description:
    Fix some test cases failures
    
    - Tests Description:
    Auto Test
    
    - Files Edited:
    
    - Files Added:
    
    - Files Deleted:
    
    - Notes to Build Team:
    
    - Notes to Developers:
    
    - Notes to QA:
    
    - Notes to Documentation:

--- model/org.eclipse.birt.report.model.tests/test/org/eclipse/birt/report/model/parser/TabularCubeParserTest.java
+++ model/org.eclipse.birt.report.model.tests/test/org/eclipse/birt/report/model/parser/TabularCubeParserTest.java
@@ -240,8 +240,7 @@ public class TabularCubeParserTest extends BaseTestCase
 		assertEquals( measureGroup, cube.getContent(
 				TabularCubeHandle.MEASURE_GROUPS_PROP, 0 ) );
 		assertEquals( "testMeasureGroup", measureGroup.getName( ) ); //$NON-NLS-1$
-		// test getDefaultMeasureGroup in cube
-		assertEquals( measureGroup, cube.getDefaultMeasureGroup( ) );
+
 		propHandle = measureGroup
 				.getPropertyHandle( MeasureGroupHandle.MEASURES_PROP );
 
