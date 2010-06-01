559279e879ce131b6e17871c9941d4385b42a195
heli <>
2010-06-01 02:58:03 +0000

    - Summary: NPE is thrown out when preview radar type chart for birt runtime
     - Bugzilla Bug (s) Resolved: 314810
    
     - Description:
    Adjusted extension definitions.
    
    - Tests Description:  Manual Test
    
    - Notes to Build Team: N/A
    
    - Notes to Developers: N/A
    
    - Notes to QA:  N/A
    
    - Notes to Documentation:  N/A
    
    - Files Edited:
    @changelist
    
    - Files Added:
    @addlist
    
    - Files Deleted
    @deletelist

--- chart/org.eclipse.birt.chart.examples.core/src/org/eclipse/birt/chart/examples/ChartExamplesCorePlugin.java
+++ chart/org.eclipse.birt.chart.examples.core/src/org/eclipse/birt/chart/examples/ChartExamplesCorePlugin.java
@@ -1,38 +1,15 @@
 package org.eclipse.birt.chart.examples;
 
-import org.osgi.framework.BundleActivator;
-import org.osgi.framework.BundleContext;
+import org.eclipse.core.runtime.Plugin;
 
 /**
  * Plugin class for Chart Examples Core. Holds the plugin ID
  */
-public class ChartExamplesCorePlugin implements BundleActivator {
+public class ChartExamplesCorePlugin extends Plugin
+{
 
 	/**
 	 * Plugin ID.
 	 */
 	public static final String ID = "org.eclipse.birt.chart.examples.core"; //$NON-NLS-1$
-	
-	private static BundleContext context;
-
-	static BundleContext getContext() {
-		return context;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		ChartExamplesCorePlugin.context = bundleContext;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		ChartExamplesCorePlugin.context = null;
-	}
-
 }
