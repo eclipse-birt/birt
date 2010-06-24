2d3701d466e806c3a3a88e36448f4f06a8a05e64
Xiaoying Gu <xgu@actuate.com>
2010-06-24 07:58:11 +0000

    Add check util

--- build/org.eclipse.birt.build/src/org/eclipse/birt/build/BuildUtil.java
+++ build/org.eclipse.birt.build/src/org/eclipse/birt/build/BuildUtil.java
@@ -10,8 +10,13 @@
 package org.eclipse.birt.build;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
+import java.util.jar.JarFile;
+
 
 import org.apache.tools.ant.BuildException;
 
@@ -67,5 +72,61 @@ public class BuildUtil
 		}
 
 		return null;
+	}
+	
+	public static boolean checkAboutFile(File jarname)
+	{
+		
+		try{
+		    
+			JarFile a = new JarFile(jarname);
+			
+		    if(a.getEntry("about.html")!=null){
+				//System.out.println("license exists");
+				return true;
+			}else{
+				//System.out.println("license is missing");
+				return false;
+			}
+		}catch(IOException e)
+		{
+			System.out.println(e.getMessage() + e.getStackTrace());
+			System.out.println("JAR doesn't exist");
+		}
+		return false;
+	}
+	
+	public static File[] getJarList(String dir){
+		
+		File[] flist;
+		File file = new File(dir);
+		flist = file.listFiles(new JarFileFilter());
+		return flist;
+		
+	}
+	
+	static class JarFileFilter implements FileFilter {
+
+		  public boolean accept(File pathname) {
+
+		    if (pathname.getName().endsWith(".jar"))
+		      return true;
+		    
+		    return false;
+		  }
+
+	public static void main(String[] args){
+		//BuildUtil checker = new BuildUtil();
+		String checkdir = "E:/test/2_6_0/temp/birt-updatesite-2_6_0-20100601/plugins";
+		File[] list = BuildUtil.getJarList(checkdir);
+		for(int i=0;i<list.length;i++){
+			System.out.println("Checking " + list[i].getName());
+			if(BuildUtil.checkAboutFile(list[i]))
+				;//System.out.println("pass");
+			else
+				System.out.println("File: " + list[i].getName() + "............................missing about.html");
+		}
+	}
+	
 	}
 }
