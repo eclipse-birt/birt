c4ef52c21e48bcff2d87d0479b4cd6faca816fb8
Linda Chan <lchan@actuate.com>
2010-07-08 23:18:50 +0000

    [318531] Set  default SampleDB database location in the auto-generated BIRT SampleDb driver definition

---
+++ data/org.eclipse.birt.report.data.oda.jdbc.dbprofile.sampledb.ui/src/org/eclipse/birt/report/data/oda/jdbc/dbprofile/sampledb/ui/impl/SampleDbDriverUIContributor.java
@@ -0,0 +1,27 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2010 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.birt.report.data.oda.jdbc.dbprofile.sampledb.ui.impl;
+
+import org.eclipse.datatools.connectivity.apache.derby.internal.ui.connection.drivers.DerbyEmbeddedDriverUIContributor;
+
+/**
+ * UI contributor for the generated BIRT SampleDB driver definition,
+ * based on the Embedded Derby Driver UI.
+ */
+@SuppressWarnings("restriction")
+public class SampleDbDriverUIContributor extends DerbyEmbeddedDriverUIContributor
+{
+    // no overriding behavior yet
+}
