fb629ec270b93b8f01dd72303164c28bd41e30a5
ywang1 <>
2010-07-15 03:35:18 +0000

    Fixed #28730.

--- chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemPresentationAxisImpl.java
+++ chart/org.eclipse.birt.chart.reportitem/src/org/eclipse/birt/chart/reportitem/ChartReportItemPresentationAxisImpl.java
@@ -15,9 +15,7 @@ import org.eclipse.birt.chart.exception.ChartException;
 import org.eclipse.birt.chart.factory.IDataRowExpressionEvaluator;
 import org.eclipse.birt.chart.model.Chart;
 import org.eclipse.birt.chart.model.ChartWithAxes;
-import org.eclipse.birt.chart.model.attribute.AxisType;
 import org.eclipse.birt.chart.model.attribute.Bounds;
-import org.eclipse.birt.chart.model.component.Axis;
 import org.eclipse.birt.chart.reportitem.api.ChartCubeUtil;
 import org.eclipse.birt.chart.reportitem.api.ChartReportItemConstants;
 import org.eclipse.birt.chart.util.ChartUtil;
@@ -26,8 +24,6 @@ import org.eclipse.birt.report.engine.extension.IBaseResultSet;
 import org.eclipse.birt.report.item.crosstab.core.de.AggregationCellHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
 import org.eclipse.birt.report.model.api.extension.IReportItem;
-
-import com.ibm.icu.util.Calendar;
 
 /**
  * Presentation implementation for Chart Axis in Cross tab
@@ -139,29 +135,16 @@ public final class ChartReportItemPresentationAxisImpl extends
 	protected IDataRowExpressionEvaluator createEvaluator( IBaseResultSet set )
 			throws ChartException
 	{
+		final IDataRowExpressionEvaluator evaluator = super.createEvaluator( set );
+		
 		// If no shared scale, to get evaluator from query.
 		if ( rtc.getSharedScale( ) == null || !rtc.getSharedScale( ).isShared( ) )
 		{
-			return super.createEvaluator( set );
-		}
-
-		// Check the axis type to return the dummy data with correct type
-		final boolean bDatetypeAxis;
-		if ( cm instanceof ChartWithAxes )
-		{
-			ChartWithAxes cwa = (ChartWithAxes) cm;
-			Axis yAxis = cwa.getOrthogonalAxes( cwa.getBaseAxes( )[0], true )[0];
-			bDatetypeAxis = yAxis.getType( ) == AxisType.DATE_TIME_LITERAL;
-		}
-		else
-		{
-			bDatetypeAxis = false;
+			return evaluator;
 		}
 
 		// Return a dummy data set since axis chart can render without data
 		return new IDataRowExpressionEvaluator( ) {
-
-			private int count = 1;
 
 			public void close( )
 			{
@@ -170,24 +153,23 @@ public final class ChartReportItemPresentationAxisImpl extends
 
 			public Object evaluate( String expression )
 			{
-				return bDatetypeAxis ? Calendar.getInstance( )
-						: Integer.valueOf( 1 );
+				return evaluator.evaluate( expression );
 			}
 
 			@SuppressWarnings("deprecation")
 			public Object evaluateGlobal( String expression )
 			{
-				return evaluate( expression );
+				return evaluator.evaluateGlobal( expression );
 			}
 
 			public boolean first( )
 			{
-				return true;
+				return evaluator.first( );
 			}
 
 			public boolean next( )
 			{
-				return count-- > 0;
+				return false;
 			}
 		};
 	}
