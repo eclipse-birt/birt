685ed25e38be48de163763275e8a5fe094c630eb
Rick Lu <rlu@actuate.com>
2010-09-19 07:11:28 +0000

    When converts the ODA to BIRT, some information for parameter definitions may be lost. So, must get the cached values first. Then try to restore to latest value.

--- model/org.eclipse.birt.report.model.adapter.oda/src/org/eclipse/birt/report/model/adapter/oda/impl/DataSetAdapter.java
+++ model/org.eclipse.birt.report.model.adapter.oda/src/org/eclipse/birt/report/model/adapter/oda/impl/DataSetAdapter.java
@@ -13,7 +13,6 @@ package org.eclipse.birt.report.model.adapter.oda.impl;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.birt.report.model.adapter.oda.IConstants;
@@ -27,7 +26,6 @@ import org.eclipse.birt.report.model.api.CommandStack;
 import org.eclipse.birt.report.model.api.DataSourceHandle;
 import org.eclipse.birt.report.model.api.ModuleHandle;
 import org.eclipse.birt.report.model.api.OdaDataSetHandle;
-import org.eclipse.birt.report.model.api.OdaDataSetParameterHandle;
 import org.eclipse.birt.report.model.api.OdaDataSourceHandle;
 import org.eclipse.birt.report.model.api.PropertyHandle;
 import org.eclipse.birt.report.model.api.activity.SemanticException;
@@ -93,75 +91,8 @@ class DataSetAdapter extends AbstractDataAdapter
 			return null;
 
 		DataSetDesign setDesign = designFactory.createDataSetDesign( );
-		doCreateDataSetDesign( setHandle, setDesign );
+		updateDataSetDesign( setHandle, setDesign );
 		return setDesign;
-	}
-
-	/**
-	 * @param setHandle
-	 * @param setDesign
-	 */
-
-	private void doCreateDataSetDesign( OdaDataSetHandle setHandle,
-			DataSetDesign setDesign )
-	{
-		// properties on ReportElement, like name, displayNames, etc.
-
-		setDesign.setName( setHandle.getName( ) );
-
-		String displayName = setHandle.getDisplayName( );
-		String displayNameKey = setHandle.getDisplayNameKey( );
-
-		if ( displayName != null || displayNameKey != null )
-		{
-			setDesign.setDisplayName( displayName );
-			setDesign.setDisplayNameKey( displayNameKey );
-		}
-
-		// properties such as comments, extends, etc are kept in
-		// DataSourceHandle, not DataSourceDesign.
-
-		// scripts of DataSource are kept in
-		// DataSourceHandle, not DataSourceDesign.
-
-		setDesign.setOdaExtensionDataSetId( setHandle.getExtensionID( ) );
-
-		setDesign.setPublicProperties( newOdaPublicProperties(
-				setHandle.getExtensionPropertyDefinitionList( ), setHandle ) );
-
-		setDesign.setPrivateProperties( newOdaPrivateProperties( setHandle
-				.privateDriverPropertiesIterator( ) ) );
-
-		setDesign.setPrimaryResultSetName( setHandle.getResultSetName( ) );
-
-		setDesign.setQueryText( setHandle.getQueryText( ) );
-
-		// create a new data source design for this set design.
-
-		OdaDataSourceHandle sourceHandle = (OdaDataSourceHandle) setHandle
-				.getDataSource( );
-
-		if ( sourceHandle != null )
-			setDesign.setDataSourceDesign( new DataSourceAdapter( )
-					.createDataSourceDesign( sourceHandle ) );
-
-		DataSetParameterAdapter dataParamAdapter = new DataSetParameterAdapter(
-				setHandle, setDesign );
-
-		Iterator<OdaDataSetParameterHandle> tmpParams = setHandle
-				.parametersIterator( );
-		List<OdaDataSetParameterHandle> setDefinedParams = new ArrayList<OdaDataSetParameterHandle>( );
-		while ( tmpParams.hasNext( ) )
-			setDefinedParams.add( tmpParams.next( ) );
-
-		setDesign.setParameters( dataParamAdapter
-				.newOdaDataSetParams( setDefinedParams ) );
-
-		new ResultSetsAdapter( setHandle, setDesign )
-				.updateOdaResultSetDefinition( );
-
-		updateODAMessageFile( setDesign.getDataSourceDesign( ),
-				setHandle.getModuleHandle( ) );
 	}
 
 	/**
@@ -211,6 +142,10 @@ class DataSetAdapter extends AbstractDataAdapter
 		if ( sourceHandle != null )
 			setDesign.setDataSourceDesign( new DataSourceAdapter( )
 					.createDataSourceDesign( sourceHandle ) );
+
+		// when converts the ODA to BIRT, some information for parameter
+		// definitions may be lost. So, must get the cached values first. Then
+		// try to restore to latest value.
 
 		String strDesignValues = setHandle.getDesignerValues( );
 
