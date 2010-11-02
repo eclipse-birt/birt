13661d5134cf51dbad208a618542b10ecab76cd4
Dazheng Gao <dgao@actuate.com>
2010-11-02 02:34:42 +0000

    - Summary:When export the design file to a library.notify the resource change and library save change event.
    - Bugzilla Bug (s) Resolved:
    
    
    
    - Description:
    
    
    
    - Tests Description  :
    
    Manual test
    
    - Files Edited:
    
    
    
    - Files Added:
    
    
    
    - Files Deleted:
    
    
    
    - Notes to Build Team:
    
    
    
    - Notes to Developers:
    
    
    
    - Notes to QA:
    
    
    
    -          Notes to Documentation:

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/views/actions/ExportToLibraryAction.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/views/actions/ExportToLibraryAction.java
@@ -15,6 +15,7 @@ import java.io.File;
 
 import org.eclipse.birt.report.designer.internal.ui.util.ExceptionHandler;
 import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
+import org.eclipse.birt.report.designer.internal.ui.views.LibrarySaveChangeEvent;
 import org.eclipse.birt.report.designer.internal.ui.views.ReportResourceChangeEvent;
 import org.eclipse.birt.report.designer.internal.ui.wizards.ExportReportWizardPage;
 import org.eclipse.birt.report.designer.nls.Messages;
@@ -263,6 +264,8 @@ public class ExportToLibraryAction extends AbstractViewAction
 					{
 						synchronizer.notifyResourceChanged( new ReportResourceChangeEvent( this,
 								Path.fromOSString( filename ), IReportResourceChangeEvent.NewResource|IReportResourceChangeEvent.LibraySaveChange ) );
+						synchronizer.notifyResourceChanged( new LibrarySaveChangeEvent( this,
+								Path.fromOSString( filename ), IReportResourceChangeEvent.LibraySaveChange, Path.fromOSString( filename ).toOSString( ) ) );
 					}
 				}
 			}
