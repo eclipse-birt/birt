41a58c98cec1286b1fb612c84b2253cf86600116
wtu <>
2010-11-08 05:47:02 +0000

    Checkin:Fail to show data with dataobject dataset which has event handler[28646]

---
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/IDataSetInterceptor.java
@@ -0,0 +1,29 @@
+package org.eclipse.birt.report.data.adapter.api;
+
+import java.util.Map;
+
+import org.eclipse.birt.core.exception.BirtException;
+import org.eclipse.birt.data.engine.api.IBaseDataSetDesign;
+import org.eclipse.birt.data.engine.api.IBaseDataSourceDesign;
+import org.eclipse.birt.report.model.api.ModuleHandle;
+
+/**
+ * Interceptor for one type of data set.
+ * 
+ *
+ */
+public interface IDataSetInterceptor
+{
+	/**
+	 * 
+	 * @param appContext
+	 * @param dsource
+	 * @param dset
+	 * @param moduleHandle
+	 * @throws BirtException
+	 */
+	void preDefineDataSet( Map appContext,
+			IBaseDataSourceDesign dsource,
+			IBaseDataSetDesign dset,
+			ModuleHandle moduleHandle ) throws BirtException;
+}
