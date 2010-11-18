299b8f43c1c79ccb72904d5f0420b39161c7db44
Xiaoying Gu <xgu@actuate.com>
2010-11-18 07:15:14 +0000

    add tagging rcp.nl and wtp.nl features

--- build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag.sh
+++ build/org.eclipse.birt.releng.birtbuilder/extras/cvsTag.sh
@@ -32,6 +32,12 @@ ant -f $SrcDir/fetch_org.eclipse.birt.report.designer.ui.rcp.xml -propertyfile c
 echo "================== Tagging nl feature ======================="> tagNL.log
 ant -f $SrcDir/fetch_org.eclipse.birt.nl.xml -propertyfile cvsTag.properties -DfeatureOnly=true -DfeatureAndPlugins=true -DfeaturesRecursively=true >> tagNL.log
 
+echo "================== Tagging rcp nl feature ======================="> tagNL.log
+ant -f $SrcDir/fetch_org.eclipse.birt.rcp.nl.xml -propertyfile cvsTag.properties -DfeatureOnly=true -DfeatureAndPlugins=true -DfeaturesRecursively=true >> tagNL.log
+
+echo "================== Tagging wtp nl feature ======================="> tagNL.log
+ant -f $SrcDir/fetch_org.eclipse.birt.wtp.nl.xml -propertyfile cvsTag.properties -DfeatureOnly=true -DfeatureAndPlugins=true -DfeaturesRecursively=true >> tagNL.log
+
 echo "================== Tagging rcp feature =======================">> tagRCP.log
 ant -f $SrcDir/fetch_org.eclipse.pde.build.container.feature.xml -propertyfile cvsTag.properties -DfeatureOnly=true -DfeatureAndPlugins=true -DfeaturesRecursively=true >> tagRCP.log
 
