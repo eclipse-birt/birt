ea0bc8dc8a7079c6c25e93089b79f2d495a11494
Xiaoying Gu <xgu@actuate.com>
2010-12-01 08:05:06 +0000

    The version of BIRT Charting Runtime Component & BIRT Reporting Runtime Component should be updated to 2.6.1

--- viewer/org.eclipse.birt.integration.wtp.ui/src/org/eclipse/birt/integration/wtp/ui/project/facet/IBirtFacetConstants.java
+++ viewer/org.eclipse.birt.integration.wtp.ui/src/org/eclipse/birt/integration/wtp/ui/project/facet/IBirtFacetConstants.java
@@ -21,5 +21,5 @@ public interface IBirtFacetConstants
 	// project facet id of Birt Runtime Component
 	public static final String BIRT_RUNTIME_FACET_ID = "birt.runtime"; //$NON-NLS-1$
 	
-	public static final String BIRT_RUNTIME_FACET_VERSION = "2.6.0"; //$NON-NLS-1$
+	public static final String BIRT_RUNTIME_FACET_VERSION = "2.6.2"; //$NON-NLS-1$
 }
