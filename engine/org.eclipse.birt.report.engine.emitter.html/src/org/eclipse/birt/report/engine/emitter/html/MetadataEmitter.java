2e17bd9e1851d202a102ddb8d3def3ff0061a1bb
Gang Liu <gliu@actuate.com>
2010-12-20 07:30:56 +0000

    Fix No visutal indicator to modify crosstab in BIRT Studio(T28535)

--- engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/MetadataEmitter.java
+++ engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/MetadataEmitter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 Actuate Corporation.
+ * Copyright (c) 2004, 2010 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -606,6 +606,11 @@ public class MetadataEmitter
 		{
 			TemplateDesign templateDesign = (TemplateDesign) genBy;
 			handle = templateDesign.getHandle( );
+		}
+		else if (genBy instanceof ExtendedItemDesign)
+		{
+			ExtendedItemDesign extendedItemDesign = (ExtendedItemDesign)genBy;
+			handle = extendedItemDesign.getHandle( );
 		}
 		// judge the content is belong table template element or not.
 		if ( ( null != handle ) && handle.isTemplateParameterValue( ) )
