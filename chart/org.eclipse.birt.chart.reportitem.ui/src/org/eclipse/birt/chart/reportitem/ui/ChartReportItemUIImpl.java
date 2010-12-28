a0e50d8be7e994ac36ac503985c093ad0cd2b193
yulin <>
2010-12-28 07:16:29 +0000

    TED#29525 The fix is to not update chart model bound size in design time, and defer this to runtime only. Now size change in designer won't update chart model, so this operation won't flatten chart model from library to report design any more.

--- chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIImpl.java
+++ chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIImpl.java
@@ -17,7 +17,6 @@ import java.util.Map;
 import org.eclipse.birt.chart.device.IDisplayServer;
 import org.eclipse.birt.chart.log.ILogger;
 import org.eclipse.birt.chart.log.Logger;
-import org.eclipse.birt.chart.model.Chart;
 import org.eclipse.birt.chart.model.ChartWithAxes;
 import org.eclipse.birt.chart.model.attribute.Bounds;
 import org.eclipse.birt.chart.model.component.Axis;
@@ -27,7 +26,6 @@ import org.eclipse.birt.chart.reportitem.api.ChartItemUtil;
 import org.eclipse.birt.chart.reportitem.api.ChartReportItemConstants;
 import org.eclipse.birt.chart.reportitem.ui.i18n.Messages;
 import org.eclipse.birt.chart.ui.util.ChartUIUtil;
-import org.eclipse.birt.chart.util.ChartUtil;
 import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.report.designer.core.model.DesignElementHandleAdapter;
 import org.eclipse.birt.report.designer.internal.ui.editors.schematic.figures.ReportElementFigure;
@@ -36,7 +34,6 @@ import org.eclipse.birt.report.designer.util.DEUtil;
 import org.eclipse.birt.report.designer.util.ImageManager;
 import org.eclipse.birt.report.item.crosstab.core.de.AggregationCellHandle;
 import org.eclipse.birt.report.model.api.DesignElementHandle;
-import org.eclipse.birt.report.model.api.DimensionHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
 import org.eclipse.birt.report.model.api.StyleHandle;
 import org.eclipse.birt.report.model.api.activity.NotificationEvent;
@@ -46,7 +43,6 @@ import org.eclipse.birt.report.model.api.core.Listener;
 import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
 import org.eclipse.birt.report.model.api.metadata.DimensionValue;
-import org.eclipse.birt.report.model.api.util.DimensionUtil;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Point;
@@ -169,151 +165,30 @@ public class ChartReportItemUIImpl extends ReportItemFigureProvider
 		try
 		{
 			eih.loadExtendedElement( );
-			final ChartReportItemImpl crii = (ChartReportItemImpl) eih.getReportItem( );
 			// UPDATE THE MODEL
-			crii.setHandle( eih );
+			eih.getReportItem( ).setHandle( eih );
 			
 			refreshBackgroundImage( eih, (ReportElementFigure) ifg );
-
-			final boolean bAxisChart = ChartCubeUtil.isAxisChart( eih );
-			final ExtendedItemHandle hostChart;
-			final Chart cm;
-			if ( bAxisChart )
-			{
-				hostChart = (ExtendedItemHandle) eih.getElementProperty( ChartReportItemConstants.PROPERTY_HOST_CHART );
-				cm = ChartCubeUtil.getChartFromHandle( hostChart );
-			}
-			else
-			{
-				hostChart = null;
-				cm = (Chart) crii.getProperty( ChartReportItemConstants.PROPERTY_CHART );
-			}
-			if ( cm == null )
-			{
-				return;
-			}
-
-			Bounds defaultBounds = ChartItemUtil.createDefaultChartBounds( eih,
-					cm );
-
-			// Default size for null dimension
-			double dHeightInPoints = defaultBounds.getHeight( );
-			double dWidthInPoints = defaultBounds.getWidth( );
-
-			final DimensionHandle dhHeight;
-			final DimensionHandle dhWidth;
-			if ( bAxisChart )
-			{
-				// Use plot chart's size as axis chart's. Even if model sizes
-				// are different, the output size are same
-				if ( ChartXTabUIUtil.isTransposedChartWithAxes( cm ) )
-				{
-					dhHeight = eih.getHeight( );
-					dhWidth = hostChart.getWidth( );
-				}
-				else
-				{
-					dhHeight = hostChart.getHeight( );
-					dhWidth = eih.getWidth( );
-				}
-			}
-			else
-			{
-				dhHeight = eih.getHeight( );
-				dhWidth = eih.getWidth( );
-			}
-
-			double dOriginalHeight = dhHeight.getMeasure( );
-			String sHeightUnits = dhHeight.getUnits( );
-
-			double dOriginalWidth = dhWidth.getMeasure( );
-			String sWidthUnits = dhWidth.getUnits( );
-
+			
 			// USE THE SWT DISPLAY SERVER TO CONVERT POINTS TO PIXELS
 			final IDisplayServer idsSWT = ChartUIUtil.getDisplayServer( );
-
-			if ( sHeightUnits != null )
-			{
-				// Convert from pixels to points first...since DimensionUtil
-				// does not provide conversion services to and from Pixels
-				if ( sHeightUnits == DesignChoiceConstants.UNITS_PX )
-				{
-					dOriginalHeight = ChartUtil.convertPixelsToPoints( idsSWT,
-							dOriginalHeight );
-					sHeightUnits = DesignChoiceConstants.UNITS_PT;
-				}
-				// convert percentage to points
-				if ( sHeightUnits == DesignChoiceConstants.UNITS_PERCENTAGE )
-				{
-					IFigure parentFigure = ifg.getParent( );
-					if ( parentFigure != null )
-					{
-						int height = (int) ( ( parentFigure.getSize( ).height - parentFigure.getInsets( )
-								.getHeight( ) )
-								* dOriginalHeight / 100 );
-						dOriginalHeight = ChartUtil.convertPixelsToPoints( idsSWT,
-								height );
-						sHeightUnits = DesignChoiceConstants.UNITS_PT;
-					}
-				}
-				dHeightInPoints = DimensionUtil.convertTo( dOriginalHeight,
-						sHeightUnits,
-						DesignChoiceConstants.UNITS_PT ).getMeasure( );
-			}
-
-			if ( sWidthUnits != null )
-			{
-				// Convert from pixels to points first...since DimensionUtil
-				// does not provide conversion services to and from Pixels
-				if ( sWidthUnits == DesignChoiceConstants.UNITS_PX )
-				{
-					dOriginalWidth = ( dOriginalWidth * 72d )
-							/ idsSWT.getDpiResolution( );
-					sWidthUnits = DesignChoiceConstants.UNITS_PT;
-				}
-				// convert percentage to points
-				if ( sWidthUnits == DesignChoiceConstants.UNITS_PERCENTAGE )
-				{
-					IFigure parentFigure = ifg.getParent( );
-					if ( parentFigure != null )
-					{
-
-						int width = (int) ( ( parentFigure.getSize( ).width - parentFigure.getInsets( )
-								.getWidth( ) )
-								* dOriginalWidth / 100 );
-						dOriginalWidth = ChartUtil.convertPixelsToPoints( idsSWT,
-								width );
-						sWidthUnits = DesignChoiceConstants.UNITS_PT;
-					}
-				}
-				dWidthInPoints = DimensionUtil.convertTo( dOriginalWidth,
-						sWidthUnits,
-						DesignChoiceConstants.UNITS_PT ).getMeasure( );
-			}
-			
-			final double dHeightInPixels = ( idsSWT.getDpiResolution( ) * dHeightInPoints ) / 72d;
-			final double dWidthInPixels = ( idsSWT.getDpiResolution( ) * dWidthInPoints ) / 72d;
-
-			// Do not modify size for axis chart since it uses reference as
-			// model
-			if ( cm != null && !bAxisChart )
-			{
-				if ( dWidthInPoints >= 0 )
-					cm.getBlock( ).getBounds( ).setWidth( dWidthInPoints );
-				if ( dHeightInPoints >= 0 )
-					cm.getBlock( ).getBounds( ).setHeight( dHeightInPoints );
-			}
-			if ( crii.getDesignerRepresentation( ) != null )
-			{
-				( (DesignerRepresentation) crii.getDesignerRepresentation( ) ).setDirty( true );
-			}
+			final int dpi = idsSWT.getDpiResolution( );
+			Bounds bounds = ChartItemUtil.computeChartBounds( eih, dpi );
+			final double dHeightInPixels = ( dpi * bounds.getHeight( ) ) / 72d;
+			final double dWidthInPixels = ( dpi * bounds.getWidth( ) ) / 72d;			
 
 			// UPDATE THE FIGURE
+			( (DesignerRepresentation) ifg ).setDirty( true );
+			
 			Dimension newSize = ifg.getBounds( ).getCopy( ).getSize( );
 			if ( dWidthInPixels >= 0 )
+			{
 				newSize.width = (int) dWidthInPixels;
+			}
 			if ( dHeightInPixels >= 0 )
+			{
 				newSize.height = (int) dHeightInPixels;
+			}
 			ifg.setSize( newSize );
 		}
 		catch ( BirtException ex )
