6b61ce51184d4809aa887400afcfc0d04cab0b59
Rick Lu <rlu@actuate.com>
2011-01-24 02:45:32 +0000

    To reduce the class loading time, introduce the XML parser pool to read ROM and design files.

---
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/util/ParserFactory.java
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.util;
+
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+
+import org.xml.sax.SAXException;
+
+/**
+ * The XML parser factory. Internally, this factory use the xml parser pool to
+ * improve the performance.
+ */
+
+public class ParserFactory
+{
+
+	/**
+	 * The default pool size.
+	 */
+
+	private static final int POOL_SIZE = 50;
+
+	/**
+	 * Single instance.
+	 */
+	private static ParserFactory factory;
+
+	/**
+	 * Singleton pool instance
+	 */
+
+	private static XMLParserPool pool;
+
+	private ParserFactory( )
+	{
+		this( POOL_SIZE );
+
+	}
+
+	private ParserFactory( int poolSize )
+	{
+		pool = new XMLParserPoolImpl( poolSize );
+	}
+
+	/**
+	 * Returns the singleton instance.
+	 * 
+	 * @return the factory instance
+	 */
+
+	public static ParserFactory getInstance( )
+	{
+		if ( factory == null )
+		{
+			factory = new ParserFactory( );
+		}
+
+		return factory;
+	}
+
+	/**
+	 * Retrieves a parser from the pool given specified properties and features.
+	 * If parser can't be created using specified properties or features, an
+	 * exception can be thrown.
+	 * 
+	 * @param properties
+	 *            a map of a parser properties and their values.
+	 * @return A parser instance with given properties. *
+	 * 
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
+
+	public SAXParser getParser( Map<String, ?> properties )
+			throws SAXException, ParserConfigurationException
+	{
+		return pool.get( properties );
+	}
+
+	/**
+	 * Retrieves a parser from the pool given specified properties and features.
+	 * If parser can't be created using specified properties or features, an
+	 * exception can be thrown.
+	 * 
+	 * @param parser
+	 *            a parser instance with given properties.
+	 * @param properties
+	 *            a map of a parser properties and their values.
+	 * 
+	 * @throws SAXException
+	 * @throws ParserConfigurationException
+	 */
+
+	public void releaseParser( SAXParser parser, Map<String, ?> properties )
+			throws SAXException, ParserConfigurationException
+	{
+		pool.release( parser, properties );
+	}
+}
---
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/util/XMLParserPool.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.model.util;
+
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+
+import org.xml.sax.SAXException;
+
+/**
+ * The interface for reusing SAX parsers in a multi-thread environment.
+ */
+
+interface XMLParserPool
+{
+
+	/**
+	 * Retrieves a parser from the pool given specified properties. If parser
+	 * can't be created using specified properties, an exception can be thrown.
+	 * 
+	 * @param properties
+	 *            a map of a parser properties and their values.
+	 * @return A parser instance with given properties.
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 */
+
+	public SAXParser get( Map<String, ?> properties )
+			throws ParserConfigurationException, SAXException;
+
+	/**
+	 * Returns the parser to the pool.
+	 * 
+	 * @param parser
+	 *            the parser in the pool.
+	 * @param properties
+	 *            a map of a parser properties and their values.
+	 */
+	public void release( SAXParser parser, Map<String, ?> properties );
+
+}
