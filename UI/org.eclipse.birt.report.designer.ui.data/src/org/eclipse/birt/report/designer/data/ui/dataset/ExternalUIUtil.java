a1df3f3afb1414ff601272b84353362803bb8160
Lin Zhu <lzhu@actuate.com>
2011-02-18 06:50:24 +0000

    CheckIn: Fix an issue-Exception is thrown out when finishing data set with Property Bindings[37235].

--- UI/org.eclipse.birt.report.designer.ui.data/src/org/eclipse/birt/report/designer/data/ui/dataset/ExternalUIUtil.java
+++ UI/org.eclipse.birt.report.designer.ui.data/src/org/eclipse/birt/report/designer/data/ui/dataset/ExternalUIUtil.java
@@ -84,7 +84,7 @@ public class ExternalUIUtil
 
 			DummyEngineTask engineTask = new DummyEngineTask( engine,
 					new ReportEngineHelper( engine ).openReportDesign( copy ),
-					copy, dataSetHandle );
+					copy );
 
 			DataRequestSession session = engineTask.getDataSession( );
 
