7cdf4ea6975e114e69f9dc3ec183ea2abd56fc76
Gang Liu <gliu@actuate.com>
2011-03-11 08:30:50 +0000

    Fix Null Toc with child toc node like table/group doesn't show top null toc(T38466)

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/executor/ReportItemExecutor.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/executor/ReportItemExecutor.java
@@ -775,10 +775,18 @@ public abstract class ReportItemExecutor implements IReportItemExecutor
 									.getTOC( );
 							if ( expr != null )
 							{
+								String bookmark = content.getBookmark( );
 								TOCEntry parentTOCEntry = getParentTOCEntry( );
-								tocEntry = tocBuilder.startDummyEntry(
-										parentTOCEntry, hiddenFormats );
+								long elementId = getElementId( );
+								tocEntry = tocBuilder.startEntry(
+										parentTOCEntry, null, bookmark,
+										hiddenFormats, elementId );
+								if ( bookmark == null )
+								{
+									content.setBookmark( tocEntry.getNodeId( ) );
+								}
 							}
+							
 						}
 					}
 				}
