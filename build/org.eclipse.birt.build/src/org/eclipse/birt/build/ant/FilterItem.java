6d94f7286d12f7975cbb1607987bcf60fe2bf95a
Xiaoying Gu <xgu@actuate.com>
2011-04-19 03:04:08 +0000

    Add the init code to package non-osgi runtime

---
+++ build/org.eclipse.birt.build/src/org/eclipse/birt/build/ant/FilterItem.java
@@ -0,0 +1,18 @@
+
+package org.eclipse.birt.build.ant;
+
+public class FilterItem
+{
+
+	private String pattern;
+
+	public void setPattern( String pattern )
+	{
+		this.pattern = pattern;
+	}
+
+	public String getPattern( )
+	{
+		return this.pattern;
+	}
+}
