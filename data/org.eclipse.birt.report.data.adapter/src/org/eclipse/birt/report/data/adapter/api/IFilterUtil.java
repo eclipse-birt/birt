c5626ba81a71b02d97c3aab2e450c3de8fb5a967
lzhu <lzhu@actuate.com>
2012-03-06 20:51:15 -0800

    Summary: Introduce Utility API for filter optimization
    Description of Issue: DtE client need to know which filter operator can
    be optimized.
    
    Description of Resolution: Add utility API IFilterUtil to return the
    optimizable operators.
    
    Project ID:1604
    Manual Test Description:Manually test on UI
    Tests Automated Cases Executed:Automatic test cases added

---
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/IFilterUtil.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2004,2012 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+
+package org.eclipse.birt.report.data.adapter.api;
+
+import org.eclipse.birt.report.model.api.DataSetHandle;
+
+/**
+ * The filter util interface definition that is used for filter related optimization job.
+ */
+public interface IFilterUtil
+{
+
+	String[] populatedPushdownableFilterOperators( DataSetHandle dataSet, String[] candidateOperator ) throws AdapterException;
+
+}
---
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/impl/FilterUtil.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2004,2012 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.report.data.adapter.impl;
+
+import org.eclipse.birt.report.data.adapter.api.IFilterUtil;
+import org.eclipse.birt.report.model.api.DataSetHandle;
+
+
+public class FilterUtil implements IFilterUtil
+{
+
+	/*
+	 * 
+	 */
+	public String[] populatedPushdownableFilterOperators( DataSetHandle dataSet, String[] candidateOperator )
+	{
+		throw new UnsupportedOperationException( );
+	}
+}
