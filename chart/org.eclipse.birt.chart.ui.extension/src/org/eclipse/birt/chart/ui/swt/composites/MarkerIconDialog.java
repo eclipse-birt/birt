f694dec77a50d89737fea08883100dec13c28da1
shuaizhang <shuai.zhang@actuate.com>
2012-03-26 10:38:44 +0800

    reverse to old code

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/MarkerIconDialog.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/composites/MarkerIconDialog.java
@@ -353,9 +353,8 @@ public class MarkerIconDialog extends TrayDialog
 		buttonBar.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
 		buttonBar.setLayout( gl );
 
-		Label description = new Label( buttonBar, SWT.WRAP );
-		description.setLayoutData( new GridData( GridData.FILL_HORIZONTAL
-				| GridData.HORIZONTAL_ALIGN_BEGINNING ) );
+		Label description = new Label( buttonBar, SWT.NONE );
+		description.setLayoutData( new GridData( GridData.HORIZONTAL_ALIGN_BEGINNING ) );
 		description.setText( Messages.getString( "MarkerIconDialog.Label.Description.EmbeddedImage" ) ); //$NON-NLS-1$
 		
 		btnBrowse = new Button( buttonBar, SWT.PUSH );
