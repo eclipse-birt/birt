6041a8ce848145f9304bd0095f808d7280c6144e
yuchen <ychen@actuate.com>
2012-05-24 17:04:24 +0800

    Add new API setStartRow() in IDatasetPreviewTask

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/IDatasetPreviewTask.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/IDatasetPreviewTask.java
@@ -15,6 +15,8 @@ public interface IDatasetPreviewTask extends IExtractionTask
 
 	public void setMaxRow( int maxRow );
 	
+	public void setStartRow( int startRow );
+	
 	/**
 	 * select columns from the data set.
 	 * @param columnNames the selected column names
