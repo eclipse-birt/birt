f0069f6618d44e3d894056cd01186451f9bd907e
Yulin Wang <yulin.wang@actuate.com>
2012-07-18 16:58:36 +0800

    TED#51853 Now fixed by scaling image correctly, and also improved display server based and svg image class.

--- chart/org.eclipse.birt.chart.device.extension/src/org/eclipse/birt/chart/device/g2d/G2dRendererBase.java
+++ chart/org.eclipse.birt.chart.device.extension/src/org/eclipse/birt/chart/device/g2d/G2dRendererBase.java
@@ -624,7 +624,8 @@ public class G2dRendererBase extends DeviceAdapter
 				}
 				_g2d.setClip( ar2 );
 
-				final Size szImage = computeImageSize( img );
+				img = scaleImage( img );
+				final Size szImage = _ids.getSize( img );
 
 				int iXRepeat = (int) ( Math.ceil( r2d.width
 						/ szImage.getWidth( ) ) );
@@ -635,7 +636,6 @@ public class G2dRendererBase extends DeviceAdapter
 				{
 					for ( int j = 0; j < iYRepeat; j++ )
 					{
-						img = scaleImage( img );
 						_g2d.drawImage( img,
 								(int) ( r2d.x + i * szImage.getWidth( ) ),
 								(int) ( r2d.y + j * szImage.getHeight( ) ),
@@ -646,21 +646,6 @@ public class G2dRendererBase extends DeviceAdapter
 				_g2d.setClip( shClip ); // RESTORE
 			}
 		}
-	}
-
-	/**
-	 * Computes the actual output size of image according to output DPI.
-	 * 
-	 * @param img
-	 * @return
-	 */
-	private Size computeImageSize( Image img )
-	{
-		Size size = _ids.getSize( img );
-		double scale = _ids.getDpiResolution( ) / 72d;
-		size.setWidth( size.getWidth( ) * scale );
-		size.setHeight( size.getHeight( ) * scale );
-		return size;
 	}
 
 	@Override
@@ -1359,16 +1344,10 @@ public class G2dRendererBase extends DeviceAdapter
 	 */
 	private java.awt.Image scaleImage( java.awt.Image img )
 	{
-		if ( img instanceof BufferedImage )
-		{
-			double scale = (int) ( this._ids.getDpiResolution( ) / 72d );
-			int newWidth = (int) ( ( (BufferedImage) img ).getWidth( ) * scale );
-			int newHeight = (int) ( ( (BufferedImage) img ).getHeight( ) * scale );
-			return img.getScaledInstance( newWidth,
-					newHeight,
-					Image.SCALE_DEFAULT );
-		}
-		return img;
+		double scale = this._ids.getDpiResolution( ) / 72d;
+		int newWidth = (int) ( img.getWidth( (ImageObserver) getDisplayServer( ).getObserver( ) ) * scale );
+		int newHeight = (int) ( img.getHeight( (ImageObserver) getDisplayServer( ).getObserver( ) ) * scale );
+		return img.getScaledInstance( newWidth, newHeight, Image.SCALE_DEFAULT );
 	}
 	
 
