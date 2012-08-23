f0c212c50d11a56392f14ff75c103ddc02e735fb
ning.zhang <ning.zhang@actuate.com>
2012-08-23 11:11:53 +0800

    Remove the restriction on cube element on the expression builder when creating a derived measure.

--- xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/dialogs/CrosstabComputedMeasureExpressionProvider.java
+++ xtab/org.eclipse.birt.report.item.crosstab.ui/src/org/eclipse/birt/report/item/crosstab/internal/ui/dialogs/CrosstabComputedMeasureExpressionProvider.java
@@ -31,38 +31,38 @@ public class CrosstabComputedMeasureExpressionProvider extends
 		super( handle, null );
 	}
 
-	protected void addFilterToProvider( )
-	{
-		this.addFilter( new ExpressionFilter( ) {
-
-			public boolean select( Object parentElement, Object element )
-			{
-				if ( ExpressionFilter.CATEGORY.equals( parentElement )
-						&& ExpressionProvider.CURRENT_CUBE.equals( element ) )
-				{
-					// return false;
-				}
-				if ( CURRENT_CUBE.equals( parentElement ) )
-				{
-					PropertyHandle handle = null;
-					if ( element instanceof PropertyHandle )
-						handle = (PropertyHandle) element;
-					else if ( element instanceof IAdaptable
-							&& ( (IAdaptable) element ).getAdapter( PropertyHandle.class ) instanceof PropertyHandle )
-						handle = (PropertyHandle) ( (IAdaptable) element ).getAdapter( PropertyHandle.class );
-
-					if ( handle != null
-							&& handle.getPropertyDefn( )
-									.getName( )
-									.equals( ICubeModel.MEASURE_GROUPS_PROP ) )
-					{
-						return true;
-					}
-					return false;
-				}
-				return true;
-			}
-		} );
-	}
+//	protected void addFilterToProvider( )
+//	{
+//		this.addFilter( new ExpressionFilter( ) {
+//
+//			public boolean select( Object parentElement, Object element )
+//			{
+//				if ( ExpressionFilter.CATEGORY.equals( parentElement )
+//						&& ExpressionProvider.CURRENT_CUBE.equals( element ) )
+//				{
+//					// return false;
+//				}
+//				if ( CURRENT_CUBE.equals( parentElement ) )
+//				{
+//					PropertyHandle handle = null;
+//					if ( element instanceof PropertyHandle )
+//						handle = (PropertyHandle) element;
+//					else if ( element instanceof IAdaptable
+//							&& ( (IAdaptable) element ).getAdapter( PropertyHandle.class ) instanceof PropertyHandle )
+//						handle = (PropertyHandle) ( (IAdaptable) element ).getAdapter( PropertyHandle.class );
+//
+//					if ( handle != null
+//							&& handle.getPropertyDefn( )
+//									.getName( )
+//									.equals( ICubeModel.MEASURE_GROUPS_PROP ) )
+//					{
+//						return true;
+//					}
+//					return false;
+//				}
+//				return true;
+//			}
+//		} );
+//	}
 
 }
