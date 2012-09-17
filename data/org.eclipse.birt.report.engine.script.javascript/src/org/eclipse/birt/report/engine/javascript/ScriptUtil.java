c8a60901edb9ce77afb991f48b89ec4e36bbdbe9
wyan <wyan@actuate.com>
2012-09-17 16:43:55 -0700

    T53427 Add java security checking for report script

--- data/org.eclipse.birt.report.engine.script.javascript/src/org/eclipse/birt/report/engine/javascript/ScriptUtil.java
+++ data/org.eclipse.birt.report.engine.script.javascript/src/org/eclipse/birt/report/engine/javascript/ScriptUtil.java
@@ -11,19 +11,12 @@
 
 package org.eclipse.birt.report.engine.javascript;
 
-import java.security.AccessControlContext;
-import java.security.AccessControlException;
-import java.security.AccessController;
-import java.security.Permission;
-import java.security.PermissionCollection;
-import java.security.PrivilegedAction;
-import java.security.ProtectionDomain;
-import java.util.Enumeration;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.CodeSource;
 
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.GeneratedClassLoader;
-import org.mozilla.javascript.Script;
-import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.PolicySecurityController;
 import org.mozilla.javascript.SecurityController;
 
 public class ScriptUtil
@@ -31,152 +24,31 @@ public class ScriptUtil
 
 	public static SecurityController createSecurityController( )
 	{
-		return new SecurityController( ) {
-
-			public GeneratedClassLoader createClassLoader(
-					final ClassLoader parentLoader, Object securityDomain )
-			{
-				final ProtectionDomain domain = (ProtectionDomain) securityDomain;
-				return AccessController
-						.doPrivileged( new PrivilegedAction<GeneratedClassLoader>( ) {
-
-							public GeneratedClassLoader run( )
-							{
-								return new Loader( parentLoader, domain );
-							}
-						} );
-			}
-			
-			public Object getDynamicSecurityDomain( Object securityDomain )
-			{
-				ProtectionDomain staticDomain = (ProtectionDomain) securityDomain;
-				return getDynamicDomain( staticDomain );
-			}
-
-			public Object execWithDomain( Context cx, Scriptable scope,
-					Script script, Object securityDomain )
-			{
-		        return script.exec( cx, scope );
-			}
-
-			private ProtectionDomain getDynamicDomain(
-					ProtectionDomain staticDomain )
-			{
-				ContextPermissions permission = new ContextPermissions(
-						staticDomain );
-				ProtectionDomain contextDomain = new ProtectionDomain(
-						null, permission );
-				return contextDomain;
-			}
-		};
+		return new PolicySecurityController( );
 	}
 
-	public static class Loader extends ClassLoader
-			implements
-				GeneratedClassLoader
+	public static Object getSecurityDomain( final String file )
 	{
-
-		private ProtectionDomain domain;
-		private ClassLoader parent;
-
-		Loader( ClassLoader parent, ProtectionDomain domain )
+		if ( System.getSecurityManager( ) == null )
 		{
-			super( );
-			this.domain = domain;
-			this.parent = parent;
+			return null;
 		}
-
-		public Class loadClass( String name, boolean resolve )
-				throws ClassNotFoundException
+		try
 		{
-			Class cl = findLoadedClass( name );
-			if ( cl == null )
-			{
-				if ( parent != null )
-				{
-					cl = parent.loadClass( name );
-				}
-				else
-				{
-					cl = findSystemClass( name );
-				}
-			}
-			if ( resolve )
-			{
-				resolveClass( cl );
-			}
-			return cl;
+			return new CodeSource( new URL( file ),
+					(java.security.cert.Certificate[]) null );
 		}
-
-		public Class defineClass( String name, byte[] data )
+		catch ( MalformedURLException ex )
 		{
-			return super.defineClass( name, data, 0, data.length, domain );
-		}
-
-		public void linkClass( Class cl )
-		{
-			resolveClass( cl );
-		}
-	}
-
-	// Represents permissions that permits an action only if it is permitted by
-	// staticDomain and by security context of Java stack on the moment of
-	// constructor invocation
-	public static class ContextPermissions extends PermissionCollection
-	{
-		private transient AccessControlContext context;
-		private PermissionCollection statisPermissions;
-		private static final long serialVersionUID = -1721494496320750721L;
-
-		public ContextPermissions( ProtectionDomain staticDomain )
-		{
-			context = AccessController.getContext( );
-			if ( staticDomain != null )
-			{
-				statisPermissions = staticDomain.getPermissions( );
-			}
-			setReadOnly( );
-		}
-
-		public void add( Permission permission )
-		{
-			throw new RuntimeException( "NOT IMPLEMENTED" );
-		}
-
-		public boolean implies( Permission permission )
-		{
-			if ( statisPermissions != null )
-			{
-				if ( !statisPermissions.implies( permission ) )
-				{
-					return false;
-				}
-			}
 			try
 			{
-				context.checkPermission( permission );
-				return true;
+				return new CodeSource( new File( file ).toURI( ).toURL( ),
+						(java.security.cert.Certificate[]) null );
 			}
-			catch ( AccessControlException ex )
+			catch ( MalformedURLException e )
 			{
-				return false;
+				return null;
 			}
-		}
-
-		public Enumeration elements( )
-		{
-			return new Enumeration( ) {
-
-				public boolean hasMoreElements( )
-				{
-					return false;
-				}
-
-				public Object nextElement( )
-				{
-					return null;
-				}
-			};
 		}
 	}
 }
