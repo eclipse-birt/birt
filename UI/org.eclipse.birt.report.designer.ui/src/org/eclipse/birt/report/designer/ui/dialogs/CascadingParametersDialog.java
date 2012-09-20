f83ad6e1d13871506cf63f35c1c6c5b99b12c9b1
Xusong Yuan <cchen@actuate.com>
2012-09-20 14:09:30 +0800

    - Summary: Set "Allow Mutiple Values" option disable if display type is combo box when parameter selected.
        - Bugzilla Bug (s) Resolved:
        
        
        
        - Description:
        
        
        
        - Tests Description :
        
        Manual test

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/CascadingParametersDialog.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/CascadingParametersDialog.java
@@ -2196,7 +2196,7 @@ public class CascadingParametersDialog extends BaseDialog
 		isRequired.setSelection( selectedParameter.isRequired( ) );
 
 		if ( selectedParameter == inputParameterGroup.getParameters( )
-				.get( inputParameterGroup.getParameters( ).getCount( ) - 1 ) )
+				.get( inputParameterGroup.getParameters( ).getCount( ) - 1 ) && selectedParameter.isMustMatch() )//For TED 52772:Add isMustMatch expression to control isMultiple disable if display type is combo box when parameter selected.
 		{
 			isMultiple.setEnabled( true );
 			isMultiple.setSelection( DesignChoiceConstants.SCALAR_PARAM_TYPE_MULTI_VALUE.equals( selectedParameter.getParamType( ) ) );
