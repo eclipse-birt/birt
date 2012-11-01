5ab908adfb3dccbb2f7a7f928cf4ecd004667b8b
mwu <mwu@actuate.com>
2012-11-01 18:03:29 +0800

    Fix issue Exception when try to add a group for SQB dataset[49911]

---
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/IPushedDownExpression.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.birt.data.engine.impl;
+
+import org.eclipse.birt.data.engine.api.IBaseExpression;
+
+public interface IPushedDownExpression 
+{
+	public IBaseExpression getOriginalExpression( );
+}
---
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/impl/PushedBreExpression.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.birt.data.engine.impl;
+
+import org.eclipse.birt.data.engine.api.IBaseExpression;
+import org.eclipse.birt.data.engine.api.querydefn.ScriptExpression;
+
+public class PushedBreExpression extends ScriptExpression implements IPushedDownExpression
+{
+	
+	private IBaseExpression originalBreScript;
+
+	public PushedBreExpression( String effectiveJavascriptScript, IBaseExpression originalScript )
+	{
+		super( effectiveJavascriptScript );
+		this.originalBreScript = originalScript;
+	}
+
+	public IBaseExpression getOriginalExpression( )
+	{
+		return this.originalBreScript;
+	}
+
+}
