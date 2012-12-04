05c7177017e235fa753a44fdd2daf659f2e14515
cchao <cchen@actuate.com>
2012-12-04 18:25:19 +0800

    Exception occur if delete data object cube in rptdesign(T54997)
    Check the input element is deleted, if it's deleted, don't refresh the
    properties page.

--- UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/AttributePage.java
+++ UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/AttributePage.java
@@ -11,17 +11,25 @@
 
 package org.eclipse.birt.report.designer.internal.ui.views.attributes.page;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.eclipse.birt.report.designer.core.model.SessionHandleAdapter;
+import org.eclipse.birt.report.designer.core.util.mediator.request.ReportRequest;
 import org.eclipse.birt.report.designer.internal.ui.editors.parts.event.IFastConsumerProcessor;
 import org.eclipse.birt.report.designer.internal.ui.swt.custom.FormWidgetFactory;
 import org.eclipse.birt.report.designer.internal.ui.util.SortMap;
 import org.eclipse.birt.report.designer.internal.ui.util.UIUtil;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.Section;
 import org.eclipse.birt.report.designer.ui.views.attributes.TabPage;
+import org.eclipse.birt.report.designer.util.DEUtil;
 import org.eclipse.birt.report.model.api.DesignElementHandle;
+import org.eclipse.birt.report.model.api.GroupElementHandle;
+import org.eclipse.birt.report.model.api.ModuleHandle;
 import org.eclipse.birt.report.model.api.activity.NotificationEvent;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
@@ -47,7 +55,7 @@ public abstract class AttributePage extends TabPage implements
 	 * The list kept Property & PropertyDescriptor pair.
 	 */
 	protected HashMap propertiesMap = new HashMap( 7 );
-	
+
 	/**
 	 * The current selection.
 	 */
@@ -55,6 +63,15 @@ public abstract class AttributePage extends TabPage implements
 
 	public void refresh( )
 	{
+		Object element = DEUtil.getInputFirstElement( input );
+		if ( element == null )
+			return;
+		if ( element instanceof DesignElementHandle
+				&& getTopContainer( (DesignElementHandle) element ) == null )
+		{
+			return;
+		}
+
 		Section[] sectionArray = getSections( );
 		for ( int i = 0; i < sectionArray.length; i++ )
 		{
@@ -94,15 +111,19 @@ public abstract class AttributePage extends TabPage implements
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.eclipse.birt.report.designer.internal.ui.editors.parts.event.IFastConsumerProcessor#isOverdued()
+	 * @see org.eclipse.birt.report.designer.internal.ui.editors.parts.event.
+	 * IFastConsumerProcessor#isOverdued()
 	 */
 	public boolean isOverdued( )
 	{
-		return container == null || container.isDisposed( ) ;
+		return container == null || container.isDisposed( );
 	}
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.birt.report.designer.ui.extensions.IPropertyTabUI#dispose()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.birt.report.designer.ui.extensions.IPropertyTabUI#dispose()
 	 */
 	public void dispose( )
 	{
@@ -252,12 +273,34 @@ public abstract class AttributePage extends TabPage implements
 
 	public void postElementEvent( )
 	{
+		Object element = DEUtil.getInputFirstElement( input );
+		if ( element == null )
+			return;
+		if ( element instanceof DesignElementHandle
+				&& getTopContainer( (DesignElementHandle) element ) == null )
+		{
+			return;
+		}
+
 		Section[] sectionArray = getSections( );
 		for ( int i = 0; i < sectionArray.length; i++ )
 		{
 			Section section = (Section) sectionArray[i];
 			section.load( );
 		}
+	}
+
+	protected ModuleHandle getTopContainer( DesignElementHandle element )
+	{
+		if(element instanceof ModuleHandle)
+			return (ModuleHandle) element;
+		while ( !( element.getContainer( ) instanceof ModuleHandle ) )
+		{
+			element = element.getContainer( );
+			if ( element == null )
+				return null;
+		}
+		return (ModuleHandle) element.getContainer( );
 	}
 
 	public Object getAdapter( Class adapter )
