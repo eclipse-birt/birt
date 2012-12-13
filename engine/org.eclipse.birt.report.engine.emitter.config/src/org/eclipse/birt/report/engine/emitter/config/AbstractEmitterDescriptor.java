7900d533e13e15e0ce8e0128343d577bf74be5a5
gliu <gliu@actuate.com>
2012-12-13 14:54:43 +0800

    Enhance the emitter configuration to disable some of the option (T55142)
    Enhance engine to parse RenderOptions.xml in config jar. The new xml
    file can config whether the option is enabled.

--- engine/org.eclipse.birt.report.engine.emitter.config/src/org/eclipse/birt/report/engine/emitter/config/AbstractEmitterDescriptor.java
+++ engine/org.eclipse.birt.report.engine.emitter.config/src/org/eclipse/birt/report/engine/emitter/config/AbstractEmitterDescriptor.java
@@ -11,15 +11,22 @@
 
 package org.eclipse.birt.report.engine.emitter.config;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.eclipse.birt.core.framework.IBundle;
 import org.eclipse.birt.core.framework.Platform;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
 /**
  * AbstractEmitterDescriptor
@@ -29,10 +36,20 @@ public abstract class AbstractEmitterDescriptor implements IEmitterDescriptor
 
 	protected Map initParams = null;
 	protected Locale locale;
-	protected Properties defaultValues = null;	
+	protected Map<String, RenderOptionDefn> renderOptionDefns = new HashMap<String, RenderOptionDefn>();
 	protected IConfigurableOption[] options;
 	
 	private static final String OPTIONS_CONFIG_FILE = "RenderDefaults.cfg";
+	
+	private static final String RENDER_OPTIONS_FILE = "RenderOptions.xml";
+	
+	private static final String OPTION_QNAME = "option";
+	
+	private static final String OPTION_NAME = "name";
+	
+	private static final String OPTION_DEFAULT = "default";
+	
+	private static final String OPTION_ENABLED = "enabled";
 	
 	public void setInitParameters( Map params )
 	{
@@ -83,34 +100,72 @@ public abstract class AbstractEmitterDescriptor implements IEmitterDescriptor
 	{
 		try
 		{
-			URL url = getConfigURL( bundleName );
+			URL url = getResourceURL( bundleName, OPTIONS_CONFIG_FILE );
+			if( url != null )
+			{
+				InputStream in = url.openStream( );
+				Properties defaultValues = new Properties( );
+				defaultValues.load( in );
+				for ( Entry<Object, Object> entry : defaultValues.entrySet( ) )
+				{
+					String name = entry.getKey( ).toString( );
+					String value = entry.getValue( ).toString( );
+					renderOptionDefns.put( name, new RenderOptionDefn( name,
+							value,
+							true ) );
+				}
+				in.close( );
+			}
+			url = getResourceURL( bundleName, RENDER_OPTIONS_FILE );
 			if ( url != null )
 			{
 				InputStream in = url.openStream( );
-				defaultValues = new Properties( );
-				defaultValues.load( in );
+				doLoadDefaultValues( in );
 				in.close( );
-				return true;
 			}
 		}
-		catch ( IOException e )
+		catch ( Exception e )
 		{
-			defaultValues = null;
-			return false;
+			renderOptionDefns = null;
 		}
-		return false;
+		return renderOptionDefns != null && !renderOptionDefns.isEmpty( );
 	}
 	
+	private void doLoadDefaultValues( InputStream in ) throws Exception
+	{
+		
+		SAXParser parser = SAXParserFactory.newInstance( ).newSAXParser( );
+		try
+		{
+			parser.parse( in, new RenderOptionHandler( ) );			
+		}
+		finally
+		{
+			// even there is XML exception, need to release the resource. 
+			try
+			{
+				parser.reset( );
+				parser = null;
+			}
+			catch ( Exception e1 )
+			{
+				
+			}
+		}
+	}
+
 	private void applyDefaultValue( IConfigurableOption option )
 	{
-		if ( defaultValues == null || defaultValues.isEmpty( ) )
+		if ( renderOptionDefns == null || renderOptionDefns.isEmpty( ) )
 		{
 			return;
 		}
-		String value = (String) defaultValues.get( option.getName( ) );
-		if ( value != null )
+		RenderOptionDefn defn = renderOptionDefns.get( option.getName( ) );
+		if ( defn != null )
 		{
+			String value = defn.getValue( );
 			ConfigurableOption optionImpl = (ConfigurableOption) option;
+			optionImpl.setEnabled( defn.isEnabled( ) );
 			switch ( option.getDataType( ) )
 			{
 				case STRING :
@@ -149,14 +204,46 @@ public abstract class AbstractEmitterDescriptor implements IEmitterDescriptor
 		}
 	}
 	
-	private URL getConfigURL( String bundleName )
+	private URL getResourceURL( String bundleName, String resourceName )
 	{
 		IBundle bundle = Platform.getBundle( bundleName ); //$NON-NLS-1$
 		if ( bundle != null )
 		{
-			return bundle.getEntry( OPTIONS_CONFIG_FILE );
+			return bundle.getEntry( resourceName );
 		}
 		return null;
 	}
 	
+	private class RenderOptionHandler extends DefaultHandler
+	{
+
+		@Override
+		public void startElement( String uri, String localName, String qName,
+				Attributes attributes ) throws SAXException
+		{
+			if ( OPTION_QNAME.equalsIgnoreCase( qName ) )
+			{
+				String name = attributes.getValue( OPTION_NAME );
+				if ( !isEmpty( name ) )
+				{
+					String defualt = attributes.getValue( OPTION_DEFAULT );
+					Boolean enabled = Boolean.TRUE;
+					String enabledStr = attributes.getValue( OPTION_ENABLED );
+					if ( !isEmpty( enabledStr ) )
+					{
+						enabled = Boolean.valueOf( enabledStr );
+					}
+					renderOptionDefns.put( name, new RenderOptionDefn( name,
+							defualt,
+							enabled ) );
+				}
+			}
+		}
+
+	}
+	
+	private boolean isEmpty( String str )
+	{
+		return str == null || str.isEmpty( );
+	}
 }
