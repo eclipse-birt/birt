efd0fe8cf33d8881ab78b9dae09046a9ce072e50
heli <heng.li@actuate.com>
2012-12-18 13:37:15 +0800

    Summary: Refactoring chart code for extra works.(T54831)
    Bugzilla Bug(s) Resolved:
    
    Description:
    Refactoring chart code for extra works.(T54831)

--- chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIUtil.java
+++ chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIUtil.java
@@ -14,9 +14,14 @@ package org.eclipse.birt.chart.reportitem.ui;
 import org.eclipse.birt.chart.reportitem.ui.dialogs.ChartExpressionProvider;
 import org.eclipse.birt.chart.ui.swt.interfaces.IUIServiceProvider;
 import org.eclipse.birt.chart.util.ChartUtil;
+import org.eclipse.birt.report.designer.util.DEUtil;
+import org.eclipse.birt.report.model.api.DesignElementHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
+import org.eclipse.birt.report.model.api.StyleHandle;
+import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
 import org.eclipse.birt.report.model.api.extension.IReportItem;
+import org.eclipse.birt.report.model.api.metadata.DimensionValue;
 
 /**
  * ChartReportItemUIUtil
@@ -134,5 +139,113 @@ public class ChartReportItemUIUtil
 					| ChartExpressionProvider.CATEGORY_WITH_BIRT_VARIABLES;
 		}
 		return ChartExpressionProvider.CATEGORY_BASE;
+	}
+	
+	
+	/**
+	 * Get background image setting from design element handle.
+	 * 
+	 * @param handle
+	 *            The handle of design element.
+	 * @return background image
+	 */
+	public static String getBackgroundImage( DesignElementHandle handle )
+	{
+		return handle.getStringProperty( StyleHandle.BACKGROUND_IMAGE_PROP );
+	}
+
+	/**
+	 * Get background position settings from design element handle.
+	 * 
+	 * @param handle
+	 *            The handle of design element.
+	 * @return background position
+	 */
+	public static Object[] getBackgroundPosition( DesignElementHandle handle )
+	{
+		Object x = null;
+		Object y = null;
+
+		if ( handle != null )
+		{
+			Object px = handle.getProperty( StyleHandle.BACKGROUND_POSITION_X_PROP );
+			Object py = handle.getProperty( StyleHandle.BACKGROUND_POSITION_Y_PROP );
+
+			if ( px instanceof String )
+			{
+				x = px;
+			}
+			else if ( px instanceof DimensionValue )
+			{
+				// {0%,0%}
+				if ( DesignChoiceConstants.UNITS_PERCENTAGE.equals( ( (DimensionValue) px ).getUnits( ) ) )
+				{
+					x = px;
+				}
+				else
+				{
+					// {1cm,1cm}
+					x = Integer.valueOf( (int) DEUtil.convertoToPixel( px ) );
+				}
+			}
+
+			if ( py instanceof String )
+			{
+				y = py;
+			}
+			else if ( py instanceof DimensionValue )
+			{
+				// {0%,0%}
+				if ( DesignChoiceConstants.UNITS_PERCENTAGE.equals( ( (DimensionValue) py ).getUnits( ) ) )
+				{
+					y = py;
+				}
+				else
+				{
+					// {1cm,1cm}
+					y = Integer.valueOf( (int) DEUtil.convertoToPixel( py ) );
+				}
+			}
+		}
+		return new Object[]{
+				x, y
+		};
+	}
+
+	/**
+	 * Get background repeat property from design element handle.
+	 * 
+	 * @param handle
+	 *            The handle of design element.
+	 * @return background repeat property
+	 */
+	public static int getBackgroundRepeat( DesignElementHandle handle )
+	{
+		return getRepeat( handle.getStringProperty( StyleHandle.BACKGROUND_REPEAT_PROP ) );
+	}
+	
+	/**
+	 *  Get repeat identifier according to its value
+	 *  
+	 * @param repeat
+	 * 	Given string
+	 * @return
+	 * 	The repeat value
+	 */
+	public static int getRepeat( String repeat )
+	{
+		if ( DesignChoiceConstants.BACKGROUND_REPEAT_REPEAT_X.equals( repeat ) )
+		{
+			return 1;
+		}
+		else if ( DesignChoiceConstants.BACKGROUND_REPEAT_REPEAT_Y.equals( repeat ) )
+		{
+			return 2;
+		}
+		else if ( DesignChoiceConstants.BACKGROUND_REPEAT_REPEAT.equals( repeat ) )
+		{
+			return 3;
+		}
+		return 0;
 	}
 }
