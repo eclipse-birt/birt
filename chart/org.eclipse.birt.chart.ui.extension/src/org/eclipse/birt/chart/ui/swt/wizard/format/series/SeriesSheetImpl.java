abd8d9bdb468fb39d02370d77e5e4388773a4327
Frank Ding <fding@actuate.com>
2013-02-22 18:22:36 +0800

    - Summary: Enhance accessibility support in Chart Builder (T53994)
    - Bugzilla Bug(s) Resolved: 390745
    
    - Description:
    
    - Resolution: Added some accessibility for series sheets. (T53994)

--- chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/wizard/format/series/SeriesSheetImpl.java
+++ chart/org.eclipse.birt.chart.ui.extension/src/org/eclipse/birt/chart/ui/swt/wizard/format/series/SeriesSheetImpl.java
@@ -408,6 +408,8 @@ public class SeriesSheetImpl extends SubtaskSheetImpl implements
 				GridData gd = new GridData( GridData.FILL_HORIZONTAL );
 				txtTitle.setLayoutData( gd );
 				txtTitle.addListener( this );
+				ChartUIUtil.addScreenreaderAccessbility( txtTitle,
+						Messages.getString( "SeriesSheetImpl.Label.Title" ) ); //$NON-NLS-1$
 			}
 
 			cmbTypes = new Combo( parent, SWT.DROP_DOWN | SWT.READ_ONLY );
@@ -420,6 +422,8 @@ public class SeriesSheetImpl extends SubtaskSheetImpl implements
 				{
 					cmbTypes.setEnabled( false );
 				}
+				ChartUIUtil.addScreenreaderAccessbility( cmbTypes,
+						Messages.getString( "SeriesSheetImpl.Label.Type" ) ); //$NON-NLS-1$
 			}
 
 			if ( !series.getClass( ).isAssignableFrom( SeriesImpl.class ) )
@@ -467,6 +471,7 @@ public class SeriesSheetImpl extends SubtaskSheetImpl implements
 						: ChartCheckbox.STATE_UNSELECTED )
 						: ChartCheckbox.STATE_GRAYED );
 				btnTranslucent.addSelectionListener( this );
+				btnTranslucent.addScreenReaderAccessiblity( Messages.getString( "SeriesSheetImpl.Label.Translucent" ) ); //$NON-NLS-1$
 			}
 		}
 
@@ -484,6 +489,7 @@ public class SeriesSheetImpl extends SubtaskSheetImpl implements
 						: ChartCheckbox.STATE_UNSELECTED )
 						: ChartCheckbox.STATE_GRAYED );
 				btnVisible.addSelectionListener( this );
+				btnVisible.addScreenReaderAccessiblity( Messages.getString( "SeriesSheetImpl.Label.Visible" ) ); //$NON-NLS-1$
 			}
 		}
 
@@ -521,6 +527,7 @@ public class SeriesSheetImpl extends SubtaskSheetImpl implements
 				}
 				btnStack.setSelectionState( state );
 				btnStack.addSelectionListener( this );
+				btnStack.addScreenReaderAccessiblity( Messages.getString( "SeriesSheetImpl.Label.Stacked" ) );//$NON-NLS-1$
 			}
 		}
 
