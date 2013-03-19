52034ab41dc9026c9c0fd289f42d53c38046b4d8
lzhang <lzhang@actuate.com>
2013-03-19 17:36:34 +0800

    CubeQueryUtil.getInvalidBindings( ICubeQueryDefinition ) method needs to be enhanced to handle query defn from linked data model [57886]

--- data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/LinkedDataSetUtil.java
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/LinkedDataSetUtil.java
@@ -1,22 +1,30 @@
 package org.eclipse.birt.report.data.adapter.api;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.eclipse.birt.report.model.api.ReportItemHandle;
-import org.eclipse.birt.report.model.api.olap.CubeHandle;
 
 
 public class LinkedDataSetUtil
 {
+	private static String GET_LINKED_DATA_MODEL_METHOD = "getLinkedDataModel";
+	
 	public static boolean bindToLinkedDataSet( ReportItemHandle reportItemHandle )
 			throws IllegalArgumentException, IllegalAccessException,
 			InvocationTargetException
 	{
-		return false;
-	}
-	
-	public static boolean isLinkedDataSetCube( CubeHandle handle )
-	{
+		Method[] methods = reportItemHandle.getClass( ).getMethods( );
+		for ( int i = 0; i < methods.length; i++ )
+		{
+			String name = methods[i].getName( );
+			if ( name.equals( GET_LINKED_DATA_MODEL_METHOD ) )
+			{
+				Object result = methods[i].invoke( reportItemHandle );
+				if ( result != null )
+					return true;
+			}
+		}
 		return false;
 	}
 }
