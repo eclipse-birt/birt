15aa89123815cca761eaf772f3816faf6c7c744b
xwu <xwu@actuate.com>
2013-03-27 14:18:02 +0800

    Bugzilla bug 402794 - Support Access to mongoDB.

---
+++ data/org.eclipse.birt.data.oda.mongodb.ui/src/org/eclipse/birt/data/oda/mongodb/ui/impl/MongoDBAdvancedSettingsDialog.java
@@ -0,0 +1,460 @@
+/*
+ *************************************************************************
+ * Copyright (c) 2013 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation - initial API and implementation
+ *  
+ *************************************************************************
+ */
+
+package org.eclipse.birt.data.oda.mongodb.ui.impl;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.StatusDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.PlatformUI;
+
+import org.eclipse.birt.data.oda.mongodb.impl.MongoDBDriver.ReadPreferenceChoice;
+import org.eclipse.birt.data.oda.mongodb.internal.impl.QueryProperties;
+import org.eclipse.birt.data.oda.mongodb.ui.i18n.Messages;
+import org.eclipse.birt.data.oda.mongodb.ui.util.IHelpConstants;
+import org.eclipse.birt.data.oda.mongodb.ui.util.UIHelper;
+
+public class MongoDBAdvancedSettingsDialog extends StatusDialog
+{
+
+	private static String DIALOG_TITLE = Messages.getString( "MongoDBAdvancedSettingsDialog.dialogTitle" ); //$NON-NLS-1$
+
+	private String batchSizeValue, docSearchLimitValue, maxSkipDocValue,
+			indexExpr, queryPreferenceValue;
+
+	private Combo queryPreferenceCombo;
+	private Text batchSizeText, docSearchLimitText, maxSkipDocText,
+			indexExprText;
+	private Button noTimeOutCheckbox, allowPartialResultsCheckbox,
+			flattenNestedDocCheckbox;
+	private boolean noTimeOut, allowsPartialResults, flattenNestedDocument;
+
+	public MongoDBAdvancedSettingsDialog( Shell parent )
+	{
+		super( parent );
+	}
+
+	public void create( )
+	{
+		super.create( );
+
+		Point pt = getShell( ).computeSize( -1, -1 );
+		pt.x = Math.max( pt.x, 250 );
+		pt.y = Math.max( pt.y, 300 );
+		getShell( ).setSize( pt );
+		getShell( ).setText( DIALOG_TITLE );
+
+		validatePageStatus( );
+
+	}
+
+	protected boolean isResizable( )
+	{
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets
+	 * .Composite)
+	 */
+	protected Control createDialogArea( Composite parent )
+	{
+		Composite composite = (Composite) super.createDialogArea( parent );
+		GridLayout layout = new GridLayout( );
+		layout.marginHeight = 20;
+		layout.marginWidth = 20;
+		layout.numColumns = 2;
+		composite.setLayout( layout );
+
+		createMainArea( composite );
+
+		initDialogControls( );
+
+		UIHelper.setSystemHelp( composite,
+				IHelpConstants.CONTEXT_ID_DIALOG_MONGODB_DATASET_ADVANCED_SETTING );
+
+		return composite;
+
+	}
+
+	private void createMainArea( Composite parent )
+	{
+		Label maxResObjLabel = new Label( parent, SWT.NONE );
+		maxResObjLabel.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Label.MaxResultObjects" ) ); //$NON-NLS-1$
+
+		batchSizeText = new Text( parent, SWT.BORDER );
+		batchSizeText.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		batchSizeText.addModifyListener( new ModifyListener( ) {
+
+			public void modifyText( ModifyEvent e )
+			{
+				batchSizeValue = batchSizeText.getText( ).trim( );
+				validatePageStatus( );
+
+			}
+
+		} );
+
+		Label maxSearchDocLabel = new Label( parent, SWT.NONE );
+		maxSearchDocLabel.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Label.MaxDocumentsToSearch" ) ); //$NON-NLS-1$
+
+		docSearchLimitText = new Text( parent, SWT.BORDER );
+		docSearchLimitText.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		docSearchLimitText.addModifyListener( new ModifyListener( ) {
+
+			public void modifyText( ModifyEvent e )
+			{
+				docSearchLimitValue = docSearchLimitText.getText( ).trim( );
+				validatePageStatus( );
+
+			}
+
+		} );
+
+		Label maxSkipDocLabel = new Label( parent, SWT.NONE );
+		maxSkipDocLabel.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Label.MaxDocumentsToSkip" ) ); //$NON-NLS-1$
+
+		maxSkipDocText = new Text( parent, SWT.BORDER );
+		maxSkipDocText.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		maxSkipDocText.addModifyListener( new ModifyListener( ) {
+
+			public void modifyText( ModifyEvent e )
+			{
+				maxSkipDocValue = maxSkipDocText.getText( ).trim( );
+				validatePageStatus( );
+
+			}
+
+		} );
+
+		Label indexExprLabel = new Label( parent, SWT.NONE );
+		indexExprLabel.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Label.IndexHintExpression" ) ); //$NON-NLS-1$
+
+		indexExprText = new Text( parent, SWT.BORDER );
+		indexExprText.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		indexExprText.addModifyListener( new ModifyListener( ) {
+
+			public void modifyText( ModifyEvent e )
+			{
+				indexExpr = indexExprText.getText( ).trim( );
+				validatePageStatus( );
+
+			}
+
+		} );
+
+		Label queryCursorPrefLabel = new Label( parent, SWT.NONE );
+		queryCursorPrefLabel.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Label.QueryCursorPreference" ) ); //$NON-NLS-1$
+
+		queryPreferenceCombo = new Combo( parent, SWT.BORDER | SWT.READ_ONLY );
+		queryPreferenceCombo.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+		queryPreferenceCombo.addSelectionListener( new SelectionListener( ) {
+
+			public void widgetSelected( SelectionEvent e )
+			{
+				queryPreferenceValue = queryPreferenceCombo.getText( ).trim( );
+			}
+
+			public void widgetDefaultSelected( SelectionEvent e )
+			{
+
+			}
+
+		} );
+
+		initQueryCursorPreferenceSelection( );
+
+		GridData gd = new GridData( GridData.FILL_HORIZONTAL );
+		gd.horizontalSpan = 2;
+
+		noTimeOutCheckbox = new Button( parent, SWT.CHECK | SWT.WRAP );
+		noTimeOutCheckbox.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.text.NoCursorTimeOut" ) ); //$NON-NLS-1$
+		noTimeOutCheckbox.setToolTipText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.tooltip.NoCursorTimeOut" ) ); //$NON-NLS-1$
+		noTimeOutCheckbox.setLayoutData( gd );
+		noTimeOutCheckbox.addSelectionListener( new SelectionListener( ) {
+
+			public void widgetSelected( SelectionEvent e )
+			{
+				noTimeOut = noTimeOutCheckbox.getSelection( );
+			}
+
+			public void widgetDefaultSelected( SelectionEvent e )
+			{
+
+			}
+
+		} );
+
+		allowPartialResultsCheckbox = new Button( parent, SWT.CHECK | SWT.WRAP );
+		allowPartialResultsCheckbox.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.text.AllowPartialResults" ) ); //$NON-NLS-1$
+		allowPartialResultsCheckbox.setToolTipText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.tooltip.AllowPartialResults" ) ); //$NON-NLS-1$
+		gd = new GridData( GridData.FILL_HORIZONTAL );
+		gd.horizontalSpan = 2;
+		allowPartialResultsCheckbox.setLayoutData( gd );
+		allowPartialResultsCheckbox.addSelectionListener( new SelectionListener( ) {
+
+			public void widgetSelected( SelectionEvent e )
+			{
+				allowsPartialResults = allowPartialResultsCheckbox.getSelection( );
+			}
+
+			public void widgetDefaultSelected( SelectionEvent e )
+			{
+
+			}
+
+		} );
+
+		flattenNestedDocCheckbox = new Button( parent, SWT.CHECK | SWT.WRAP );
+		flattenNestedDocCheckbox.setText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.text.FlattenNestedDocuments" ) ); //$NON-NLS-1$
+		flattenNestedDocCheckbox.setToolTipText( Messages.getString( "MongoDBAdvancedSettingsDialog.Button.tooltip.FlattenNestedDocuments" ) ); //$NON-NLS-1$
+		gd = new GridData( GridData.FILL_HORIZONTAL );
+		gd.horizontalSpan = 2;
+		flattenNestedDocCheckbox.setLayoutData( gd );
+		flattenNestedDocCheckbox.addSelectionListener( new SelectionListener( ) {
+
+			public void widgetSelected( SelectionEvent e )
+			{
+				flattenNestedDocument = flattenNestedDocCheckbox.getSelection( );
+			}
+
+			public void widgetDefaultSelected( SelectionEvent e )
+			{
+
+			}
+
+		} );
+
+	}
+
+	private void initQueryCursorPreferenceSelection( )
+	{
+		queryPreferenceCombo.add( ReadPreferenceChoice.PRIMARY.displayName( ) );
+		queryPreferenceCombo.add( ReadPreferenceChoice.PRIMARY_PREFERRED.displayName( ) );
+		queryPreferenceCombo.add( ReadPreferenceChoice.SECONDARY.displayName( ) );
+		queryPreferenceCombo.add( ReadPreferenceChoice.SECONDARY_PREFERRED.displayName( ) );
+		queryPreferenceCombo.add( ReadPreferenceChoice.NEAREST.displayName( ) );
+
+		queryPreferenceCombo.select( 0 );
+	}
+
+	private void validatePageStatus( )
+	{
+		Status status = null;
+
+		if ( !UIHelper.isEmptyString( batchSizeValue )
+				&& !UIHelper.isNumber( batchSizeValue ) )
+		{
+			status = getMiscStatus( IStatus.ERROR,
+					Messages.getString( "MongoDBAdvancedSettingsDialog.error.NotNumberFormat.MaxResultObjects" ) ); //$NON-NLS-1$
+		}
+		else if ( !UIHelper.isEmptyString( docSearchLimitValue )
+				&& !UIHelper.isNumber( docSearchLimitValue ) )
+		{
+			status = getMiscStatus( IStatus.ERROR,
+					Messages.getString( "MongoDBAdvancedSettingsDialog.error.NotNumberFormat.MaxDocumentsToSearch" ) ); //$NON-NLS-1$
+		}
+		else if ( !UIHelper.isEmptyString( maxSkipDocValue )
+				&& !UIHelper.isNumberOrZero( maxSkipDocValue ) )
+		{
+			status = getMiscStatus( IStatus.ERROR,
+					Messages.getString( "MongoDBAdvancedSettingsDialog.error.NotNumberFormat.MaxDocumentsToSkip" ) ); //$NON-NLS-1$
+		}
+		else
+		{
+			status = getOKStatus( );
+		}
+
+		if ( status != null )
+		{
+			updateStatus( status );
+		}
+	}
+
+	private Status getMiscStatus( int severity, String message )
+	{
+		return new Status( severity,
+				PlatformUI.PLUGIN_ID,
+				severity,
+				message,
+				null );
+	}
+
+	private Status getOKStatus( )
+	{
+		return getMiscStatus( IStatus.OK, "" ); //$NON-NLS-1$
+	}
+
+	private void initDialogControls( )
+	{
+		if ( batchSizeValue != null )
+		{
+			batchSizeText.setText( batchSizeValue );
+		}
+
+		if ( docSearchLimitValue != null )
+		{
+			docSearchLimitText.setText( docSearchLimitValue );
+		}
+
+		if ( maxSkipDocValue != null )
+		{
+			maxSkipDocText.setText( maxSkipDocValue );
+		}
+
+		if ( indexExpr != null )
+		{
+			indexExprText.setText( indexExpr );
+		}
+
+		if ( queryPreferenceValue != null )
+		{
+			queryPreferenceCombo.setText( queryPreferenceValue );
+		}
+
+		if ( docSearchLimitValue != null )
+		{
+			docSearchLimitText.setText( docSearchLimitValue );
+		}
+
+		noTimeOutCheckbox.setSelection( noTimeOut );
+		allowPartialResultsCheckbox.setSelection( allowsPartialResults );
+		flattenNestedDocCheckbox.setSelection( flattenNestedDocument );
+
+	}
+
+	protected void initQueryProps( QueryProperties queryProps )
+	{
+		this.docSearchLimitValue = ""; //$NON-NLS-1$
+		this.batchSizeValue = ""; //$NON-NLS-1$
+		this.maxSkipDocValue = ""; //$NON-NLS-1$
+
+		if ( queryProps.hasRuntimeMetaDataSearchLimit( ) )
+		{
+			this.docSearchLimitValue = String.valueOf( queryProps.getRuntimeMetaDataSearchLimit( ) );
+		}
+		if ( queryProps.hasBatchSize( ) )
+		{
+			this.batchSizeValue = String.valueOf( queryProps.getBatchSize( ) );
+		}
+		if ( queryProps.hasNumDocsToSkip( ) )
+		{
+			this.maxSkipDocValue = String.valueOf( queryProps.getNumDocsToSkip( ) );
+		}
+		if ( queryProps.getQueryReadPreference( ) != null )
+		{
+			this.queryPreferenceValue = String.valueOf( queryProps.getQueryReadPreference( ) );
+		}
+		else
+		{
+			this.queryPreferenceValue = ReadPreferenceChoice.DEFAULT.displayName( );
+		}
+
+		this.indexExpr = String.valueOf( queryProps.getIndexHints( ) );
+
+		this.noTimeOut = queryProps.hasNoTimeOut( );
+		this.flattenNestedDocument = queryProps.isAutoFlattening( );
+		this.allowsPartialResults = queryProps.isPartialResultsOk( );
+	}
+
+	protected void updateQueryProperties( QueryProperties queryProps )
+	{
+		if ( queryPreferenceValue != null )
+		{
+			queryProps.setQueryReadPreference( queryPreferenceValue );
+		}
+
+		if ( hasMaxDocumentToSkip( ) )
+		{
+			queryProps.setNumDocsToSkip( getMaxDocumentToSkip( ) );
+		}
+		else
+		{
+			queryProps.setNumDocsToSkip( null );
+		}
+		
+		if ( hasBatchSize( ) )
+		{
+			queryProps.setBatchSize( getMaxBatchSize( ) );
+		}
+		else
+		{
+			queryProps.setBatchSize( null );
+		}
+
+		if ( hasDocumentSearchLimit( ) )
+		{
+			queryProps.setRuntimeMetaDataSearchLimit( getDocumentSearchLimit( ) );
+		}
+
+		if ( indexExpr != null )
+		{
+			queryProps.setIndexHints( indexExpr );
+		}
+
+		queryProps.setNoTimeOut( noTimeOut );
+		queryProps.setPartialResultsOk( allowsPartialResults );
+		queryProps.setAutoFlattening( flattenNestedDocument );
+
+	}
+
+	private boolean hasBatchSize( )
+	{
+		return !UIHelper.isEmptyString( this.batchSizeValue );
+	}
+
+	protected Integer getMaxBatchSize( )
+	{
+		return Integer.parseInt( this.batchSizeValue );
+	}
+
+	private boolean hasDocumentSearchLimit( )
+	{
+		return !UIHelper.isEmptyString( this.docSearchLimitValue );
+	}
+
+	protected Integer getDocumentSearchLimit( )
+	{
+		return Integer.parseInt( this.docSearchLimitValue );
+	}
+
+	private boolean hasMaxDocumentToSkip( )
+	{
+		return !UIHelper.isEmptyString( this.maxSkipDocValue );
+	}
+
+	protected Integer getMaxDocumentToSkip( )
+	{
+		return Integer.parseInt( this.maxSkipDocValue );
+	}
+
+}
