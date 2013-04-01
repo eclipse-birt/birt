660c26f210c08ac69b7e398572e737773a409454
lzhang <lzhang@actuate.com>
2013-04-01 14:10:46 +0800

    Remove the exception lists from the API which judges whether a ReportItemHandle binds to a Linked Data Set

--- data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/LinkedDataSetUtil.java
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/api/LinkedDataSetUtil.java
@@ -1,6 +1,5 @@
 package org.eclipse.birt.report.data.adapter.api;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import org.eclipse.birt.report.model.api.ReportItemHandle;
@@ -11,8 +10,6 @@ public class LinkedDataSetUtil
 	private static String GET_LINKED_DATA_MODEL_METHOD = "getLinkedDataModel";
 	
 	public static boolean bindToLinkedDataSet( ReportItemHandle reportItemHandle )
-			throws IllegalArgumentException, IllegalAccessException,
-			InvocationTargetException
 	{
 		Method[] methods = reportItemHandle.getClass( ).getMethods( );
 		for ( int i = 0; i < methods.length; i++ )
@@ -20,7 +17,15 @@ public class LinkedDataSetUtil
 			String name = methods[i].getName( );
 			if ( name.equals( GET_LINKED_DATA_MODEL_METHOD ) )
 			{
-				Object result = methods[i].invoke( reportItemHandle );
+				Object result = null ;
+				try
+				{
+					result = methods[i].invoke( reportItemHandle );
+				}
+				catch ( Exception e )
+				{
+					return false;
+				}
 				if ( result != null )
 					return true;
 			}
