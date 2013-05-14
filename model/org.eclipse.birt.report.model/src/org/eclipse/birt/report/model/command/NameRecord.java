0a74e4c804460353d0653fe930cc1974544ad996
azhu <azhu@actuate.com>
2013-05-14 13:54:48 +0800

    Fix Master page of report item auto changed to default one after rename the MasterPage in Outline.(T59969)

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/command/NameRecord.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/command/NameRecord.java
@@ -21,8 +21,11 @@ import org.eclipse.birt.report.model.api.command.NameEvent;
 import org.eclipse.birt.report.model.core.DesignElement;
 import org.eclipse.birt.report.model.core.Module;
 import org.eclipse.birt.report.model.elements.DataSource;
+import org.eclipse.birt.report.model.elements.MasterPage;
+import org.eclipse.birt.report.model.elements.interfaces.IStyleModel;
 import org.eclipse.birt.report.model.i18n.MessageConstants;
 import org.eclipse.birt.report.model.util.CommandLabelFactory;
+import org.eclipse.birt.report.model.util.ContentIterator;
 
 /**
  * Records a change to the name of an element.
@@ -97,6 +100,41 @@ public class NameRecord extends SimpleRecord
 				else
 					root.updateCacheForRename( (DataSource) element, oldName,
 							newName );
+			}
+		}
+		
+		// If the element is referenced by name, the referencing elements need
+		// to be updated.
+		if( element instanceof MasterPage )
+		{
+			Module root = element.getRoot( );
+			if ( root != null )
+			{
+				if ( undo )
+					updatePropertyForRename( root,
+							IStyleModel.MASTER_PAGE_PROP,
+							newName,
+							oldName );
+				else
+					updatePropertyForRename( root,
+							IStyleModel.MASTER_PAGE_PROP,
+							oldName,
+							newName );
+			}
+		}
+	}
+	
+	private void updatePropertyForRename( Module root, String propName,
+			String oldName, String newName )
+	{
+		ContentIterator iterator = new ContentIterator( root, root );
+		while ( iterator.hasNext( ) )
+		{
+			DesignElement elem = iterator.next( );
+			Object prop = elem.getProperty( root, propName );
+			if ( prop != null && prop.equals( oldName ) )
+			{
+				elem.setProperty( propName, newName );
 			}
 		}
 	}
