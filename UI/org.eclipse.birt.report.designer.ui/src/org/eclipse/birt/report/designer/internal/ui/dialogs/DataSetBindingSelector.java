d9f65cb56dd1199556a4b8c079cab270dfaa162f
Xusong Yuan <cchen@actuate.com>
2013-08-28 15:46:54 +0800

    Fixed a potential defect that can't insert table without binding dataset.

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/dialogs/DataSetBindingSelector.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/internal/ui/dialogs/DataSetBindingSelector.java
@@ -26,7 +26,6 @@ import org.eclipse.birt.report.designer.internal.ui.util.IHelpContextIds;
 import org.eclipse.birt.report.designer.nls.Messages;
 import org.eclipse.birt.report.designer.ui.dialogs.BaseDialog;
 import org.eclipse.birt.report.designer.ui.util.UIUtil;
-import org.eclipse.birt.report.designer.ui.views.attributes.providers.ChoiceSetFactory;
 import org.eclipse.birt.report.designer.ui.views.attributes.providers.LinkedDataSetAdapter;
 import org.eclipse.birt.report.designer.util.DEUtil;
 import org.eclipse.birt.report.model.api.CachedMetaDataHandle;
@@ -252,11 +251,8 @@ public class DataSetBindingSelector extends BaseDialog
 			Label dateSetLabel = new Label( parent, SWT.NONE );
 			dateSetLabel.setText( Messages.getString( "DataSetBindingSelector.Combo.DataSet" ) ); //$NON-NLS-1$
 			dataSetCombo = new Combo( parent, SWT.BORDER | SWT.READ_ONLY );
-			ModuleHandle handle = SessionHandleAdapter.getInstance( ).getReportDesignHandle( );
-			datasets=org.eclipse.birt.report.designer.internal.ui.util.UIUtil.getVisibleDataSetHandles(handle);
-			for(int i=0;i<datasets.size();i++){
-				dataSetCombo.add(datasets.get(i).getQualifiedName());
-			}
+			initDateSetHandles();
+			dataSetCombo.setItems(getDataSetComboList());
 			dataSetCombo.select( 0 );
 			GridData data = new GridData( GridData.FILL_HORIZONTAL );
 			dataSetCombo.setLayoutData( data );
@@ -272,8 +268,29 @@ public class DataSetBindingSelector extends BaseDialog
 		}
 
 	}
-	private DataSetHandle getSelectedDataSet(){
-		return (DataSetHandle)datasets.get(dataSetCombo.getSelectionIndex());
+
+	private void initDateSetHandles() {
+		ModuleHandle handle = SessionHandleAdapter.getInstance()
+				.getReportDesignHandle();
+		datasets = org.eclipse.birt.report.designer.internal.ui.util.UIUtil
+				.getVisibleDataSetHandles(handle);
+	}
+
+	private String[] getDataSetComboList() {
+		String[] comboList = new String[datasets.size() + 1];
+		comboList[0] = NONE;
+		for (int i = 0; i < datasets.size(); i++) {
+			comboList[i + 1] = datasets.get(i).getQualifiedName();
+		}
+		return comboList;
+	}
+
+	private DataSetHandle getSelectedDataSet() {
+		if (dataSetCombo.getSelectionIndex() > 0) {
+			return (DataSetHandle) datasets.get(dataSetCombo
+					.getSelectionIndex() - 1);
+		}
+		return null;
 	}
 	protected void handleDatasetComboSelectedEvent( )
 	{
@@ -320,7 +337,7 @@ public class DataSetBindingSelector extends BaseDialog
 	{
 	
 		result = new Object[3];
-		if ( dataSetName != null || dataSetCombo.getSelectionIndex( ) > -1 )
+		if ( dataSetName != null || dataSetCombo.getSelectionIndex( ) > 0 )
 		{
 			if ( dataSetName == null )
 			{
