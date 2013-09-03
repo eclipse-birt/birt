c4adbff7c7d050822bf70635644e7614d8fdd9ae
mwu <mwu@actuate.com>
2013-09-03 10:04:27 +0800

    Add missing modified class.

--- data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/impl/DataSetIterator.java
+++ data/org.eclipse.birt.report.data.adapter/src/org/eclipse/birt/report/data/adapter/impl/DataSetIterator.java
@@ -498,6 +498,15 @@ public class DataSetIterator implements IDatasetIterator
 		
 		/**
 		 * 
+		 * @param type
+		 */
+		public void setType( int type )
+		{
+			this.type = type;
+		}
+		
+		/**
+		 * 
 		 * @return
 		 */
 		public boolean isLevelKey( )
