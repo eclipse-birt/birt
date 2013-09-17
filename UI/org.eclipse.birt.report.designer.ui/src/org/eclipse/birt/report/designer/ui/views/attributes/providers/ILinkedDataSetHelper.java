420cfea28e95c86efe374f5c51ba086a08a46b94
cchao <cchen@actuate.com>
2013-09-17 14:50:27 +0800

    Selecting duplicate name of data model and data set that always gets the first column in data binding dlg

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/views/attributes/providers/ILinkedDataSetHelper.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/views/attributes/providers/ILinkedDataSetHelper.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.eclipse.birt.report.model.api.DataSetHandle;
 import org.eclipse.birt.report.model.api.ModuleHandle;
 import org.eclipse.birt.report.model.api.ReportItemHandle;
+import org.eclipse.birt.report.model.api.olap.CubeHandle;
 
 public interface ILinkedDataSetHelper
 {
@@ -16,7 +17,11 @@ public interface ILinkedDataSetHelper
 	public boolean setLinkedDataModel( ReportItemHandle handle, Object value );
 
 	public Iterator getResultSetIterator( String datasetName );
-	
-	public List<DataSetHandle> getVisibleLinkedDataSetsDataSetHandles(ModuleHandle handle);
+
+	public List<DataSetHandle> getVisibleLinkedDataSetsDataSetHandles(
+			ModuleHandle handle );
+
+	public List<CubeHandle> getVisibleLinkedDataSetsCubeHandles(
+			ModuleHandle handle );
 
 }
