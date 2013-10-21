2e27e12133bfff29d6e8b16114ce515f934642b1
xwu <xwu@actuate.com>
2013-10-21 12:09:04 +0800

    Implementation of Project 1892 BIRT: Support for HiveServer2 using the Hive ODA connector.

--- data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/model/FilterConfig.java
+++ data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/model/FilterConfig.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008 Actuate Corporation.
+ * Copyright (c) 2008, 2013 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Actuate Corporation  - initial API and implementation
+ *  Actuate Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.birt.report.data.oda.jdbc.ui.model;
 
@@ -21,6 +21,8 @@ public class FilterConfig
 	public static enum Type {
 		ALL, TABLE, VIEW, PROCEDURE
 	};
+
+	public final static String JDBC_TYPE_NO_LIMIT = "NO_LIMIT"; //$NON-NLS-1$
 	public final static String JDBC_TYPE_TABLE = "TABLE"; //$NON-NLS-1$
 	public final static String JDBC_TYPE_VIEW = "VIEW"; //$NON-NLS-1$
 	public final static String JDBC_TYPE_SYSTEM_TABLE = "SYSTEM TABLE"; //$NON-NLS-1$
@@ -28,20 +30,33 @@ public class FilterConfig
 	public final static String JDBC_TYPE_LOCAL_TEMPORARY = "LOCAL TEMPORARY"; //$NON-NLS-1$
 	public final static String JDBC_TYPE_ALIAS = "ALIAS"; //$NON-NLS-1$
 	public final static String JDBC_TYPE_SYNONYM = "SYNONYM"; //$NON-NLS-1$
-	
-	private Type type;
+
+	private TableType type;
 	private boolean isShowSystemTable;
 	private boolean isShowAlias;
-	private String schemaName; //null if no schema specified
+	private String schemaName; // null if no schema specified
 	private String namePattern;
-	
+
 	private int maxSchemaCount;
 	private int maxTableCountPerSchema;
-	
+
 	public FilterConfig( String schemaName, Type type, String namePattern,
-			 boolean isShowSystemTable, boolean isShowAlias,
-			 int maxSchemaCount, 
-			 int maxTableCountPerSchema )
+			boolean isShowSystemTable, boolean isShowAlias, int maxSchemaCount,
+			int maxTableCountPerSchema )
+	{
+		assert type != null && maxSchemaCount > 0 && maxTableCountPerSchema > 0;
+		this.schemaName = schemaName;
+		this.type = TableType.valueOf( type );
+		this.isShowSystemTable = isShowSystemTable;
+		this.isShowAlias = isShowAlias;
+		this.namePattern = generatePattern( namePattern );
+		this.maxSchemaCount = maxSchemaCount;
+		this.maxTableCountPerSchema = maxTableCountPerSchema;
+	}
+
+	public FilterConfig( String schemaName, TableType type, String namePattern,
+			boolean isShowSystemTable, boolean isShowAlias, int maxSchemaCount,
+			int maxTableCountPerSchema )
 	{
 		assert type != null && maxSchemaCount > 0 && maxTableCountPerSchema > 0;
 		this.schemaName = schemaName;
@@ -55,74 +70,80 @@ public class FilterConfig
 
 	public Type getType( )
 	{
+		return type.getTypeID( );
+	}
+
+	public TableType getTableType( )
+	{
 		return type;
 	}
-	
+
 	public boolean isShowSystemTable( )
 	{
 		return isShowSystemTable;
 	}
-	
+
 	public boolean isShowAlias( )
 	{
 		return isShowAlias;
 	}
-	
+
 	public String getNamePattern( )
 	{
 		return namePattern;
 	}
-	
+
 	public String getSchemaName( )
 	{
 		return schemaName;
 	}
 
-	
 	public int getMaxSchemaCount( )
 	{
 		return maxSchemaCount;
 	}
 
-	
 	public int getMaxTableCountPerSchema( )
 	{
 		return maxTableCountPerSchema;
 	}
-	
+
 	/**
 	 * @return null if no table/view needed to query
 	 */
 	public String[] getTableTypesForJDBC( )
 	{
-		ArrayList<String> types = new ArrayList<String>();
-		switch ( type )
+		ArrayList<String> types = new ArrayList<String>( );
+
+		if ( type == TableType.PROCEDURE )
 		{
-			case PROCEDURE:
-				break;
-			case TABLE:
-				types.add( JDBC_TYPE_TABLE );
-				populateSystemTableOption( types );
-				populateAliasOption( types );
-				break;
-			case VIEW:
-				types.add( JDBC_TYPE_VIEW );
-				populateAliasOption( types );
-				break;
-			case ALL:
-				types.add( JDBC_TYPE_TABLE );
-				types.add( JDBC_TYPE_VIEW );
-				populateSystemTableOption( types );
-				populateAliasOption( types );
-				break;
-			default:
-				//should never goes here
-				assert false;
-				break;
+
+		}
+		else if ( type == TableType.TABLE )
+		{
+			types.add( JDBC_TYPE_TABLE );
+			populateSystemTableOption( types );
+			populateAliasOption( types );
+		}
+		else if ( type == TableType.VIEW )
+		{
+			types.add( JDBC_TYPE_VIEW );
+			populateAliasOption( types );
+		}
+		else if ( type == TableType.ALL )
+		{
+			types.add( JDBC_TYPE_TABLE );
+			types.add( JDBC_TYPE_VIEW );
+			populateSystemTableOption( types );
+			populateAliasOption( types );
+		}
+		else
+		{
+			types.add( type.getTypeName( ) );
 		}
 		return types.isEmpty( ) ? null : types.toArray( new String[]{} );
 	}
-	
+
 	private void populateSystemTableOption( final ArrayList<String> types )
 	{
 		if ( isShowSystemTable( ) )
@@ -130,7 +151,7 @@ public class FilterConfig
 			types.add( JDBC_TYPE_SYSTEM_TABLE );
 		}
 	}
-	
+
 	private void populateAliasOption( final ArrayList<String> types )
 	{
 		if ( isShowAlias( ) )
@@ -139,7 +160,7 @@ public class FilterConfig
 			types.add( JDBC_TYPE_SYNONYM );
 		}
 	}
-	
+
 	private static String generatePattern( String input )
 	{
 		if ( input != null )
@@ -155,24 +176,22 @@ public class FilterConfig
 		}
 		return input;
 	}
-	
+
 	public static String getTypeDisplayText( Type type )
 	{
 		assert type != null;
 		switch ( type )
 		{
-			case PROCEDURE:
+			case PROCEDURE :
 				return JdbcPlugin.getResourceString( "tablepage.text.procedure" ); //$NON-NLS-1$
-			case TABLE:
+			case TABLE :
 				return JdbcPlugin.getResourceString( "tablepage.text.tabletype" ); //$NON-NLS-1$
-			case VIEW:	
+			case VIEW :
 				return JdbcPlugin.getResourceString( "tablepage.text.viewtype" ); //$NON-NLS-1$
-			case ALL:
+			case ALL :
 				return JdbcPlugin.getResourceString( "tablepage.text.All" ); //$NON-NLS-1$
-			default:
-				//should never goes here
-				assert false;
-				return ""; //$NON-NLS-1$
+			default :
+				return type.name( );
 		}
 	}
 }
--- data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/model/SchemaNode.java
+++ data/org.eclipse.birt.report.data.oda.jdbc.ui/src/org/eclipse/birt/report/data/oda/jdbc/ui/model/SchemaNode.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2008 Actuate Corporation.
+ * Copyright (c) 2008, 2013 Actuate Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  Actuate Corporation  - initial API and implementation
+ *  Actuate Corporation - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.birt.report.data.oda.jdbc.ui.model;
@@ -57,6 +57,10 @@ public class SchemaNode extends ChildrenAllowedNode
 		List<IDBNode> children = new ArrayList<IDBNode>( );
 		if ( tableTypes != null )
 		{
+			if ( tableTypes.length == 1
+					&& TableType.NO_LIMIT.getTypeName( ).equals( tableTypes[0] ) )
+				tableTypes = null;
+
 			ResultSet rs = JdbcMetaDataProvider.getInstance( )
 					.getAlltables( schemaName, fc.getNamePattern( ), tableTypes );
 
