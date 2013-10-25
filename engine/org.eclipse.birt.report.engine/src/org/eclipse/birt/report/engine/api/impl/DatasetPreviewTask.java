82fe8ff9bcf0807cebd1d135ea33ef25e6334c44
azhu <azhu@actuate.com>
2013-10-25 17:02:55 +0800

    Fixed Exception occur if use "weighted average" function (T67766)
    Add data type on bindings generated by DatasetPreviewTask.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/DatasetPreviewTask.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/api/impl/DatasetPreviewTask.java
@@ -15,7 +15,6 @@ import org.eclipse.birt.core.exception.BirtException;
 import org.eclipse.birt.data.engine.api.DataEngineContext;
 import org.eclipse.birt.data.engine.api.IBaseExpression;
 import org.eclipse.birt.data.engine.api.IBasePreparedQuery;
-import org.eclipse.birt.data.engine.api.IBinding;
 import org.eclipse.birt.data.engine.api.IConditionalExpression;
 import org.eclipse.birt.data.engine.api.IExpressionCollection;
 import org.eclipse.birt.data.engine.api.IFilterDefinition;
@@ -23,9 +22,7 @@ import org.eclipse.birt.data.engine.api.IQueryDefinition;
 import org.eclipse.birt.data.engine.api.IQueryResults;
 import org.eclipse.birt.data.engine.api.IScriptExpression;
 import org.eclipse.birt.data.engine.api.ISortDefinition;
-import org.eclipse.birt.data.engine.api.querydefn.Binding;
 import org.eclipse.birt.data.engine.api.querydefn.QueryDefinition;
-import org.eclipse.birt.data.engine.api.querydefn.ScriptExpression;
 import org.eclipse.birt.data.engine.core.DataException;
 import org.eclipse.birt.report.data.adapter.api.DataRequestSession;
 import org.eclipse.birt.report.data.adapter.api.DataSessionContext;
@@ -53,6 +50,7 @@ import org.eclipse.birt.report.model.api.AbstractScalarParameterHandle;
 import org.eclipse.birt.report.model.api.DataSetHandle;
 import org.eclipse.birt.report.model.api.ModuleHandle;
 import org.eclipse.birt.report.model.api.ReportDesignHandle;
+import org.eclipse.birt.report.model.api.elements.structures.ResultSetColumn;
 
 public class DatasetPreviewTask extends EngineTask implements IDatasetPreviewTask
 {
@@ -372,15 +370,15 @@ public class DatasetPreviewTask extends EngineTask implements IDatasetPreviewTas
 		query.setDataSetName( dataset.getQualifiedName( ) );
 		Set<String> existBindings = new HashSet<String>( );
 
+		Map<String, ResultSetColumn> columns = QueryUtil.getResultSetColumns( dataset );
 		if ( this.selectedColumns == null )
 		{
-			List<String> columnNames = QueryUtil.getColumnNames( dataset );
-			for ( String column : columnNames )
+			for ( ResultSetColumn column : columns.values( ) )
 			{
-				if ( !existBindings.contains( column ) )
+				if ( !existBindings.contains( column.getColumnName( ) ) )
 				{
 					QueryUtil.addBinding( query, column );
-					existBindings.add( column );
+					existBindings.add( column.getColumnName( ) );
 				}
 			}
 		}
@@ -390,8 +388,12 @@ public class DatasetPreviewTask extends EngineTask implements IDatasetPreviewTas
 			{
 				if ( !existBindings.contains( column ) )
 				{
-					QueryUtil.addBinding( query, column );
-					existBindings.add( column );
+					ResultSetColumn col = columns.get( column );
+					if ( col != null )
+					{
+						QueryUtil.addBinding( query, col );
+						existBindings.add( column );
+					}
 				}
 			}
 		}
