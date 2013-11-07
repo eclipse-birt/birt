eb55cbe05c2675e296a9c2e0c33430b6332d7fae
gliu <gliu@actuate.com>
2013-11-07 14:59:39 +0800

    Fix Optimize the performance of ULocale.forLocale

---
+++ core/org.eclipse.birt.core/src/org/eclipse/birt/core/util/LocaleUtil.java
@@ -0,0 +1,27 @@
+package org.eclipse.birt.core.util;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+import com.ibm.icu.util.ULocale;
+
+
+public class LocaleUtil
+{
+
+	private static HashMap<Locale, ULocale> localeMap = new HashMap( );
+
+	public synchronized static ULocale forLocale( Locale locale )
+	{
+		if ( localeMap.containsKey( locale ) )
+		{
+			return localeMap.get( locale );
+		}
+		else
+		{
+			ULocale ulocale = ULocale.forLocale( locale );
+			localeMap.put( locale, ulocale );
+			return ulocale;
+		}
+	}
+}
