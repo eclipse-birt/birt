2dd22633e90df7b101f581e059fd05d08448f1e9
Zhiqiang Qian <zqian@actuate.com>
2013-11-12 17:10:52 -0800

    Add general "import file" support for resource selection dialog.

--- data/org.eclipse.birt.report.data.oda.excel.ui/src/org/eclipse/birt/report/data/oda/excel/ui/wizards/RelativeFileSelectionDialog.java
+++ data/org.eclipse.birt.report.data.oda.excel.ui/src/org/eclipse/birt/report/data/oda/excel/ui/wizards/RelativeFileSelectionDialog.java
@@ -16,24 +16,42 @@ package org.eclipse.birt.report.data.oda.excel.ui.wizards;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
+import org.eclipse.birt.report.data.oda.excel.ui.i18n.Messages;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
@@ -52,8 +70,11 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 			.getImage( ISharedImages.IMG_OBJ_FILE );
 
 	private File rootFolder;
+	private String[] extensions;
+	private Button importButton;
 
-	public RelativeFileSelectionDialog( Shell parent, File rootFolder,String[] extensions )
+	public RelativeFileSelectionDialog( Shell parent, File rootFolder,
+			String[] extensions )
 	{
 		super( parent, new LabelProvider( ), new ContentProvider( extensions ) );
 		setHelpAvailable( false );
@@ -64,6 +85,7 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 		this.setInput( rootFolder.getAbsolutePath( ) );
 		this.setTitle( "SelectFile.Title" ); //$NON-NLS-1$
 		this.rootFolder = rootFolder;
+		this.extensions = extensions;
 	}
 
 	public URI getSelectedURI( ) throws URISyntaxException
@@ -82,7 +104,7 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 
 	/*
 	 * (non-Javadoc)
-	 *
+	 * 
 	 * @see
 	 * org.eclipse.ui.dialogs.ElementTreeSelectionDialog#createDialogArea(org
 	 * .eclipse.swt.widgets.Composite)
@@ -90,13 +112,194 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 	protected Control createDialogArea( Composite parent )
 	{
 		Control c = super.createDialogArea( parent );
+
+		createImportFileArea( (Composite) c );
+
 		this.getTreeViewer( ).expandToLevel( 2 );
 		return c;
 	}
 
+	private void createImportFileArea( Composite parent )
+	{
+		importButton = new Button( parent, SWT.PUSH );
+		importButton.setText( Messages.getString( "RelativeFileSelectionDialog.button.importFile" ) ); //$NON-NLS-1$
+		importButton.addSelectionListener( new SelectionAdapter( ) {
+
+			public void widgetSelected( org.eclipse.swt.events.SelectionEvent e )
+			{
+				FileDialog dialog = new FileDialog( getShell( ) );
+				dialog.setFilterExtensions( extensions );
+
+				String selectedLocation = dialog.open( );
+				if ( selectedLocation != null )
+				{
+					File targetFolder = getSelectedFolder( );
+					File srcFile = new File( selectedLocation );
+					File targetFile = new File( targetFolder, srcFile.getName( ) );
+
+					if ( targetFile.exists( )
+							&& !MessageDialog.openConfirm( getShell( ),
+									Messages.getString( "RelativeFileSelectionDialog.title.overwrite" ), //$NON-NLS-1$
+									Messages.getString( "RelativeFileSelectionDialog.overwrite.msg" ) ) ) //$NON-NLS-1$
+					{
+						return;
+					}
+
+					importFile( targetFile, srcFile );
+					
+					refreshResource( targetFile );
+
+					getTreeViewer( ).refresh( targetFolder );
+					getTreeViewer( ).expandToLevel( targetFolder, 1 );
+				}
+			};
+		} );
+	}
+
+	private void refreshResource( File file )
+	{
+		IPath resPath = Path.fromOSString( file.getAbsolutePath( ) );
+		
+		IResource[] res = ResourcesPlugin.getWorkspace( )
+				.getRoot( )
+				.findFilesForLocation( resPath );
+
+		if ( res.length == 0 )
+		{
+			res = ResourcesPlugin.getWorkspace( )
+					.getRoot( )
+					.findContainersForLocation( resPath );
+
+			if ( res.length == 0 )
+			{
+				// not resources within the workspace
+				return;
+			}
+		}
+
+		try
+		{
+			final IResource[] targes = res;
+
+			for ( IResource rc : targes )
+			{
+				rc.refreshLocal( IResource.DEPTH_INFINITE, null );
+			}
+		}
+		catch ( Exception e )
+		{
+			// ignore
+		}
+
+	}
+
+	private File getSelectedFolder( )
+	{
+		Object[] selection = getResult( );
+		if ( selection != null && selection.length > 0 )
+		{
+			if ( selection[0] instanceof File )
+			{
+				File f = (File) selection[0];
+
+				if ( f.isFile( ) )
+				{
+					return f.getParentFile( );
+				}
+				return f;
+			}
+		}
+		return null;
+	}
+
+	private void importFile( final File target, final File src )
+	{
+		try
+		{
+			new ProgressMonitorDialog( getShell( ) ).run( true,
+					false,
+					new IRunnableWithProgress( ) {
+
+						@Override
+						public void run( IProgressMonitor monitor )
+								throws InvocationTargetException,
+								InterruptedException
+						{
+							monitor.beginTask( Messages.getString( "RelativeFileSelectionDialog.import.msg" ), //$NON-NLS-1$
+									1 );
+
+							try
+							{
+								doImport( target, src );
+							}
+							catch ( IOException e )
+							{
+								throw new InvocationTargetException( e );
+							}
+							finally
+							{
+								monitor.done( );
+							}
+						}
+					} );
+		}
+		catch ( Exception e )
+		{
+			MessageDialog.openError( getShell( ),
+					Messages.getString( "RelativeFileSelectionDialog.title.error" ), //$NON-NLS-1$
+					e.getLocalizedMessage( ) );
+		}
+	}
+
+	private void doImport( File target, File src ) throws IOException
+	{
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		FileChannel fcin = null;
+		FileChannel fcout = null;
+
+		try
+		{
+			fis = new FileInputStream( src );
+			fos = new FileOutputStream( target );
+			fcin = fis.getChannel( );
+			fcout = fos.getChannel( );
+
+			fcin.transferTo( 0, fcin.size( ), fcout );
+		}
+		finally
+		{
+			if ( fis != null )
+			{
+				fis.close( );
+			}
+			if ( fos != null )
+			{
+				fos.close( );
+			}
+			if ( fcin != null )
+			{
+				fcin.close( );
+			}
+			if ( fcout != null )
+			{
+				fcout.close( );
+			}
+		}
+	}
+
+	@Override
+	protected void updateOKStatus( )
+	{
+		super.updateOKStatus( );
+
+		importButton.setEnabled( getSelectedFolder( ) != null );
+	}
+
 	private static class ContentProvider implements ITreeContentProvider
 	{
-		private String[] exts;	
+
+		private String[] exts;
 		private final String ALL_EXT = "*.*"; //$NON-NLS-1$
 
 		public ContentProvider( String[] extensionFilter )
@@ -114,7 +317,7 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 				exts = filters.toArray( new String[]{} );
 			}
 		}
-		
+
 		public Object[] getChildren( Object arg0 )
 		{
 			if ( arg0 instanceof File )
@@ -246,9 +449,8 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 
 	}
 
-	private static class SelectionValidator
-			implements
-				ISelectionStatusValidator
+	private static class SelectionValidator implements
+			ISelectionStatusValidator
 	{
 
 		public IStatus validate( Object[] selections )
@@ -279,7 +481,6 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 
 	}
 
-
 	public String[] getSelectedItems( )
 	{
 		List<String> result = new ArrayList<String>( );
@@ -298,10 +499,9 @@ public class RelativeFileSelectionDialog extends ElementTreeSelectionDialog
 		return result.toArray( new String[0] );
 	}
 
-	public static class FileComparator
-			implements
-				Comparator<File>,
-				Serializable
+	public static class FileComparator implements
+			Comparator<File>,
+			Serializable
 	{
 
 		private static final long serialVersionUID = 1L;
