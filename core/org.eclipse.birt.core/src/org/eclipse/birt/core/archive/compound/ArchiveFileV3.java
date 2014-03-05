35298c62f164bf65be3a8b1f9e1a628b6016fc43
jouyang <jouyang@actuate.com>
2014-03-04 18:07:49 -0800

    72136 Correct the algorithm to calculate transitent document size in javaserver side

--- core/org.eclipse.birt.core/src/org/eclipse/birt/core/archive/compound/ArchiveFileV3.java
+++ core/org.eclipse.birt.core/src/org/eclipse/birt/core/archive/compound/ArchiveFileV3.java
@@ -189,22 +189,9 @@ public class ArchiveFileV3 implements IArchiveFile
 		}
 	}
 
-	public long getSize( )
+	public long getLength( )
 	{
-		long result = 0;
-		List<String> entries = listEntries( null );
-		for( String entry : entries )
-		{
-			try
-			{
-				result += openEntry( entry ).getLength( );
-			}
-			catch ( IOException e )
-			{
-				e.printStackTrace();
-			}
-		}
-		return result;
+		return fs == null ? 0 : fs.length( );
 	}
 
 	synchronized public void unlockEntry( Object locker ) throws IOException
