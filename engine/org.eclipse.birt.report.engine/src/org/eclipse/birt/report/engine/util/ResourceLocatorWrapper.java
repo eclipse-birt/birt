d89861eb6d60ae965a55e4ef2a238b1eb085579c
Pengcheng Bian <pbian@actuate.com>
2014-09-02 16:49:11 -0700

    422282 Add ResourceBundle to Logger.
    Signed-off-by: Pengcheng Bian <pbian@actuate.com>

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/util/ResourceLocatorWrapper.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/util/ResourceLocatorWrapper.java
@@ -8,6 +8,7 @@
  * Contributors:
  *  Actuate Corporation  - initial API and implementation
  *******************************************************************************/
+
 package org.eclipse.birt.report.engine.util;
 
 import java.io.ByteArrayOutputStream;
@@ -19,6 +20,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.eclipse.birt.report.engine.i18n.EngineResourceHandle;
 import org.eclipse.birt.report.engine.i18n.MessageConstants;
 import org.eclipse.birt.report.model.api.ModuleHandle;
 
@@ -27,15 +29,15 @@ public class ResourceLocatorWrapper
 
 	private HashMap<URL, byte[]> cache;
 	private static final byte[] DUMMY_BYTES = new byte[0];
-	
-	protected static Logger logger = Logger
-			.getLogger( ResourceLocatorWrapper.class.getName( ) );
+	private static final String RESOURCE_BUNDLE = "org.eclipse.birt.report.engine.i18n.Messages";
+	protected static Logger logger = Logger.getLogger(
+			ResourceLocatorWrapper.class.getName( ), RESOURCE_BUNDLE );
 
 	public ResourceLocatorWrapper( )
 	{
 		cache = new HashMap<URL, byte[]>( );
 	}
-	
+
 	public void dispose( )
 	{
 		synchronized ( cache )
@@ -52,18 +54,19 @@ public class ResourceLocatorWrapper
 	 * @param appContext
 	 * @return
 	 */
-	public byte[] findResource( ModuleHandle design,
-			String fileName, int fileType, Map appContext )
+	public byte[] findResource( ModuleHandle design, String fileName,
+			int fileType, Map appContext )
 	{
 		URL url = design.findResource( fileName, fileType, appContext );
 		if ( url == null )
 		{
-			logger.log( Level.WARNING, MessageConstants.RESOURCE_NOT_ACCESSIBLE, fileName );
+			logger.log( Level.WARNING,
+					MessageConstants.RESOURCE_NOT_ACCESSIBLE, fileName );
 			return DUMMY_BYTES;
 		}
 		return findResource( url );
 	}
-	
+
 	/**
 	 * Finds a resource from the given URL. If the URL is not accessible, it
 	 * will return a 0-size byte array.
@@ -102,8 +105,9 @@ public class ResourceLocatorWrapper
 			return inBytes;
 		}
 	}
-	
-	private byte[] getByteArrayFromInputStream( InputStream in ) throws IOException
+
+	private byte[] getByteArrayFromInputStream( InputStream in )
+			throws IOException
 	{
 		ByteArrayOutputStream out = new ByteArrayOutputStream( );
 		byte[] buffer = new byte[1024];
