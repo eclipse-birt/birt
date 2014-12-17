c88d499ebfca7b339346a1c440400b39304192be
Yulin Wang <yulin.wang@actuate.com>
2014-12-16 17:14:47 -0800

    Code refactor to customize implementation in factory

--- chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIFactory.java
+++ chart/org.eclipse.birt.chart.reportitem.ui/src/org/eclipse/birt/chart/reportitem/ui/ChartReportItemUIFactory.java
@@ -14,7 +14,19 @@ package org.eclipse.birt.chart.reportitem.ui;
 import org.eclipse.birt.chart.log.ILogger;
 import org.eclipse.birt.chart.log.Logger;
 import org.eclipse.birt.chart.model.Chart;
+import org.eclipse.birt.chart.model.ChartWithAxes;
+import org.eclipse.birt.chart.model.component.Series;
+import org.eclipse.birt.chart.model.component.impl.SeriesImpl;
+import org.eclipse.birt.chart.model.data.BaseSampleData;
+import org.eclipse.birt.chart.model.data.DataFactory;
+import org.eclipse.birt.chart.model.data.OrthogonalSampleData;
+import org.eclipse.birt.chart.model.data.SampleData;
+import org.eclipse.birt.chart.model.data.SeriesDefinition;
+import org.eclipse.birt.chart.model.data.impl.SeriesDefinitionImpl;
+import org.eclipse.birt.chart.model.impl.ChartWithAxesImpl;
+import org.eclipse.birt.chart.model.type.impl.BarSeriesImpl;
 import org.eclipse.birt.chart.reportitem.ChartReportItemImpl;
+import org.eclipse.birt.chart.reportitem.api.ChartReportItemConstants;
 import org.eclipse.birt.chart.reportitem.ui.views.attributes.ChartPageGenerator;
 import org.eclipse.birt.chart.ui.swt.ChartUIFactory;
 import org.eclipse.birt.chart.ui.swt.interfaces.IChartDataSheet;
@@ -22,13 +34,17 @@ import org.eclipse.birt.chart.ui.swt.interfaces.IDataServiceProvider;
 import org.eclipse.birt.chart.ui.swt.interfaces.IImageServiceProvider;
 import org.eclipse.birt.chart.ui.swt.interfaces.IUIServiceProvider;
 import org.eclipse.birt.chart.ui.swt.wizard.ChartWizardContext;
+import org.eclipse.birt.chart.ui.util.ChartUIUtil;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.AbstractFilterHandleProvider;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.FilterHandleProvider;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.ISectionHelper;
+import org.eclipse.birt.report.designer.ui.ReportPlugin;
 import org.eclipse.birt.report.designer.ui.extensions.IMenuBuilder;
 import org.eclipse.birt.report.designer.ui.preferences.IStatusChangeListener;
 import org.eclipse.birt.report.designer.ui.views.ElementAdapterManager;
+import org.eclipse.birt.report.model.api.DesignElementHandle;
 import org.eclipse.birt.report.model.api.ExtendedItemHandle;
+import org.eclipse.birt.report.model.api.extension.ExtendedElementException;
 import org.eclipse.core.resources.IProject;
 
 /**
@@ -153,6 +169,56 @@ public class ChartReportItemUIFactory extends ChartUIFactory
 			baseProvider = new FilterHandleProvider( );
 		}
 		return baseProvider;
+	}
+	
+	public ExtendedItemHandle createChartViewHandle( DesignElementHandle host )
+			throws ExtendedElementException
+	{
+		// Create chart
+		ChartWithAxes cm = ChartWithAxesImpl.createDefault( );
+		cm.setType( "Bar Chart" );//$NON-NLS-1$
+		cm.setSubType( "Side-by-side" );//$NON-NLS-1$
+
+		// Add base series
+		SeriesDefinition sdBase = SeriesDefinitionImpl.createDefault( );
+		Series series = SeriesImpl.createDefault( );
+		sdBase.getSeries( ).add( series );
+		cm.getBaseAxes( )[0].getSeriesDefinitions( ).add( sdBase );
+
+		// Add orthogonal series
+		SeriesDefinition sdOrth = SeriesDefinitionImpl.createDefault( );
+		series = BarSeriesImpl.createDefault( );
+		sdOrth.getSeries( ).add( series );
+		cm.getOrthogonalAxes( cm.getBaseAxes( )[0], true )[0].getSeriesDefinitions( )
+				.add( sdOrth );
+		ChartUIUtil.setSeriesName( cm );
+
+		// Add sample data
+		SampleData sampleData = DataFactory.eINSTANCE.createSampleData( );
+		sampleData.getBaseSampleData( ).clear( );
+		sampleData.getOrthogonalSampleData( ).clear( );
+		// Create Base Sample Data
+		BaseSampleData sampleDataBase = DataFactory.eINSTANCE.createBaseSampleData( );
+		sampleDataBase.setDataSetRepresentation( "A, B, C" ); //$NON-NLS-1$
+		sampleData.getBaseSampleData( ).add( sampleDataBase );
+		// Create Orthogonal Sample Data (with simulation count of 2)
+		OrthogonalSampleData sampleDataOrth = DataFactory.eINSTANCE.createOrthogonalSampleData( );
+		sampleDataOrth.setDataSetRepresentation( "5,4,12" ); //$NON-NLS-1$
+		sampleDataOrth.setSeriesDefinitionIndex( 0 );
+		sampleData.getOrthogonalSampleData( ).add( sampleDataOrth );
+		cm.setSampleData( sampleData );
+
+		// Create a new item handle.
+		String name = ReportPlugin.getDefault( )
+				.getCustomName( ChartReportItemConstants.CHART_EXTENSION_NAME );
+		ExtendedItemHandle itemHandle = host.getElementFactory( )
+				.newExtendedItem( name,
+						ChartReportItemConstants.CHART_EXTENSION_NAME );
+
+		itemHandle.getReportItem( )
+				.setProperty( ChartReportItemConstants.PROPERTY_CHART, cm );
+
+		return itemHandle;
 	}
 
 }
