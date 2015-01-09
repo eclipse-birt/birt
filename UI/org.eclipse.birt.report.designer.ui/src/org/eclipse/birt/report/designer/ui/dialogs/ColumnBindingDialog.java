b076267f8c8166ce3b4056f2321ff9e608686dab
Carl Thronson <cthronson@actuate.com>
2015-01-08 16:00:49 -0800

    Fixed a potential NPE.
    Signed-off-by: Carl Thronson <cthronson@actuate.com>

--- UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ColumnBindingDialog.java
+++ UI/org.eclipse.birt.report.designer.ui/src/org/eclipse/birt/report/designer/ui/dialogs/ColumnBindingDialog.java
@@ -58,7 +58,6 @@ import org.eclipse.birt.report.model.api.metadata.IChoiceSet;
 import org.eclipse.birt.report.model.api.metadata.PropertyValueException;
 import org.eclipse.birt.report.model.api.util.StringUtil;
 import org.eclipse.birt.report.model.elements.interfaces.IReportItemModel;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
@@ -1428,7 +1427,7 @@ public class ColumnBindingDialog extends BaseDialog
 				}
 			}
 		}
-		if( ExtendedDataModelUIAdapterHelper.isBoundToExtendedData( inputElement ) )
+		if( getSelectColumnHandle( ) != null && ExtendedDataModelUIAdapterHelper.isBoundToExtendedData( inputElement ) )
 		{
 			DataModelAdapterStatus status = DataModelAdapterUtil.validateRelativeTimePeriod( inputElement, getSelectColumnHandle( ));
 			if( status.getStatus( ) == DataModelAdapterStatus.Status.FAIL )
