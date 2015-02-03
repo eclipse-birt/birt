1c3a8d47575fe397b4a63c288f53db8efc1e10a4
sguan <sguan@actuate.com>
2015-02-02 18:03:36 -0800

    Refactor Text box in PPTX
    Signed-off-by: sguan <sguan@actuate.com>

--- engine/org.eclipse.birt.report.engine.emitter.pptx/src/org/eclipse/birt/report/engine/emitter/pptx/TextWriter.java
+++ engine/org.eclipse.birt.report.engine.emitter.pptx/src/org/eclipse/birt/report/engine/emitter/pptx/TextWriter.java
@@ -376,15 +376,20 @@ public class TextWriter
 			BoxStyle style = container.getBoxStyle( );
 			Color color = style.getBackgroundColor( );
 			BackgroundImageInfo image = style.getBackgroundImage( );
-			if ( color != null )
-			{
-				canvas.setBackgroundColor( color );
-			}
 			if ( image != null )
 			{
 				canvas.setBackgroundImg( canvas.getImageRelationship( image ),
 						0,
 						0 );
+			}
+			else if ( color != null )
+			{
+				canvas.setBackgroundColor( color );
+			}
+			else
+			{
+				writer.openTag( "a:noFill" );
+				writer.closeTag( "a:noFill" );
 			}
 
 			writeLineStyle( );
