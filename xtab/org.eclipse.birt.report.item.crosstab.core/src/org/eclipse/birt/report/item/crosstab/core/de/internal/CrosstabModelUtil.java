92d360cf9755f251608a4a883cca4bb231eca71c
Carl Thronson <cthronson@actuate.com>
2016-01-15 16:30:32 -0800

    Revert "Make logic more simple.  If it already exists, do not recreate."
    This reverts commit 3698db40062736c128a36731095f60f73d64df31.

--- xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/de/internal/CrosstabModelUtil.java
+++ xtab/org.eclipse.birt.report.item.crosstab.core/src/org/eclipse/birt/report/item/crosstab/core/de/internal/CrosstabModelUtil.java
@@ -593,11 +593,6 @@ public final class CrosstabModelUtil implements ICrosstabConstants
 			String name = generateComputedColumnName( measureView,
 					colLevel,
 					rowLevel );
-			Map<String, String> map = CrosstabUtil.getBindingExpressMap( crosstab );
-			if ( map.containsKey( name ) )
-			{
-				return;
-			}
 			ComputedColumn column = StructureFactory.newComputedColumn( crosstab.getModelHandle( ),
 					name );
 			String dataType = measureView.getDataType( );
@@ -611,7 +606,7 @@ public final class CrosstabModelUtil implements ICrosstabConstants
 				{
 					// throw case
 					return;
-				}
+				}				
 				column.setExpression( ExpressionUtil.createDataSetRowExpression( measureName ) );
 			}
 			else
