08b9f65f5d9df18c3ad37809eb2f1fda9dc4cab4
sguan <sguan@actuate.com>
2016-06-15 11:55:51 -0700

    Review and clean up test
    Signed-off-by: sguan <sguan@actuate.com>

--- engine/org.eclipse.birt.report.engine.emitter.prototype.excel.tests/test/org/eclipse/birt/report/engine/emitter/prototype/excel/ExcelWriterTest.java
+++ engine/org.eclipse.birt.report.engine.emitter.prototype.excel.tests/test/org/eclipse/birt/report/engine/emitter/prototype/excel/ExcelWriterTest.java
@@ -12,20 +12,13 @@
 package org.eclipse.birt.report.engine.emitter.prototype.excel;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 
 import junit.framework.TestCase;
 
 import org.eclipse.birt.report.engine.api.EXCELRenderOption;
-import org.eclipse.birt.report.engine.api.EngineException;
-import org.eclipse.birt.report.engine.api.HTMLActionHandler;
-import org.eclipse.birt.report.engine.api.HTMLCompleteImageHandler;
-import org.eclipse.birt.report.engine.api.HTMLEmitterConfig;
-import org.eclipse.birt.report.engine.api.HTMLRenderOption;
 import org.eclipse.birt.report.engine.emitter.EngineEmitterServices;
 import org.eclipse.birt.report.engine.emitter.excel.BookmarkDef;
-import org.eclipse.birt.report.engine.emitter.excel.ExcelEmitter;
 import org.eclipse.birt.report.engine.emitter.excel.ExcelWriter;
 import org.eclipse.birt.report.engine.emitter.excel.SheetData;
 import org.eclipse.birt.report.engine.emitter.excel.StyleEntry;
@@ -34,15 +27,16 @@ import org.eclipse.birt.report.engine.emitter.excel.layout.ExcelContext;
 
 public class ExcelWriterTest extends TestCase
 {
+	final static String WORKSPACE_DIR = "./excelReport"; //$NON-NLS-1$
 
+	@SuppressWarnings("nls")
 	public void testStartSheet( )
 	{
+		EXCELRenderOption renderOption = new EXCELRenderOption( );
 		try
 		{
-			ExcelEmitter emitter = new ExcelEmitter( );
-			EXCELRenderOption renderOption = new EXCELRenderOption( );
-			new File( "./excelReport" ).mkdir( );
-			renderOption.setOutputFileName( "./excelReport/report.xls" ); //$NON-NLS-1$
+			new File( WORKSPACE_DIR ).mkdir( );
+			renderOption.setOutputFileName( WORKSPACE_DIR + "/report.xls" ); //$NON-NLS-1$
 			EngineEmitterServices services = new EngineEmitterServices( null,
 					renderOption, null );
 
