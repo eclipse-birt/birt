fd5b7c80b4c7a54a26c6ac2e73a3c39fe98949bb
Aswani B <aswanib@opentext.com>
2017-03-17 09:45:22 +0530

    Revert "Margins are not being set in the doc/docx format of BIRT reports"
    This reverts commit 0e6a054729fbb483984f7642db478392f628ebf6.

--- engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/writer/AbstractWordXmlWriter.java
+++ engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/writer/AbstractWordXmlWriter.java
@@ -226,11 +226,11 @@ public abstract class AbstractWordXmlWriter
 	{
 		writer.openTag( "w:tbl" );
 		writer.openTag( "w:tblPr" );
-		writeTableIndent( style );
+		writeTableIndent( );
 		writeAttrTag( "w:tblStyle", "TableGrid" );
 		writeAttrTag( "w:tblOverlap", "Never" );
 		writeBidiTable( );
-		writeTableWidth( style, tablewidth );
+		writeTableWidth( tablewidth );
 		writeAttrTag( "w:tblLook", "01E0" );
 		writeTableLayout( );
 		writeTableBorders( style );
@@ -289,23 +289,12 @@ public abstract class AbstractWordXmlWriter
 		writer.closeTag( "w:tblW" );
 	}
 
-	private void writeTableIndent( IStyle style )
+	private void writeTableIndent( )
 	{
 		writer.openTag( "w:tblInd" );
-		writer.attribute( "w:w",
-				WordUtil.milliPt2Twips( PropertyUtil.getDimensionValue( style
-						.getProperty( StyleConstants.STYLE_MARGIN_LEFT ) ) ) );
+		writer.attribute( "w:w", 0 );
 		writer.attribute( "w:type", "dxa" );
 		writer.closeTag( "w:tblInd" );
-	}
-
-	private void writeTableWidth( IStyle style, int tablewidth )
-	{
-		int leftSpace = WordUtil.milliPt2Twips( PropertyUtil.getDimensionValue(
-				style.getProperty( StyleConstants.STYLE_MARGIN_LEFT ) ) );
-		int rightSpace = WordUtil.milliPt2Twips( PropertyUtil.getDimensionValue(
-				style.getProperty( StyleConstants.STYLE_MARGIN_RIGHT ) ) );
-		writeTableWidth( tablewidth - leftSpace - rightSpace );
 	}
 
 	protected void writeBorders( IStyle style, int bottomMargin, int topMargin,
