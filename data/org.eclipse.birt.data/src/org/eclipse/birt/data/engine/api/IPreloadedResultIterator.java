6fc368f9df8b214254444ade22a38ce82aa47727
Yulin Wang <wangy@opentext.com>
2018-09-17 14:14:44 -0700

    Introduce IPreloadedResultIterator to push down max row number and starting row index from report engine to data engine for better execution performance.

---
+++ data/org.eclipse.birt.data/src/org/eclipse/birt/data/engine/api/IPreloadedResultIterator.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Actuate Corporation.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.birt.data.engine.api;
+
+/**
+ * This iterator only loads predefined number of rows from starting row index.
+ * Max row numbers and starting row index must be set immediately after new
+ * instance or before accessing data. This extends
+ * <class>IResultIterator</class> and provides better performance.
+ * 
+ * @since 4.8
+ */
+
+public interface IPreloadedResultIterator extends IResultIterator
+{
+
+	/**
+	 * Sets max number of rows.
+	 * 
+	 * @param rowNum
+	 *            max number of rows.
+	 * @since 4.8
+	 */
+	void setMaxRows( int rowNum );
+
+	/**
+	 * Sets starting row index.
+	 * 
+	 * @param startIndex
+	 *            starting row index
+	 * @since 4.8
+	 */
+	void setStartingRow( int startIndex );
+}
