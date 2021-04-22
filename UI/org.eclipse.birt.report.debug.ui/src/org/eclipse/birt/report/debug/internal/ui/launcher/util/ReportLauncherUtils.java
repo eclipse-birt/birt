7af430d1c85882ba2936f9f17077ed1570209b61
Wim Jongman <wim.jongman@remainsoftware.com>
2021-04-22 15:46:06 +0200

    How to import BIRT code into eclipse #628
    Forbidden references should not be a warning but an error.
    ReportLauncherUtils failed to compile with internal API
    Jetty and Lucense upgrade
    Removed shady references from the updatesite pom
    Moved BIRT target to latest Eclipse
    Moved debug.ui to Java11
    Upgrade fop to 4.6 in chart.device.pdf
    Removed version constrains in features

--- UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/internal/ui/launcher/util/ReportLauncherUtils.java
+++ UI/org.eclipse.birt.report.debug.ui/src/org/eclipse/birt/report/debug/internal/ui/launcher/util/ReportLauncherUtils.java
@@ -15,88 +15,84 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Properties;
 import java.util.TreeSet;
 
 import org.eclipse.birt.report.debug.internal.ui.launcher.IReportLauncherSettings;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Preferences;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstallType;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.pde.core.plugin.IPluginModelBase;
 import org.eclipse.pde.core.plugin.ModelEntry;
-import org.eclipse.pde.internal.core.ICoreConstants;
 import org.eclipse.pde.internal.core.PDECore;
 
 import com.ibm.icu.util.StringTokenizer;
 
 /**
  * ReportLauncherUtils
- * 
+ *
  * @deprecated
  */
 public class ReportLauncherUtils {
 
-	public static HashMap getAutoStartPlugins(ILaunchConfiguration config) {
-		boolean useDefault = true;
-		String customAutoStart = ""; //$NON-NLS-1$
-		try {
-			useDefault = config.getAttribute(IReportLauncherSettings.CONFIG_USE_DEFAULT, true);
-			customAutoStart = config.getAttribute(IReportLauncherSettings.CONFIG_AUTO_START, ""); //$NON-NLS-1$
-		} catch (CoreException e) {
-		}
-		return getAutoStartPlugins(useDefault, customAutoStart);
-	}
+//	public static HashMap getAutoStartPlugins(ILaunchConfiguration config) {
+//		boolean useDefault = true;
+//		String customAutoStart = ""; //$NON-NLS-1$
+//		try {
+//			useDefault = config.getAttribute(IReportLauncherSettings.CONFIG_USE_DEFAULT, true);
+//			customAutoStart = config.getAttribute(IReportLauncherSettings.CONFIG_AUTO_START, ""); //$NON-NLS-1$
+//		} catch (CoreException e) {
+//		}
+//		return getAutoStartPlugins(useDefault, customAutoStart);
+//	}
 
-	public static IPath getEclipseHome() {
-		Preferences preferences = PDECore.getDefault().getPluginPreferences();
-		return new Path(preferences.getString(ICoreConstants.PLATFORM_PATH));
-	}
+//	public static IPath getEclipseHome() {
+//		Preferences preferences = PDECore.getDefault().getPluginPreferences();
+//		return new Path(preferences.getString(ICoreConstants.PLATFORM_PATH));
+//	}
 
-	public static HashMap getAutoStartPlugins(boolean useDefault, String customAutoStart) {
-		HashMap list = new HashMap();
-		if (true)
-		// !PDECore.getDefault( ).getModelManager( ).isOSGiRuntime( ) )
-		{
-			list.put("org.eclipse.core.boot", Integer.valueOf(0)); //$NON-NLS-1$
-		} else {
-			String bundles = null;
-			if (useDefault) {
-				Properties prop = getConfigIniProperties(getEclipseHome().toOSString(), "configuration/config.ini"); //$NON-NLS-1$
-				if (prop != null)
-					bundles = prop.getProperty("osgi.bundles"); //$NON-NLS-1$
-				if (prop == null || bundles == null) {
-					String path = getOSGiPath();
-					if (path != null)
-						prop = getConfigIniProperties(path, "eclipse.properties"); //$NON-NLS-1$
-					if (prop != null)
-						bundles = prop.getProperty("osgi.bundles"); //$NON-NLS-1$
-				}
-			} else {
-				bundles = customAutoStart;
-			}
-			if (bundles != null) {
-				StringTokenizer tokenizer = new StringTokenizer(bundles, ","); //$NON-NLS-1$
-				while (tokenizer.hasMoreTokens()) {
-					String token = tokenizer.nextToken().trim();
-					int index = token.indexOf('@');
-					if (index == -1 || index == token.length() - 1)
-						continue;
-					String start = token.substring(index + 1);
-					if (start.indexOf("start") != -1 || !useDefault) { //$NON-NLS-1$
-						Integer level = index != -1 ? getStartLevel(start) : new Integer(-1);
-						list.put(index != -1 ? token.substring(0, token.indexOf('@')) : token, level);
-					}
-				}
-			}
-		}
-		return list;
-	}
+//	public static HashMap getAutoStartPlugins(boolean useDefault, String customAutoStart) {
+//		HashMap list = new HashMap();
+//		if (true)
+//		// !PDECore.getDefault( ).getModelManager( ).isOSGiRuntime( ) )
+//		{
+//			list.put("org.eclipse.core.boot", Integer.valueOf(0)); //$NON-NLS-1$
+//		} else {
+//			String bundles = null;
+//			if (useDefault) {
+//				Properties prop = getConfigIniProperties(getEclipseHome().toOSString(), "configuration/config.ini"); //$NON-NLS-1$
+//				if (prop != null)
+//					bundles = prop.getProperty("osgi.bundles"); //$NON-NLS-1$
+//				if (prop == null || bundles == null) {
+//					String path = getOSGiPath();
+//					if (path != null)
+//						prop = getConfigIniProperties(path, "eclipse.properties"); //$NON-NLS-1$
+//					if (prop != null)
+//						bundles = prop.getProperty("osgi.bundles"); //$NON-NLS-1$
+//				}
+//			} else {
+//				bundles = customAutoStart;
+//			}
+//			if (bundles != null) {
+//				StringTokenizer tokenizer = new StringTokenizer(bundles, ","); //$NON-NLS-1$
+//				while (tokenizer.hasMoreTokens()) {
+//					String token = tokenizer.nextToken().trim();
+//					int index = token.indexOf('@');
+//					if (index == -1 || index == token.length() - 1)
+//						continue;
+//					String start = token.substring(index + 1);
+//					if (start.indexOf("start") != -1 || !useDefault) { //$NON-NLS-1$
+//						Integer level = index != -1 ? getStartLevel(start) : new Integer(-1);
+//						list.put(index != -1 ? token.substring(0, token.indexOf('@')) : token, level);
+//					}
+//				}
+//			}
+//		}
+//		return list;
+//	}
 
 	public static Properties getConfigIniProperties(String directory, String filename) {
 		File iniFile = new File(directory, filename);
@@ -137,15 +133,15 @@ public class ReportLauncherUtils {
 		return Integer.valueOf(-1);
 	}
 
-	public static String getPrimaryFeatureId() {
-		boolean isOSGi = true;
-		// PDECore.getDefault( ).getModelManager( ).isOSGiRuntime( );
-		String filename = isOSGi ? "configuration/config.ini" : "install.ini"; //$NON-NLS-1$ //$NON-NLS-2$
-		Properties properties = getConfigIniProperties(getEclipseHome().toOSString(), filename);
-
-		String property = isOSGi ? "eclipse.product" : "feature.default.id"; //$NON-NLS-1$ //$NON-NLS-2$
-		return (properties == null) ? null : properties.getProperty(property);
-	}
+//	public static String getPrimaryFeatureId() {
+//		boolean isOSGi = true;
+//		// PDECore.getDefault( ).getModelManager( ).isOSGiRuntime( );
+//		String filename = isOSGi ? "configuration/config.ini" : "install.ini"; //$NON-NLS-1$ //$NON-NLS-2$
+//		Properties properties = getConfigIniProperties(getEclipseHome().toOSString(), filename);
+//
+//		String property = isOSGi ? "eclipse.product" : "feature.default.id"; //$NON-NLS-1$ //$NON-NLS-2$
+//		return (properties == null) ? null : properties.getProperty(property);
+//	}
 
 	public static File createConfigArea(String name) {
 		IPath statePath = PDECore.getDefault().getStateLocation();
