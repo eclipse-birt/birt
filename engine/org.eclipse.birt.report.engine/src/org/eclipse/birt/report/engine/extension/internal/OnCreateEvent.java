d8ad93a84ee14d2674f35e38826321ea3c59d4bb
Steve Schafer <sschafer@innoventsolutions.com>
2022-01-08 14:58:46 -0700

    A few more licences.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnCreateEvent.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnCreateEvent.java
@@ -10,7 +10,7 @@ import org.eclipse.birt.report.model.api.ReportElementHandle;
 /*******************************************************************************
  * Copyright (c) 2008 Actuate Corporation. All rights reserved. This program and
  * the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
+ * Public License v2.0 which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors: Actuate Corporation - initial API and implementation
