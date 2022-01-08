15789800e562433efd7fd050111267e22ff23434
Steve Schafer <sschafer@innoventsolutions.com>
2022-01-08 09:50:13 -0700

    Move license to EPL 2 #683
    Changes to report/org.eclipse.birt.report.engine.  This has a
    greater variety of file types than the previous PR.
    
    Signed-off-by: Steve Schafer <sschafer@innoventsolutions.com>

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnRenderEvent.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnRenderEvent.java
@@ -11,7 +11,7 @@ import org.eclipse.birt.report.model.api.ReportElementHandle;
  * Copyright (c) 2008 Actuate Corporation. All rights reserved. This program and
  * the accompanying materials are made available under the terms of the Eclipse
  * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
  * 
  * Contributors: Actuate Corporation - initial API and implementation
  ******************************************************************************/
