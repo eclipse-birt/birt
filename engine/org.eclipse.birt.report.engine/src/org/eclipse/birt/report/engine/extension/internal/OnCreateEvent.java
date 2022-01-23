6e7ba413276a8fed1bf0df29475c3d2fd9f42089
Steve Schafer <sschafer@innoventsolutions.com>
2022-01-23 13:18:37 -0700

    Move license to EPL 2 #683 (#783)
    Move license to EPL 2 #683
    
    * Change all occurrences of Eclipse Public License version 1 to version 2 in UI/org.eclipse.birt.report.debug.core.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnCreateEvent.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/extension/internal/OnCreateEvent.java
@@ -10,8 +10,8 @@ import org.eclipse.birt.report.model.api.ReportElementHandle;
 /*******************************************************************************
  * Copyright (c) 2008 Actuate Corporation. All rights reserved. This program and
  * the accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors: Actuate Corporation - initial API and implementation
  *******************************************************************************/
