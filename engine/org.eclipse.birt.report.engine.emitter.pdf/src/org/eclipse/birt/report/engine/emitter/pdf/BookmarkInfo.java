a140e0f56fb56648d27dba7ba0f51cbb6b7bcad7
Henning von Bargen <h.vonbargen@t-p.com>
2022-02-24 10:01:40 +0100

    Edited license comment

--- engine/org.eclipse.birt.report.engine.emitter.pdf/src/org/eclipse/birt/report/engine/emitter/pdf/BookmarkInfo.java
+++ engine/org.eclipse.birt.report.engine.emitter.pdf/src/org/eclipse/birt/report/engine/emitter/pdf/BookmarkInfo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022 Triestram & Partner GmbH
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
