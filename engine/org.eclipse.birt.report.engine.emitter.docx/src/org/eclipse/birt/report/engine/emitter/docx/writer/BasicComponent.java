5dd0edd979f7996d88bb7b111e849d0dc0ac4d0e
Henning von Bargen <h.vonbargen@t-p.com>
2022-03-10 22:19:16 +0100

    Fix extra space in docx #850 (#851)

--- engine/org.eclipse.birt.report.engine.emitter.docx/src/org/eclipse/birt/report/engine/emitter/docx/writer/BasicComponent.java
+++ engine/org.eclipse.birt.report.engine.emitter.docx/src/org/eclipse/birt/report/engine/emitter/docx/writer/BasicComponent.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2013 Actuate Corporation.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0/.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  *
  * Contributors:
  *  Actuate Corporation  - initial API and implementation
@@ -387,6 +387,7 @@ public abstract class BasicComponent extends AbstractWordXmlWriter {
 		Element body = null;
 		ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
 		HTMLWriter htmlWriter = new HTMLWriter();
+		htmlWriter.setEnableCompactMode(true); // HVB, bug 519375
 		htmlWriter.open(byteOut);
 		if (doc != null) {
 			NodeList bodys = doc.getElementsByTagName("body");
