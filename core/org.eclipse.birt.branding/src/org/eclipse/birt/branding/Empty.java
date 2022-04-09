6f3b78dc0fbfba74a8cf1d9d9c58eb763fef264f
Wim Jongman <wim.jongman@remainsoftware.com>
2022-04-09 14:24:16 +0200

    BIRT executable story #694 + Include Java in products
    https://github.com/eclipse/birt/issues/694
    https://github.com/eclipse/birt/issues/882
    
    Added a java jre to the products
    Added icons to the products
    Added about dialog text and image
    Updated old logo to new logo
    Start all in one in correct perspective

---
+++ core/org.eclipse.birt.branding/src/org/eclipse/birt/branding/Empty.java
@@ -0,0 +1,10 @@
+package org.eclipse.birt.branding;
+
+/**
+ * This class is only here to provide content for the source directory.
+ * 
+ * Delete when other classes are added.
+ */
+public class Empty {
+
+}
