f5cb70eca3cb6bcb2e32dc9abb6676ae23c5b4f9
Henning von Bargen <h.vonbargen@t-p.com>
2023-01-27 09:23:52 +0100

    Add special handling for Soft Hyphen (SHY) unicode symbol to DOCX emi… (#1180)
    Add special handling for Soft Hyphen (SHY) unicode symbol to DOCX emitter and PDF emitter as follows:
    
    The SOFT HYPHEN Hyphen Unicode symbol (often abbreviated as "SHY", code point 173 = '\u00ad' ) is something like a shy dash: It is invisible except when line-breaking occurs at this place.
    Its intention is to mark good locations for hyphenation. 
    For US/English readers: Long words are quite common in some languages, so hyphenation is much more important there than in English texts.
    For example, the German word "Bundestag" (the parliament) can be hyphenated as "Bun-des-tag". Let's assume that the text ist stored as "Bun\u00addes\u00adtag"(with a SHY symbol instead of the ASCII MINUS symbol between the syllables). If a line-break occurs, this may result in "Bundes-" at the end of line 1 and "tag" at the start of line 2.
    TTF fonts usually calculate a width of 0 for this symbol.
    
    BIRT behaves like the CSS property hyphens: auto is set (see https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Text/Wrapping_Text).
    But until now, BIRT did not handle the SHY symbol correctly with the PDF emitter and (in some cases) with the DOCX emitter.
    
    This PR adds special handling for the SHY symbol to the DOCX emitter and PDF emitters to handle this correctly.
    Other emitters are not changed.

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/hyphen/Word.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/hyphen/Word.java
@@ -10,14 +10,72 @@
  *
  * Contributors:
  * Actuate Corporation - initial API and implementation
+ * Henning von Bargen - Added at least a bit of JavaDoc, added SOFT HYPHEN support.
  ***********************************************************************/
+
 package org.eclipse.birt.report.engine.layout.pdf.hyphen;
 
+/**
+ * <p>
+ * Despite its name, this describes a <em>fragment</em> of a word of text.
+ * </p>
+ * <p>
+ * If the word does not contain possible hyphenation / line-breaking points,
+ * then it is a whole word. But if the word contains Unicode MINUS or HYPHEN or
+ * SOFT HYPHEN symbols, then the {@link BreakIterator} splits this whole word
+ * into more than one Word instances.
+ * </p>
+ * <p>
+ * For example, "extra-ordinary" will be split into two Word instances "extra-"
+ * and "ordinary".
+ * </p>
+ */
 public class Word {
 	protected int start;
 	protected int end;
 	protected String text;
 
+	private boolean keepTrailingSoftHyphen = true;
+
+	/**
+	 * Should a trailing Unicode SOFT HYPHEN (SHY) symbol be kept or omitted?
+	 *
+	 * @return true if a trailing soft hyphen should be kept, false if it should be
+	 *         omitted.
+	 *
+	 * @since 4.13
+	 */
+	public boolean isKeepTrailingSoftHyphen() {
+		return keepTrailingSoftHyphen;
+	}
+
+	/**
+	 * Set whether a trailing Unicode SOFT HYPHEN (SHY) symbol should be kept or
+	 * omitted. The default value is <tt>true</tt>, so this is usually only called
+	 * to omit it.
+	 *
+	 * @apiNote This is not really used inside the Word class. But a Word object is
+	 *          used to transmit the information piggyback to the
+	 *          {@link org.eclipse.birt.report.engine.nLayout.area.impl.TextArea}
+	 *          object, where the information is needed.
+	 *
+	 * @param keepTrailingSoftHyphen whether to keep the last soft hyphen or not.
+	 *
+	 * @since 4.13
+	 */
+	public void setKeepTrailingSoftHyphen(boolean keepTrailingSoftHyphen) {
+		this.keepTrailingSoftHyphen = keepTrailingSoftHyphen;
+	}
+
+	/**
+	 * Create a Word instance as a substring of a given text.
+	 *
+	 * @see String#substring(int,int)
+	 *
+	 * @param text  Text
+	 * @param start start index of the substring
+	 * @param end   end index of the substring (exclusive).
+	 */
 	public Word(String text, int start, int end) {
 		this.text = text;
 		this.start = start;
--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/TextArea.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/nLayout/area/impl/TextArea.java
@@ -21,12 +21,63 @@ import org.eclipse.birt.report.engine.nLayout.area.ITextArea;
 import org.eclipse.birt.report.engine.nLayout.area.style.TextStyle;
 
 import com.ibm.icu.text.Bidi;
+import com.ibm.icu.text.BreakIterator;
 
+/**
+ * <p>
+ * An abstract representation of a line of styled text (eg. with a font and font
+ * size specified etc.) or a fragment thereof.
+ * </p>
+ */
 public class TextArea extends AbstractArea implements ITextArea {
 
 	protected String text;
 
 	protected String cachedText = null;
+
+	/**
+	 * <p>
+	 * The soft hyphen Unicode symbol.
+	 * </p>
+	 * <p>
+	 * It needs special handling, because it should only be visible when a
+	 * line-break occurs there and hidden otherwise.
+	 * </p>
+	 * <p>
+	 * See
+	 * {@link org.eclipse.birt.report.engine.emitter.wpml.writer.AbstractWordXmlWriter#SOFT_HYPHEN}
+	 * for more detail.
+	 * </p>
+	 */
+	private static final char SOFT_HYPHEN = '\u00ad';
+
+	/**
+	 * <p>
+	 * This controls if Unicode SOFT HYPHEN symbols in a text should be removed from
+	 * the output. The default value is <tt>true</tt> - remove soft hyphens.
+	 * </p>
+	 * <p>
+	 * By setting the system property <tt>org.eclipse.birt.softhyphen.remove</tt> to
+	 * <tt>false</tt>, the old, incorrect behavior of keeping them can be restored.
+	 * </p>
+	 */
+	private boolean removeSoftHyphens = "true".equals(System.getProperty("org.eclipse.birt.softhyphen.remove", "true")); // $NON-NLS-1
+
+	/**
+	 * <p>
+	 * This controls if a Unicode SOFT HYPHEN at the end of the text area should be
+	 * kept in the output or removed with the other SOFT HYPHENs when
+	 * {@link #removeSoftHyphens} is set.
+	 * </p>
+	 * <p>
+	 * Note that sometimes the same visible line of text can consist of more than
+	 * one TextAreas. The text content of these text areas are the result of a
+	 * {@link BreakIterator}. A pre-hyphenated word, e.g. "extra\u00adordinary" will
+	 * be split by the {@link BreakIterator} into two
+	 * {@link org.eclipse.birt.report.engine.layout.pdf.hyphen.Word "words"} can
+	 * result in two TextAreas with the texts "
+	 */
+	private boolean keepTrailingSoftHyphen = true;
 
 	protected int runLevel;
 
@@ -115,12 +166,35 @@ public class TextArea extends AbstractArea implements ITextArea {
 		return textLength;
 	}
 
+	/**
+	 * <p>
+	 * Get a string with the text this TextArea represents.
+	 * </p>
+	 * <p>
+	 * SOFT HYPHEN Unicode symbols inside the text are usually removed (depending on
+	 * {@link #removeSoftHyphens}), except a trailing one (depending on
+	 * {@link #keepTrailingSoftHyphen}).
+	 * </p>
+	 *
+	 * @return The unformatted text.
+	 */
 	private String calculateText() {
 		if (blankLine || text == null) {
 			return "";
-		} else {
-			return text.substring(offset, offset + textLength);
 		}
+		String textResult = text.substring(offset, offset + textLength);
+		if (removeSoftHyphens) {
+			// Remove all Unicode SOFT HYPHEN symbols except a trailing one.
+			// FIXME: This is possibly worth performance tuning!
+			int indxSoftHyphen = textResult.indexOf(SOFT_HYPHEN);
+			for (; indxSoftHyphen >= 0; indxSoftHyphen = textResult.indexOf(SOFT_HYPHEN)) {
+				String remaining = textResult.substring(indxSoftHyphen + 1);
+				if (keepTrailingSoftHyphen && remaining.strip().length() == 0)
+					break;
+				textResult = textResult.substring(0, indxSoftHyphen) + remaining;
+			}
+		}
+		return textResult;
 	}
 
 	public void addWord(int textLength, float wordWidth) {
@@ -237,4 +311,30 @@ public class TextArea extends AbstractArea implements ITextArea {
 	public boolean needClip() {
 		return needClip;
 	}
+
+	/**
+	 * Whether a Unicode SOFT HYPHEN at the end of the text area should be kept in
+	 * the output or removed.
+	 *
+	 * @see #keepTrailingSoftHyphen
+	 *
+	 * @return true if the soft hyphen shall be kept.
+	 */
+	public boolean isKeepTrailingSoftHyphen() {
+		return keepTrailingSoftHyphen;
+	}
+
+	/**
+	 * Control whether a Unicode SOFT HYPHEN at the end of the text area should be
+	 * kept in the output or removed.
+	 *
+	 * @see #keepTrailingSoftHyphen
+	 *
+	 * @param keepTrailingSoftHyphen true if the soft hyphen shall be kept.
+	 */
+	public void setKeepTrailingSoftHyphen(boolean keepTrailingSoftHyphen) {
+		this.keepTrailingSoftHyphen = keepTrailingSoftHyphen;
+	}
+
+
 }
