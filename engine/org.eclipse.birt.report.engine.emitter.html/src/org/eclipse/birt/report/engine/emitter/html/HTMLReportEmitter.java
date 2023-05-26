3a051807f0e0cf00539a41a60dfc49bc92d960d0
Ed Merks <Ed.Merks@gmail.com>
2023-05-26 15:58:53 +0200

    Update the library versions used of dependencies used by the build (#1291)
    Update the setup to provide a functional targlet task that generates the
    org.eclipse.birt.target.target to match.
    
    Do not use any repository reference for the build but rather use purely
    the *.target.
    
    Use plugin-source's feature-source for feature source generation and
    ensure now warnings about plugins/features without sources.
    
    Reduce noise from Javadoc generation.
    
    Ensure that skipTests is the default and enable those tests that
    are currently running in the builds and also enable many of the other
    tests that were not currently running and fix tests failing with the
    latest libraries and versions.
    
    https://github.com/eclipse/birt/issues/1286

--- engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/HTMLReportEmitter.java
+++ engine/org.eclipse.birt.report.engine.emitter.html/src/org/eclipse/birt/report/engine/emitter/html/HTMLReportEmitter.java
@@ -579,27 +579,26 @@ public class HTMLReportEmitter extends ContentEmitterAdapter {
 
 	protected void addCellDiagonalSpecial() {
 		writer.writeCode("<script type=\"text/javascript\">");
-
-		String fctDiagonal = "";
-		fctDiagonal += "\nfunction combineBgImageAndDiagonal(id, diagUri) {";
-		fctDiagonal += "\n	var nTd = document.getElementById(id);";
-		fctDiagonal += "\n	if (nTd) {";
-		fctDiagonal += "\n		var nStyle = getComputedStyle(nTd);";
-		fctDiagonal += "\n		if (nStyle && nStyle.backgroundImage) {";
-		fctDiagonal += "\n			var bgStyle = '';";
-		fctDiagonal += "\n			bgStyle += 'background-image:' + diagUri + ', ' + nStyle.backgroundImage + ';'	;";
-		fctDiagonal += "\n			bgStyle += 'background-size:100% 100%, ' + nStyle.backgroundSize + ';'			;";
-		fctDiagonal += "\n			bgStyle += 'background-repeat:no-repeat, ' + nStyle.backgroundRepeat + ';'		;";
-		fctDiagonal += "\n			bgStyle += 'background-position: center, ' + nStyle.backgroundPosition + ';'	;";
-		fctDiagonal += "\n			bgStyle += 'background-position-x:' + nStyle.backgroundPositionY + ';'			;";
-		fctDiagonal += "\n			bgStyle += 'background-position-y:' + nStyle.backgroundPositionX + ';'			;";
-		fctDiagonal += "\n			bgStyle += 'background-attachment:' + nStyle.backgroundAttachment + ';'			;";
-		fctDiagonal += "\n			bgStyle += 'overflow:hidden;';";
-		fctDiagonal += "\n			nTd.setAttribute('style' , bgStyle);";
-		fctDiagonal += "\n		}";
-		fctDiagonal += "\n	}";
-		fctDiagonal += "\n}";
-		writer.writeCode(fctDiagonal);
+		writer.writeCode(" //<![CDATA["); //$NON-NLS-1$
+		writer.writeCode("   function combineBgImageAndDiagonal(id, diagUri) {");
+		writer.writeCode("     var nTd = document.getElementById(id);");
+		writer.writeCode("     if (nTd) {");
+		writer.writeCode("       var nStyle = getComputedStyle(nTd);");
+		writer.writeCode("       if (nStyle && nStyle.backgroundImage) {");
+		writer.writeCode("         var bgStyle = '';");
+		writer.writeCode("         bgStyle += 'background-image:' + diagUri + ', ' + nStyle.backgroundImage + ';'	;");
+		writer.writeCode("         bgStyle += 'background-size:100% 100%, ' + nStyle.backgroundSize + ';'			;");
+		writer.writeCode("         bgStyle += 'background-repeat:no-repeat, ' + nStyle.backgroundRepeat + ';'		;");
+		writer.writeCode("         bgStyle += 'background-position: center, ' + nStyle.backgroundPosition + ';'	;");
+		writer.writeCode("         bgStyle += 'background-position-x:' + nStyle.backgroundPositionY + ';'			;");
+		writer.writeCode("         bgStyle += 'background-position-y:' + nStyle.backgroundPositionX + ';'			;");
+		writer.writeCode("         bgStyle += 'background-attachment:' + nStyle.backgroundAttachment + ';'			;");
+		writer.writeCode("         bgStyle += 'overflow:hidden;';");
+		writer.writeCode("         nTd.setAttribute('style' , bgStyle);");
+		writer.writeCode("       }");
+		writer.writeCode("     }");
+		writer.writeCode("   }");
+		writer.writeCode(" //]]>"); //$NON-NLS-1$
 		writer.writeCode("</script>"); //$NON-NLS-1$
 	}
 
@@ -712,18 +711,17 @@ public class HTMLReportEmitter extends ContentEmitterAdapter {
 			htmlEmitter = new HTMLVisionOptimize(this, writer, fixedReport, enableInlineStyle, htmlRtLFlag,
 					browserVersion);
 		}
-		// diagonal & antidiagonal special function
-		addCellDiagonalSpecial();
 
 		if (isEmbeddable) {
+			openRootTag();
+			writeBidiFlag();
 			outputCSSStyles(reportDesign, designHandle);
+
 			if (needFixTransparentPNG) {
 				fixTransparentPNG();
 			}
 			fixRedirect();
 
-			openRootTag();
-			writeBidiFlag();
 			// output the report default style
 			if (report != null) {
 				String defaultStyleName = report.getDesign().getRootStyleName();
@@ -743,6 +741,9 @@ public class HTMLReportEmitter extends ContentEmitterAdapter {
 				}
 			}
 
+			// diagonal & antidiagonal special function
+			addCellDiagonalSpecial();
+
 			outputDIVTitle(report);
 			outputClientScript(report);
 
@@ -752,6 +753,7 @@ public class HTMLReportEmitter extends ContentEmitterAdapter {
 		openRootTag();
 		writeBidiFlag();
 		writer.openTag(HTMLTags.TAG_HEAD);
+
 
 		// write the title of the report in html.
 		outputReportTitle(report);
@@ -776,11 +778,15 @@ public class HTMLReportEmitter extends ContentEmitterAdapter {
 			}
 		}
 
+
 		outputCSSStyles(reportDesign, designHandle);
 
 		if (needFixTransparentPNG) {
 			fixTransparentPNG();
 		}
+
+		// diagonal & antidiagonal special function
+		addCellDiagonalSpecial();
 
 		fixRedirect();
 		// client initialize
