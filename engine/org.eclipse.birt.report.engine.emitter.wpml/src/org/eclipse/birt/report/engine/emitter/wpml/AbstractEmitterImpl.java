16c4f114fad0f0201c019f1a7dddccbb538a7409
Thomas Gutmann <gutmann-thomas@web.de>
2024-01-24 21:49:46 +0100

    Fix image issue with docx (#1549) and eclipse preview (#1550)

--- engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/AbstractEmitterImpl.java
+++ engine/org.eclipse.birt.report.engine.emitter.wpml/src/org/eclipse/birt/report/engine/emitter/wpml/AbstractEmitterImpl.java
@@ -1067,10 +1067,10 @@ public abstract class AbstractEmitterImpl {
 				referenceHeight = imageInfo.getHeight();
 				height = WordUtil.convertImageSize(image.getHeight(), referenceHeight, PropertyUtil.getImageDpi(image, imageFileHeightDpi, 0));
 			}
-			if (image.getWidth() == null && height > 0) {
+			if (image.getWidth() == null && width == 0 && height > 0) {
 				width = height;
 			}
-			if (width > 0 && image.getHeight() == null) {
+			if (width > 0 && image.getHeight() == null && height == 0) {
 				height = width;
 			}
 
@@ -1129,6 +1129,7 @@ public abstract class AbstractEmitterImpl {
 	protected void writeSectionInBody() throws IOException, BirtException {
 		wordWriter.startSection();
 		writeHeaderFooter();
+		leftMargin = 0;
 		wordWriter.writePageProperties(pageHeight, pageWidth, headerHeight, footerHeight,
 				topMargin + pageTopBorderWidth / 8 * 20, bottomMargin + pageBottomBorderWidth / 8 * 20,
 				leftMargin + pageLeftBorderWidth / 8 * 20, rightMargin + pageRightBorderWidth / 8 * 20,
@@ -1385,7 +1386,7 @@ public abstract class AbstractEmitterImpl {
 		String backgroundWidth = style.getBackgroundWidth();
 
 		SimpleMasterPageDesign master = (SimpleMasterPageDesign) previousPage.getGenerateBy();
-
+		contentWidth = 12000; //-- 9639
 		if (previousPage.getPageHeader() != null || backgroundHeight != null || backgroundWidth != null) {
 			wordWriter.startHeader(!master.isShowHeaderOnFirst() && previousPage.getPageNumber() == 1, headerHeight,
 					contentWidth);
