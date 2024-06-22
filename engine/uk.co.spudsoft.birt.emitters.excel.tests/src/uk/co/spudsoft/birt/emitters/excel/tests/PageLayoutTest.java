9ec800bf11318d6c87393874e212de3654d0b608
Thomas Gutmann <41593722+speckyspooky@users.noreply.github.com>
2024-06-22 17:49:14 +0200

    Change the method call to set the page margin (#1747) (#1748)

--- engine/uk.co.spudsoft.birt.emitters.excel.tests/src/uk/co/spudsoft/birt/emitters/excel/tests/PageLayoutTest.java
+++ engine/uk.co.spudsoft.birt.emitters.excel.tests/src/uk/co/spudsoft/birt/emitters/excel/tests/PageLayoutTest.java
@@ -1,14 +1,14 @@
 /*************************************************************************************
- * Copyright (c) 2011, 2012, 2013 James Talbut.
+ * Copyright (c) 2011, 2012, 2013, 2024 James Talbut and others
  *  jim-emitters@spudsoft.co.uk
  *
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0/.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  *     James Talbut - Initial implementation.
  ************************************************************************************/
@@ -24,8 +24,10 @@ import java.io.InputStream;
 import org.apache.poi.hssf.usermodel.HSSFPrintSetup;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.PageMargin;
 import org.apache.poi.ss.usermodel.PaperSize;
 import org.apache.poi.ss.usermodel.PrintOrientation;
+import org.apache.poi.ss.usermodel.PrintSetup;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFPrintSetup;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -33,8 +35,20 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.birt.core.exception.BirtException;
 import org.junit.Test;
 
+/**
+ * JUnit test of the page layout
+ *
+ * @since 3.3
+ *
+ */
 public class PageLayoutTest extends ReportRunner {
 
+	/**
+	 * Constructor
+	 *
+	 * @throws BirtException
+	 * @throws IOException
+	 */
 	@Test
 	public void testRunReportXlsx() throws BirtException, IOException {
 
@@ -54,16 +68,22 @@ public class PageLayoutTest extends ReportRunner {
 			assertEquals(PrintOrientation.LANDSCAPE, printSetup.getOrientation());
 			assertEquals(1.0 / 2.54, printSetup.getHeaderMargin(), 0.01);
 			assertEquals(1.0 / 2.54, printSetup.getFooterMargin(), 0.01);
-			assertEquals(0.7 / 2.54, sheet0.getMargin(Sheet.LeftMargin), 0.01);
-			assertEquals(0.7 / 2.54, sheet0.getMargin(Sheet.RightMargin), 0.01);
-			assertEquals(1.7 / 2.54, sheet0.getMargin(Sheet.TopMargin), 0.01);
-			assertEquals(1.7 / 2.54, sheet0.getMargin(Sheet.BottomMargin), 0.01);
+			assertEquals(0.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.LeftMargin)), 0.01);
+			assertEquals(0.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.RightMargin)), 0.01);
+			assertEquals(1.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.TopMargin)), 0.01);
+			assertEquals(1.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.BottomMargin)), 0.01);
 
 		} finally {
 			inputStream.close();
 		}
 	}
 
+	/**
+	 * Runner method to execute the XLS creation
+	 *
+	 * @throws BirtException
+	 * @throws IOException
+	 */
 	@Test
 	public void testRunReportXls() throws BirtException, IOException {
 
@@ -79,20 +99,26 @@ public class PageLayoutTest extends ReportRunner {
 
 			HSSFSheet sheet0 = workbook.getSheetAt(0);
 			HSSFPrintSetup printSetup = sheet0.getPrintSetup();
-			assertEquals(HSSFPrintSetup.A4_PAPERSIZE, printSetup.getPaperSize());
+			assertEquals(PrintSetup.A4_PAPERSIZE, printSetup.getPaperSize());
 			assertEquals(true, printSetup.getLandscape());
 			assertEquals(1.0 / 2.54, printSetup.getHeaderMargin(), 0.01);
 			assertEquals(1.0 / 2.54, printSetup.getFooterMargin(), 0.01);
-			assertEquals(0.7 / 2.54, sheet0.getMargin(Sheet.LeftMargin), 0.01);
-			assertEquals(0.7 / 2.54, sheet0.getMargin(Sheet.RightMargin), 0.01);
-			assertEquals(1.7 / 2.54, sheet0.getMargin(Sheet.TopMargin), 0.01);
-			assertEquals(1.7 / 2.54, sheet0.getMargin(Sheet.BottomMargin), 0.01);
+			assertEquals(0.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.LeftMargin)), 0.01);
+			assertEquals(0.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.RightMargin)), 0.01);
+			assertEquals(1.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.TopMargin)), 0.01);
+			assertEquals(1.7 / 2.54, sheet0.getMargin(PageMargin.getByShortValue(Sheet.BottomMargin)), 0.01);
 
 		} finally {
 			inputStream.close();
 		}
 	}
 
+	/**
+	 * Runner method to execute the XLSX creation (pixel-report)
+	 *
+	 * @throws BirtException
+	 * @throws IOException
+	 */
 	@Test
 	public void testRunReportPixelsXlsx() throws BirtException, IOException {
 
@@ -112,16 +138,22 @@ public class PageLayoutTest extends ReportRunner {
 			assertEquals(PrintOrientation.LANDSCAPE, printSetup.getOrientation());
 			assertEquals(0.3, printSetup.getHeaderMargin(), 0.01);
 			assertEquals(0.3, printSetup.getFooterMargin(), 0.01);
-			assertEquals(0.7, sheet0.getMargin(Sheet.LeftMargin), 0.01);
-			assertEquals(0.7, sheet0.getMargin(Sheet.RightMargin), 0.01);
-			assertEquals(0.75, sheet0.getMargin(Sheet.TopMargin), 0.01);
-			assertEquals(0.75, sheet0.getMargin(Sheet.BottomMargin), 0.01);
+			assertEquals(0.7, sheet0.getMargin(PageMargin.getByShortValue(Sheet.LeftMargin)), 0.01);
+			assertEquals(0.7, sheet0.getMargin(PageMargin.getByShortValue(Sheet.RightMargin)), 0.01);
+			assertEquals(0.75, sheet0.getMargin(PageMargin.getByShortValue(Sheet.TopMargin)), 0.01);
+			assertEquals(0.75, sheet0.getMargin(PageMargin.getByShortValue(Sheet.BottomMargin)), 0.01);
 
 		} finally {
 			inputStream.close();
 		}
 	}
 
+	/**
+	 * Runner method to execute the XLS creation (pixel-report)
+	 *
+	 * @throws BirtException
+	 * @throws IOException
+	 */
 	@Test
 	public void testRunReportPixelsXls() throws BirtException, IOException {
 
@@ -137,14 +169,14 @@ public class PageLayoutTest extends ReportRunner {
 
 			HSSFSheet sheet0 = workbook.getSheetAt(0);
 			HSSFPrintSetup printSetup = sheet0.getPrintSetup();
-			assertEquals(HSSFPrintSetup.A4_PAPERSIZE, printSetup.getPaperSize());
+			assertEquals(PrintSetup.A4_PAPERSIZE, printSetup.getPaperSize());
 			assertEquals(true, printSetup.getLandscape());
 			assertEquals(0.5, printSetup.getHeaderMargin(), 0.01);
 			assertEquals(0.5, printSetup.getFooterMargin(), 0.01);
-			assertEquals(0.75, sheet0.getMargin(Sheet.LeftMargin), 0.01);
-			assertEquals(0.75, sheet0.getMargin(Sheet.RightMargin), 0.01);
-			assertEquals(1.0, sheet0.getMargin(Sheet.TopMargin), 0.01);
-			assertEquals(1.0, sheet0.getMargin(Sheet.BottomMargin), 0.01);
+			assertEquals(0.75, sheet0.getMargin(PageMargin.getByShortValue(Sheet.LeftMargin)), 0.01);
+			assertEquals(0.75, sheet0.getMargin(PageMargin.getByShortValue(Sheet.RightMargin)), 0.01);
+			assertEquals(1.0, sheet0.getMargin(PageMargin.getByShortValue(Sheet.TopMargin)), 0.01);
+			assertEquals(1.0, sheet0.getMargin(PageMargin.getByShortValue(Sheet.BottomMargin)), 0.01);
 
 		} finally {
 			inputStream.close();
