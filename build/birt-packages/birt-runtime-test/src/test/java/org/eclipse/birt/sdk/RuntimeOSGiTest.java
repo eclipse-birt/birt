739975e8a7dee3c828e71af6c394efe464de1b76
Ed Merks <ed.merks@gmail.com>
2024-06-30 14:28:53 +0200

    Enable the birt-runtime-test of birt-runtime
    -  RuntimeOSGiTest to test birt-runtime-osgi is still disabled because
    it fails on the servers

--- build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/RuntimeOSGiTest.java
+++ build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/RuntimeOSGiTest.java
@@ -1,35 +1,49 @@
 /*******************************************************************************
  * Copyright (c) 2013 Actuate Corporation.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0/.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  *******************************************************************************/
 package org.eclipse.birt.sdk;
 
 import java.io.File;
-import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
-/**
- *
- */
-
-public class RuntimeOSGiTest extends BaseTestTemplate {
-	@Override
+@SuppressWarnings("javadoc")
+// TODO This works locally for me on Windows but fails on the Linux build machines.
+// One must first do a full Maven build for these artifacts to be available in the target folder.
+public abstract class RuntimeOSGiTest extends BaseTestTemplate {
 	public int run(String[] args) throws Exception {
-		if (mainClass == null) {
-			System.setProperty("BIRT_HOME", new File("./target/birt-runtime-osgi/ReportEngine").getAbsolutePath());
-			loader = createClassLoader("./target/birt-runtime-osgi/ReportEngine/lib"); //$NON-NLS-1$
-			mainClass = loader.loadClass("org.eclipse.birt.report.engine.api.ReportRunner"); //$NON-NLS-1$
-		}
-		Constructor constructor = mainClass.getConstructor(String[].class);
-		Object runner = constructor.newInstance(new Object[] { args });
-		Method execute = mainClass.getMethod("execute", null);
-		Object result = execute.invoke(runner, null);
-		return ((Integer) result).intValue();
+		System.setProperty("BIRT_HOME",
+				new File("./target/birt-runtime-osgi/ReportEngine/platform/").getAbsolutePath());
+
+		ClassLoader loader = createClassLoader("./target/birt-runtime-osgi/ReportEngine/lib");
+
+		// Start the Platform to start the Equinox framework.
+		Class<?> platformClass = loader.loadClass("org.eclipse.birt.core.framework.Platform"); //$NON-NLS-1$
+		platformClass.getMethod("startup").invoke(null);
+
+		// Get the launcher from the started Platform.
+		Field launcherField = platformClass.getDeclaredField("launcher");
+		launcherField.setAccessible(true);
+		Object launcher = launcherField.get(null);
+
+		// Get the org.eclipse.birt.report.engine bundle from the launcher.
+		Method getBundleMethod = launcher.getClass().getDeclaredMethod("getBundle", String.class);
+		getBundleMethod.setAccessible(true);
+		Object birtReportEngineBundle = getBundleMethod.invoke(launcher, "org.eclipse.birt.report.engine");
+
+		// Load the org.eclipse.birt.report.engine.api.ReportRunner class from the
+		// bundle to ensure we have loaded the instance class from the actual OSGi
+		// runtime..c
+		Method loadClassMethod = birtReportEngineBundle.getClass().getMethod("loadClass", String.class);
+		Class<?> mainClass = (Class<?>) loadClassMethod.invoke(birtReportEngineBundle,
+				"org.eclipse.birt.report.engine.api.ReportRunner");
+		return run(mainClass, args);
 	}
 }
