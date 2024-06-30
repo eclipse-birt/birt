0903e35c09886e97045e0ddedfad9101b6c203b6
Ed Merks <ed.merks@gmail.com>
2024-06-30 14:28:53 +0200

    Enable the birt-runtime-test of birt-runtime
    -  RuntimeOSGiTest to test birt-runtime-osgi is still disabled because
    it fails on the servers

--- build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/BaseTestTemplate.java
+++ build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/BaseTestTemplate.java
@@ -1,23 +1,27 @@
 /*******************************************************************************
  * Copyright (c) 2013 Actuate Corporation.
- * 
+ *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0/.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  *******************************************************************************/
 package org.eclipse.birt.sdk;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,17 +30,15 @@ import org.junit.Test;
  *
  */
 
+@SuppressWarnings("javadoc")
 public abstract class BaseTestTemplate {
-
-	protected Class mainClass;
-	protected ClassLoader loader;
 
 	@Test
 	public void testMain() throws Exception {
 		String output = "./target/output.html";
 		new File(output).delete();
-		int result = run(new String[] { "-o", output, "-m", "RunAndRender",
-				"./target/birt-runtime/ReportEngine/samples/hello_world.rptdesign" });
+		int result = run(new String[] { "-o", output, "-m", "RunAndRender", "-p", "paramInteger=1", "-p",
+				"paramList=1,2,3", "./target/birt-runtime/ReportEngine/samples/hello_world.rptdesign" });
 		Assert.assertEquals(0, result);
 		Assert.assertTrue(new File(output).exists());
 		Assert.assertTrue(new String(Files.readAllBytes(Paths.get(output)), StandardCharsets.UTF_8)
@@ -79,7 +81,6 @@ public abstract class BaseTestTemplate {
 
 	protected File[] listJars(String folder) {
 		return new File(folder).listFiles(new FilenameFilter() {
-
 			@Override
 			public boolean accept(File dir, String name) {
 				if (name.endsWith(".jar")) {
@@ -90,14 +91,31 @@ public abstract class BaseTestTemplate {
 		});
 	}
 
-	protected ClassLoader createClassLoader(String folder) throws MalformedURLException {
-		File[] jarFiles = listJars(folder);
-		URL[] urls = new URL[jarFiles.length];
-		for (int i = 0; i < jarFiles.length; i++) {
-			urls[i] = jarFiles[i].toURI().toURL();
+	protected Set<URL> getURLs(String... roots) throws IOException {
+		Set<URL> urls = new LinkedHashSet<URL>();
+		for (String root : roots) {
+			File[] files = new File(root).listFiles();
+			for (int i = 0; i < files.length; i++) {
+				File file = files[i];
+				urls.add(file.toURI().toURL());
+			}
 		}
-		return new URLClassLoader(urls);
+		return urls;
+	}
+
+	protected ClassLoader createClassLoader(String... roots) throws IOException {
+		Set<URL> urls = getURLs(roots);
+		return new URLClassLoader(urls.toArray(new URL[urls.size()]));
 	}
 
 	public abstract int run(String[] args) throws Exception;
+
+	protected int run(Class<?> mainClass, String[] args) throws Exception {
+		Constructor<?> constructor = mainClass.getConstructor(String[].class);
+		Object runner = constructor.newInstance(new Object[] { args });
+		Method execute = mainClass.getMethod("execute");
+		Object result = execute.invoke(runner);
+		return ((Integer) result).intValue();
+	}
+
 }
--- build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/RuntimeTest.java
+++ build/birt-packages/birt-runtime-test/src/test/java/org/eclipse/birt/sdk/RuntimeTest.java
@@ -13,23 +13,15 @@
 
 package org.eclipse.birt.sdk;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-
+@SuppressWarnings("javadoc")
 public class RuntimeTest extends BaseTestTemplate {
 
 	@Override
 	public int run(String[] args) throws Exception {
-		if (mainClass == null) {
-			System.clearProperty("BIRT_HOME");
-			loader = createClassLoader("./target/birt-runtime/ReportEngine/lib"); //$NON-NLS-1$
-			mainClass = loader.loadClass("org.eclipse.birt.report.engine.api.ReportRunner"); //$NON-NLS-1$
-		}
-		Constructor constructor = mainClass.getConstructor(String[].class);
-		Object runner = constructor.newInstance(new Object[] { args });
-		Method execute = mainClass.getMethod("execute", null);
-		Object result = execute.invoke(runner, null);
-		return ((Integer) result).intValue();
-	}
+		System.clearProperty("BIRT_HOME");
+		ClassLoader loader = createClassLoader("./target/birt-runtime/ReportEngine/lib"); //$NON-NLS-1$
+		Class<?> mainClass = loader.loadClass("org.eclipse.birt.report.engine.api.ReportRunner"); //$NON-NLS-1$
 
+		return run(mainClass, args);
+	}
 }
