3ea5ed0c8e01e03499c080748be906db1ff6fc61
Vladimir Bychkov <github@bychkov.name>
2024-07-17 16:26:15 +0200

    Migration to Jakarta EE

--- data/org.eclipse.birt.data.oda.mongodb/src/org/eclipse/birt/data/oda/mongodb/impl/MDbResultSet.java
+++ data/org.eclipse.birt.data.oda.mongodb/src/org/eclipse/birt/data/oda/mongodb/impl/MDbResultSet.java
@@ -27,8 +27,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.xml.bind.DatatypeConverter;
-
 import org.bson.Document;
 import org.bson.types.BSONTimestamp;
 import org.eclipse.birt.data.oda.mongodb.internal.impl.DriverUtil;
@@ -45,6 +43,8 @@ import org.eclipse.datatools.connectivity.oda.impl.Blob;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.DBCursor;
+
+import jakarta.xml.bind.DatatypeConverter;
 
 /**
  * Implementation class of IResultSet for the MongoDB ODA runtime driver.
