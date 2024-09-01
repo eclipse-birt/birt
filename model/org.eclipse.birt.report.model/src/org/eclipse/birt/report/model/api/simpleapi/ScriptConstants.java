b3541db3e7449c7e31cb85311781a8344eea5930
Thomas Gutmann <41593722+speckyspooky@users.noreply.github.com>
2024-09-01 15:37:27 +0200

    Added java doc comments for JavaScript constants (#1882)

--- model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/simpleapi/ScriptConstants.java
+++ model/org.eclipse.birt.report.model/src/org/eclipse/birt/report/model/api/simpleapi/ScriptConstants.java
@@ -19,37 +19,90 @@ import org.eclipse.birt.report.model.api.elements.DesignChoiceConstants;
 /**
  * Constants for script
  */
-
 public class ScriptConstants {
+
+	/** property, operator: equal */
 	public static final String FILTER_OPERATOR_EQ = DesignChoiceConstants.FILTER_OPERATOR_EQ; // $NON-NLS-1$
+
+	/** property, operator: not equal */
 	public static final String FILTER_OPERATOR_NE = DesignChoiceConstants.FILTER_OPERATOR_NE; // $NON-NLS-1$
+
+	/** property, operator: lower then */
 	public static final String FILTER_OPERATOR_LT = DesignChoiceConstants.FILTER_OPERATOR_LT; // $NON-NLS-1$
+
+	/** property, operator: lower equal */
 	public static final String FILTER_OPERATOR_LE = DesignChoiceConstants.FILTER_OPERATOR_LE; // $NON-NLS-1$
+
+	/** property, operator: greater equal */
 	public static final String FILTER_OPERATOR_GE = DesignChoiceConstants.FILTER_OPERATOR_GE; // $NON-NLS-1$
+
+	/** property, operator: greater then */
 	public static final String FILTER_OPERATOR_GT = DesignChoiceConstants.FILTER_OPERATOR_GT; // $NON-NLS-1$
+
+	/** property, operator: between */
 	public static final String FILTER_OPERATOR_BETWEEN = DesignChoiceConstants.FILTER_OPERATOR_BETWEEN; // $NON-NLS-1$
+
+	/** property, operator: not between */
 	public static final String FILTER_OPERATOR_NOT_BETWEEN = DesignChoiceConstants.FILTER_OPERATOR_NOT_BETWEEN; // $NON-NLS-1$
+
+	/** property, operator: is null */
 	public static final String FILTER_OPERATOR_NULL = DesignChoiceConstants.FILTER_OPERATOR_NULL; // $NON-NLS-1$
+
+	/** property, operator: is not null */
 	public static final String FILTER_OPERATOR_NOT_NULL = DesignChoiceConstants.FILTER_OPERATOR_NOT_NULL; // $NON-NLS-1$
+
+	/** property, operator: is true */
 	public static final String FILTER_OPERATOR_TRUE = DesignChoiceConstants.FILTER_OPERATOR_TRUE; // $NON-NLS-1$
+
+	/** property, operator: is false */
 	public static final String FILTER_OPERATOR_FALSE = DesignChoiceConstants.FILTER_OPERATOR_FALSE; // $NON-NLS-1$
+
+	/** property, operator: like */
 	public static final String FILTER_OPERATOR_LIKE = DesignChoiceConstants.FILTER_OPERATOR_LIKE; // $NON-NLS-1$
+
+	/** property, operator: top-n */
 	public static final String FILTER_OPERATOR_TOP_N = DesignChoiceConstants.FILTER_OPERATOR_TOP_N; // $NON-NLS-1$
+
+	/** property, operator: botom-n */
 	public static final String FILTER_OPERATOR_BOTTOM_N = DesignChoiceConstants.FILTER_OPERATOR_BOTTOM_N; // $NON-NLS-1$
+
+	/** property, operator: top-percent */
 	public static final String FILTER_OPERATOR_TOP_PERCENT = DesignChoiceConstants.FILTER_OPERATOR_TOP_PERCENT; // $NON-NLS-1$
+
+	/** property, operator: bottom-percent */
 	public static final String FILTER_OPERATOR_BOTTOM_PERCENT = DesignChoiceConstants.FILTER_OPERATOR_BOTTOM_PERCENT; // $NON-NLS-1$
+
+	/** property, operator: not in */
 	public static final String FILTER_OPERATOR_NOT_IN = DesignChoiceConstants.FILTER_OPERATOR_NOT_IN; // $NON-NLS-1$
+
+	/** property, operator: match */
 	public static final String FILTER_OPERATOR_MATCH = DesignChoiceConstants.FILTER_OPERATOR_MATCH; // $NON-NLS-1$
+
+	/** property, operator: not like */
 	public static final String FILTER_OPERATOR_NOT_LIKE = DesignChoiceConstants.FILTER_OPERATOR_NOT_LIKE; // $NON-NLS-1$
+
+	/** property, operator: not match */
 	public static final String FILTER_OPERATOR_NOT_MATCH = DesignChoiceConstants.FILTER_OPERATOR_NOT_MATCH; // $NON-NLS-1$
+
+	/** property, operator: in */
 	public static final String FILTER_OPERATOR_IN = DesignChoiceConstants.FILTER_OPERATOR_IN; // $NON-NLS-1$
 
+
+	/** property, sort direction: ascending */
 	public static final String SORT_DIRECTION_ASC = DesignChoiceConstants.SORT_DIRECTION_ASC; // $NON-NLS-1$
+
+	/** property, sort direction: descending */
 	public static final String SORT_DIRECTION_DESC = DesignChoiceConstants.SORT_DIRECTION_DESC; // $NON-NLS-1$
 
+	/** property, hyperlink action: none */
 	public static final String ACTION_LINK_TYPE_NONE = DesignChoiceConstants.ACTION_LINK_TYPE_NONE; // $NON-NLS-1$
-	public static final String ACTION_LINK_TYPE_HYPERLINK = DesignChoiceConstants.ACTION_LINK_TYPE_HYPERLINK; // $NON-NLS-1$
-	public static final String ACTION_LINK_TYPE_DRILL_THROUGH = DesignChoiceConstants.ACTION_LINK_TYPE_DRILL_THROUGH; // $NON-NLS-1$
-	public static final String ACTION_LINK_TYPE_BOOKMARK_LINK = DesignChoiceConstants.ACTION_LINK_TYPE_BOOKMARK_LINK;// $NON-NLS-1$
 
+	/** property, hyperlink action: hyperlink */
+	public static final String ACTION_LINK_TYPE_HYPERLINK = DesignChoiceConstants.ACTION_LINK_TYPE_HYPERLINK; // $NON-NLS-1$
+
+	/** property, hyperlink action: drill through */
+	public static final String ACTION_LINK_TYPE_DRILL_THROUGH = DesignChoiceConstants.ACTION_LINK_TYPE_DRILL_THROUGH; // $NON-NLS-1$
+
+	/** property, hyperlink action: bookmark link */
+	public static final String ACTION_LINK_TYPE_BOOKMARK_LINK = DesignChoiceConstants.ACTION_LINK_TYPE_BOOKMARK_LINK;// $NON-NLS-1$
 }
