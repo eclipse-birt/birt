63af36250c92b0786b33fc1743fa9e5879714255
Thomas Gutmann <gutmann-thomas@web.de>
2025-03-01 21:24:50 +0100

    Enhance font classes to set types at collections and maps and add java doc (#2069)

--- engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/font/FontConfigHandler.java
+++ engine/org.eclipse.birt.report.engine/src/org/eclipse/birt/report/engine/layout/pdf/font/FontConfigHandler.java
@@ -21,48 +21,82 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Stack;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
+/**
+ * Font configuration handler
+ *
+ * @since 3.3
+ *
+ */
 public class FontConfigHandler extends DefaultHandler {
 
 	private FontMappingConfig config;
 
-	private Stack states = new Stack();
+	private Stack<ParseState> states = new Stack<ParseState>();
 
+	/**
+	 * Constructor
+	 *
+	 * @param config font mapping configuration
+	 */
 	public FontConfigHandler(FontMappingConfig config) {
 		this.config = config;
 		this.states.push(new RootState());
 	}
 
+	/**
+	 * @throws SAXException
+	 */
 	@Override
 	public void startElement(String uri, String localName, String rawName, Attributes attrs) throws SAXException {
-		ParseState state = (ParseState) states.peek();
+		ParseState state = states.peek();
 		state = state.startElement(rawName);
 		state.parseAttrs(attrs);
 		states.push(state);
 	}
 
+	/**
+	 * @throws SAXException
+	 */
 	@Override
 	public void endElement(String uri, String localName, String rawName) throws SAXException {
-		ParseState elementState = (ParseState) states.pop();
+		ParseState elementState = states.pop();
 		elementState.end();
-		ParseState state = (ParseState) states.peek();
+		ParseState state = states.peek();
 		state.endElement(elementState);
 	}
 
 	private static class ParseState {
 
+		/**
+		 * Parse element attribute
+		 *
+		 * @param attrs attribute
+		 */
 		public void parseAttrs(Attributes attrs) {
 		}
 
+		/**
+		 * Handle the start of element
+		 *
+		 * @param tagName tag name
+		 * @return parsed element instance
+		 */
 		public ParseState startElement(String tagName) {
 			return AnyElementState.instance;
 		}
 
+		/**
+		 * Handle the end of element
+		 *
+		 * @param state parsed element instance
+		 */
 		public void endElement(ParseState state) {
 		}
 
@@ -240,9 +274,9 @@ public class FontConfigHandler extends DefaultHandler {
 
 	private class SearchSequencesState extends ParseState {
 
-		private HashMap sequences;
+		private HashMap<String, String[]> sequences;
 
-		SearchSequencesState(HashMap sequences) {
+		SearchSequencesState(HashMap<String, String[]> sequences) {
 			this.sequences = sequences;
 		}
 
@@ -258,7 +292,7 @@ public class FontConfigHandler extends DefaultHandler {
 		class SequenceState extends ParseState {
 
 			private String locale;
-			private ArrayList catalogs = new ArrayList();
+			private ArrayList<String> catalogs = new ArrayList<String>();
 
 			@Override
 			public void parseAttrs(Attributes attrs) {
@@ -298,30 +332,30 @@ public class FontConfigHandler extends DefaultHandler {
 
 	private class AllFontState extends ParseState {
 
-		LinkedHashMap fonts = new LinkedHashMap();
+		LinkedHashMap<String, LinkedHashMap<String, ArrayList<CharSegment>>> fonts = new LinkedHashMap<String, LinkedHashMap<String, ArrayList<CharSegment>>>();
 
 		void addComponent(String fontName, int start, int end, String fontFamily) {
-			LinkedHashMap font = (LinkedHashMap) fonts.get(fontName);
+			LinkedHashMap<String, ArrayList<CharSegment>> font = fonts.get(fontName);
 			if (font == null) {
-				font = new LinkedHashMap();
+				font = new LinkedHashMap<String, ArrayList<CharSegment>>();
 				fonts.put(fontName, font);
 			}
-			ArrayList charSegs = (ArrayList) font.get(fontFamily);
+			ArrayList<CharSegment> charSegs = font.get(fontFamily);
 			if (charSegs == null) {
-				charSegs = new ArrayList();
+				charSegs = new ArrayList<CharSegment>();
 				font.put(fontFamily, charSegs);
 			}
 			charSegs.add(new CharSegment(start, end, fontFamily));
 		}
 
-		CompositeFontConfig createCompositeFont(String name, Map fonts) {
+		CompositeFontConfig createCompositeFont(String name, Map<String, ArrayList<CharSegment>> fonts) {
 			CompositeFontConfig config = new CompositeFontConfig(name);
-			Iterator iter = fonts.entrySet().iterator();
+			Iterator<Entry<String, ArrayList<CharSegment>>> iter = fonts.entrySet().iterator();
 			while (iter.hasNext()) {
-				Map.Entry entry = (Map.Entry) iter.next();
-				String fontName = (String) entry.getKey();
-				Collection charSegs = (Collection) entry.getValue();
-				CharSegment[] segment = (CharSegment[]) charSegs.toArray(new CharSegment[] {});
+				Map.Entry<String, ArrayList<CharSegment>> entry = iter.next();
+				String fontName = entry.getKey();
+				Collection<CharSegment> charSegs = entry.getValue();
+				CharSegment[] segment = charSegs.toArray(new CharSegment[] {});
 				CharSegment.sort(segment);
 				config.addFont(fontName, null);
 				config.addCharSegment(fontName, segment);
@@ -331,11 +365,12 @@ public class FontConfigHandler extends DefaultHandler {
 
 		@Override
 		public void end() {
-			Iterator iter = fonts.entrySet().iterator();
+			Iterator<Entry<String, LinkedHashMap<String, ArrayList<CharSegment>>>> iter = fonts.entrySet().iterator();
 			while (iter.hasNext()) {
-				Map.Entry entry = (Map.Entry) iter.next();
-				String fontName = (String) entry.getKey();
-				Map fonts = (Map) entry.getValue();
+				Entry<String, LinkedHashMap<String, ArrayList<CharSegment>>> entry = iter.next();
+				String fontName = entry.getKey();
+				LinkedHashMap<String, ArrayList<CharSegment>> fonts = entry
+						.getValue();
 				CompositeFontConfig fontConfig = createCompositeFont(fontName, fonts);
 				if (!fontConfig.getAllFonts().isEmpty()) {
 					config.addCompositeFont(fontConfig);
@@ -393,10 +428,10 @@ public class FontConfigHandler extends DefaultHandler {
 
 		private String fontName;
 		private String defaultFont;
-		private LinkedHashSet allFonts = new LinkedHashSet();
-		private HashMap fontCatalogs = new HashMap();
-		private HashMap fontCharacters = new HashMap();
-		private LinkedHashMap fontBlocks = new LinkedHashMap();
+		private LinkedHashSet<String> allFonts = new LinkedHashSet<String>();
+		private HashMap<String, String> fontCatalogs = new HashMap<String, String>();
+		private HashMap<Integer, String> fontCharacters = new HashMap<Integer, String>();
+		private LinkedHashMap<String, ArrayList<CharSegment>> fontBlocks = new LinkedHashMap<String, ArrayList<CharSegment>>();
 
 		private void addCharacter(String fontFamily, int ch) {
 			fontCharacters.put(Integer.valueOf(ch), fontFamily);
@@ -404,9 +439,9 @@ public class FontConfigHandler extends DefaultHandler {
 
 		private void addBlock(String fontFamily, int start, int end) {
 			allFonts.add(fontFamily);
-			ArrayList list = (ArrayList) fontBlocks.get(fontFamily);
+			ArrayList<CharSegment> list = fontBlocks.get(fontFamily);
 			if (list == null) {
-				list = new ArrayList();
+				list = new ArrayList<CharSegment>();
 				fontBlocks.put(fontFamily, list);
 			}
 			list.add(new CharSegment(start, end, fontFamily));
@@ -422,16 +457,16 @@ public class FontConfigHandler extends DefaultHandler {
 				CompositeFontConfig fontConfig = new CompositeFontConfig(fontName);
 				fontConfig.setDefaultFont(defaultFont);
 				// the character always has the highest priority
-				ArrayList characters = new ArrayList();
-				Iterator iter = fontCharacters.entrySet().iterator();
-				while (iter.hasNext()) {
-					Map.Entry entry = (Map.Entry) iter.next();
-					int ch = ((Integer) entry.getKey()).intValue();
-					String fontFamily = (String) entry.getValue();
+				ArrayList<CharSegment> characters = new ArrayList<CharSegment>();
+				Iterator<Entry<Integer, String>> iterChars = fontCharacters.entrySet().iterator();
+				while (iterChars.hasNext()) {
+					Entry<Integer, String> entry = iterChars.next();
+					int ch = entry.getKey().intValue();
+					String fontFamily = entry.getValue();
 					characters.add(new CharSegment(ch, ch, fontFamily));
 				}
 				if (!characters.isEmpty()) {
-					CharSegment[] seg = (CharSegment[]) characters.toArray(new CharSegment[] {});
+					CharSegment[] seg = characters.toArray(new CharSegment[] {});
 					CharSegment.sort(seg);
 					fontConfig.setSpecialCharacters(seg);
 				}
@@ -440,12 +475,12 @@ public class FontConfigHandler extends DefaultHandler {
 				// add the font catalog
 				fontConfig.fontCatalogs.putAll(fontCatalogs);
 				// append the font indexes
-				iter = fontBlocks.entrySet().iterator();
-				while (iter.hasNext()) {
-					Map.Entry entry = (Map.Entry) iter.next();
-					String fontFamily = (String) entry.getKey();
-					Collection blocks = (Collection) entry.getValue();
-					CharSegment[] seg = (CharSegment[]) blocks.toArray(new CharSegment[] {});
+				Iterator<Entry<String, ArrayList<CharSegment>>> iterBlocks = fontBlocks.entrySet().iterator();
+				while (iterBlocks.hasNext()) {
+					Entry<String, ArrayList<CharSegment>> entry = iterBlocks.next();
+					String fontFamily = entry.getKey();
+					Collection<CharSegment> blocks = entry.getValue();
+					CharSegment[] seg = blocks.toArray(new CharSegment[] {});
 					CharSegment.normalize(seg);
 					fontConfig.addCharSegment(fontFamily, seg);
 				}
