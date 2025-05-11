7e9642359e520bdc714e9cfaa8da0eab349042cf
Thomas Gutmann <gutmann-thomas@web.de>
2025-05-11 16:20:31 +0200

    Enhance on designer master page with vertical display of header/footer options

--- UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/HeaderFooterPage.java
+++ UI/org.eclipse.birt.report.designer.ui.views/src/org/eclipse/birt/report/designer/internal/ui/views/attributes/page/HeaderFooterPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004 Actuate Corporation.
+ * Copyright (c) 2004, 2025 Actuate Corporation and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -14,10 +14,11 @@
 
 package org.eclipse.birt.report.designer.internal.ui.views.attributes.page;
 
+import org.eclipse.birt.report.designer.internal.ui.util.WidgetUtil;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.provider.PropertyDescriptorProvider;
 import org.eclipse.birt.report.designer.internal.ui.views.attributes.section.CheckSection;
-import org.eclipse.birt.report.model.api.SimpleMasterPageHandle;
 import org.eclipse.birt.report.model.api.elements.ReportDesignConstants;
+import org.eclipse.birt.report.model.elements.interfaces.ISimpleMasterPageModel;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -27,20 +28,20 @@ public class HeaderFooterPage extends AttributePage {
 	@Override
 	public void buildUI(Composite parent) {
 		super.buildUI(parent);
-		container.setLayout(WidgetUtil.createGridLayout(2, 15));
+		container.setLayout(WidgetUtil.createGridLayout(1, 15));
 
 		PropertyDescriptorProvider headerProvider = new PropertyDescriptorProvider(
-				SimpleMasterPageHandle.SHOW_HEADER_ON_FIRST_PROP, ReportDesignConstants.SIMPLE_MASTER_PAGE_ELEMENT);
+				ISimpleMasterPageModel.SHOW_HEADER_ON_FIRST_PROP, ReportDesignConstants.SIMPLE_MASTER_PAGE_ELEMENT);
 		CheckSection headerSection = new CheckSection(container, true);
 		headerSection.setProvider(headerProvider);
 		headerSection.setLayoutNum(1);
 		addSection("HEADERSECTION", headerSection); //$NON-NLS-1$
 
 		PropertyDescriptorProvider footerProvider = new PropertyDescriptorProvider(
-				SimpleMasterPageHandle.SHOW_FOOTER_ON_LAST_PROP, ReportDesignConstants.SIMPLE_MASTER_PAGE_ELEMENT);
+				ISimpleMasterPageModel.SHOW_FOOTER_ON_LAST_PROP, ReportDesignConstants.SIMPLE_MASTER_PAGE_ELEMENT);
 		CheckSection footerSection = new CheckSection(container, true);
 		footerSection.setProvider(footerProvider);
-		footerSection.setLayoutNum(1);
+		footerSection.setLayoutNum(2);
 		addSection("FOOTERSECTION", footerSection); //$NON-NLS-1$
 
 		createSections();
